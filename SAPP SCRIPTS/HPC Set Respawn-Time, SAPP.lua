45f21ac20dab1cd6437f9ebd1701325cc55c8a49
Jericho <Chalwk77@users.noreply.github.com>
2016-09-14 20:51:29 +1200

    Updated Script Description
    Updated Script Description

--- SAPP SCRIPTS/HPC Set Respawn-Time, SAPP.lua
+++ SAPP SCRIPTS/HPC Set Respawn-Time, SAPP.lua
@@ -1,14 +1,13 @@
 --[[    
 ------------------------------------
-Description: HPC Set Respawn-Time, SAPP
+Script Name: HPC Set Respawn-Time, SAPP
     - Implementing API version: 1.10.0.0
+    
+Description: This script will allow you to set player respawn time (in seconds)
     
 Copyright © 2016 Jericho Crosby <jericho.crosby227@gmail.com>
 * Notice: You can use this document subject to the following conditions:
 https://github.com/Chalwk77/Halo-Scripts-Phasor-V2-/blob/master/LICENSE
-
-If you want the Phasor V2+ version please visit:
-https://github.com/Chalwk77/Halo-Scripts-Phasor-V2-/blob/master/HPC%20SetRespawnTime%2C%20Phasor%20V2%2B.lua
 
 * IGN: Chalwk
 * Written by Jericho Crosby
@@ -16,7 +15,7 @@ https://github.com/Chalwk77/Halo-Scripts-Phasor-V2-/blob/master/HPC%20SetRespawn
 ]]-- 
 
 api_version = "1.10.0.0"
-RespawnTime = 1.5
+RespawnTime = 1.5 -- 1.5 seconds
 
 function OnScriptLoad()
     register_callback(cb['EVENT_DIE'],"OnPlayerKill")
