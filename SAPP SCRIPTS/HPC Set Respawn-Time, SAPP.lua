a254f0c41e76742dd5cb893b70a10b0d68f51f8e
Jericho <Chalwk77@users.noreply.github.com>
2016-09-14 20:43:58 +1200

    Update HPC Set Respawn-Time, SAPP.lua

--- SAPP SCRIPTS/HPC Set Respawn-Time, SAPP.lua
+++ SAPP SCRIPTS/HPC Set Respawn-Time, SAPP.lua
@@ -1,15 +1,21 @@
 --[[    
 ------------------------------------
-Description: HPC Set Respawn-Time, SAPP, Inplementing API version 1.10.0.0
-Copyright © 2016-2017 Jericho Crosby
-* Author: Jericho Crosby
+Description: HPC Set Respawn-Time, SAPP
+    - Implementing API version: 1.10.0.0
+    
+Copyright Â© 2016 Jericho Crosby <jericho.crosby227@gmail.com>
+
 * IGN: Chalwk
-* Written and Created by Jericho Crosby
+* Written by Jericho Crosby
 -----------------------------------
 ]]-- 
 
 api_version = "1.10.0.0"
 RespawnTime = 1.5
+
+function OnScriptLoad()
+    register_callback(cb['EVENT_DIE'],"OnPlayerKill")
+end
 
 function OnScriptUnload( ) 
     
@@ -19,10 +25,6 @@ function OnPlayerKill(player_index)
     local player = get_player(player_index)
     write_dword(player + 0x2C, RespawnTime * 33)
 end	
-
-function OnScriptLoad()
-    register_callback(cb['EVENT_DIE'],"OnPlayerKill")
-end
 
 function OnError(Message)
     print(debug.traceback())
