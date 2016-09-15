4c2d67e6d0ec4e3f8a06f41473e5055f4952bb5a
Jericho <Chalwk77@users.noreply.github.com>
2016-09-16 09:42:00 +1200

    Updated Script
    Updated Script

--- SAPP SCRIPTS/HPC OnGameEnd messages, SAPP (BROKEN).lua
+++ SAPP SCRIPTS/HPC OnGameEnd messages, SAPP (BROKEN).lua
@@ -5,7 +5,7 @@ Script Name: HPC OnGameEnd messages, SAPP (BROKEN)
     
 Description: This script will display taunting messages to the player on game end
 
-Copyright © 2016 Jericho Crosby <jericho.crosby227@gmail.com>
+Copyright Â© 2016 Jericho Crosby <jericho.crosby227@gmail.com>
 * Notice: You can use this document subject to the following conditions:
 https://github.com/Chalwk77/Halo-Scripts-Phasor-V2-/blob/master/LICENSE
 
@@ -16,7 +16,7 @@ https://github.com/Chalwk77/Halo-Scripts-Phasor-V2-/blob/master/LICENSE
 
 api_version = "1.10.0.0"
 
-function OnScriptUnload( ) 
+function OnScriptUnload( )
     
 end
 
@@ -29,8 +29,23 @@ function OnGameEnd(PlayerIndex)
     
     local player = get_player(PlayerIndex)
     local kills = read_word(player + 0x9C)
-        if kills == 0 then
-        print("What a noob! You have 0 kills!")
+    if kills == 0 then
+        say(PlayerIndex, "You have no kills... noob alert!")
+    end
+    if kills == 1 then
+        say(PlayerIndex, "One kill? You must be new at this...")
+    end
+    if kills == 2 then
+        say(PlayerIndex, "Eh, two kills... not bad. But you still suck!")
+    end
+    if kills == 3 then
+        say(PlayerIndex, "Relax son! 3 kills does not mean you're the champion of the fucking world.")
+    end
+    if kills == 4 then
+        say(PlayerIndex, "Duhn Duhn Duhn... them 4 kills though!")
+    end
+    if kills > 4 then
+        say(PlayerIndex, "Well, you've got more than 4 kills... #AchievingTheImpossible")
     end
 end
 
