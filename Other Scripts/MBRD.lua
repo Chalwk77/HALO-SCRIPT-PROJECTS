59d15e08a9bd6df95ce1617b3c05ec307c8ad601
Chalwk <jericho.crosby227@gmail.com>
2016-09-16 19:13:41 +1200

    Updated repository and scripts

--- Other Scripts/MBRD.lua
+++
@@ -1,9 +0,0 @@
-function GetRequiredVersion() return 200 end
-function OnScriptLoad(processid, game, persistent) end
-function OnScriptUnload() end
-
-OPJM1_Message = "Welcome to « Ö W V » Arms Race!   <|>   Hosted by the O W V Clan"
-
-function OnPlayerJoin(player)
-	privatesay(player, OPJM1_Message, false)
-end
--- Other Scripts/OTC.lua
+++
@@ -1,4 +0,0 @@
-function GetRequiredVersion() return 200 end
-function OnScriptLoad(processid, game, persistent) end
-function OnScriptUnload() end
-function OnTeamChange(player, old_team, new_team, relevant) if relevant then return false end end
