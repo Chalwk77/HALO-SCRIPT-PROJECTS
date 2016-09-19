9146ac27fcba17b6f4fe4505841a25897b7d51d3
Chalwk <jericho.crosby227@gmail.com>
2016-09-19 20:52:03 +1200

    sdfgsdfg

--- SAPP SCRIPTS/HPC vm351 - Game Settings.lua
+++ SAPP SCRIPTS/HPC vm351 - Game Settings.lua
@@ -1,11 +1,11 @@
---[[    
+--[[
 ------------------------------------
 Script Name: HPC vm351 - Game Settings
     - Implementing API version: 1.10.0.0
-    
+
     Only on github for safe keeping
-    
-Description: 
+
+Description:
 
 Copyright © 2016 Jericho Crosby <jericho.crosby227@gmail.com>
 * Notice: You can use this document subject to the following conditions:
@@ -42,29 +42,29 @@ function OnGameEnd()
 end
 
 function OnPlayerPrejoin(PlayerIndex)
-    
+
     os.execute("echo \7")
     local timestamp = os.date("%A %d %B %Y - %X")
     local hash = get_var(PlayerIndex, "$hash")
     local ip = get_var(PlayerIndex, "$ip")
-    
+
     cprint("---------------------------------------------------------------------------------------------------")
     cprint("            - - |   P L A Y E R   A T T E M P T I N G   T O   J O I N   | - -")
     cprint("                 - - - - - - - - - - - - - - - - - - - - - - - - - - - -                    ")
-    cprint("CD Hash: <" ..hash.. ">")
-    cprint("Join Time: " ..timestamp)
-    cprint("IP Address: " ..ip)
+    cprint("CD Hash: <" .. hash .. ">")
+    cprint("Join Time: " .. timestamp)
+    cprint("IP Address: " .. ip)
 end
 
 function OnPlayerJoin(PlayerIndex)
-    
+
     local name = get_var(PlayerIndex, "$name")
     local hash = get_var(PlayerIndex, "$hash")
     local ip = get_var(PlayerIndex, "$ip")
     local id = get_var(PlayerIndex, "$n")
-    
-    cprint("Player: " ..name.. " - connected successfully.")
-    cprint("IndexID: [" ..id.. "]")
+
+    cprint("Player: " .. name .. " - connected successfully.")
+    cprint("IndexID: [" .. id .. "]")
     cprint("---------------------------------------------------------------------------------------------------")
 end
 
@@ -77,23 +77,23 @@ function OnPlayerLeave(PlayerIndex)
     local ping = get_var(PlayerIndex, "$ping")
     local timestamp = os.date("%A %d %B %Y - %X")
 
-    
+
     cprint("---------------------------------------------------------------------------------------------------")
-    cprint(name.. " quit the game! - IndexID [" ..id.. "]")
-    cprint("Time: " ..timestamp)
-    cprint("IP Address: [" ..ip.. "]")
-    cprint("Player Ping: [" ..ping.. "]")
-    cprint("CD Hash: <" ..hash.. ">")
+    cprint(name .. " quit the game! - IndexID [" .. id .. "]")
+    cprint("Time: " .. timestamp)
+    cprint("IP Address: [" .. ip .. "]")
+    cprint("Player Ping: [" .. ping .. "]")
+    cprint("CD Hash: <" .. hash .. ">")
     cprint("---------------------------------------------------------------------------------------------------")
     cprint("")
 end
 
 function consoleLogo()
-    
+
     local timestamp = os.date("%A, %d %B %Y - %X")
     cprint("===================================================================================================")
-	cprint(timestamp)
-	cprint("")
+    cprint(timestamp)
+    cprint("")
     cprint("                  '||'                  ||     ..|'''.|                   .'|.   .")
     cprint("                   ||    ....  ... ..  ...   .|'     '  ... ..   ....   .||.   .||.")
     cprint("                   ||  .|...||  ||' ''  ||   ||          ||' '' '' .||   ||     ||")
@@ -102,8 +102,8 @@ function consoleLogo()
     cprint("                '''")
     cprint("                      ->-<->-<->-<->-<->-<->-<->-<->-<->-<->-<->-<->-<->-")
     cprint("                                         Chalwk's Realm")
-	cprint("                                 vm153 - Pro Snipers + (no lag)")
-	cprint("                      ->-<->-<->-<->-<->-<->-<->-<->-<->-<->-<->-<->-<->-")
-	cprint("")
+    cprint("                                 vm153 - Pro Snipers + (no lag)")
+    cprint("                      ->-<->-<->-<->-<->-<->-<->-<->-<->-<->-<->-<->-<->-")
+    cprint("")
     cprint("===================================================================================================")
 end
