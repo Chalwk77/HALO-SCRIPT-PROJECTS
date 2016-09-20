bd182f540ce2b6211af1b9af805d7f8cb7350dd3
Jericho <Chalwk77@users.noreply.github.com>
2016-09-21 03:51:00 +1200

    Update HPC Chat Logging, for SAPP.lua

--- SAPP SCRIPTS/HPC Chat Logging, for SAPP.lua
+++ SAPP SCRIPTS/HPC Chat Logging, for SAPP.lua
@@ -70,3 +70,10 @@ end
 function OnError(Message)
     print(debug.traceback())
 end
+
+--[[
+
+SAPP will log player chat to the sapp.log file, however, it's difficult to wade through all the other event logs it handles.
+Personally, I find it convenient to have a 'dedicated' server chat.txt file. Which is where this script comes into play.
+
+]]
