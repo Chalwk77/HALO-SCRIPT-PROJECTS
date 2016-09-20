1841c8c6d2697495f31354db50a8b3bb051e4b36
Chalwk <jericho.crosby227@gmail.com>
2016-09-21 03:43:55 +1200

    Complte rewrite of this script. * Change log:     [1] I had to rewrite this script. It was driving me crazy.     [2] Does the same thing. Just more efficient and clean.     [3] Seems legit.     [4] Enjoy.

--- SAPP SCRIPTS/HPC Chat Logging, for SAPP.lua
+++ SAPP SCRIPTS/HPC Chat Logging, for SAPP.lua
@@ -1,11 +1,15 @@
 --[[
 ------------------------------------
-Script Name: HPC Chat Logging, for SAPP
+Script Name: HPC Chat Logger V2, for SAPP
     - Implementing API version: 1.10.0.0
 
-Description: This script will log player chat to /logs/Server Chat.txt
+Description: This script will log player chat to <sapp server>/logs/Chat.txt
 
-* Notes at the bottom of the script.
+* Change log:
+    [1] I had to rewrite this script. It was driving me crazy. 
+    [2] Does the same thing. Just more efficient and clean.
+    [3] Seems legit.
+    [4] Enjoy.
 
 Copyright © 2016 Jericho Crosby <jericho.crosby227@gmail.com>
 * Notice: You can use this document subject to the following conditions:
@@ -14,138 +18,55 @@ https://github.com/Chalwk77/Halo-Scripts-Phasor-V2-/blob/master/LICENSE
 * IGN: Chalwk
 * Written by Jericho Crosby
 -----------------------------------
-]]-- 
+]]--
 
 api_version = "1.10.0.0"
-
-file_name = "writelog.lua"
-WriteToFile = true
-local fileDirectory = "logs\\Server Chat.txt"
-
-function OnScriptUnload()
-end
+local dir = 'sapp\\Server Chat.txt'
+local scriptname = "chatlogger.lua"
 
 function OnScriptLoad()
+    
     register_callback(cb['EVENT_CHAT'], "OnPlayerChat")
     register_callback(cb['EVENT_GAME_START'], "OnNewGame")
 end
 
-function OnPlayerChat(PlayerIndex, Message)
-
-    --      Format: [timestamp]     PlayerName [INDEX ID]: <message>
-    --      Example Output: [15:01:05 - 16/09/2016]     Chalwk: [1]: Seems to be working fine.
-
-    local name = get_var(PlayerIndex, "$name")
-    local id = get_var(PlayerIndex, "$n")
-    local GetChatFormat = string.format("[" .. get_var(PlayerIndex, "$n") .. "]: " ..(tostring(Message)))
-
-    WriteLog(fileDirectory, name .. " " .. GetChatFormat)
+function OnScriptUnload() 
+    
 end
 
-function WriteLog(fileDirectory, value)
-    local file = io.open(fileDirectory, "a")
-    if file then
-        if WriteToFile == true then
-            local timestamp = os.date("[%H:%M:%S - %d/%m/%Y]")
-            local chatValue = string.format("%s\t%s\n", timestamp, tostring(value))
-            file:write(chatValue)
-            -- Write the value of timestamp & GetChatFormat to "/logs/Server Chat.txt"
-            -- cprint("[SCRIPT] " .. file_name .. " creating chat entry " .. fileDirectory)
-            file:close()
-            -- Close the file.
-        end
-    else
-        --  If the file path "/logs/Server Chat.txt" does not exist, we call CreateDirectory() to produce it.
-        cprint("[SCRIPT] " .. file_name .. " - [Function] on WriteLog() - File not Found: " .. fileDirectory)
-        cprint("[SCRIPT] " .. file_name .. " - Creating file(s)...")
-        CreateDirectory()
-    end
-end
-
---  I may update this function in the future to detect if either Logs Folder or Server Chat.txt exists independently.
-function CreateDirectory()
-    local file = io.open(fileDirectory, "a+")
-    if file == nil then
-        os.execute("mkdir logs")
-        -- Create Logs folder.
-        createFile = io.open("logs\\Server Chat.txt", "w")
-        -- Create Server Chat.txt
-        local openInitiate = string.format("File(s) Created!\n")
-        createFile:write(openInitiate)
-        cprint("[SCRIPT] " .. file_name .. " - successfully created files.")
-        createFile:close()
-        NewLine(fileDirectory)
-    else
-        return false
-        -- File already exists!
-    end
-end
-
-function OnNewGame(fileDirectory, value)
-	MAP = get_var(0,"$map")
-	GAME_TYPE = get_var(0,"$mode")
-    local file = io.open("logs\\Server Chat.txt", "r")
-    if file == nil then 
-    -- File does not exist, so we create it.
-        CreateDirectory() 
-    end
-    -- File exists, format NewLine.
+function OnNewGame()
+    
+    local file = io.open(dir, "a+")
     if file ~= nil then
-        local file = io.open("logs\\Server Chat.txt", "a+")
-        lineFile(fileDirectory)
-    end
-end
-
--- Necessary for a workaround
-function lineFile(fileDirectory, value)
-    local file = io.open("logs\\Server Chat.txt", "a+")
-    if WriteToFile == true then
-        MAP = get_var(0,"$map")
-        GAME_TYPE = get_var(0,"$mode")
-        local timestamp = os.date("[%A %d %B %Y] - %X - A new game has started on " ..MAP.. ", Mode: " ..GAME_TYPE)
-        NewLine = "\n"
-        Time = (timestamp)
-        Divider = "\n---------------------------------------------------------------------------------------------------\n"
-        NewLine = "\n"
-        file:write(NewLine, Time, Divider, NewLine)
+        local map = get_var(0,"$map")
+        local gt = get_var(0,"$mode")
+        local n1 = "\n"
+        local t1 = os.date("[%A %d %B %Y] - %X - A new game has started on " ..map.. ", Mode: " ..gt)
+        local n2 = "\n--------------------------------------------------------------------------------------------\n"
+        file:write(n1, t1, n2)
         file:close()
     end
 end
 
--- Formatted for better readability
-function NewLine(fileDirectory, value)
-    local file = io.open(fileDirectory, "a")
-    if file == nil then CreateDirectory()
-    elseif file then
-        if WriteToFile == true then
-            MAP = get_var(0,"$map")
-            GAME_TYPE = get_var(0,"$mode")
-            local timestamp = os.date("[%A %d %B %Y] - %X - A new game has started on " ..MAP.. ", Mode: " ..GAME_TYPE)
-            NewLine = "\n"
-            Time = (timestamp)
-            Divider = "\n---------------------------------------------------------------------------------------------------\n"
-            NewLine = "\n"
-            file:write(NewLine, Time, Divider, NewLine)
-            file:close()
-        end
+function WriteData(dir, value)
+    
+    local file = io.open(dir, "a+")
+    if file ~= nil then
+        local timestamp = os.date("[%H:%M:%S - %d/%m/%Y]    ")
+        local chatValue = string.format("%s\t%s\n", timestamp, tostring(value))
+        file:write(chatValue)
+        file:close()
     end
+end
+
+function OnPlayerChat(PlayerIndex, Message)
+    
+    local name = get_var(PlayerIndex, "$name")
+    local id = get_var(PlayerIndex, "$n")
+    local GetChatFormat = string.format("[" .. tonumber(id) .. "]: " ..(tostring(Message)))
+    WriteData(dir, name .. " " .. GetChatFormat)
 end
 
 function OnError(Message)
     print(debug.traceback())
 end
-
---[[
-SAPP will log player chat to the sapp.log file, however, it's difficult to wade through all the other event logs it handles.
-Personally, I find it convenient to have a 'dedicated' chat.txt file of sorts. Which is where this script comes into play.
-
-
-
-To do:
-
-    - Display map name and game type On New Game.
-    - Format: New Game, Map: <map type>, Mode: <gametype>
-        e.g:  New Game, Map: bloodgulch, Mode: ctf
-        e.g:  New Game, Map: ratrace, Mode: classic_slayer
-
-]]
