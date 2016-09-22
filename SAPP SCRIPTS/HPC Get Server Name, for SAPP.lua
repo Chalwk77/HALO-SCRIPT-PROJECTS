23935c80ce2a6e1fbfd4b5648762d2a839c8b9d0
Jericho <Chalwk77@users.noreply.github.com>
2016-09-22 22:37:06 +1200

    Update HPC Get Server Name, for SAPP.lua

--- SAPP SCRIPTS/HPC Get Server Name, for SAPP.lua
+++ SAPP SCRIPTS/HPC Get Server Name, for SAPP.lua
@@ -3,7 +3,7 @@
 Script Name: HPC Get Server Name, for SAPP
     - Implementing API version: 1.11.0.0
 
-Copyright © 2016 Jericho Crosby <jericho.crosby227@gmail.com>
+Copyright Â© 2016 Jericho Crosby <jericho.crosby227@gmail.com>
 * Notice: You can use this document subject to the following conditions:
 https://github.com/Chalwk77/Halo-Scripts-Phasor-V2-/blob/master/LICENSE
 
@@ -22,7 +22,7 @@ function OnScriptUnload() end
 
 function OnNewGame(map)
     local network_struct = read_dword(sig_scan("F3ABA1????????BA????????C740??????????E8????????668B0D") + 3)
-	servername = read_widestring(network_struct + 0x8, 0x42)
+    servername = read_widestring(network_struct + 0x8, 0x42)
     -- Do something here. 
     -- I'm just going to output the name of the server to console on game start.
     cprint(servername)
