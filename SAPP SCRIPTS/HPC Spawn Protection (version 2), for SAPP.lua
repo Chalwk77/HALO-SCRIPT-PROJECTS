5401fc3550c7cc1a5f0aad48031f3082ff2a9535
Jericho <Chalwk77@users.noreply.github.com>
2016-10-08 21:20:53 +1300

    Update HPC Spawn Protection (version 2), for SAPP.lua

--- SAPP SCRIPTS/HPC Spawn Protection (version 2), for SAPP.lua
+++ SAPP SCRIPTS/HPC Spawn Protection (version 2), for SAPP.lua
@@ -2,23 +2,22 @@
 Script Name: HPC Spawn Protection (version 2), for SAPP
 - Implementing API version: 1.11.0.0
 
-    Description: By default, you will spawn with an active Camouflage & Godmode for 5 seconds for every 10 consecutive deaths.
+    Description: By default, you will spawn with invisibility/Godmode/speedboost for 5 seconds for every 10 consecutive deaths.
     
     This script will allow you to:
-        - optionally turn on temporary 'godmode' (invulnerability)
-        - optionally turn on temporary speed boost.
+        - optionally turn on:
+            * temporary godmode (invulnerability)
+            * temporary speed boost
+            * temporary invisibility
         
         There are two modes:
             Mode 1 uses a method based on 'consecutive deaths'. 
-            If this setting is enabled, for every 10 consecutive deaths you have, you will spawn with special attributes.
+            If this setting is enabled, for every 10 consecutive deaths you have, you will spawn with the defined attributes.
             
-            Mode 2 uses a similar method based on a Death Counter in increments of every 5.
-            If this mode is enabled, by default you will receive special attributes for every 5 deaths.
+            Mode 2 uses a similar method based on a Death Counter in increments of 5.
+            If this mode is enabled, by default you will receive the defined attributes for every 5 deaths.
             
     Default configuration is listed at the bottom of this script.
-        
-Suggestions?
-https://github.com/Chalwk77/HALO-SCRIPT-PROJECTS/issues/5
 
 Copyright © 2016 Jericho Crosby <jericho.crosby227@gmail.com>
 * Notice: You can use this document subject to the following conditions:
@@ -34,6 +33,7 @@ api_version = "1.11.0.0"
 local settings = {
     ["UseConsecutiveDeaths"] = true,
     ["UseBasedOnDeathCount"] = false,
+    ["UseCamo"] = true,
     ["UseInvulnerability"] = true,
     ["UseSpeedBoost"] = true,
 }
@@ -91,6 +91,10 @@ function OnPlayerDeath(VictimIndex, KillerIndex)
     end
 end
 
+function ApplyCamo(PlayerIndex)
+    execute_command("camo me " ..CamoTime, PlayerIndex)
+end
+
 function Invulnerability(PlayerIndex)
     timer(Invulnerable * 1000, "ResetInvulnerability", PlayerIndex)	
     execute_command_sequence("god me;s me 1.75;hp me +2", PlayerIndex)
@@ -127,47 +131,62 @@ function OnPlayerSpawn(PlayerIndex)
     if PlayerIndex then
         if settings["UseConsecutiveDeaths"] and not settings["UseBasedOnDeathCount"] then
             if DEATHS[PlayerIndex][1] == ConsecutiveDeaths then
-                execute_command("camo me " ..CamoTime, PlayerIndex)
-                DEATHS[PlayerIndex][1] = 0
+                if settings["UseCamo"] then
+                -- Using a timer here beacuse the player cannot pick up an active camouflage before or after 'CamoTime' has deactivated
+                -- if you call ApplyCamo immediately after they spawn. There has to be some delay for whatever reason.
+                    timer(0, "ApplyCamo", PlayerIndex)
+                end
                 if settings["UseSpeedBoost"] then 
                     GiveSpeedBoost(PlayerIndex)
                 end
                 if settings["UseInvulnerability"] then
                     Invulnerability(PlayerIndex)
                 end
+                DEATHS[PlayerIndex][1] = 0
                 say(PlayerIndex, "You have received Spawn Protection!")
+                if settings["UseCamo"] == false and settings["UseSpeedBoost"] == false and settings["UseInvulnerability"] == false then
+                local note = string.format("[SCRIPT ERROR] Spawn Protection - You don't have any settings enabled for UseConsecutiveDeaths!")
+                cprint("[SCRIPT ERROR] Spawn Protection - You don't have any settings enabled for UseConsecutiveDeaths!", 4+8)
+                execute_command("log_note \""..note.."\"")
+                return false
+                end
             end
         end
         if settings["UseBasedOnDeathCount"] and not settings["UseConsecutiveDeaths"] then
             if DEATHS[PlayerIndex][1] == _5_Deaths then
-                execute_command("camo me " ..CamoTime, PlayerIndex)
+                if settings["UseCamo"] then timer(0, "ApplyCamo", PlayerIndex) end
             elseif DEATHS[PlayerIndex][1] == _10_Deaths then
-                execute_command("camo me " ..CamoTime, PlayerIndex)
+                if settings["UseCamo"] then timer(0, "ApplyCamo", PlayerIndex) end
             elseif DEATHS[PlayerIndex][1] == _15_Deaths then
-                execute_command("camo me " ..CamoTime, PlayerIndex)
+                if settings["UseCamo"] then timer(0, "ApplyCamo", PlayerIndex) end
             elseif DEATHS[PlayerIndex][1] == _20_Deaths then
-                execute_command("camo me " ..CamoTime, PlayerIndex)
+                if settings["UseCamo"] then timer(0, "ApplyCamo", PlayerIndex) end
             elseif DEATHS[PlayerIndex][1] == _25_Deaths then
-                execute_command("camo me " ..CamoTime, PlayerIndex)
+                if settings["UseCamo"] then timer(0, "ApplyCamo", PlayerIndex) end
             elseif DEATHS[PlayerIndex][1] == _30_Deaths then
-                execute_command("camo me " ..CamoTime, PlayerIndex)
+                if settings["UseCamo"] then timer(0, "ApplyCamo", PlayerIndex) end
             elseif DEATHS[PlayerIndex][1] == _35_Deaths then
-                execute_command("camo me " ..CamoTime, PlayerIndex)
+                if settings["UseCamo"] then timer(0, "ApplyCamo", PlayerIndex) end
             elseif DEATHS[PlayerIndex][1] == _40_Deaths then
-                execute_command("camo me " ..CamoTime, PlayerIndex)
+                if settings["UseCamo"] then timer(0, "ApplyCamo", PlayerIndex) end
             elseif DEATHS[PlayerIndex][1] == _45_Deaths then
-                execute_command("camo me " ..CamoTime, PlayerIndex)
+                if settings["UseCamo"] then timer(0, "ApplyCamo", PlayerIndex) end
             elseif DEATHS[PlayerIndex][1] == _50_Deaths then
-                execute_command("camo me " ..CamoTime, PlayerIndex)
+                if settings["UseCamo"] then timer(0, "ApplyCamo", PlayerIndex) end
                 if settings["UseSpeedBoost"] then 
                     GiveSpeedBoost(PlayerIndex)
-                    timer(SpeedDuration*1000, "ResetPlayerSpeed", PlayerIndex)
                 end
                 if settings["UseInvulnerability"] then
                     Invulnerability(PlayerIndex)
                 end
             end
             say(PlayerIndex, "You have received Spawn Protection!")
+            if settings["UseCamo"] == false and settings["UseSpeedBoost"] == false and settings["UseInvulnerability"] == false then
+            local note = string.format("[SCRIPT ERROR] Spawn Protection - You don't have any settings enabled for UseBasedOnDeathCount!")
+            cprint("[SCRIPT ERROR] Spawn Protection - You don't have any settings enabled for UseBasedOnDeathCount!", 4+8)
+            execute_command("log_note \""..note.."\"")
+            return false
+            end
         end
     end
 end
