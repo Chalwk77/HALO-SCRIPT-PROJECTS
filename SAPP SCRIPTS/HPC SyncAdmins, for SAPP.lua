3a39764961856f18dace1aa33f98371a84e82769
Jericho <Chalwk77@users.noreply.github.com>
2016-10-13 18:59:16 +1300

    Update HPC SyncAdmins, for SAPP.lua

--- SAPP SCRIPTS/HPC SyncAdmins, for SAPP.lua
+++ SAPP SCRIPTS/HPC SyncAdmins, for SAPP.lua
@@ -9,6 +9,10 @@ Credits to 002 for HTTP Code: https://github.com/Halogen002/SAPP-HTTP-Client
 This script is also available on my github! Check my github for regular updates on my projects, including this script.
 https://github.com/Chalwk77/HALO-SCRIPT-PROJECTS
 
+TO DO:
+. File Encoding
+. GoToBackup (function)
+
 Copyright ©2016 Jericho Crosby <jericho.crosby227@gmail.com>
 * Notice: You can use this document subject to the following conditions:
 https://github.com/Chalwk77/Halo-Scripts-Phasor-V2-/blob/master/LICENSE
@@ -21,61 +25,64 @@ https://github.com/Chalwk77/Halo-Scripts-Phasor-V2-/blob/master/LICENSE
 api_version = "1.11.0.0"
 
 function OnScriptLoad() 
-    register_callback(cb['EVENT_CHAT'], "OnChatMessage")
+    register_callback(cb['EVENT_COMMAND'], "OnServerCommand")
     SyncAdmins() 
 end
 -- Change this url accordingly.
-url = 'http://files.enjin.com/1096950/'
+url = 'http://example.com/files'
 admins = 'sapp\\admins.txt'
 users = 'sapp\\users.txt'
 prefix = "[SCRIPT] - SyncAdmins.lua|n"
 Sync_Admins = true
 Sync_Users = true
-function SyncAdmins()
+function SyncAdmins(executor, Command, PlayerIndex, count)
     admin_page = GetPage(tostring(url) .. "admins.txt")
     users_page = GetPage(tostring(url) .. "users.txt")
+    PlayerIndex = tonumber(PlayerIndex)
+    response = nil
     if Sync_Admins then
         if admin_page == nil then 
-            respond(prefix .. 'Error: ' .. url .. 'admins.txt does not exist or the remote server is offline.')
+        respond(prefix .. 'Error: ' .. url .. 'admins.txt does not exist or the remote server is offline.', PlayerIndex)
         else
-            proceed = true
+            response = true
             if string.find(admin_page, "[A-Za-z0-9]:[1-4]") == nil then
-                proceed = false
-                respond('Error: Failed to read from admins.txt on remote server.')
+                response = false
+                respond('Error: Failed to read from admins.txt on remote server.', PlayerIndex)
             end
-            if proceed then
+            if response then
                 local file = io.open(admins, "w")
                 local line = tokenizestring(admin_page, "*")
                 for i = 1, #line do
                     file:write(line[i])
-                    respond('Syncing Admins...|n' .. line[i])
+                    respond('Syncing Admins...|n' .. line[i], PlayerIndex)
                 end
                 file:close()
-                respond('admins.txt successfully Synced!|n')
+                respond('admins.txt successfully Synced!|n', PlayerIndex, Message)
             end
         end
     end
     if Sync_Users then
         if users_page == nil then 
-            respond(prefix .. 'Error: ' .. url .. 'users.txt does not exist or the remote server is offline.')
+            respond(prefix .. 'Error: ' .. url .. 'users.txt does not exist or the remote server is offline.', PlayerIndex)
         else
-            proceed = true
+            response = true
             if string.find(users_page, "[A-Za-z0-9]:[1-4]:") == nil then
-                proceed = false
-                respond('Error: Failed to read from users.txt on remote server.')
+                response = false
+                respond('Error: Failed to read from users.txt on remote server.', PlayerIndex)
             end
-            if proceed then
+            if response then
                 local file = io.open(users, "w")
                 local line = tokenizestring(users_page, "*")
                 for i = 1, #line do
                     file:write(line[i])
-                    respond('Syncing Users...|n' .. line[i])
+                    respond('Syncing Users...|n' .. line[i], PlayerIndex)
                 end
                 file:close()
-                respond('users.txt successfully Synced!|n')
+                respond('users.txt successfully Synced!|n', PlayerIndex, Message)
             end
         end
     end
+    return response
 end
 
 function respond(Message, PlayerIndex)
@@ -87,8 +94,9 @@ function respond(Message, PlayerIndex)
 		end
 		PlayerIndex = tonumber(PlayerIndex)
 		if tonumber(PlayerIndex) and PlayerIndex ~= nil and PlayerIndex ~= -1 and PlayerIndex >= 0 and PlayerIndex < 16 then
-            rprint(PlayerIndex, Message)
+            cprint("Response to: " .. get_var(PlayerIndex, "$name"), 4+8)
             cprint(Message, 2+8)
+            rprint(PlayerIndex, Message)
 		else
 			cprint(Message, 2+8)
 		end
@@ -97,33 +105,23 @@ end
 
 function OnScriptUnload() end
 
-function OnChatMessage(PlayerIndex, Message)
-    local name = get_var(PlayerIndex, "$name")
+function OnServerCommand(PlayerIndex, Command)
+	local response = nil
 	local isadmin = nil
-	if (tonumber(get_var(PlayerIndex,"$lvl"))) >= 1 then isadmin = true else isadmin = false end
-    local mlen = #Message
-    local count = 0
-    for i=1, #Message do
-        local c = string.sub(Message, i,i)
-        if c == ' ' then
-            count = count+1
-        end
+	if (tonumber(get_var(PlayerIndex,"$lvl"))) >= 1 then 
+        isadmin = true 
+    else 
+        isadmin = false 
     end
-    if mlen == count then
-        count = 0
-        return 0
-    end
-    local t = tokenizestring(Message, " ")
-    if t[1] == nil then
-        return nil
-    end
-    if t[1] == "/sync" or t[1] == "\\sync" then
+	local t = tokenizestring(Command)
+	local count = #t
+	if t[1] == "sync" or t[1] == "sv_sync_admins" then
         if isadmin then
-            SyncAdmins()
+            SyncAdmins(PlayerIndex, t[1], count)
         else 
-            respond('You do not have permission to execute this command!')
+            respond("You do not have permission to execute \"" .. Command .. "\"", PlayerIndex)
         end
-        return false
+        return response
     end
 end
 
