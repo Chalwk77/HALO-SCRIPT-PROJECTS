0b5a4504e05a5bb84c4bf1e8bb9900ccfbc1de27
Jericho <Chalwk77@users.noreply.github.com>
2016-10-13 17:06:50 +1300

    Update HPC SyncAdmins, for SAPP.lua

--- SAPP SCRIPTS/HPC SyncAdmins, for SAPP.lua
+++ SAPP SCRIPTS/HPC SyncAdmins, for SAPP.lua
@@ -6,11 +6,6 @@ Script Name: HPC SyncAdmins, for SAPP
 Description: This script will sync your admins.txt file with a remote server
 Credits to 002 for HTTP Code: https://github.com/Halogen002/SAPP-HTTP-Client
 
-To do:
-. Command to sync admins.
-. File encoding
-. GoToBackup (function)
-
 This script is also available on my github! Check my github for regular updates on my projects, including this script.
 https://github.com/Chalwk77/HALO-SCRIPT-PROJECTS
 
@@ -25,9 +20,12 @@ https://github.com/Chalwk77/Halo-Scripts-Phasor-V2-/blob/master/LICENSE
 
 api_version = "1.11.0.0"
 
-function OnScriptLoad() SyncAdmins() end
+function OnScriptLoad() 
+    register_callback(cb['EVENT_CHAT'], "OnChatMessage")
+    SyncAdmins() 
+end
 -- Change this url accordingly.
-url = 'http://example.com/files/'
+url = 'http://files.enjin.com/1096950/'
 admins = 'sapp\\admins.txt'
 users = 'sapp\\users.txt'
 prefix = "[SCRIPT] - SyncAdmins.lua|n"
@@ -38,49 +36,96 @@ function SyncAdmins()
     users_page = GetPage(tostring(url) .. "users.txt")
     if Sync_Admins then
         if admin_page == nil then 
-            cprint(prefix .. "Error: " .. url .. "admins.txt does not exist or the remote server is offline.", 4+8)
+            respond(prefix .. 'Error: ' .. url .. 'admins.txt does not exist or the remote server is offline.')
         else
             proceed = true
             if string.find(admin_page, "[A-Za-z0-9]:[1-4]") == nil then
                 proceed = false
-                cprint(prefix .. "Error: Failed to read from admins.txt on remote server.", 4+8)
+                respond('Error: Failed to read from admins.txt on remote server.')
             end
             if proceed then
                 local file = io.open(admins, "w")
-                local line = tokenizestring(admin_page, "*")
+                local line = tokenizestring(admin_page, "*')
                 for i = 1, #line do
                     file:write(line[i])
-                    cprint("Syncing Admins...|n" .. line[i], 2+8)
+                    respond('Syncing Admins...|n" .. line[i]')
                 end
                 file:close()
-                cprint("admins.txt successfully Synced!|n", 2+8)
+                respond('admins.txt successfully Synced!|n')
             end
         end
     end
     if Sync_Users then
         if users_page == nil then 
-            cprint(prefix .. "Error: " .. url .. "users.txt does not exist or the remote server is offline.", 4+8)
+            respond(prefix .. 'Error: ' .. url .. 'users.txt does not exist or the remote server is offline.')
         else
             proceed = true
             if string.find(users_page, "[A-Za-z0-9]:[1-4]:") == nil then
                 proceed = false
-                cprint(prefix .. "Error: Failed to read from users.txt on remote server.", 4+8)
+                respond('Error: Failed to read from users.txt on remote server.')
             end
             if proceed then
                 local file = io.open(users, "w")
                 local line = tokenizestring(users_page, "*")
                 for i = 1, #line do
                     file:write(line[i])
-                    cprint("Syncing Users...|n" .. line[i], 2+8)
+                    respond('Syncing Users...|n' .. line[i])
                 end
                 file:close()
-                cprint("users.txt successfully Synced!|n", 2+8)
+                respond('users.txt successfully Synced!|n')
             end
         end
     end
 end
 
+function respond(Message, PlayerIndex)
+	if Message then
+		if Message == "" then 
+			return 
+		elseif type(Message) == "table" then
+			Message = Message[1]
+		end
+		PlayerIndex = tonumber(PlayerIndex)
+		if tonumber(PlayerIndex) and PlayerIndex ~= nil and PlayerIndex ~= -1 and PlayerIndex >= 0 and PlayerIndex < 16 then
+            rprint(PlayerIndex, Message)
+            cprint(Message, 2+8)
+		else
+			cprint(Message, 2+8)
+		end
+	end
+end
+
 function OnScriptUnload() end
+
+function OnChatMessage(PlayerIndex, Message)
+    local name = get_var(PlayerIndex, "$name")
+	local isadmin = nil
+	if (tonumber(get_var(PlayerIndex,"$lvl"))) >= 1 then isadmin = true else isadmin = false end
+    local mlen = #Message
+    local count = 0
+    for i=1, #Message do
+        local c = string.sub(Message, i,i)
+        if c == ' ' then
+            count = count+1
+        end
+    end
+    if mlen == count then
+        count = 0
+        return 0
+    end
+    local t = tokenizestring(Message, " ")
+    if t[1] == nil then
+        return nil
+    end
+    if t[1] == "/sync" or t[1] == "\\sync" then
+        if isadmin then
+            SyncAdmins()
+        else 
+            respond('You do not have permission to execute this command!')
+        end
+        return false
+    end
+end
 
 -- Found this neat little function here:
 -- http://stackoverflow.com/questions/1426954/split-string-in-lua
