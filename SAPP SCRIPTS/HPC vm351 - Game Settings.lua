273c19792bf5f25e3e65c3c89f5de3b9af1c5673
Jericho <Chalwk77@users.noreply.github.com>
2016-10-17 16:03:03 +1300

    Update HPC vm351 - Game Settings.lua

--- SAPP SCRIPTS/HPC vm351 - Game Settings.lua
+++ SAPP SCRIPTS/HPC vm351 - Game Settings.lua
@@ -3,10 +3,11 @@
     Script Name: HPC vm351 - Game Settings
     
     Copyright © 2016 Jericho Crosby <jericho.crosby227@gmail.com>
+    All Rights Reserved.
     You do not have permission to use this script.
     
     * IGN: Chalwk
-    * Written by Jericho Crosby
+    * Written by Jericho Crosby (Chalwk)
     -----------------------------------
 ]]-- 
 api_version = "1.11.0.0"
