40e993ac2d4a8b7fd8a53a45c630f68ce01ac90f
Jericho <Chalwk77@users.noreply.github.com>
2016-10-17 23:47:14 +1300

    Update HPC AdminChat, for SAPP.lua

--- SAPP SCRIPTS/HPC AdminChat, for SAPP.lua
+++ SAPP SCRIPTS/HPC AdminChat, for SAPP.lua
@@ -6,16 +6,16 @@ Script Name: HPC AdminChat, for SAPP
 Description: Admin Chat! Chat privately with other admins. 
              Command: /achat on|off
     
-    ~ B E T A ~
-    In Development
+    This script is still in development and does not function! 
+    Please do not download until an (Updated [date]) tag appears in the file name.
 
 This script is also available on my github! Check my github for regular updates on my projects, including this script.
 https://github.com/Chalwk77/HALO-SCRIPT-PROJECTS
         
-Copyright ©2016 Jericho Crosby <jericho.crosby227@gmail.com>
+Copyright Â©2016 Jericho Crosby <jericho.crosby227@gmail.com>
 
 * IGN: Chalwk
-* Written by Jericho Crosby (Chalwk)
+* Written by Jericho Crosby
 -----------------------------------
 ]]--
 
@@ -25,40 +25,72 @@ function OnScriptLoad()
     register_callback(cb['EVENT_CHAT'], "OnAdminChat")
 end
 
+api_version = "1.11.0.0"
+
+function OnScriptLoad()
+    register_callback(cb['EVENT_CHAT'], "OnAdminChat")
+end
+
 function OnAdminChat(PlayerIndex, Message)
+    name = get_var(PlayerIndex, "$name")
     local t = tokenizestring(Message)
     count = #t
     local Message = tostring(Message)
+    local words = tokenizestring(Message)
+    if #words == 0 then
+        return nil
+    end
     achattoggle = nil
-    if string.sub(t[1], 1, 1) == "/" then
-        cmd = t[1]:gsub("\\", "/")
-        if cmd == "/achat" then
-            if t[2] == "off" then
-                rprint(PlayerIndex, "Admin Chat Toggled off!")
-                chattoggle = false
-            elseif t[2] == "on" then
-               rprint(PlayerIndex, "Admin Chat Toggled on!")
-               chattoggle = true
-               if chattoggle then
-               local achat = tostring(Message)
-               
-               -----  >>>>  -----
-            -- chat   code  here --
-               -----  <<<<  -----
-            elseif t[20] == nil then
-                say(PlayerIndex, "Invalid Syntax - /achat on|off")
-                chattoggle = false
+    if (tonumber(get_var(PlayerIndex,"$lvl"))) >0 then
+        Admin = tonumber(PlayerIndex)
+        if Admin then
+            if string.sub(t[1], 1, 1) == "/" then
+                cmd = t[1]:gsub("\\", "/")
+                if cmd == "/achat" then
+                    if t[2] == "on" then
+                        rprint(PlayerIndex, "Admin Chat Toggled on!")
+                        admincaht = true
+                        goto achat
+                    elseif t[2] == "off" then
+                        admincaht = false
+                        rprint(PlayerIndex, "Admin Chat Toggled off!")
+                        goto achatoff 
+                    elseif t[2] == nil then
+                        admincaht = false
+                        rprint(PlayerIndex, "Invalid Syntax! Type /achat on|off")
+                    end
+                end
             end
-            return false
         end
-    end    
+    end
+    ::achat::
+    if admincaht == true then
+        for i = 0, #words do
+            if words[i] then
+                if string.sub(words[1], 1, 1) == "/" or string.sub(words[1], 1, 1) == "\\" then 
+                    return true
+                else 
+                    AdminChat(Message, Admin) 
+                    return false
+                end
+            end
+        end
+    end
+    ::achatoff::
+    if admincaht == false then
+        for i = 0, #words do
+            if words[i] then
+                return
+            end
+        end
+    end
 end
 
-function AdminChat(Message, AdminIndex) 
+function AdminChat(Message, Admin) 
     for i = 1,16 do
-        if i ~= AdminIndex then
-            rprint(i, Message)
-        end
+        --if i ~= Admin then
+            rprint(i, "[ADMIN CHAT]  " .. name.. ":      " .. Message)
+        --end
     end
 end
 
