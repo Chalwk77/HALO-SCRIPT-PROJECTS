6ea4c8b773a8ae5e1321fc75767a42a0d4f8cb15
Jericho <Chalwk77@users.noreply.github.com>
2017-02-08 16:43:57 +1300

    Update example.lua

--- SAPP SCRIPTS/example.lua
+++ SAPP SCRIPTS/example.lua
@@ -1,5 +1,5 @@
     red_rotation[1] = {83.29, -150.58, 60}
-    blue_rotation[1] = {41.1, -90.28, 0.12}
+    blue_rotation[1] = {41.1, -90.28, 10}
     
-    red_spawn_coord[1] = {83.29, -150.58, 60}
-    blu_spawn_coord[1] = {41.1, -90.28, 0.12}
+    red_spawn_coord[1] = {83.29, -150.58, 1.88}
+    blu_spawn_coord[1] = {41.1, -90.28, 2.20}
