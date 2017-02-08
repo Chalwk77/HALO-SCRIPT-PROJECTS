a67b166d7a7019f0c848534802d042d20e0aaf54
Jericho <Chalwk77@users.noreply.github.com>
2017-02-08 16:47:48 +1300

    Update example.lua

--- SAPP SCRIPTS/example.lua
+++ SAPP SCRIPTS/example.lua
@@ -1,6 +1,17 @@
+    -- RED ROTATION
     red_rotation[1] = {83.29, -150.58, 60} -- 60 degrees of Z-axis rotation relative to spawn coordinates
-    blue_rotation[1] = {41.1, -90.28, 10} -- 10 degrees of Z-axis rotation relative to spawn coordinates
+    red_rotation[2] = {98.84, -157.58, 10} -- 10 degrees of Z-axis rotation relative to spawn coordinates
     
-    red_spawn_coord[1] = {83.29, -150.58, 1.88}
-    blu_spawn_coord[1] = {41.1, -90.28, 2.20}
+    -- BLUE ROTATION
+    blue_rotation[1] = {41.1, -90.28, 15} -- 15 degrees of Z-axis rotation relative to spawn coordinates
+    blue_rotation[2] = {38.87, -94.89, 25} -- 25 degrees of Z-axis rotation relative to spawn coordinates
     
+----------------------------------------------------------------------------------------------------------
+    
+    -- RED SPAWN COORDINATES
+    red_spawn_coord[1] = {83.29, -150.58, -0}
+    red_spawn_coord[2] = {98.84, -157.58, -0.29}
+    
+    -- BLUE SPAWN COORDINATES
+    blu_spawn_coord[1] = {41.1, -90.28, 0.12}
+    blu_spawn_coord[2] = {38.87, -94.89, -0.14}
