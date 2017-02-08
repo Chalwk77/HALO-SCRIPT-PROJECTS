ab8cf7e9409ea54f0a3466795bad946ada38ab63
Jericho <Chalwk77@users.noreply.github.com>
2017-02-08 16:42:32 +1300

    Create example.lua

---
+++ SAPP SCRIPTS/example.lua
@@ -0,0 +1,5 @@
+    red_rotation[1] = {83.29, -150.58, 60}
+    blue_rotation[1] = {41.1, -90.28, 0.12}
+    
+    red_spawn_coord[1] = {83.29, -150.58, 60}
+    blu_spawn_coord[1] = {41.1, -90.28, 0.12}
