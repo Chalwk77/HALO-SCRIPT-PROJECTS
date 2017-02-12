7b3109ea275b7e0215cfc5e861e50e958783cac9
Jericho <Chalwk77@users.noreply.github.com>
2017-02-12 20:52:02 +1300

    Update GetId.lua

--- SAPP SCRIPTS/GetId.lua
+++ SAPP SCRIPTS/GetId.lua
@@ -9,13 +9,13 @@ function OnScriptUnload()
 end
 
 function OnWeaponPickup(PlayerIndex, WeaponIndex, Type)
-	if tonumber(Type) == 1 then -- 1: weapon 2: nade
+	if tonumber(Type) == 1 then
 		local PlayerObj = get_dynamic_player(PlayerIndex)
 		local WeaponObj = get_object_memory(read_dword(PlayerObj + 0x2F8 + (tonumber(WeaponIndex) - 1) * 4))
 		local name = read_string(read_dword(read_word(WeaponObj) * 32 + 0x40440038))
 		local MetaIndex = read_dword(WeaponObj)
-		local str = string.format("Player %u picked up a %s, MetaIndex: 0x%X Slot: %u", PlayerIndex, name, MetaIndex, WeaponIndex)
-		rprint("Picked up: " .. str)
+		local str = string.format("Picked up a %s, MetaIndex: 0x%X Slot: %u", name, MetaIndex, WeaponIndex)
+		rprint(PlayerIndex, "Picked up: " .. str)
 		cprint(str)
 	end
 end
