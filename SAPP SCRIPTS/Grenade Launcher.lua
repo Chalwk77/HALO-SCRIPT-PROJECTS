280516d38775eaf2fa9d8a926149d4c0cd87dd14
Jericho <Chalwk77@users.noreply.github.com>
2017-02-19 22:39:41 +1300

    Update Grenade Launcher.lua

--- SAPP SCRIPTS/Grenade Launcher.lua
+++ SAPP SCRIPTS/Grenade Launcher.lua
@@ -36,34 +36,34 @@ end
 
 function OnObjectSpawn(PlayerIndex, MapID, ParentID, ObjectID) 
     if MapID == get_tag_info("proj", "weapons\\pistol\\bullet") then
-        timer(0, "HandleProjectile", PlayerIndex, ParentID)
+        timer(0, "ChangeProjectile", ParentID)
         return false
     end	
 end
 
-function HandleProjectile(PlayerIndex)
+function Thread_ChangeProjectile(id, count, PlayerIndex)
     ChangeProjectile(PlayerIndex)
+    return false
 end
 
 function ChangeProjectile(PlayerIndex)
-    local m_object = get_dynamic_player(PlayerIndex)
-    if (m_object ~= 0) then
-        local WeaponID = get_object_memory(read_dword(m_object + 0x2F8))
-        local X_Aim = read_float(WeaponID, 0x230)
-        local Y_aim = read_float(WeaponID, 0x234)
-        local Z_aim = read_float(WeaponID, 0x238)
-        local x = read_float(WeaponID, 0x5C)
-        local y = read_float(WeaponID, 0x60)
-        local z = read_float(WeaponID, 0x64)
+    local player_object = get_dynamic_player(PlayerIndex)
+    if player_object ~= nil then
+        local m_objectId --[[something]]--
+        local X_Aim = read_float(m_objectId, 0x230)
+        local Y_aim = read_float(m_objectId, 0x234)
+        local Z_aim = read_float(m_objectId, 0x238)
+        local x = read_float(m_objectId, 0x5C)
+        local y = read_float(m_objectId, 0x60)
+        local z = read_float(m_objectId, 0x64)
         local ProjX = x + DISTANCE * math.sin(X_Aim)
         local ProjY = y + DISTANCE * math.sin(Y_aim)
         local ProjZ = z + DISTANCE * math.sin(Z_aim) + 0.5
         local projId = spawn_object("proj", "weapons\\frag grenade\\frag grenade", ProjX, ProjY, ProjZ)
-        local WeaponObj = get_object_memory(projId)
-        if WeaponObj then
-            write_float(WeaponObj, 0x68, tonumber(VELOCITY) * math.sin(X_Aim))
-            write_float(WeaponObj, 0x6C, tonumber(VELOCITY) * math.sin(Y_aim))
-            write_float(WeaponObj, 0x70, tonumber(VELOCITY) * math.sin(Z_aim))
+        local proj_obj = get_object_memory(projId)
+        if proj_obj then
+            write_float(proj_obj, 0x68, tonumber(VELOCITY) * math.sin(X_Aim))
+            write_float(proj_obj, 0x6C, tonumber(VELOCITY) * math.sin(Y_aim))
         end
     end
 end
