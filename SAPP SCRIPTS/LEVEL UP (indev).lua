36870330f8f9712b25e824b1a252865de0df2dbf
Jericho <Chalwk77@users.noreply.github.com>
2017-02-28 17:51:21 +1300

    Update LEVEL UP (indev).lua

--- SAPP SCRIPTS/LEVEL UP (indev).lua
+++ SAPP SCRIPTS/LEVEL UP (indev).lua
@@ -12,10 +12,10 @@ https://github.com/Chalwk77/HALO-SCRIPT-PROJECTS
 Copyright (c) 2016-2017, Jericho Crosby <jericho.crosby227@gmail.com>
 * Notice: You can use this document subject to the following conditions:
 https://github.com/Chalwk77/Halo-Scripts-Phasor-V2-/blob/master/LICENSE
-    
+
 * IGN: Chalwk
 * This is my extension of a progression based game (for Phasor V2+) by OZ Clan
-* Rewritten, converted to SAPP, and heavily modified by Jericho Crosby, (Chalwk).
+* Written by Jericho Crosby (Chalwk)
 ]]
 
 api_version = "1.11.0.0"
