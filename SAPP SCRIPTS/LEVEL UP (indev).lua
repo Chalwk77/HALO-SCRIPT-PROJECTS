02b9d5990675e10684baa3ad28d54fa7f3be7c34
Jericho <Chalwk77@users.noreply.github.com>
2017-02-28 17:46:06 +1300

    Update LEVEL UP (indev).lua

--- SAPP SCRIPTS/LEVEL UP (indev).lua
+++ SAPP SCRIPTS/LEVEL UP (indev).lua
@@ -5,16 +5,17 @@ Implementing API version: 1.11.0.0
     Acknowledgments
     Credits to "Giraffe" for his AutoVehicle-Flip functions.
     Credits to 002 for his get_tag_info function (return metaid)
-    
+
 This script is also available on my github! Check my github for regular updates on my projects, including this script.
 https://github.com/Chalwk77/HALO-SCRIPT-PROJECTS
 
 Copyright (c) 2016-2017, Jericho Crosby <jericho.crosby227@gmail.com>
 * Notice: You can use this document subject to the following conditions:
 https://github.com/Chalwk77/Halo-Scripts-Phasor-V2-/blob/master/LICENSE
-
+    
 * IGN: Chalwk
-* Written by Jericho Crosby (Chalwk)
+* This is my extension of a progression based game (for Phasor V2+) by OZ Clan
+* Rewritten, converted to SAPP, and heavily modified by Jericho Crosby, (Chalwk).
 ]]
 
 api_version = "1.11.0.0"
