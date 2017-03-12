264b25c27a6b5f672fbe5364032124b74c95a889
Jericho <Chalwk77@users.noreply.github.com>
2017-03-12 22:57:17 +1300

    Create medals.lua

---
+++ Focal Point Data Entries/medals.lua
@@ -0,0 +1,32 @@
+return { 
+    ["6c8f0bc306e0108b4904812110185edd"] = { 
+        ["class"] = { 
+            ["assists"] = "Iron",
+            ["closequarters"] = "Iron",
+            ["crackshot"] = "Iron",
+            ["grenadier"] = "Iron",
+            ["heavyweapons"] = "Iron",
+            ["jackofalltrades"] = "Iron",
+            ["mobileasset"] = "Iron",
+            ["multikill"] = "Iron",
+            ["roadrage"] = "Iron",
+            ["sidearm"] = "Iron",
+            ["sprees"] = "Iron",
+            ["triggerman"] = "Iron"
+        },
+        ["count"] = { 
+            ["assists"] = 0,
+            ["closequarters"] = 0,
+            ["crackshot"] = 0,
+            ["grenadier"] = 0,
+            ["heavyweapons"] = 0,
+            ["jackofalltrades"] = 0,
+            ["moblieasset"] = 0,
+            ["multikill"] = 0,
+            ["roadrage"] = 0,
+            ["sidearm"] = 0,
+            ["sprees"] = 0,
+            ["triggerman"] = 0
+        }
+    }
+}
