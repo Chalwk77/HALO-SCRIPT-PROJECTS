34e3c02c6fde47c70d128c924dcf0f089cf71410
Jericho <Chalwk77@users.noreply.github.com>
2017-03-12 22:56:54 +1300

    Create killstats.lua

---
+++ Focal Point Data Entries/killstats.lua
@@ -0,0 +1,13 @@
+return { 
+    ["6c8f0bc306e0108b4904812110185edd"] = { 
+        ["total"] = { 
+            ["assists"] = 0,
+            ["betrays"] = 0,
+            ["credits"] = 0,
+            ["deaths"] = 0,
+            ["kills"] = 0,
+            ["rank"] = "Recruit",
+            ["suicides"] = 0
+        }
+    }
+}
