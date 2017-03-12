d74d8f83a1e3dda76c6dfd81d2188ea0038d8503
Jericho <Chalwk77@users.noreply.github.com>
2017-03-12 22:57:33 +1300

    Create Sprees.lua

---
+++ Focal Point Data Entries/Sprees.lua
@@ -0,0 +1,24 @@
+return { 
+    ["6c8f0bc306e0108b4904812110185edd"] = { 
+        ["count"] = { 
+            ["anomgstopkillingme"] = 0,
+            ["double"] = 0,
+            ["invincible"] = 0,
+            ["killimanjaro"] = 0,
+            ["killingfrenzy"] = 0,
+            ["killingspree"] = 0,
+            ["killionaire"] = 0,
+            ["killpocalypse"] = 0,
+            ["killtacular"] = 0,
+            ["killtastrophe"] = 0,
+            ["killtrocity"] = 0,
+            ["overkill"] = 0,
+            ["rampage"] = 0,
+            ["runningriot"] = 0,
+            ["timeaslms"] = 0,
+            ["triple"] = 0,
+            ["unfrigginbelievable"] = 0,
+            ["untouchable"] = 0
+        }
+    }
+}
