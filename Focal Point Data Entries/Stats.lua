60d6d998c2e71b703a04e79133ffd088dc3a8ce1
Jericho <Chalwk77@users.noreply.github.com>
2017-03-12 22:57:50 +1300

    Create Stats.lua

---
+++ Focal Point Data Entries/Stats.lua
@@ -0,0 +1,30 @@
+return { 
+    ["6c8f0bc306e0108b4904812110185edd"] = { 
+        ["kills"] = { 
+            ["assaultrifle"] = 0,
+            ["banshee"] = 0,
+            ["bansheefuelrod"] = 0,
+            ["chainhog"] = 0,
+            ["flagmelee"] = 0,
+            ["flamethrower"] = 0,
+            ["fragnade"] = 0,
+            ["fuelrod"] = 0,
+            ["ghost"] = 0,
+            ["grenadestuck"] = 0,
+            ["melee"] = 0,
+            ["needler"] = 0,
+            ["oddballmelee"] = 0,
+            ["pistol"] = 0,
+            ["plasmanade"] = 0,
+            ["plasmapistol"] = 0,
+            ["plasmarifle"] = 0,
+            ["rocket"] = 0,
+            ["shotgun"] = 0,
+            ["sniper"] = 0,
+            ["splatter"] = 0,
+            ["tankmachinegun"] = 0,
+            ["tankshell"] = 0,
+            ["turret"] = 0
+        }
+    }
+}
