d66a7b9b3416d3b0dcaa32747325932b0cdcde3e
Jericho <Chalwk77@users.noreply.github.com>
2017-03-16 19:12:45 +1300

    Update CalculatePlayers.lua

--- CalculatePlayers.lua
+++ CalculatePlayers.lua
@@ -1,4 +1,25 @@
+current_players = 0
+
+function OnNewGame()
+    for i = 1, 16 do
+        if player_present(i) then
+            current_players = current_players + 1
+        end
+    end
+end
+
+function OnPlayerLeave(PlayerIndex)
+    current_players = current_players - 1
+end
+
+function OnPlayerJoin(PlayerIndex)
+    current_players = current_players + 1
+end
+
 function CalculatePlayers(PlayerIndex)
 
 end
-    
+
+function OnGameEnd()
+    current_players = 0
+end
