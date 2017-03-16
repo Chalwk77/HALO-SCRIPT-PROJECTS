5601f7b8849d8bdacfa3c64f918fb614fd7bb550
Jericho <Chalwk77@users.noreply.github.com>
2017-03-16 19:35:13 +1300

    Update CalculatePlayers.lua

--- CalculatePlayers.lua
+++ CalculatePlayers.lua
@@ -1,5 +1,8 @@
 current_players = 0
 
+var_offset_1 = 0.10
+var_offset_1 = 0.20
+
 function OnNewGame()
     for i = 1, 16 do
         if player_present(i) then
@@ -16,23 +19,22 @@ function OnPlayerJoin(PlayerIndex)
     current_players = current_players + 1
 end
 
-function math.round(number, place)
-    return math.floor(number *(10 ^(place or 0)) + 0.5) /(10 ^(place or 0))
-end
-
 function CalculatePlayers(PlayerIndex)
     -- between 1-5
     if current_players >= 1 and current_players <= 5 then
         local mapname = get_var(1, "$map")
-        PlayerSpeed = player_speed[players[PlayerIndex][1]][mapname]
+        local PlayerSpeed = player_speed[players[PlayerIndex][1]][mapname]
+        execute_command("s " .. PlayerIndex .. " :" .. tonumber(PlayerSpeed))
     -- between 5-10
     elseif current_players >= 5 and current_players <= 10 then
         local mapname = get_var(1, "$map")
-        local PlayerSpeed = player_speed[players[PlayerIndex][1]][mapname] + math.floor(math.round(0.5))
+        local PlayerSpeed = player_speed[players[PlayerIndex][1]][mapname] + var_offset_1
+        execute_command("s " .. PlayerIndex .. " :" .. tonumber(PlayerSpeed))
     -- between 10-16
     elseif current_players >= 10 and current_players <= 16 then
         local mapname = get_var(1, "$map")
-        PlayerSpeed = player_speed[players[PlayerIndex][1]][mapname] + math.floor(math.round(0.10))
+        local PlayerSpeed = player_speed[players[PlayerIndex][1]][mapname] + var_offset_2
+        execute_command("s " .. PlayerIndex .. " :" .. tonumber(PlayerSpeed))
     end
 end
 
