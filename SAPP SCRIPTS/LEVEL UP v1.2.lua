8dbb6ff8e5c5975121e35c873367558a655b366e
Jericho <Chalwk77@users.noreply.github.com>
2017-03-17 22:37:38 +1300

    Update LEVEL UP v1.2.lua

--- SAPP SCRIPTS/LEVEL UP v1.2.lua
+++ SAPP SCRIPTS/LEVEL UP v1.2.lua
@@ -216,7 +216,7 @@ RocketLauncher_Multiplier = 1.090
 -------------------------------------------------------------------------------------
 
 -- ============== Player Running Speed Configuration ============== --
--- You can specify player running speeds on a per level / per map basis in a function called "UpdatePlayerSpeed" on line 1576.
+-- You can specify player running speeds on a per level / per map basis in a function called "UpdatePlayerSpeed" on line 1599.
 -- This means that you can specify how fast all player's will run on the map XXX if they are level X and so on.
 -- There is a lot of flexibility here.
 
