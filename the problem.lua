d0c01a587c595b1c8ecfad976f58384e86f8f458
Jericho <Chalwk77@users.noreply.github.com>
2017-03-23 23:11:25 +1300

    Create the problem.lua

---
+++ the problem.lua
@@ -0,0 +1,9 @@
+-- CHAT
+I type: /afk 1
+Dedicated Server Console tells me the value of the second argument was 1 like so:
+-- The value was: 1
+
+-- RCON
+I type: sv_afk 1
+Dedicated Server Console tells me the value of the second argument was "1" (in double quotes) like so:
+-- The value was: "1"
