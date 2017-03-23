d7c7b8d8ba5ea575f05d60e1f10b24f2a08dfd9e
Jericho <Chalwk77@users.noreply.github.com>
2017-03-23 23:12:29 +1300

    Update the problem.lua

--- the problem.lua
+++ the problem.lua
@@ -1,3 +1,5 @@
+Just to clarify, argument 1 is /afk, and argument 2 is the player's number, in this case, 1 (being me) as I am the only person in the server.
+
 -- CHAT
 I type: /afk 1
 Dedicated Server Console tells me the value of the second argument was 1 like so:
