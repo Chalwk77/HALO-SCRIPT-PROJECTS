1918e818d35e60a945b539fc7808b8ea7814f57a
Jericho <Chalwk77@users.noreply.github.com>
2017-05-01 02:28:59 +1200

    Create speed variables.lua

---
+++ speed variables.lua
@@ -0,0 +1,11 @@
+Each level your speed will increase by either SpeedMin or SpeedMax. This selection is random.
+level 1 = SpeedMin = 1/10 * 1 + 0.025 or SpeedMax = 2/10 * 1 + 0.025
+level 2 = SpeedMin = 1/10 * 1 + 0.045 or SpeedMax = 2/10 * 1 + 0.045
+level 3 = SpeedMin = 1/10 * 1 + 0.065 or SpeedMax = 2/10 * 1 + 0.065
+level 4 = SpeedMin = 1/10 * 1 + 0.085 or SpeedMax = 2/10 * 1 + 0.085
+level 5 = SpeedMin = 1/10 * 1 + 0.10 or SpeedMax = 2/10 * 1 + 0.10
+level 6 = SpeedMin = 1/10 * 1 + 0.11 or SpeedMax = 2/10 * 1 + 0.11
+level 7 = SpeedMin = 1/10 * 1 + 0.12 or SpeedMax = 2/10 * 1 + 0.12
+level 8 = SpeedMin = 1/10 * 1 + 0.13 or SpeedMax = 2/10 * 1 + 0.13
+level 9 = SpeedMin = 1/10 * 1 + 0.14 or SpeedMax = 2/10 * 1 + 0.14
+level 10 = SpeedMin = 1/10 * 1 + 0.15 or SpeedMax = 2/10 * 1 + 0.15
