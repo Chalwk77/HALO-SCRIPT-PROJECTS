08919017f8e3f026e655594eb9ff0ee540078f75
Jericho <Chalwk77@users.noreply.github.com>
2017-05-01 13:24:15 +1200

    Update speed variables.lua

--- speed variables.lua
+++ speed variables.lua
@@ -1,4 +1,4 @@
-Each level your speed will increase by either SpeedMin or SpeedMax. This selection is random.
+Old Speed Values:
 level 1 = SpeedMin = 1/10 * 1 + 0.025 or SpeedMax = 2/10 * 1 + 0.025
 level 2 = SpeedMin = 1/10 * 1 + 0.045 or SpeedMax = 2/10 * 1 + 0.045
 level 3 = SpeedMin = 1/10 * 1 + 0.065 or SpeedMax = 2/10 * 1 + 0.065
@@ -9,3 +9,15 @@ level 7 = SpeedMin = 1/10 * 1 + 0.12 or SpeedMax = 2/10 * 1 + 0.12
 level 8 = SpeedMin = 1/10 * 1 + 0.13 or SpeedMax = 2/10 * 1 + 0.13
 level 9 = SpeedMin = 1/10 * 1 + 0.14 or SpeedMax = 2/10 * 1 + 0.14
 level 10 = SpeedMin = 1/10 * 1 + 0.15 or SpeedMax = 2/10 * 1 + 0.15
+
+New Speed Values:
+level 1 = SpeedMin = 1/10 * 1 + 0.0125 or SpeedMax = 2/10 * 1 + 0.0125
+level 2 = SpeedMin = 1/10 * 1 + 0.0225 or SpeedMax = 2/10 * 1 + 0.0225
+level 3 = SpeedMin = 1/10 * 1 + 0.0325 or SpeedMax = 2/10 * 1 + 0.0325
+level 4 = SpeedMin = 1/10 * 1 + 0.0425 or SpeedMax = 2/10 * 1 + 0.0425
+level 5 = SpeedMin = 1/10 * 1 + 0.05 or SpeedMax = 2/10 * 1 + 0.05
+level 6 = SpeedMin = 1/10 * 1 + 0.055 or SpeedMax = 2/10 * 1 + 0.055
+level 7 = SpeedMin = 1/10 * 1 + 0.06 or SpeedMax = 2/10 * 1 + 0.06
+level 8 = SpeedMin = 1/10 * 1 + 0.065 or SpeedMax = 2/10 * 1 + 0.065
+level 9 = SpeedMin = 1/10 * 1 + 0.07 or SpeedMax = 2/10 * 1 + 0.07
+level 10 = SpeedMin = 1/10 * 1 + 0.075 or SpeedMax = 2/10 * 1 + 0.075
