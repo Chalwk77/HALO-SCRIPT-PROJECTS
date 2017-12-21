7e1615daa8afa16ae0bac938a4d57504f48fb92a
Chalwk77 <jericho.crosby227@gmail.com>
2017-12-21 21:07:32 +1300

    Repository Clean Up

--- SAPP SCRIPTS/[COMMAND] Get Ping.lua
+++ SAPP SCRIPTS/[COMMAND] Get Ping.lua
@@ -1,9 +1,9 @@
 --[[
 --=====================================================================================================--
-Script Name: Get Hash (command), for SAPP (PC & CE)
-Description: Check yours or someone else's hash
+Script Name: Ping Me (command), for SAPP (PC & CE)
+Description: Check yours or someone else's ping
 
-    Command Syntax: /hash me|1-16
+    Command Syntax: /ping me|1-16
 
 Copyright (c) 2016-2017, Jericho Crosby <jericho.crosby227@gmail.com>
 * Notice: You can use this document subject to the following conditions:
@@ -16,12 +16,12 @@ https://github.com/Chalwk77/Halo-Scripts-Phasor-V2-/blob/master/LICENSE
 api_version = "1.11.0.0"
 
 -- Custom Command
-custom_command = "hash"
--- Message sent to command executor if they type /hash, or /hash me
-respondWith1 = "Your hash is $HASH"
--- Message sent to command executor if they type /hash 1-16
-respondWith2 = "$PLAYER_NAME's hash is $HASH"
--- Minimum admin level required to execute /hash command. (-1 for all players)
+custom_command = "ping"
+-- Message sent to command executor if they type /ping, or /ping me
+respondWith1 = "Your ping is $PING"
+-- Message sent to command executor if they type /ping 1-16
+respondWith2 = "$PLAYER_NAME's ping is $PING"
+-- Minimum admin level required to execute /ping command. (-1 for all players)
 PERMISSION_LEVEL = -1
 -- Message Alignment | Left = l,    Right = r,    Center = c,    Tab: t
 Alignment = "l"
@@ -41,7 +41,7 @@ function OnServerCommand(PlayerIndex, Command)
                 local index = tonumber(t[2])
                 if (t[2] == nil) or(t[2] ~= nil and t[2] == "me") then
                     UnknownCMD = false
-                    rprint(PlayerIndex, "|" .. Alignment .. string.gsub(respondWith1, "$HASH", get_var(PlayerIndex, "$hash")))
+                    rprint(PlayerIndex, "|" .. Alignment .. string.gsub(respondWith1, "$PING", get_var(PlayerIndex, "$ping")))
                 elseif (t[2] ~= nil and t[2] ~= "me") then
                     if not string.match(t[2], "%d") then
                         UnknownCMD = false
@@ -50,7 +50,7 @@ function OnServerCommand(PlayerIndex, Command)
                         if index ~= nil and index > 0 and index < 17 then
                             if player_present(index) then
                                 UnknownCMD = false
-                                rprint(PlayerIndex, "|" .. Alignment .. string.gsub(string.gsub(respondWith2, "$PLAYER_NAME", get_var(PlayerIndex, "$name")), "$HASH", get_var(PlayerIndex, "$hash")))
+                                rprint(PlayerIndex, "|" .. Alignment .. string.gsub(string.gsub(respondWith2, "$PLAYER_NAME", get_var(PlayerIndex, "$name")), "$PING", get_var(PlayerIndex, "$ping")))
                             else
                                 UnknownCMD = false
                                 rprint(PlayerIndex, "|" .. Alignment .. "Invalid Player ID!")
@@ -60,7 +60,7 @@ function OnServerCommand(PlayerIndex, Command)
                 end
             else
                 UnknownCMD = false
-                rprint(PlayerIndex, "|" .. Alignment .. "You do not have permission to execute that command!")
+                rprint(PlayerIndex, "|" .. Alignment .. "You do not have permission to execute that command")
             end
         end
     end
