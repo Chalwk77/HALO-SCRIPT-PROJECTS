ae6718a2440b4c73861c16c5311e0970a5228830
Jericho <Chalwk77@users.noreply.github.com>
2017-12-21 19:58:37 +1300

    Update Ping Me.lua

--- SAPP SCRIPTS/Ping Me.lua
+++ SAPP SCRIPTS/Ping Me.lua
@@ -1,11 +1,9 @@
 --[[
 --=====================================================================================================--
-Script Name: Ping Me, for SAPP | (PC & CE)
+Script Name: Ping Me, for SAPP (PC & CE)
+Description: Check yours or someone else's ping
 
-Description: Display yours or someone else's ping.
-
-Command Syntax: /ping me|1-16
-
+    Command Syntax: /ping me|1-16
 
 Copyright (c) 2016-2017, Jericho Crosby <jericho.crosby227@gmail.com>
 * Notice: You can use this document subject to the following conditions:
@@ -18,10 +16,16 @@ https://github.com/Chalwk77/Halo-Scripts-Phasor-V2-/blob/master/LICENSE
 
 api_version = "1.11.0.0"
 
+-- Custom Command
 custom_command = "ping"
+-- Message sent to command executor if they type /ping, or /ping me
 respondWith1 = "Your ping is $PING"
+-- Message sent to command executor if they type /ping 1-16
 respondWith2 = "$PLAYER_NAME's ping is $PING"
+-- Minimum admin level required to execute /ping command. (-1 for all players)
 PERMISSION_LEVEL = -1
+-- Message Alignment | Left = l,    Right = r,    Center = c,    Tab: t
+Alignment = "l"
 
 function OnScriptLoad()
     register_callback(cb['EVENT_COMMAND'], "OnServerCommand")
@@ -30,40 +34,38 @@ end
 function OnScriptUnload() end
 
 function OnServerCommand(PlayerIndex, Command)
-    local silent_UnknownCMD = nil
+    local UnknownCMD = nil
     local t = tokenizestring(Command)
     if t[1] ~= nil then
         if t[1] == string.lower(custom_command) then
-            execute_command("msg_prefix \"\"")
             if tonumber(get_var(PlayerIndex, "$lvl")) >= PERMISSION_LEVEL then
                 local index = tonumber(t[2])
                 if (t[2] == nil) or(t[2] ~= nil and t[2] == "me") then
-                    silent_UnknownCMD = false
-                    say(PlayerIndex, string.gsub(respondWith1, "$PING", get_var(PlayerIndex, "$ping")))
+                    UnknownCMD = false
+                    rprint(PlayerIndex, "|" .. Alignment .. string.gsub(respondWith1, "$PING", get_var(PlayerIndex, "$ping")))
                 elseif (t[2] ~= nil and t[2] ~= "me") then
                     if not string.match(t[2], "%d") then
-                        silent_UnknownCMD = false
-                        say(PlayerIndex, "Arg2 was not a number!")
+                        UnknownCMD = false
+                        rprint(PlayerIndex, "|" .. Alignment .. "Arg2 was not a number!")
                     else
                         if index ~= nil and index > 0 and index < 17 then
                             if player_present(index) then
-                                silent_UnknownCMD = false
-                                say(PlayerIndex, string.gsub(string.gsub(respondWith2, "$PLAYER_NAME", get_var(PlayerIndex, "$name")), "$PING", get_var(PlayerIndex, "$ping")))
+                                UnknownCMD = false
+                                rprint(PlayerIndex, "|" .. Alignment .. string.gsub(string.gsub(respondWith2, "$PLAYER_NAME", get_var(PlayerIndex, "$name")), "$PING", get_var(PlayerIndex, "$ping")))
                             else
-                                silent_UnknownCMD = false
-                                say(PlayerIndex, "Invalid Player ID!")
+                                UnknownCMD = false
+                                rprint(PlayerIndex, "|" .. Alignment .. "Invalid Player ID!")
                             end
                         end
                     end
                 end
             else
-                silent_UnknownCMD = false
-                say(PlayerIndex, "You do not have permission to execute that command")
+                UnknownCMD = false
+                rprint(PlayerIndex, "|" .. Alignment .. "You do not have permission to execute that command")
             end
-            execute_command("msg_prefix \"** SERVER ** \"")
         end
     end
-    return silent_UnknownCMD
+    return UnknownCMD
 end
 
 function tokenizestring(inputstr, sep)
