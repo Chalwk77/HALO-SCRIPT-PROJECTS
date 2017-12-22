17ca8d5d429774aa3c4dd04c259bd9947f4137e2
Jericho <Chalwk77@users.noreply.github.com>
2017-12-22 23:51:56 +1300

    Update Juggernaut.lua

--- SAPP SCRIPTS/Juggernaut.lua
+++ SAPP SCRIPTS/Juggernaut.lua
@@ -18,10 +18,18 @@ https://github.com/Chalwk77/Halo-Scripts-Phasor-V2-/blob/master/LICENSE
 ]]-- 
 
 api_version = "1.11.0.0"
--- Tables --
-players = { }
+-- tables --
 players_available = { }
--- Counts --
+players = { }
+weapons = { }
+weapon = { }
+frags = { }
+plasmas = { }
+weapons[00000] = "nil\\nil\\nil"
+-- booleans --
+MapIsListed = nil
+bool = nil
+-- counts --
 current_players = 0
 
 --============= CONFIGURATION STARTS HERE =============--
@@ -29,6 +37,84 @@ current_players = 0
 player_count_threashold = 3
 -- Message to send to all players when a new Juggernaut is assigned.
 JuggernautAssignMessage = "$NAME is now the Juggernaut!"
+
+weapons[1] = "weapons\\pistol\\pistol"
+weapons[2] = "weapons\\sniper rifle\\sniper rifle"
+weapons[3] = "weapons\\rocket launcher\\rocket launcher"
+
+-- Message Alignment | Left = l,    Right = r,    Center = c,    Tab: t
+Alignment = "l"
+
+gamesettings = {
+    ["AssignFragGrenades"] = true,
+    ["AssignPlasmaGrenades"] = true
+}
+
+function GrenadeTable()
+--  frag grenade table --
+    frags = {    
+        beavercreek = 4,
+        bloodgulch = 4,
+        boardingaction = 4,
+        carousel = 4,
+        dangercanyon = 4,
+        deathisland = 4,
+        gephyrophobia = 4,
+        icefields = 4,
+        infinity = 4,
+        sidewinder = 4,
+        timberland = 4,
+        hangemhigh = 4,
+        ratrace = 4,
+        damnation = 4,
+        putput = 4,
+        prisoner = 4,
+        wizard = 4
+    }
+--  plasma grenades table --
+    plasmas = {
+        beavercreek = 4,
+        bloodgulch = 4,
+        boardingaction = 4,
+        carousel = 4,
+        dangercanyon = 4,
+        deathisland = 4,
+        gephyrophobia = 4,
+        icefields = 4,
+        infinity = 4,
+        sidewinder = 4,
+        timberland = 4,
+        hangemhigh = 4,
+        ratrace = 4,
+        damnation = 4,
+        putput = 4,
+        prisoner = 4,
+        wizard = 4
+    }
+end
+    
+function LoadMaps()
+    -- mapnames table --
+    mapnames = {
+        "beavercreek",
+        "bloodgulch",
+        "boardingaction",
+        "carousel",
+        "dangercanyon",
+        "deathisland",
+        "gephyrophobia",
+        "icefields",
+        "infinity",
+        "sidewinder",
+        "timberland",
+        "hangemhigh",
+        "ratrace",
+        "damnation",
+        "putput",
+        "prisoner",
+        "wizard"
+    }
+end
 --============= CONFIGURATION ENDS HERE =============--
 
 function OnScriptLoad()
@@ -38,12 +124,49 @@ function OnScriptLoad()
     register_callback(cb['EVENT_GAME_END'], "OnGameEnd")
     register_callback(cb['EVENT_LEAVE'], "OnPlayerLeave")
     register_callback(cb['EVENT_GAME_START'], "OnNewGame")
+    register_callback(cb['EVENT_SPAWN'], "OnPlayerSpawn")
     for i = 1, 16 do
         if player_present(i) then
             players[get_var(i, "$n")].current_juggernaut = nil
         end
     end
+    if get_var(0, "$gt") ~= "n/a" then
+        mapname = get_var(0, "$map")
+        GrenadeTable()
+        LoadMaps()
+    end
     current_players = 0
+end
+
+function OnPlayerSpawn(PlayerIndex)
+    weapon[PlayerIndex] = 0
+    mapname = get_var(0, "$map")
+end
+
+function AssignGrenades(PlayerIndex)
+    if player_alive(PlayerIndex) then
+        local player_object = get_dynamic_player(PlayerIndex)
+        if (player_object ~= 0) then
+            if (gamesettings["AssignFragGrenades"] == true) then
+                if (frags[mapname] == nil) then 
+                    Error = 'Error: ' .. mapname .. ' is not listed in the Frag Grenade Table - Line 55 | Unable to set frags.'
+                    cprint(Error, 4+8)
+                    execute_command("log_note \""..Error.."\"")
+                else
+                    write_word(player_object + 0x31E, frags[mapname])
+                end
+            end
+            if (gamesettings["AssignPlasmaGrenades"] == true) then
+                if (plasmas[mapname] == nil) then 
+                    Error = 'Error: ' .. mapname .. ' is not listed in the Plasma Grenade Table - Line 75 | Unable to set plasmas.'
+                    cprint(Error, 4+8)
+                    execute_command("log_note \""..Error.."\"")
+                else
+                    write_word(player_object + 0x31F, plasmas[mapname])
+                end
+            end
+        end
+    end
 end
 
 function OnTick()
@@ -58,6 +181,31 @@ function OnTick()
                         if i ~= nil then
                             -- Remove NAV marker (it points to red base for some reason)
                             write_word(m_player + 0x88, player + 10)
+                        end
+                    end
+                end
+            end
+        end
+    end
+    for j = 1, 16 do
+        if (player_alive(j)) then
+            if (j == players[get_var(j, "$n")].current_juggernaut) then
+                if MapIsListed == false then
+                    return false
+                else
+                    local player = get_dynamic_player(j)
+                    if (weapon[j] == 0) then
+                        execute_command("wdel " .. j)
+                        local x, y, z = read_vector3d(player + 0x5C)
+                        if (mapname == "bloodgulch") then
+                            assign_weapon(spawn_object("weap", weapons[3], x, y, z), j)
+                            assign_weapon(spawn_object("weap", weapons[1], x, y, z), j)
+                            assign_weapon(spawn_object("weap", weapons[2], x, y, z), j)
+                            weapon[j] = 1
+                            if (bool == true) then
+                                AssignGrenades(j)
+                                bool = false
+                            end
                         end
                     end
                 end
@@ -89,6 +237,9 @@ end
 
 function OnNewGame()
     gamestarted = true
+    mapname = get_var(0, "$map")
+    GrenadeTable()
+    LoadMaps()
     for i = 1, 16 do
         if player_present(i) then
             current_players = current_players + 1
@@ -97,7 +248,15 @@ function OnNewGame()
     end
     -- If there are 3 or more players, select a random Juggernaut
     if current_players >= player_count_threashold then
-        timer(1000 * 5, "SelectNewJuggernaut")
+        timer(1000 * 3, "SelectNewJuggernaut")
+    end
+    if (table.match(mapnames, mapname) == nil) then 
+        MapIsListed = false
+        Error = 'Error: ' .. mapname .. ' is not listed in "mapnames table" - line 98'
+        cprint(Error, 4+8)
+        execute_command("log_note \""..Error.."\"")
+    else
+        MapIsListed = true
     end
 end
 
@@ -117,6 +276,7 @@ function SelectNewJuggernaut()
                     SetNavMarker(i)
                     -- Clear the Table
                     players_available = { }
+                    bool = true
                 end
             end
         end
@@ -126,20 +286,31 @@ end
 function OnPlayerDeath(PlayerIndex, KillerIndex)
     local victim = tonumber(PlayerIndex)
     local killer = tonumber(KillerIndex)
-    -- Neither Killer or Victim are the current Juggernaut >> Make Killer the new Juggernaut - (first person to kill becomes the Juggernaut)
+    -- Killer is Juggernaut | Victim is Not Juggernaut | Update Score
+    if (killer == players[get_var(killer, "$n")].current_juggernaut) and (victim ~= players[get_var(victim, "$n")].current_juggernaut) then
+        increment = 1
+        setscore(killer, increment)
+        rprint(KillerIndex, "|" .. Alignment .. "+3")
+    end
+    -- Neither Killer or Victim are the Juggernaut | Make Killer Juggernaut
     if (current_players == 2) then
-        if killer ~= players[get_var(killer, "$n")].current_juggernaut and(victim ~= players[get_var(PlayerIndex, "$n")].current_juggernaut) then
+        if (killer ~= players[get_var(killer, "$n")].current_juggernaut) and (victim ~= players[get_var(PlayerIndex, "$n")].current_juggernaut) then
             players[get_var(killer, "$n")].current_juggernaut = killer
+            bool = true
             SetNavMarker(KillerIndex)
+            increment = 1
+            setscore(killer, increment)
+            rprint(KillerIndex, "|" .. Alignment .. "+3")
             execute_command("msg_prefix \"\"")
             say_all(string.gsub(JuggernautAssignMessage, "$NAME", get_var(killer, "$name")))
             execute_command("msg_prefix \"** SERVER ** \"")
         end
     end
-    -- Killer annihilates the current Juggernaut > Make them the new Juggernaut (only if there is 3 or more players online)
-    if (current_players > 2) then
-        if killer ~= players[get_var(killer, "$n")].current_juggernaut and(victim == players[get_var(PlayerIndex, "$n")].current_juggernaut) then
+    -- Killer is not Juggernaut | Victim is Current Juggernaut | Make Killer Juggernaut (only if there is 2 or more players online)
+    if (current_players >= 2) then
+        if (victim == players[get_var(victim, "$n")].current_juggernaut) and (killer ~= players[get_var(killer, "$n")].current_juggernaut) then
             players[get_var(killer, "$n")].current_juggernaut = killer
+            players[get_var(victim, "$n")].current_juggernaut = nil
             SetNavMarker(KillerIndex)
             execute_command("msg_prefix \"\"")
             say_all(string.gsub(JuggernautAssignMessage, "$NAME", get_var(killer, "$name")))
@@ -159,6 +330,28 @@ function SetNavMarker(Juggernaut)
                 else
                     write_word(m_player + 0x88, player)
                 end
+            end
+        end
+    end
+end
+
+function table.match(table, value)
+    for k,v in pairs(table) do
+        if v == value then
+            return k
+        end
+    end
+end
+
+function setscore(PlayerIndex, score)
+    if tonumber(score) then
+        if get_var(0, "$gt") == "slayer" then
+            if score >= 0x7FFF then
+                execute_command("score " .. PlayerIndex .. " +1")
+            elseif score <= -0x7FFF then
+                execute_command("score " .. PlayerIndex .. " -1")
+            else
+                execute_command("score " .. PlayerIndex .. " " .. score)
             end
         end
     end
