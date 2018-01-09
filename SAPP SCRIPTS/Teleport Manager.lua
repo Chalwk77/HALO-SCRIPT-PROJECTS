1933a180512cde5bea72a4f07932afe879b81d0b
Jericho <Chalwk77@users.noreply.github.com>
2018-01-09 19:15:02 +1300

    Create Teleport Manager.lua

---
+++ SAPP SCRIPTS/Teleport Manager.lua
@@ -0,0 +1,128 @@
+--[[
+--=====================================================================================================--
+Script Name: Teleport Management (utility), for SAPP (PC & CE)
+Description: Set custom Teleport Locations
+
+Command Syntax:     /settp [name]
+                    /tpo [name]
+                    /tplist
+
+
+Use this command to set a new teleport location
+/settp [teleport name]
+
+Use this command to teleport
+/tpo [teleport name]
+
+Use this command to list all custom portals
+/tplist
+             
+Copyright (c) 2016-2018, Jericho Crosby <jericho.crosby227@gmail.com>
+* Notice: You can use this document subject to the following conditions:
+https://github.com/Chalwk77/Halo-Scripts-Phasor-V2-/blob/master/LICENSE
+
+* Written by Jericho Crosby (Chalwk)
+--=====================================================================================================--
+]]-- 
+
+-- configuration starts --
+
+set_command = "setportal"
+goto_command = "tpo"
+list_command = "tplist"
+sapp_dir = "sapp\\teleports.txt"
+-- configuration ends  --
+
+api_version = "1.12.0.0"
+
+function OnScriptLoad()
+    register_callback(cb['EVENT_COMMAND'], "OnServerCommand")
+end
+
+function OnScriptUnload() end
+
+function OnServerCommand(PlayerIndex, Command, Environment)
+    local UnknownCMD = nil
+    local t = tokenizestring(Command)
+    ---------------------------------------------------------
+    -- SET COMMAND --
+    if t[1] ~= nil then
+        if t[1] == string.lower(set_command) then
+            if t[2] ~= nil then
+                local player = get_dynamic_player(PlayerIndex)
+                local x, y, z = read_vector3d(player + 0x5C)
+                local file = io.open(sapp_dir, "a+")
+                local line = t[2] .. ": " .. math.floor(x) .. ", " .. math.floor(y) .. ", " .. math.floor(z)
+                file:write(line, "\n")
+                file:close()
+                say(PlayerIndex, "Teleport location set to: " .. math.floor(x) .. ", " .. math.floor(y) .. ", " .. math.floor(z))
+            else
+                say(PlayerIndex, "Invalid Syntax. Command Usage: /" .. set_command .. " <teleport name>")
+            end
+            UnknownCMD = false
+        end
+    end
+    ---------------------------------------------------------
+    -- GO TO COMMAND --
+    if t[1] ~= nil then
+        if t[1] == string.lower(goto_command) then
+            if t[2] ~= nil then
+                local file = sapp_dir
+                local lines = lines_from(file)
+                for k,v in pairs(lines) do
+                    if t[2] == (v:match("[%a_]+[%a%d_]*")) then
+                        cprint("working", 2+8)
+                    end
+                end
+            else
+                say(PlayerIndex, "Invalid Syntax. Command Usage: /" .. goto_command .. " <teleport name>")
+            end
+            UnknownCMD = false
+        end
+    end
+    ---------------------------------------------------------
+    -- LIST COMMAND --
+    if t[1] ~= nil then
+        if t[1] == string.lower(list_command) then
+            local file = sapp_dir
+            local lines = lines_from(file)
+            for k,v in pairs(lines) do
+                rprint(PlayerIndex, "["..k.."] " .. v)
+            end
+            UnknownCMD = false
+        end
+    end
+    return UnknownCMD
+end
+
+-- Check if file exists
+function file_exists(file)
+    local File = io.open(file, "rb")
+    if File then 
+        File:close() 
+    end
+    return File ~= nil
+end
+
+function lines_from(file)
+    if not file_exists(file) then 
+        return {} 
+    end
+    lines = {}
+    for line in io.lines(file) do 
+        lines[#lines + 1] = line
+    end
+    return lines
+end
+
+function tokenizestring(inputString, separator)
+    if separator == nil then
+        separator = "%s"
+    end
+    local t = { }; i = 1
+    for str in string.gmatch(inputString, "([^" .. separator .. "]+)") do
+        t[i] = str
+        i = i + 1
+    end
+    return t
+end
