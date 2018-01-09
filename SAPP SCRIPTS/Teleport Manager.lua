34e9c4b5ba36d83f335a2788f8851dc30f163cb0
Jericho <Chalwk77@users.noreply.github.com>
2018-01-09 19:20:24 +1300

    Update Teleport Manager.lua

--- SAPP SCRIPTS/Teleport Manager.lua
+++ SAPP SCRIPTS/Teleport Manager.lua
@@ -1,7 +1,7 @@
 --[[
 --=====================================================================================================--
 Script Name: Teleport Management (utility), for SAPP (PC & CE)
-Description: Set custom Teleport Locations
+Description: Allows the user to create custom teleports and warp to them on demand.
 
 Command Syntax:     /settp [name]
                     /tpo [name]
@@ -31,6 +31,7 @@ set_command = "setportal"
 goto_command = "tpo"
 list_command = "tplist"
 sapp_dir = "sapp\\teleports.txt"
+permission_level = -1
 -- configuration ends  --
 
 api_version = "1.12.0.0"
@@ -48,16 +49,20 @@ function OnServerCommand(PlayerIndex, Command, Environment)
     -- SET COMMAND --
     if t[1] ~= nil then
         if t[1] == string.lower(set_command) then
-            if t[2] ~= nil then
-                local player = get_dynamic_player(PlayerIndex)
-                local x, y, z = read_vector3d(player + 0x5C)
-                local file = io.open(sapp_dir, "a+")
-                local line = t[2] .. ": " .. math.floor(x) .. ", " .. math.floor(y) .. ", " .. math.floor(z)
-                file:write(line, "\n")
-                file:close()
-                say(PlayerIndex, "Teleport location set to: " .. math.floor(x) .. ", " .. math.floor(y) .. ", " .. math.floor(z))
+            if tonumber(get_var(PlayerIndex, "$lvl")) >= permission_level then
+                if t[2] ~= nil then
+                    local player = get_dynamic_player(PlayerIndex)
+                    local x, y, z = read_vector3d(player + 0x5C)
+                    local file = io.open(sapp_dir, "a+")
+                    local line = t[2] .. ": " .. math.floor(x) .. ", " .. math.floor(y) .. ", " .. math.floor(z)
+                    file:write(line, "\n")
+                    file:close()
+                    say(PlayerIndex, "Teleport location set to: " .. math.floor(x) .. ", " .. math.floor(y) .. ", " .. math.floor(z))
+                else
+                    say(PlayerIndex, "Invalid Syntax. Command Usage: /" .. set_command .. " <teleport name>")
+                end
             else
-                say(PlayerIndex, "Invalid Syntax. Command Usage: /" .. set_command .. " <teleport name>")
+                say(PlayerIndex, "You're not allowed to execute /" .. set_command)
             end
             UnknownCMD = false
         end
@@ -66,16 +71,20 @@ function OnServerCommand(PlayerIndex, Command, Environment)
     -- GO TO COMMAND --
     if t[1] ~= nil then
         if t[1] == string.lower(goto_command) then
-            if t[2] ~= nil then
-                local file = sapp_dir
-                local lines = lines_from(file)
-                for k,v in pairs(lines) do
-                    if t[2] == (v:match("[%a_]+[%a%d_]*")) then
-                        cprint("working", 2+8)
+            if tonumber(get_var(PlayerIndex, "$lvl")) >= permission_level then
+                if t[2] ~= nil then
+                    local file = sapp_dir
+                    local lines = lines_from(file)
+                    for k,v in pairs(lines) do
+                        if t[2] == (v:match("[%a_]+[%a%d_]*")) then
+                            cprint("working", 2+8)
+                        end
                     end
+                else
+                    say(PlayerIndex, "Invalid Syntax. Command Usage: /" .. goto_command .. " <teleport name>")
                 end
             else
-                say(PlayerIndex, "Invalid Syntax. Command Usage: /" .. goto_command .. " <teleport name>")
+                say(PlayerIndex, "You're not allowed to execute /" .. goto_command)
             end
             UnknownCMD = false
         end
@@ -84,10 +93,14 @@ function OnServerCommand(PlayerIndex, Command, Environment)
     -- LIST COMMAND --
     if t[1] ~= nil then
         if t[1] == string.lower(list_command) then
-            local file = sapp_dir
-            local lines = lines_from(file)
-            for k,v in pairs(lines) do
-                rprint(PlayerIndex, "["..k.."] " .. v)
+            if tonumber(get_var(PlayerIndex, "$lvl")) >= permission_level then
+                local file = sapp_dir
+                local lines = lines_from(file)
+                for k,v in pairs(lines) do
+                    rprint(PlayerIndex, "["..k.."] " .. v)
+                end
+            else
+                say(PlayerIndex, "You're not allowed to execute /" .. list_command)
             end
             UnknownCMD = false
         end
