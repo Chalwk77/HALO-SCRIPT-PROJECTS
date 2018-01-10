2be97d66de2127914b17d98c7c6823a92be8dcbe
Jericho <Chalwk77@users.noreply.github.com>
2018-01-10 22:04:03 +1300

    Update and rename Teleport Manager [INDEV].lua to Teleport Manager (utility).lua

--- SAPP SCRIPTS/Teleport Manager (utility).lua
+++ SAPP SCRIPTS/Teleport Manager (utility).lua
@@ -1,6 +1,6 @@
 --[[
 --=====================================================================================================--
-Script Name: Teleport Management (utility), for SAPP (PC & CE)
+Script Name: Teleport Manager (utility), for SAPP (PC & CE)
 Description: Allows the user to create custom teleports and warp to them on demand.
 
 Warning: This script implements a heavy use of pattern matching (regex) and is extremely complicated. 
@@ -95,7 +95,7 @@ function OnServerCommand(PlayerIndex, Command, Environment)
                             local regex_14 = ("X%s*-%d+.%d+,%s*Y%s*-%d+.%d+,%s*Z%s*%d+.%d+")
                             local regex_15 = ("X%s*-%d+.%d+,%s*Y%s*%d+.%d+,%s*Z%s*-%d+.%d+")
                             local regex_16 = ("X%s*%d+.%d+,%s*Y%s*-%d+.%d+,%s*Z%s*-%d+.%d+")
-                            -- no decimal -----------------------------------------------------------------------------
+                            -- numbers without decimal points -----------------------------------------------------------------------------
                             if string.match(v, ("X%s*%d+,%s*Y%s*%d+,%s*Z%s*%d+")) then
                                 valid = true -- 0
                                 x = string.gsub(tostring(string.match(v, "X%s*%d+")), "X%s*%d+", string.match(tostring(string.match(v, "X%s*%d+")), "%d+"))
@@ -136,35 +136,47 @@ function OnServerCommand(PlayerIndex, Command, Environment)
                                 x = string.gsub(tostring(string.match(v, "X%s*%d+")), "X%s*%d+", string.match(tostring(string.match(v, "X%s*%d+")), "%d+"))
                                 y = string.gsub(tostring(string.match(v, "Y%s*-%d+")), "Y%s*-%d+", string.match(tostring(string.match(v, "Y%s*-%d+")), "-%d+"))
                                 z = string.gsub(tostring(string.match(v, "Z%s*-%d+")), "Z%s*-%d+", string.match(tostring(string.match(v, "Z%s*-%d+")), "-%d+")) 
-                            -- decimal -----------------------------------------------------------------------------
+                            -- numbers with decimal points -----------------------------------------------------------------------------
                             elseif string.match(v, regex_9) then
-                                valid = true
+                                valid = true -- 0
                                 x = string.gsub(tostring(string.match(v, "X%s*%d+.%d+")), "X%s*%d+.%d+", string.match(tostring(string.match(v, "X%s*%d+.%d+")), "%d+.%d+"))
                                 y = string.gsub(tostring(string.match(v, "Y%s*%d+.%d+")), "Y%s*%d+.%d+", string.match(tostring(string.match(v, "Y%s*%d+.%d+")), "%d+.%d+"))
                                 z = string.gsub(tostring(string.match(v, "Z%s*%d+.%d+")), "Z%s*%d+.%d+", string.match(tostring(string.match(v, "Z%s*%d+.%d+")), "%d+.%d+"))
                             elseif string.match(v, regex_10) then
---===============================================================
+                                valid = true -- *
+                                x = string.gsub(tostring(string.match(v, "X%s*-%d+.%d+")), "X%s*-%d+.%d+", string.match(tostring(string.match(v, "X%s*-%d+.%d+")), "-%d+.%d+"))
+                                y = string.gsub(tostring(string.match(v, "Y%s*-%d+.%d+")), "Y%s*-%d+.%d+", string.match(tostring(string.match(v, "Y%s*-%d+.%d+")), "-%d+.%d+"))
+                                z = string.gsub(tostring(string.match(v, "Z%s*-%d+.%d+")), "Z%s*-%d+.%d+", string.match(tostring(string.match(v, "Z%s*-%d+.%d+")), "-%d+.%d+"))
                             elseif string.match(v, regex_11) then
-                                valid = true
+                                valid = true -- 1
                                 x = string.gsub(tostring(string.match(v, "X%s*-%d+.%d+")), "X%s*-%d+.%d+", string.match(tostring(string.match(v, "X%s*-%d+.%d+")), "-%d+.%d+"))
                                 y = string.gsub(tostring(string.match(v, "Y%s*%d+.%d+")), "Y%s*%d+.%d+", string.match(tostring(string.match(v, "Y%s*%d+.%d+")), "%d+.%d+"))
                                 z = string.gsub(tostring(string.match(v, "Z%s*%d+.%d+")), "Z%s*%d+.%d+", string.match(tostring(string.match(v, "Z%s*%d+.%d+")), "%d+.%d+"))
                             elseif string.match(v, regex_12) then
---===============================================================
+                                valid = true -- 2
+                                x = string.gsub(tostring(string.match(v, "X%s*%d+.%d+")), "X%s*%d+.%d+", string.match(tostring(string.match(v, "X%s*%d+.%d+")), "%d+.%d+"))
+                                y = string.gsub(tostring(string.match(v, "Y%s*-%d+.%d+")), "Y%s*-%d+.%d+", string.match(tostring(string.match(v, "Y%s*-%d+.%d+")), "-%d+.%d+"))
+                                z = string.gsub(tostring(string.match(v, "Z%s*%d+.%d+")), "Z%s*%d+.%d+", string.match(tostring(string.match(v, "Z%s*%d+.%d+")), "%d+.%d+"))
                             elseif string.match(v, regex_13) then
-                                valid = true
+                                valid = true -- 3
                                 x = string.gsub(tostring(string.match(v, "X%s*%d+.%d+")), "X%s*%d+.%d+", string.match(tostring(string.match(v, "X%s*%d+.%d+")), "%d+.%d+"))
                                 y = string.gsub(tostring(string.match(v, "Y%s*%d+.%d+")), "Y%s*%d+.%d+", string.match(tostring(string.match(v, "Y%s*%d+.%d+")), "%d+.%d+"))
                                 z = string.gsub(tostring(string.match(v, "Z%s*-%d+.%d+")), "Z%s*-%d+.%d+", string.match(tostring(string.match(v, "Z%s*-%d+.%d+")), "-%d+.%d+"))
                             elseif string.match(v, regex_14) then
---===============================================================
+                                valid = true -- 1 & 2
+                                x = string.gsub(tostring(string.match(v, "X%s*-%d+.%d+")), "X%s*-%d+.%d+", string.match(tostring(string.match(v, "X%s*-%d+.%d+")), "-%d+.%d+"))
+                                y = string.gsub(tostring(string.match(v, "Y%s*-%d+.%d+")), "Y%s*-%d+.%d+", string.match(tostring(string.match(v, "Y%s*-%d+.%d+")), "-%d+.%d+"))
+                                z = string.gsub(tostring(string.match(v, "Z%s*%d+.%d+")), "Z%s*%d+.%d+", string.match(tostring(string.match(v, "Z%s*%d+.%d+")), "%d+.%d+"))
                             elseif string.match(v, regex_15) then
-                                valid = true
+                                valid = true -- 1 & 3
                                 x = string.gsub(tostring(string.match(v, "X%s*-%d+.%d+")), "X%s*-%d+.%d+", string.match(tostring(string.match(v, "X%s*-%d+.%d+")), "-%d+.%d+"))
                                 y = string.gsub(tostring(string.match(v, "Y%s*%d+.%d+")), "Y%s*%d+.%d+", string.match(tostring(string.match(v, "Y%s*%d+.%d+")), "%d+.%d+"))
                                 z = string.gsub(tostring(string.match(v, "Z%s*-%d+.%d+")), "Z%s*-%d+.%d+", string.match(tostring(string.match(v, "Z%s*-%d+.%d+")), "-%d+.%d+"))
                             elseif string.match(v, regex_16) then
---===============================================================
+                                valid = true  -- 2 & 3
+                                x = string.gsub(tostring(string.match(v, "X%s*%d+.%d+")), "X%s*%d+.%d+", string.match(tostring(string.match(v, "X%s*%d+.%d+")), "%d+.%d+"))
+                                y = string.gsub(tostring(string.match(v, "Y%s*-%d+.%d+")), "Y%s*-%d+.%d+", string.match(tostring(string.match(v, "Y%s*-%d+.%d+")), "-%d+.%d+"))
+                                z = string.gsub(tostring(string.match(v, "Z%s*-%d+.%d+")), "Z%s*-%d+.%d+", string.match(tostring(string.match(v, "Z%s*-%d+.%d+")), "-%d+.%d+"))
                             else
                                 rprint(PlayerIndex, "Script Error! Coordinates for that teleport do not match the regex expression!")
                                 cprint("Script Error! Coordinates for that teleport do not match the regex expression!", 4+8)
