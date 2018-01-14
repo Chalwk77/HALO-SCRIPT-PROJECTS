6d3f8357f28ee5c184832eba5e4b5184074de0aa
Jericho <Chalwk77@users.noreply.github.com>
2018-01-15 00:16:40 +1300

    Update Grenade Launcher.lua

--- SAPP SCRIPTS/Grenade Launcher.lua
+++ SAPP SCRIPTS/Grenade Launcher.lua
@@ -17,18 +17,18 @@ api_version = "1.12.0.0"
 projectile_type = "weapons\\frag grenade\\frag grenade"
 
 -- velocity to launch projectile
-default_velocity = 0.6
+velocity = 0.6
 -- distance from player to spawn projectile
-default_distance = 0.5
+distance = 0.5
 
--- minimum admin level required to execute cucsotm commands
+-- minimum admin level required to execute custom commands
 command_permission_level = 1
 
--- command to enable the grenade launcher       Syntax: /launcher on|off|1|2|true|false
+-- command to enable the grenade launcher       Syntax: /launcher on|off or 1|2 or true|false
 enable_launcher_command = "launcher"
--- command to change velocity and distance.     Syntax: /lvd [velocity] [distance]
-change_vd_command = "lvd"
--- command to change projectile type.           Syntax: /nadetype frag|plasma|1|2
+-- command to change velocity and distance.     Syntax: /vd [velocity] [distance]
+change_vd_command = "vd"
+-- command to change projectile type.           Syntax: /nadetype frag|plasma or 1|2
 change_projectile_type = "nadetype"
 -- configuration ends --
 
@@ -54,32 +54,49 @@ function OnServerCommand(PlayerIndex, Command, Environment)
     local t = tokenizestring(Command)
     if t[1] == string.lower(enable_launcher_command) then
         if tonumber(get_var(PlayerIndex, "$lvl")) >= command_permission_level then
+            -- enable|disable launcher mode
             if t[2] ~= nil then
-                if t[2] == string.lower("1") or t[2] == string.lower("on") or t[2] == string.lower("true") then
-                    launcher_mode[PlayerIndex] = true
-                    rprint(PlayerIndex, "Grenade Launcher Activated")
-                    UnknownCMD = false
-                elseif t[2] == string.lower("0") or t[2] == string.lower("off") or t[2] == string.lower("false") then
-                    launcher_mode[PlayerIndex] = false
-                    rprint(PlayerIndex, "Grenade Launcher Deactivated")
-                    UnknownCMD = false
+                if string.match(t[2], "1") or string.match(t[2], "on") or string.match(t[2], "true") then
+                    if launcher_mode[PlayerIndex] ~= true then
+                        launcher_mode[PlayerIndex] = true
+                        rprint(PlayerIndex, "Grenade Launcher Activated")
+                        UnknownCMD = false
+                    else
+                        rprint(PlayerIndex, "Grenade Launcher is already enabled!")
+                        UnknownCMD = false
+                    end
+                elseif string.match(t[2], "0") or string.match(t[2], "off") or string.match(t[2], "false") then
+                    if launcher_mode[PlayerIndex] ~= false then
+                        launcher_mode[PlayerIndex] = false
+                        rprint(PlayerIndex, "Grenade Launcher Deactivated")
+                        UnknownCMD = false
+                    else
+                        rprint(PlayerIndex, "Grenade Launcher is already disabled!")
+                        UnknownCMD = false
+                    end
                 end
             else
-                rprint(PlayerIndex, "Invalid Syntax! Type: /" .. enable_launcher_command .. " on|off|1|2|true|false")
+                rprint(PlayerIndex, "Invalid Syntax! Use: /" .. enable_launcher_command .. " on|off or 1|2 or true|false")
                 UnknownCMD = false
             end
         else
-            rprint(PlayerIndex, "You do not have permission to execute that command")
+            rprint(PlayerIndex, "You do not have permission to execute /" .. t[2])
             UnknownCMD = false
         end
+     -- set projectile velocity|distance
     elseif t[1] == string.lower(change_vd_command) then
         if tonumber(get_var(PlayerIndex, "$lvl")) >= command_permission_level then
             if t[2] ~= nil and t[3] ~= nil then
-                if string.match(t[2], "%d+") and string.match(t[3], "%d+") then
-                    values_specified[PlayerIndex] = true
-                    new_velocity = tonumber(t[2])
-                    new_distance = tonumber(t[3])
-                    rprint(PlayerIndex, "Velocity set to: " .. t[2] .. "    Distance set to: " .. t[3])
+                if launcher_mode[PlayerIndex] == true then
+                    if string.match(t[2], "%d+") and string.match(t[3], "%d+") then
+                        values_specified[PlayerIndex] = true
+                        new_velocity = tonumber(t[2])
+                        new_distance = tonumber(t[3])
+                        rprint(PlayerIndex, "Velocity set to: " .. t[2] .. "    Distance set to: " .. t[3])
+                        UnknownCMD = false
+                    end
+                else
+                    rprint(PlayerIndex, 'Grenade Launcher is not activated! Type "/launcher on" to activate.')
                     UnknownCMD = false
                 end
             else
@@ -87,19 +104,25 @@ function OnServerCommand(PlayerIndex, Command, Environment)
                 UnknownCMD = false
             end
         else
-            rprint(PlayerIndex, "You do not have permission to execute that command")
+            rprint(PlayerIndex, "You do not have permission to execute /" .. t[2])
             UnknownCMD = false
         end
+    -- change projectile type
     elseif t[1] == string.lower(change_projectile_type) then
         if tonumber(get_var(PlayerIndex, "$lvl")) >= command_permission_level then
             if t[2] ~= nil then
-                if t[2] == string.lower("frag") or t[2] == string.lower("1") then
-                    projectile_type = "weapons\\frag grenade\\frag grenade"
-                    rprint(PlayerIndex, "Now shooting Frag Grenades")
-                    UnknownCMD = false
-                elseif t[2] == string.lower("plasma") or t[2] == string.lower("2") then
-                    projectile_type = "weapons\\plasma grenade\\plasma grenade"
-                    rprint(PlayerIndex, "Now shooting Plasma Grenades")
+                if launcher_mode[PlayerIndex] == true then
+                    if string.match(t[2], "frag") or string.match(t[2], "1") then
+                        projectile_type = "weapons\\frag grenade\\frag grenade"
+                        rprint(PlayerIndex, "Now shooting Frag Grenades")
+                        UnknownCMD = false
+                    elseif string.match(t[2], "plasma") or string.match(t[2], "2") then
+                        projectile_type = "weapons\\plasma grenade\\plasma grenade"
+                        rprint(PlayerIndex, "Now shooting Plasma Grenades")
+                        UnknownCMD = false
+                    end
+                else
+                    rprint(PlayerIndex, 'Grenade Launcher is not activated! Type "/launcher on" to activate.')
                     UnknownCMD = false
                 end
             else
@@ -107,7 +130,7 @@ function OnServerCommand(PlayerIndex, Command, Environment)
                 UnknownCMD = false
             end
         else
-            rprint(PlayerIndex, "You do not have permission to execute that command")
+            rprint(PlayerIndex, "You do not have permission to execute /" .. t[2])
             UnknownCMD = false
         end
     end
@@ -116,8 +139,7 @@ end
 
 function OnObjectSpawn(PlayerIndex, MapID, ParentID, ObjectID)
     if (launcher_mode[PlayerIndex] == true) then
-        if MapID == TagInfo("proj", "weapons\\pistol\\bullet") 
-            or MapID == TagInfo("proj", "weapons\\assault rifle\\bullet") then
+        if MapID == TagInfo("proj", "weapons\\pistol\\bullet") or MapID == TagInfo("proj", "weapons\\assault rifle\\bullet") then
             return false, ChangeProjectile(PlayerIndex, ParentID)
         end
     end
@@ -137,29 +159,25 @@ function ChangeProjectile(PlayerIndex, ParentID)
             local z = read_float(object_id + 0x64)
             
             if not (values_specified[PlayerIndex]) then
-                proj_x = x + default_distance * math.sin(x_aim)
-                proj_y = y + default_distance * math.sin(y_aim)
-                proj_z = z + default_distance * math.sin(z_aim) + 0.5
+                distance = distance
+                velocity = velocity
             else
-                proj_x = x + new_distance * math.sin(x_aim)
-                proj_y = y + new_distance * math.sin(y_aim)
-                proj_z = z + new_distance * math.sin(z_aim) + 0.5
+                distance = new_distance
+                velocity = new_velocity
             end
 
+            proj_x = x + distance * math.sin(x_aim)
+            proj_y = y + distance * math.sin(y_aim)
+            proj_z = z + distance * math.sin(z_aim) + 0.5
+                
             local tag_id = TagInfo("proj", projectile_type)
             local frag_projectile = spawn_projectile(tag_id, PlayerIndex, proj_x, proj_y, proj_z)
             local frag = get_object_memory(frag_projectile)
             
             if frag then
-                if not (values_specified[PlayerIndex]) then
-                    write_float(frag + 0x68, default_velocity * math.sin(x_aim))
-                    write_float(frag + 0x6C, default_velocity * math.sin(y_aim))
-                    write_float(frag + 0x70, default_velocity * math.sin(z_aim))
-                else
-                    write_float(frag + 0x68, new_velocity * math.sin(x_aim))
-                    write_float(frag + 0x6C, new_velocity * math.sin(y_aim))
-                    write_float(frag + 0x70, new_velocity * math.sin(z_aim))
-                end
+                write_float(frag + 0x68, velocity * math.sin(x_aim))
+                write_float(frag + 0x6C, velocity * math.sin(y_aim))
+                write_float(frag + 0x70, velocity * math.sin(z_aim))
             end
         end
     end
