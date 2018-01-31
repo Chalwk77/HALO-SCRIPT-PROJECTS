1e8206e1b329cdae4af1a8355e3ca5d85e3f0935
Jericho <Chalwk77@users.noreply.github.com>
2018-01-31 17:46:18 +1300

    Update and rename Blank Script, for SAPP (PC & CE).lua to Blank Script (with useful functions).lua

--- Miscellaneous/Blank Script (with useful functions).lua
+++ Miscellaneous/Blank Script (with useful functions).lua
@@ -40,6 +40,10 @@ function OnScriptLoad()
     register_callback(cb['EVENT_DAMAGE_APPLICATION'],   "OnDamageApplication")
 end
 
+function OnScriptUnload()
+
+end
+
 function OnEcho()
     
 end
@@ -132,7 +136,7 @@ function OnPlayerLeave(PlayerIndex)
 
 end
 
-function OnObjectSpawn() 
+function OnObjectSpawn(PlayerIndex, MapID, ParentID, ObjectID) 
 
 end
 
@@ -140,7 +144,7 @@ function OnVehicleExit(PlayerIndex)
 
 end
 
-function OnWeaponPickup(PlayerIndex, Type, Slot) 
+function OnWeaponPickup(PlayerIndex, WeaponIndex, Type)
 
 end
 
@@ -164,6 +168,72 @@ function OnDamageApplication(PlayerIndex, CauserIndex, MetaID, Damage, HitString
 
 end
 
+--- USEFUL MISC FUNCTIONS
 function OnError(Message)
     print(debug.traceback())
+end
+
+function TagInfo(obj_type, obj_name)
+    local tag_id = lookup_tag(obj_type, obj_name)
+    return tag_id ~= 0 and read_dword(tag_id + 0xC) or nil
+end
+
+function tokenizestring(inputstr, sep)
+    if sep == nil then sep = "%s" end
+    local t = { }; i = 1
+    for str in string.gmatch(inputstr, "([^" .. sep .. "]+)") do
+        t[i] = str
+        i = i + 1
+    end
+    return t
+end
+
+function moveobject(ObjectID, x, y, z)
+    local object = get_object_memory(ObjectID)
+    if get_object_memory(ObjectID) ~= 0 then
+        local veh_obj = get_object_memory(read_dword(object + 0x11C))
+        write_vector3d((veh_obj ~= 0 and veh_obj or object) + 0x5C, x, y, z)
+    end
+end
+
+function GetObjectCoords(ObjectID)
+    local function round(pos, places)
+      local mult = 10^(places or 0)
+      return math.floor(pos * mult + 0.5) / mult
+    end
+    local player = get_dynamic_player(PlayerIndex)
+    if (player ~= 0) then
+        local posX, posY, posZ = read_vector3d(player + 0x5C)
+        local vehicle = get_object_memory(read_dword(player + 0x11C))
+        if (vehicle ~= 0 and vehicle ~= nil) then
+            local vehiPosX, vehiPosY, vehiPosZ = read_vector3d(vehicle + 0x5C)
+            posX = posX + vehiPosX
+            posY = posY + vehiPosY
+            posZ = posZ + vehiPosZ
+        end
+        return round(posX, 2), round(posY, 2), round(posZ, 2)
+    end
+    return nil
+end
+
+function secondsToTime(seconds, places)
+    local minutes = math.floor(seconds / 60)
+    seconds = seconds % 60
+    if places == 2 then
+        return minutes, seconds
+    end
+end
+
+function table.match(table, value)
+    for k, v in pairs(table) do
+        if v == value then
+            return k
+        end
+    end
+end
+
+function clear_console(PlayerIndex)
+    for i = 1, 30 do
+        rprint(PlayerIndex, " ")
+    end
 end
