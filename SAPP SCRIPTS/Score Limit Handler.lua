6b04b8a26b29957b0b4a00d527d7847732308952
Jericho <Chalwk77@users.noreply.github.com>
2018-02-01 22:33:59 +1300

    Update Score Limit Handler.lua

--- SAPP SCRIPTS/Score Limit Handler.lua
+++ SAPP SCRIPTS/Score Limit Handler.lua
@@ -18,16 +18,13 @@ message = "The score limit has been updated to $SCORE_LIMIT"
 initial_delay = 10
 
 -- column number represents the current player count...
--- col 1 = 1 player online, number = score
--- col 2 = 2 players online ... ect
+-- col 1 = 1 player online, the number in that row = score
+-- Example:
+-- any time there are 7 players online on the map 'wizard' the scorelimit will be set to 30.
 
--- examples:
--- any time there are 10 players online on the map infinity the scorelimit will be set to 45.
--- any time there are 4 players online on the map wizard the scorelimit will be set to 20.
+-- any time there are 9 players online on the map 'wizard' the scorelimit will be not be changed...
+-- instead the scorelimit will remain the previously set score, in this case 30 (col 7), as when there were 8 players online (col 8) the scorelimit was also ignored.
 
--- any time there are 2 players online on the map deathisland the scorelimit will be ignored and unchanged!
--- The scorelimit in this case will remain as the previously set scorelimit, in this case it was previously set to 15 when there was only 1 player online.
--- If the number in the first column is 'nil' then the initial scorelimit when there is 1 player online will revert to the gametypes default scorelimit.
 
 maps = {
     --                         col 1   col 2    col 3    col 4   col 5   col 6    col 7   col 8    col 9    col 10  col 11   col 12  col 13   col 14   col 15   col 16
