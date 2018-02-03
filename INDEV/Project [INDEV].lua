a7ccba701ca21ed84cf0807f07a8a3261b9e45f0
Chalwk77 <jericho.crosby227@gmail.com>
2018-02-03 16:38:58 +1300

    Added Blank [indev] mod to /INDEV

---
+++ INDEV/Project [INDEV].lua
@@ -0,0 +1,21 @@
+--[[
+--=====================================================================================================--
+Script Name: blank, for SAPP (PC & CE)
+Description:
+
+Copyright (c) 2016-2018, Jericho Crosby <jericho.crosby227@gmail.com>
+Notice: You can use this document subject to the following conditions:
+https://github.com/Chalwk77/Halo-Scripts-Phasor-V2-/blob/master/LICENSE
+~ Created by Jericho Crosby (Chalwk)
+--=====================================================================================================--
+]]
+
+api_version = '1.12.0.0'
+
+function OnScriptLoad()
+	register_callback(cb['EVENT_NAME'], 'function_name')
+end
+
+function OnScriptUnload()
+
+end
