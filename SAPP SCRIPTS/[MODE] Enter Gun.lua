3e897dd0488f09e0cc242883c23ccde30013c0a6
Chalwk77 <jericho.crosby227@gmail.com>
2018-02-10 09:47:14 +1300

    Updated

--- SAPP SCRIPTS/[MODE] Enter Gun.lua
+++ SAPP SCRIPTS/[MODE] Enter Gun.lua
@@ -1,6 +1,6 @@
 --[[
 --=====================================================================================================--
-Script Name: Destroy Gun, for SAPP (PC & CE)
+Script Name: [MODE] Enter Gun, for SAPP (PC & CE)
 
 Copyright (c) 2016-2018, Jericho Crosby <jericho.crosby227@gmail.com>
 Notice: You can use this document subject to the following conditions:
@@ -11,11 +11,19 @@ https://github.com/Chalwk77/Halo-Scripts-Phasor-V2-/blob/master/LICENSE
 
 api_version = '1.12.0.0'
 
-destroy_command = "destroygun"
-destroy_permission_level = 1
+command = "entergun"
+permission_level = 1
 
+vehicles = {}
+vehicles[1] = { "vehi", "vehicles\\warthog\\mp_warthog", "Warthog"}
+vehicles[2] = { "vehi", "vehicles\\ghost\\ghost_mp", "Ghost"}
+vehicles[3] = { "vehi", "vehicles\\rwarthog\\rwarthog", "RocketHog"}
+vehicles[4] = { "vehi", "vehicles\\banshee\\banshee_mp", "Banshee"}
+vehicles[5] = { "vehi", "vehicles\\scorpion\\scorpion_mp", "Tank"}
+vehicles[6] = { "vehi", "vehicles\\c gun turret\\c gun turret_mp", "Turret"}
+
+enter_mode = {}
 weapon_status = {}
-destroy_mode = {}
 function OnScriptLoad()
     register_callback(cb['EVENT_TICK'], "OnTick")
     register_callback(cb['EVENT_SPAWN'], "OnPlayerSpawn")
@@ -27,30 +35,40 @@ function OnScriptUnload()
 end
 
 function OnPlayerSpawn(PlayerIndex)
+    enter_mode[PlayerIndex] = false
     weapon_status[PlayerIndex] = 0
-    destroy_mode[PlayerIndex] = false
 end
 
 function OnTick()
     for i = 1, 16 do
-        if (player_present(i) and player_alive(i)) then
-            if (destroy_mode[i] == true) then
+        if player_present(i) and player_alive(i) then
+            if (enter_mode[i] == true) then
                 local success, target = false, nil
                 local player_object = get_dynamic_player(i)
                 local playerX, playerY, playerZ = read_float(player_object + 0x230), read_float(player_object + 0x234), read_float(player_object + 0x238)
                 local shot_fired
                 local couching = read_float(player_object + 0x50C)
                 local px, py, pz = read_vector3d(player_object + 0x5c)
-                if (couching == 0) then pz = pz + 0.65 else pz = pz + (0.35 * couching) end
+                if (couching == 0) then
+                    pz = pz + 0.65
+                else
+                    pz = pz + (0.35 * couching)
+                end
                 local ignore_player = read_dword(get_player(i) + 0x34)
                 local success, a, b, c, target = intersect(px, py, pz, playerX * 1000, playerY * 1000, playerZ * 1000, ignore_player)
                 if (success == true and target ~= nil) then
-                    shot_fired = read_float(player_object + 0x490)
-                    if(shot_fired ~= weapon_status[i] and shot_fired == 1) then
-                        destroy_object(target)
-                        rprint(i, "Object Destroyed!")
+                    local object_id = read_dword(get_object_memory(target))
+                    for k, v in pairs(vehicles) do
+                        if object_id == TagInfo(tostring(v[1]), tostring(v[2])) then
+                            shot_fired = read_float(player_object + 0x490)
+                            if(shot_fired ~= weapon_status[i] and shot_fired == 1) then
+                                enter_vehicle(target, i, 0)
+                                rprint(i, "Entering " .. v[3])
+                                break
+                            end
+                            weapon_status[i] = shot_fired
+                        end
                     end
-                    weapon_status[i] = shot_fired
                 end
             end
         end
@@ -61,16 +79,16 @@ function OnServerCommand(PlayerIndex, Command)
     local UnknownCMD = nil
     local t = tokenizestring(Command)
     if t[1] ~= nil then
-        if t[1] == string.lower(destroy_command) then
-            if tonumber(get_var(PlayerIndex, "$lvl")) >= destroy_permission_level then
+        if t[1] == string.lower(command) then
+            if tonumber(get_var(PlayerIndex, "$lvl")) >= permission_level then
                 if t[2] ~= nil then
                     if t[2] == "on" or t[2] == "1" or t[2] == "true" then
-                        destroy_mode[PlayerIndex] = true
-                        rprint(PlayerIndex, "Destroy Mode enabled.")
+                        enter_mode[PlayerIndex] = true
+                        rprint(PlayerIndex, "Weapon-Entry Mode enabled.")
                         UnknownCMD = false
                     elseif t[2] == "off" or t[2] == "0" or t[2] == "false" then
-                        destroy_mode[PlayerIndex] = false
-                        rprint(PlayerIndex, "Destroy Mode disabled.")
+                        enter_mode[PlayerIndex] = false
+                        rprint(PlayerIndex, "Weapon-Entry Mode disabled.")
                         UnknownCMD = false
                     end
                 else
