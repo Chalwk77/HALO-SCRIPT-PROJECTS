abd1fad980e6ea13d1787c68b664849534f00770
Chalwk77 <jericho.crosby227@gmail.com>
2018-02-10 21:21:20 +1300

    [+] initial commit

---
+++ INDEV/Target Shooting [GAME].lua
@@ -0,0 +1,22 @@
+--[[
+--=====================================================================================================--
+Script Name: Switch | Zombies, for SAPP (PC & CE)
+
+<in development>
+
+Copyright (c) 2016-2018, Jericho Crosby <jericho.crosby227@gmail.com>
+Notice: You can use this document subject to the following conditions:
+https://github.com/Chalwk77/Halo-Scripts-Phasor-V2-/blob/master/LICENSE
+~ Created by Jericho Crosby (Chalwk)
+--=====================================================================================================--
+]]
+
+api_version = '1.12.0.0'
+
+function OnScriptLoad()
+	register_callback(cb['EVENT_NAME'], 'function_name')
+end
+
+function OnScriptUnload()
+
+end
