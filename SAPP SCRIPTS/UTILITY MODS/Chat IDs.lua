ab2888455975b6c1212cb664e5b17e61ced7cda3
Jericho <Chalwk77@users.noreply.github.com>
2019-01-03 17:52:15 +1300

    Update Chat IDs.lua

--- SAPP SCRIPTS/UTILITY MODS/Chat IDs.lua
+++ SAPP SCRIPTS/UTILITY MODS/Chat IDs.lua
@@ -16,7 +16,7 @@ https://github.com/Chalwk77/Halo-Scripts-Phasor-V2-/blob/master/LICENSE
 --=====================================================================================================--
 ]]--
 
-api_version = "1.12.0.0"
+api_version = "1.11.0.0"
 global_format = "%sender_name% [%index%]: %message%"
 team_format = "[%sender_name%] [%index%]: %message%"
 
