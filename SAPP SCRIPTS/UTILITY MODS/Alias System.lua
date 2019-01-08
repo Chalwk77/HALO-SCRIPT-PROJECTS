04efd2aace9832e6b5470959f2f68ab2bfcadf80
Jericho <Chalwk77@users.noreply.github.com>
2019-01-08 17:08:43 +1300

    Update Alias System.lua

--- SAPP SCRIPTS/UTILITY MODS/Alias System.lua
+++ SAPP SCRIPTS/UTILITY MODS/Alias System.lua
@@ -23,38 +23,102 @@ api_version = "1.11.0.0"
 file_name = "alias.lua" -- File is saved to server root dir/sapp/alias.lua
 base_command = "alias"
 
+-- How long should the alias results be display for? (in seconds) --
+duration = 10
+
+-- Message Alignment:
+-- Left = l,    Right = r,    Center = c,    Tab: t
+Message_Alignment = "l"
+
 -- minimum admin level required to use /alias command
 min_admin_level = 1
 -- configuration ends
 
+-- do not touch
+trigger = { }
+new_timer = { }
+players = { }
+index = nil
+
 function OnScriptLoad()
+    register_callback(cb['EVENT_TICK'], "OnTick")
     register_callback(cb['EVENT_JOIN'], "OnPlayerJoin")
+    register_callback(cb['EVENT_GAME_END'], "OnGameEnd")
+    register_callback(cb['EVENT_LEAVE'], "OnPlayerLeave")
+    register_callback(cb['EVENT_GAME_START'], "OnNewGame")
     register_callback(cb['EVENT_COMMAND'], "OnServerCommand")
     checkFile()
+    for i = 1, 16 do
+        if player_present(i) then
+            players[get_var(i, "$n")].new_timer = 0
+        end
+    end
 end
 
 function OnScriptUnload()
-    
+
 end
 
 function OnPlayerJoin(PlayerIndex)
     local name = get_var(PlayerIndex, "$name")
     local hash = get_var(PlayerIndex, "$hash")
     addAlias(name, hash)
+    players[get_var(PlayerIndex, "$n")] = { }
+    players[get_var(PlayerIndex, "$n")].new_timer = 0
 end
 
-function getAliases(PlayerIndex, index)
-    local lines = lines_from("sapp\\" .. file_name)
-    for k, v in pairs(lines) do
-        local hash = tostring(get_var(index, "$hash"))
-        if v:match(hash) then
-            local aliases = string.match(v, (":(.+)"))
-            rprint(PlayerIndex, 'Showing aliases for : "' .. hash .. '"')
-            rprint(PlayerIndex, "Aliases: " .. aliases)
-            break
+function OnPlayerLeave(PlayerIndex)
+    trigger[PlayerIndex] = false
+    players[get_var(PlayerIndex, "$n")].new_timer = 0
+end
+
+function OnNewGame()
+    for i = 1, 16 do
+        if player_present(i) then
+            if player_present(i) then
+                trigger[i] = false
+                players[get_var(i, "$n")].new_timer = 0
+            end
         end
     end
 end
+
+function OnGameEnd()
+    for i = 1, 16 do
+        if player_present(i) then
+            if player_present(i) then
+                trigger[i] = false
+                players[get_var(i, "$n")].new_timer = 0
+            end
+        end
+    end
+end
+
+function OnTick()
+    for i = 1, 1 do
+        if player_present(i) then
+            if (trigger[i] == true) then
+                players[get_var(i, "$n")].new_timer = players[get_var(i, "$n")].new_timer + 0.030
+                cls(i)
+                local lines = lines_from("sapp\\" .. file_name)
+                for k, v in pairs(lines) do
+                    local hash = tostring(get_var(index, "$hash"))
+                    if v:match(hash) then
+                        local aliases = string.match(v, (":(.+)"))
+                        rprint(i, 'Showing aliases for: "' .. hash .. '"')
+                        rprint(i, "Aliases: " .. aliases)
+                        break
+                    end
+                end
+                if players[get_var(i, "$n")].new_timer >= math.floor(duration) then
+                    trigger[i] = false
+                    players[get_var(i, "$n")].new_timer = 0
+                end
+            end
+        end
+    end
+end
+
 
 function addAlias(name, hash)
     local file = io.open("sapp\\" .. file_name, "r")
@@ -88,7 +152,7 @@ function checkFile()
     if file then
         file:close()
     else
-    local file = io.open('sapp\\' .. file_name, "a+")
+        local file = io.open('sapp\\' .. file_name, "a+")
         if file then
             file:close()
         end
@@ -97,16 +161,23 @@ end
 
 function OnServerCommand(PlayerIndex, Command)
     local t = tokenizestring(Command)
-    local index
     if isAdmin(PlayerIndex) and t[1] == string.lower(base_command) then
         if t[2] ~= nil then
             if t[2] == string.match(t[2], "[A-Za-z]") then
+                cls(PlayerIndex)
                 rprint(PlayerIndex, "Invalid player id")
             else
                 if player_present(tonumber(t[2])) then
                     index = tonumber(t[2])
-                    getAliases(PlayerIndex, index)
+                    if trigger[PlayerIndex] == true then
+                        trigger[PlayerIndex] = false
+                        cls(PlayerIndex)
+                    else
+                        trigger[PlayerIndex] = true
+                    end
                 else
+                    trigger[PlayerIndex] = false
+                    cls(PlayerIndex)
                     rprint(PlayerIndex, "Player not present")
                 end
             end
@@ -145,4 +216,10 @@ function tokenizestring(inputstr, sep)
         i = i + 1
     end
     return t
+end
+
+function cls(PlayerIndex)
+    for _ = 1, 25 do
+        rprint(PlayerIndex, " ")
+    end
 end
