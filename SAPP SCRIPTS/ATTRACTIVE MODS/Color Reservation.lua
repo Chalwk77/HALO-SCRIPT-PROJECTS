8481ccba2d512e3c7a04abda5bc3221d37a26e6c
Jericho <Chalwk77@users.noreply.github.com>
2019-01-20 23:17:43 +1300

    Update Color Reservation.lua

--- SAPP SCRIPTS/ATTRACTIVE MODS/Color Reservation.lua
+++ SAPP SCRIPTS/ATTRACTIVE MODS/Color Reservation.lua
@@ -137,16 +137,3 @@ function getHash(PlayerIndex)
     end
     return nil
 end
-
-function tokenizestring(inputstr, sep)
-    if sep == nil then
-        sep = "%s"
-    end
-    local t = { };
-    i = 1
-    for str in string.gmatch(inputstr, '([^' .. sep .. ']+)') do
-        t[i] = str
-        i = i + 1
-    end
-    return t
-end
