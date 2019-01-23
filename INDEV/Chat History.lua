9710b3664a52c0d08a070e5836f66430755dcadd
Jericho <Chalwk77@users.noreply.github.com>
2019-01-23 20:20:58 +1300

    Create Chat History.lua

---
+++ INDEV/Chat History.lua
@@ -0,0 +1,21 @@
+--[[
+--=====================================================================================================--
+Script Name: Chat History, for SAPP (PC & CE)
+Description: N/A
+
+Copyright (c) 2019, Jericho Crosby <jericho.crosby227@gmail.com>
+Notice: You can use this document subject to the following conditions:
+https://github.com/Chalwk77/Halo-Scripts-Phasor-V2-/blob/master/LICENSE
+~ Created by Jericho Crosby (Chalwk)
+--=====================================================================================================--
+]]
+
+api_version = '1.12.0.0'
+
+function OnScriptLoad()
+    
+end
+
+function OnScriptUnload()
+
+end
