55061da76386cfc1f489e8df423ce41c6c97a378
Jericho <Chalwk77@users.noreply.github.com>
2019-01-27 18:22:41 +1300

    Update Get Server Name [function].lua

--- SAPP SCRIPTS/UTILITY MODS/Get Server Name [function].lua
+++ SAPP SCRIPTS/UTILITY MODS/Get Server Name [function].lua
@@ -3,6 +3,8 @@
 Script Name: Get Server Name [function], for SAPP (PC & CE)
 Implementing API version: 1.11.0.0
 
+Call "getServerName()" | returns server name.
+
 Copyright (c) 2016-2018, Jericho Crosby <jericho.crosby227@gmail.com>
 * Notice: You can use this document subject to the following conditions:
 https://github.com/Chalwk77/Halo-Scripts-Phasor-V2-/blob/master/LICENSE
@@ -15,20 +17,21 @@ api_version = "1.12.0.0"
 
 function OnScriptLoad()
     register_callback(cb['EVENT_JOIN'], "OnPlayerJoin")
-    if halo_type == "PC" then
-        ce = 0x0
-    else
-        ce = 0x40
-    end
 end
 
 function OnScriptUnload()
+
 end
 
 function OnPlayerJoin(PlayerIndex)
+    say(PlayerIndex, "Welcome to " .. getServerName())
+end
+
+
+
+function getServerName()
     local network_struct = read_dword(sig_scan("F3ABA1????????BA????????C740??????????E8????????668B0D") + 3)
-    servername = read_widestring(network_struct + 0x8, 0x42)
-    say(PlayerIndex, "Welcome to " .. servername)
+    return read_widestring(network_struct + 0x8, 0x42)
 end
 
 function read_widestring(address, length)
