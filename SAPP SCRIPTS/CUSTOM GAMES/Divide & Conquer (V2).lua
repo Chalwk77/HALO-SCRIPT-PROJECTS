5a1d6e0100c800f8a97419af05374c77f5b7e355
Jericho <Chalwk77@users.noreply.github.com>
2019-01-30 18:26:18 +1300

    Update Divide & Conquer (V2).lua

--- SAPP SCRIPTS/CUSTOM GAMES/Divide & Conquer (V2).lua
+++ SAPP SCRIPTS/CUSTOM GAMES/Divide & Conquer (V2).lua
@@ -65,12 +65,12 @@ script_name = "Divide & Conquer V2"
 function OnScriptLoad()
     register_callback(cb["EVENT_GAME_START"], "OnNewGame")
     register_callback(cb['EVENT_TICK'], "OnTick")
-    
+
     register_callback(cb["EVENT_GAME_END"], "OnGameEnd")
 
     register_callback(cb["EVENT_JOIN"], "OnPlayerJoin")
     register_callback(cb["EVENT_LEAVE"], "OnPlayerLeave")
-    
+
     register_callback(cb['EVENT_DIE'], 'OnPlayerDeath')
     register_callback(cb['EVENT_DAMAGE_APPLICATION'], "OnDamageApplication")
 end
@@ -80,10 +80,10 @@ function OnScriptUnload()
 end
 
 function OnTick()
-    
+
     -- # Continuous message emitted when there aren't enough players to start the game.
     for i = 1, 16 do
-        if (player_present(i) and not gamestarted) then 
+        if (player_present(i) and (print_nep == true) and not (gamestarted)) then
             if (getPlayerCount() ~= nil) and (getPlayerCount() < required_players) then
                 cls(i)
                 local notEnoughPlayers = string.gsub(not_enough_players, "%%required_players%%", tonumber(required_players))
@@ -91,10 +91,10 @@ function OnTick()
             end
         end
     end
-    
+
     if (init_countdown == true) then
         countdown = countdown + 0.030
-        
+
         local seconds = secondsToTime(countdown)
         timeRemaining = delay - math.floor(seconds)
 
@@ -106,7 +106,7 @@ function OnTick()
                 rprint(i, preGameMessage)
             end
         end
-        
+
         -- Stop the countdown and begin the game...
         if (timeRemaining <= 0) then
             gamestarted = true
@@ -130,7 +130,7 @@ function OnNewGame()
     if not isTeamPlay() then
         local error = string.format('[' .. script_name .. '] does not support FFA.\n\nSupported gamemodes are:\nCTF, Team-Slayer, Team-Race, Team-KOTH and Team-OddBall, etc.')
         execute_command("log_note \"" .. error .. "\"")
-        cprint(error, 4+8)
+        cprint(error, 4 + 8)
         unregister_callback(cb['EVENT_TICK'])
         unregister_callback(cb['EVENT_GAME_END'])
         unregister_callback(cb['EVENT_JOIN'])
@@ -151,31 +151,38 @@ end
 -- #This function all parameters are set to their default values.
 function resetAllParameters()
     gamestarted = false
-    for i = 1,16 do
-        table.remove(player_count, i)
-    end
+    print_nep = false
     red_count = 0
     blue_count = 0
     stopTimer()
+    for i = 1, 16 do
+        table.remove(player_count, i)
+    end
 end
 
 function OnPlayerJoin(PlayerIndex)
     addPlayer(PlayerIndex)
-    
-    if (getPlayerCount() >= required_players) then startTimer() end
-    
+
+    if (getPlayerCount() >= required_players) then
+        startTimer()
+    end
+
+    if (getPlayerCount() >= 1 and getPlayerCount() < required_players) then
+        print_nep = true
+    end
+
     -- Only sort player into new team if the game has already started...
     if (gamestarted) then
         -- Red team has less players (join this team)
         if (red_count < blue_count) then
             SwitchTeam(PlayerIndex, "red")
             red_count = red_count + 1
-            
+
             -- Blue team has less players (join this team)
         elseif (blue_count < red_count) then
             SwitchTeam(PlayerIndex, "blue")
             blue_count = blue_count + 1
-            
+
             -- Teams are even | Choose random team.
         elseif (blue_count == red_count) then
             local function pickRandomTeam()
@@ -204,25 +211,47 @@ end
 
 function OnPlayerLeave(PlayerIndex)
     local team = get_var(PlayerIndex, "$team")
-    
+
     if (team == "red") then
         red_count = red_count - 1
     elseif (team == "blue") then
         blue_count = blue_count - 1
     end
-    
+
     -- Deduct player from 'player_count' table.
     removePlayer(tonumber(PlayerIndex))
-    
+
     if (gamestarted) then
         if ((getPlayerCount() == nil) or (getPlayerCount() <= 0)) then
             -- Ensures all parameters are set to their default values.
             resetAllParameters()
+            -- One player remains | ends the game.
         elseif ((getPlayerCount() ~= nil) and (getPlayerCount() == 1)) then
-            gameOver(nil, true)
+            for i = 1, 16 do
+                if (tonumber(i) ~= tonumber(PlayerIndex)) then
+                    if player_present(i) then
+                        local team = get_var(i, "$team")
+                        gameOver(string.gsub(end_of_game, "%%team%%", team))
+                        break
+                    end
+                end
+            end
+            -- Checks if the remaining players are both on the same team | ends the game.
+        elseif ((getPlayerCount() ~= nil) and (getPlayerCount() == 2)) then
+            for i = 1, 16 do
+                if (tonumber(i) ~= tonumber(PlayerIndex)) then
+                    if player_present(i) then
+                        local team = get_var(i, "$team")
+                        if (team == "red") and (team ~= "blue") or (team == "blue") and (team ~= "red") then
+                            gameOver(string.gsub(end_of_game, "%%team%%", team))
+                            break
+                        end
+                    end
+                end
+            end
         end
     end
-    
+
     -- Pre-Game countdown was initiated but someone left before the game began.
     -- Stop the timer, reset the count and display the continuous 
     -- message emitted when there aren't enough players to start the game.
@@ -239,17 +268,17 @@ function OnPlayerDeath(PlayerIndex, KillerIndex)
     if (gamestarted) then
         local victim = tonumber(PlayerIndex)
         local killer = tonumber(KillerIndex)
-        
+
         local kteam = get_var(killer, "$team")
         local vteam = get_var(victim, "$team")
-        
+
         if (killer ~= -1) and (killer ~= 0) and (killer ~= nil) and (killer > 0) and (victim ~= killer) then
-        
+
             if kteam == "red" then
                 SwitchTeam(victim, "red")
                 red_count = red_count + 1
                 blue_count = blue_count - 1
-                
+
             elseif kteam == "blue" then
                 SwitchTeam(victim, "blue")
                 blue_count = blue_count + 1
@@ -258,12 +287,12 @@ function OnPlayerDeath(PlayerIndex, KillerIndex)
 
             -- No one left on RED team. | BLUE Team Wins
             if (red_count == 0 and blue_count >= 1) then
-                gameOver(string.gsub(end_of_game, "%%team%%", "blue"), false)
-                
+                gameOver(string.gsub(end_of_game, "%%team%%", "blue"))
+
                 -- No one left on BLUE team. | RED Team Wins
             elseif (blue_count == 0 and red_count >= 1) then
-                gameOver(string.gsub(end_of_game, "%%team%%", "red"), false)
-                
+                gameOver(string.gsub(end_of_game, "%%team%%", "red"))
+
                 -- Game is in play | switch player
             elseif (blue_count ~= 0 and red_count ~= 0) then
                 SwitchTeam(victim, tostring(kteam))
@@ -297,38 +326,25 @@ function SwitchTeam(PlayerIndex, team)
     safe_write(true)
     write_dword(kill_message_addresss, 0x03EB01B1)
     safe_write(false)
-    
+
     -- Switch player to relevant team
     execute_command("st " .. tonumber(PlayerIndex) .. " " .. tostring(team))
-    
+
     -- Re enables default death messages
     safe_write(true)
     write_dword(kill_message_addresss, original)
     safe_write(false)
-    
+
     if (respawn_override == true) then
-        write_dword(get_player(PlayerIndex) + 0x2C, respawn_time)  
+        write_dword(get_player(PlayerIndex) + 0x2C, respawn_time)
     end
 end
 
-function gameOver(message, bool)
-    if not (bool) and (message) then
-        gamestarted = false
-        execute_command("msg_prefix \"\"")
-        say_all(message)
-        execute_command("msg_prefix \" " .. server_prefix .. "\"")
-        execute_command("sv_map_next")
-    else
-        timer(1000 * 1, "delayEndGame")
-    end
-end
-
-function delayEndGame()
-    for i = 1,16 do
-        if player_present(i) then
-            gameOver(string.gsub(end_of_game, "%%team%%", get_var(i, "$team")), false)
-        end
-    end
+function gameOver(message)
+    execute_command("msg_prefix \"\"")
+    say_all(message)
+    execute_command("msg_prefix \" " .. server_prefix .. "\"")
+    execute_command("sv_map_next")
 end
 
 function secondsToTime(seconds)
@@ -371,7 +387,7 @@ function addPlayer(PlayerIndex)
 end
 
 function removePlayer(PlayerIndex)
-    for i, v in ipairs(player_count) do 
+    for i, v in ipairs(player_count) do
         if string.match(v, tonumber(PlayerIndex)) then
             table.remove(player_count, i)
         end
@@ -381,7 +397,9 @@ end
 function getPlayerCount()
     local count
     for k, v in ipairs(player_count) do
-        if (k == 0 or nil) then count = 0 end
+        if (k == 0 or nil) then
+            count = 0
+        end
         if (count ~= 0 or count ~= nil) then
             count = tonumber(k)
         end
