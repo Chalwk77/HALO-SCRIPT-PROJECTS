683fd48d9d79302ddd9d0fdc7e049a7cca24cea9
Jericho <Chalwk77@users.noreply.github.com>
2019-02-06 15:11:59 +1300

    Update Base Game Settings.lua

--- INDEV/Base Game Settings.lua
+++ INDEV/Base Game Settings.lua
@@ -540,20 +540,6 @@ function OnScriptUnload()
     end
 end
 
-table.indexOf = function(t, object)
-    local result
-
-    if "table" == type(t) then
-        for i = 1, #t do
-            if object == t[i] then
-                result = i
-                break
-            end
-        end
-    end
-    return result
-end
-
 function OnNewGame()
 
     -- Used Globally
