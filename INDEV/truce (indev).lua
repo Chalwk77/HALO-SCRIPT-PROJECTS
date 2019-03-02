9fdcdb93c2cd9cecd289b2eedff8eac5d4183e66
Jericho <Chalwk77@users.noreply.github.com>
2019-03-02 14:25:40 +1300

    Create truce (indev).lua

---
+++ INDEV/truce (indev).lua
@@ -0,0 +1,19 @@
+--[[
+--=====================================================================================================--
+Script Name: Truce, for SAPP (PC & CE)
+Description: Initiate a truce with other players!
+
+Command Syntax: 
+    * /truce [player id]
+    * /accept [player id]
+    * /deny [player id]
+    * /untruce [player id]
+    * /trucelist
+
+Copyright (c) 2016-2019, Jericho Crosby <jericho.crosby227@gmail.com>
+* Notice: You can use this document subject to the following conditions:
+https://github.com/Chalwk77/Halo-Scripts-Phasor-V2-/blob/master/LICENSE
+
+* Written by Jericho Crosby (Chalwk)
+--=====================================================================================================--
+]] --
