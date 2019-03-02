74eecd3a7b98c65ee1531c88565275cee7217e68
Jericho <Chalwk77@users.noreply.github.com>
2019-03-02 16:42:40 +1300

    Update truce (indev).lua

--- INDEV/truce (indev).lua
+++ INDEV/truce (indev).lua
@@ -21,7 +21,11 @@ https://github.com/Chalwk77/Halo-Scripts-Phasor-V2-/blob/master/LICENSE
 api_version = "1.12.0.0"
 -- configuration [starts] -->
 
+-- Minimum privilege level required to execute /truce command. Set to -1 (negative one) for all players
+local privilege_level = -1
+
 local base_command = "truce"
+local accept_command = "accept"
 
 -- # Message Configuration:
 local on_request = {
@@ -54,37 +58,76 @@ local on_deny = {
 -- configuration [ends] <--
 
 local truce = { }
+local requests = { }
 local gsub = string.gsub
 
 function OnScriptLoad()
     register_callback(cb['EVENT_COMMAND'], "OnServerCommand")
+    register_callback(cb['EVENT_JOIN'], "OnPlayerConnect")
 end
 
 function OnScriptUnload()
 
 end
 
+function OnPlayerConnect(PlayerIndex)
+    requests[PlayerIndex] = { }
+    requests[PlayerIndex].active = 0
+end
+
+local function checkAccess(PlayerIndex)
+    if (tonumber(get_var(PlayerIndex, "$lvl"))) >= privilege_level then
+        return true
+    else
+        rprint(PlayerIndex, "Command failed. Insufficient Permission.")
+        return false
+    end
+end
+
+local function isOnline(TargetID, executor)
+    if player_present(TargetID) then
+        return true
+    else
+        rprint(executor, "Command failed. Player not online.")
+        return false
+    end
+end
+
 function OnServerCommand(PlayerIndex, Command, Environment, Password)
+    
 	local command, args = cmdsplit(Command)
     local executor = tonumber(PlayerIndex)
-	if (command == string.lower(base_command)) then
-        local TargetID = tonumber(args[1])
+    local TargetID = tonumber(args[1])
+    
+	if (command == string.lower(base_command) and checkAccess(executor)) then
         if args[1] ~= nil then
-            if player_present(TargetID) then
-                local players = {}
-                players.en = get_var(executor, "$name")
-                players.eid = tonumber(get_var(executor, "$n"))
-                players.tn = get_var(TargetID, "$name")
-                players.tid = tonumber(get_var(TargetID, "$n"))
-                truce:sendrequest(players)
-            else
-                rprint(executor, "Player not online")
+            if isOnline(TargetID, executor) then
+                if (TargetID ~= executor) then
+                    local players = {}
+                    players.en = get_var(executor, "$name")
+                    players.eid = tonumber(get_var(executor, "$n"))
+                    players.tn = get_var(TargetID, "$name")
+                    players.tid = tonumber(get_var(TargetID, "$n"))
+                    truce:sendrequest(players)
+                else
+                    rprint(executor, "Command failed. You cannot initiate a truce with yourself.")
+                end
             end
         else
-            rprint(executor, "Invalid syntax")
+            rprint(executor, "Invalid syntax. Usage: /truce [player id]")
+        end
+        return false
+    elseif (command == string.lower(accept_command) and checkAccess(executor)) then
+        if args[1] ~= nil then
+            if isOnline(TargetID, executor) then
+                -- accept logic
+            end
+        else
+            rprint(executor, "Invalid syntax. Usage: /accept [player id]")
         end
         return false
 	end
+    
 end
 
 function truce:sendrequest(params)
@@ -105,6 +148,8 @@ function truce:sendrequest(params)
         local StringFormat = (gsub(gsub(msgToTarget[k], "%%executor_name%%", executor_name), "%%executor_id%%", executor_id))
         rprint(target_id, StringFormat)
     end
+    
+    requests[target_id].active = requests[target_id].active + 1
 end
 
 function truce:enable(params)
