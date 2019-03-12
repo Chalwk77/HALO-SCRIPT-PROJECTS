bd0fb7535cf59093d578129b3f6e3125313372e4
Jericho <Chalwk77@users.noreply.github.com>
2019-03-12 18:42:08 +1300

    Big Fixes + Performance Inprovements
    Fixed a couple of bugs + Refactored about 90% of the code.

--- SAPP SCRIPTS/UTILITY MODS/Crash Player.lua
+++ SAPP SCRIPTS/UTILITY MODS/Crash Player.lua
@@ -1,11 +1,13 @@
 --[[
 --=====================================================================================================--
 Script Name: Crash Player (utility), for SAPP (PC & CE)
-Implementing API version: 1.11.0.0
-Description:    Crash someone automatically when they join the server (based on Name/Hash comparisons)
-                - or Crash someone (anyone) on demand!
-
-                Command Syntax: /crash <player id>
+Description: Crash someone automatically when they join the server (based on Name/Hash comparisons)
+            - or Crash someone (anyone) on demand!
+            Command Syntax: /crash <player id>
+            
+            
+    Change Log: 
+    * Bug Fixes + Refactored 90% of the code
 
 Copyright (c) 2016-2018, Jericho Crosby <jericho.crosby227@gmail.com>
 * Notice: You can use this document subject to the following conditions:
@@ -15,162 +17,242 @@ https://github.com/Chalwk77/Halo-Scripts-Phasor-V2-/blob/master/LICENSE
 --=====================================================================================================--
 ]]--
 
-CRASH_COMMAND = "crash"
-LEVEL = 1 -- Min admin level required to use /crash command
 api_version = "1.12.0.0"
-rocket_hog = "vehicles\\rwarthog\\rwarthog"
-function OnScriptLoad()
-    register_callback(cb['EVENT_GAME_START'], "OnGameStart")
-    safe_read(true)
-    if CheckMap() then
-        register_callback(cb['EVENT_PREJOIN'], "OnPlayerPrejoin")
-        register_callback(cb['EVENT_COMMAND'], "OnServerCommand")
-        if halo_type == "PC" then
-            ce = 0x0
-        else
-            ce = 0x40
-        end
-        network_struct = read_dword(sig_scan("F3ABA1????????BA????????C740??????????E8????????668B0D") + 3)
-        LoadTables()
-    end
-    safe_read(false)
-end
 
-function OnGameStart()
-    if CheckMap() then
-        register_callback(cb['EVENT_PREJOIN'], "OnPlayerPrejoin")
-        register_callback(cb['EVENT_COMMAND'], "OnServerCommand")
+-- Configuration [start] -------------
+
+local base_command = "crash"
+local privilege_level = 4
+local users = {
+    -- Make sure the player's name matches exactly as it will in game.
+    -- NAME             HASH
+    {["username1"] = {"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"}},
+    {["username2"] = {"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"}},
+    {["username3"] = {"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"}},
+    
+    -- repeat the structure to add more entries 
+    {["name"] = {"hash"}},
+}
+
+-- Configuration [end] -------------
+
+local mod, trigger, lower, find = { }, { }, string.lower, string.find
+
+function OnScriptLoad()
+    
+    register_callback(cb['EVENT_COMMAND'], "OnServerCommand")
+    register_callback(cb['EVENT_PREJOIN'], "OnPlayerPrejoin")
+    register_callback(cb['EVENT_TICK'], "OnTick")
+    
+    if halo_type == "PC" then
+        ce = 0x0
     else
-        unregister_callback(cb['EVENT_PREJOIN'])
-        unregister_callback(cb['EVENT_COMMAND'])
+        ce = 0x40
     end
 end
 
 function OnScriptUnload()
-    NameList = { }
-    HashList = { }
+    --
 end
 
-function OnServerCommand(PlayerIndex, Command)
-    local response = nil
-    local t = tokenizestring(Command)
-    count = #t
-    if t[1] ~= nil then
-        if tonumber(get_var(PlayerIndex, "$lvl")) >= LEVEL and t[1] == CRASH_COMMAND or t[1] == "Crash" then
-            response = false
-            if t[2] ~= nil then
-                sufferer = tonumber(t[2])
-                if sufferer ~= nil and sufferer > 0 and sufferer < 17 then
-                    sufferers_name = get_var(sufferer, "$name")
-                    if player_present(sufferer) then
-                        timer(0, "CrashPlayer", sufferer)
-                        say(PlayerIndex, "You crashed " .. sufferers_name .. "'s game!")
-                    else
-                        say(PlayerIndex, "Invalid player!")
-                    end
-                end
-            else
-                say(PlayerIndex, "Invalid Syntax! Syntax: " .. t[1] .. " [number 1-16]")
-            end
+local function checkAccess(e)
+    if (e ~= -1 and e >= 1 and e < 16) then
+        if (tonumber(get_var(e, "$lvl"))) >= privilege_level then
+            return true
+        else
+            rprint(e, "Command failed. Insufficient Permission.")
+            return false
         end
-    end
-    return response
-end
-
-function tokenizestring(inputstr, sep)
-    if sep == nil then
-        sep = "%s"
-    end
-    local t = { };
-    i = 1
-    for str in string.gmatch(inputstr, "([^" .. sep .. "]+)") do
-        t[i] = str
-        i = i + 1
-    end
-    return t
-end
-
-function LoadTables()
-    NameList = {
-        -- Make sure these names match exactly as they do ingame.
-        "Example",
-        "Example",
-        "Example",
-        "Example",
-        "Example",
-        "Example"
-    }
-    HashList = {
-        -- You can retrieve the players hash by looking it up in the sapp.log file.
-        "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-        "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-        "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-        "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-        "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-        "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-        "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-        "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-        "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-        "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-    }
-end
-
-function OnPlayerPrejoin(PlayerIndex)
-    local network_struct = read_dword(sig_scan("F3ABA1????????BA????????C740??????????E8????????668B0D") + 3)
-    local client_network_struct = network_struct + 0x1AA + ce + to_real_index(PlayerIndex) * 0x20
-    local Name = read_widestring(client_network_struct, 12)
-    local Hash = get_var(PlayerIndex, "$hash")
-    local PlayerIP = get_var(PlayerIndex, "$ip")
-    if table.match(NameList, Name) and table.match(HashList, Hash) then
-        timer(3000, "CrashPlayer", PlayerIndex)
-    end
-end
-
-function table.match(table, value)
-    for k, v in pairs(table) do
-        if v == value then
-            return k
-        end
-    end
-end
-
-function read_widestring(address, length)
-    local count = 0
-    local byte_table = { }
-    for i = 1, length do
-        if read_byte(address + count) ~= 0 then
-            byte_table[i] = string.char(read_byte(address + count))
-        end
-        count = count + 2
-    end
-    return table.concat(byte_table)
-end
-
--- Thanks to aLTis for this function!
-function CheckMap()
-    if (lookup_tag("vehi", rocket_hog) ~= 0) then
-        return true
     else
+        cprint("You cannot execute this command from the console.", 4 + 8)
         return false
     end
 end
 
--- Thanks to H® Shaft for this neat little function!
-function CrashPlayer(sufferer)
-    if player_present(sufferer) then
-        local player_object = get_dynamic_player(sufferer)
-        if (player_object ~= 0) then
-            local x, y, z = read_vector3d(player_object + 0x5C)
-            local vehicleId = spawn_object("vehi", "vehicles\\rwarthog\\rwarthog", x, y, z)
-            local veh_obj = get_object_memory(vehicleId)
-            if (veh_obj ~= 0) then
-                for j = 0, 20 do
-                    enter_vehicle(vehicleId, sufferer, j)
-                    exit_vehicle(sufferer)
-                end
-                destroy_object(vehicleId)
+local function isOnline(t, e)
+    if (t) then
+        if (t > 0 and t < 17) then
+            if player_present(t) then
+                return true
+            else
+                rprint(e, "Command failed. Player not online.")
+                return false
+            end
+        else
+            rprint(e, "Invalid player id. Please enter a number between 1-16")
+        end
+    end
+end
+
+local function cmdself(t, e)
+    if (t) then
+        if tonumber(t) == tonumber(e) then
+            rprint(e, "You cannot execute this command on yourself.")
+            return true
+        end
+    end
+end
+
+function OnTick()
+    for i = 1,16 do
+        if player_present(i) then
+            if trigger[i] and player_alive(i) then
+                trigger[i] = false
+                Crash(i, get_var(i, "$name"))
             end
         end
     end
+end
+
+function OnServerCommand(PlayerIndex, Command)
+    local command, args = cmdsplit(Command)
+    local executor = tonumber(PlayerIndex)
+    
+    local players = { }
+    
+    if (command == lower(base_command)) then
+        if (checkAccess(executor)) then
+            if (args[1] ~= nil) and (args[1]:match("%d+")) then
+                local TargetID = tonumber(args[1])
+                if isOnline(TargetID, executor) then
+                    if not cmdself(TargetID, executor) then
+                        players.eid = tonumber(get_var(executor, "$n"))
+                        players.tid = tonumber(get_var(TargetID, "$n"))
+                        players.tn = get_var(TargetID, "$name")
+                        mod:CommandCrash(players)
+                    end
+                end
+            else
+                rprint(executor, "Invalid syntax. Usage: /" .. base_command .. " [player id]")
+            end
+        end
+        return false
+    end
+end
+
+function mod:CommandCrash(params)
+    local params = params or {}
+
+    local eid = params.eid or nil
+    local tid = params.tid or nil
+    local tn = params.tn or nil
+    
+    if (lookup_tag("vehi", "vehicles\\rwarthog\\rwarthog") ~= 0) then
+        local player_object = get_dynamic_player(tid)
+        if player_object ~= 0 then
+            trigger[tid] = true
+            rprint(eid, "You have crashed " .. tn .. "'s game client")
+        end
+    else
+        rprint(eid, "Error. Crash does not work on this map!")
+    end
+end
+
+function Crash(target, name)
+    local player_object = get_dynamic_player(target)
+    if (player_object ~= 0) then
+        local x, y, z = read_vector3d(player_object + 0x5C)
+        local vehicle_id = spawn_object("vehi", "vehicles\\rwarthog\\rwarthog", x, y, z)
+        local veh_obj = get_object_memory(vehicle_id)
+        if (veh_obj ~= 0) then
+            for j = 0, 20 do
+                enter_vehicle(vehicle_id, target, j)
+                exit_vehicle(target)
+            end
+            destroy_object(vehicle_id)
+        end
+        cprint("Crashed " .. name .. "'s game client", 4+8)
+    end
     return false
+end
+
+function OnPlayerPrejoin(PlayerIndex)
+    trigger[PlayerIndex] = nil
+    if (lookup_tag("vehi", "vehicles\\rwarthog\\rwarthog") ~= 0) then
+        local ns = read_dword(sig_scan("F3ABA1????????BA????????C740??????????E8????????668B0D") + 3)
+        local cns = ns + 0x1AA + ce + to_real_index(PlayerIndex) * 0x20
+        
+        local function read_widestring(address, length)
+            local count = 0
+            local byte_table = { }
+            for i = 1, length do
+                if read_byte(address + count) ~= 0 then
+                    byte_table[i] = string.char(read_byte(address + count))
+                end
+                count = count + 2
+            end
+            return table.concat(byte_table)
+        end
+        
+        local name, hash = read_widestring(cns, 12), get_var(PlayerIndex, "$hash")
+
+        for key, _ in ipairs(users) do
+            local userdata = users[key][name]
+            if (userdata ~= nil) then
+                for i = 1,#userdata do
+                    if find(userdata[i], hash) then
+                        trigger[PlayerIndex] = true
+                        break
+                    end
+                end
+            end
+        end
+    end
+end
+
+function cmdsplit(str)
+    local subs = {}
+    local sub = ""
+    local ignore_quote, inquote, endquote
+    for i = 1, string.len(str) do
+        local bool
+        local char = string.sub(str, i, i)
+        if char == " " then
+            if (inquote and endquote) or (not inquote and not endquote) then
+                bool = true
+            end
+        elseif char == "\\" then
+            ignore_quote = true
+        elseif char == "\"" then
+            if not ignore_quote then
+                if not inquote then
+                    inquote = true
+                else
+                    endquote = true
+                end
+            end
+        end
+
+        if char ~= "\\" then
+            ignore_quote = false
+        end
+
+        if bool then
+            if inquote and endquote then
+                sub = string.sub(sub, 2, string.len(sub) - 1)
+            end
+
+            if sub ~= "" then
+                table.insert(subs, sub)
+            end
+            sub = ""
+            inquote = false
+            endquote = false
+        else
+            sub = sub .. char
+        end
+
+        if i == string.len(str) then
+            if string.sub(sub, 1, 1) == "\"" and string.sub(sub, string.len(sub), string.len(sub)) == "\"" then
+                sub = string.sub(sub, 2, string.len(sub) - 1)
+            end
+            table.insert(subs, sub)
+        end
+    end
+
+    local cmd = subs[1]
+    local args = subs
+    table.remove(args, 1)
+
+    return cmd, args
 end
