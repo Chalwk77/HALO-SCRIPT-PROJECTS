dc049dcd522cad42f86e0edc89ee967212543b1d
Jericho <Chalwk77@users.noreply.github.com>
2019-03-14 12:10:47 +1300

    Create Inventory Transactions.lua

---
+++ INDEV/Inventory Transactions.lua
@@ -0,0 +1,68 @@
+--[[
+--=====================================================================================================--
+Script Name: Inventory Transactions, for SAPP (PC & CE)
+Description: N/A
+
+Copyright (c) 2019, Jericho Crosby <jericho.crosby227@gmail.com>
+* Notice: You can use this document subject to the following conditions:
+https://github.com/Chalwk77/Halo-Scripts-Phasor-V2-/blob/master/LICENSE
+
+* Written by Jericho Crosby (Chalwk)
+--=====================================================================================================--
+]]--
+
+api_version = "1.12.0.0"
+
+-- Configuration [starts]
+
+-- Configuration [ends] -----------------------------------------------------------------
+
+local transaction = { }
+
+function OnScriptLoad()
+    --
+end
+
+function OnScriptUnload()
+    --
+end
+
+function transaction:purchase(params)
+
+end
+
+function transaction:accept(params)
+
+end
+
+function transaction:decline(params)
+
+end
+
+function transaction:getbalance(params)
+
+end
+
+function transaction:transfer(params)
+
+end
+
+function OnGameStart()
+    
+end
+
+function OnGameEnd()
+    
+end
+
+function OnPlayerConnect(PlayerIndex)
+
+end
+
+function OnPlayerDisconnect(PlayerIndex)
+
+end
+
+function OnPlayerKill(PlayerIndex, KillerIndex)
+
+end
