6926f56919a96d8cd3fcd87781b948b9a2518535
Jericho <Chalwk77@users.noreply.github.com>
2019-03-24 15:01:27 +1300

    Update Message Board.lua

--- SAPP SCRIPTS/UTILITY MODS/Message Board.lua
+++ SAPP SCRIPTS/UTILITY MODS/Message Board.lua
@@ -2,13 +2,16 @@
 --=====================================================================================================--
 Script Name: Message Board (utility), for SAPP (PC & CE)
 Description:    This mod will announce welcome messages to the newly joined player. 
-                These welcome messages are displayed in the rcon console. You can specify how long the messages are displayed on screen.
+                These welcome messages are displayed in the rcon console. 
+                You can specify how long the messages are displayed on screen.
                 
 				Change Log [26/12/2017]
-                [+] Added custom message board output variables: $SERVER_NAME, $PLAYER_NAME
+                [+] Added custom message board output variables: %server_name%, %player_name%
                 
+				Change Log [24/3/2019]
+                [+] Refactored the code.
     
-Copyright (c) 2016-2018, Jericho Crosby <jericho.crosby227@gmail.com>
+Copyright (c) 2016-2019, Jericho Crosby <jericho.crosby227@gmail.com>
 * Notice: You can use this document subject to the following conditions:
 https://github.com/Chalwk77/Halo-Scripts-Phasor-V2-/blob/master/LICENSE
 
@@ -17,33 +20,51 @@ https://github.com/Chalwk77/Halo-Scripts-Phasor-V2-/blob/master/LICENSE
 ]]--
 
 api_version = "1.12.0.0"
-welcome_timer = { }
-new_timer = { }
-players = { }
+
 --[[ 
     
 ------------- Configuration Starts -------------
 Message Board: 
-Use $SERVER_NAME variable to output the server name.
-Use $PLAYER_NAME variable to output the joining player's name.
+Use %server_name% variable to output the server name.
+Use %player_name% variable to output the joining player's name.
 
 ]]
+
 -- messages --
-message_board = {
-    "Welcome to $SERVER_NAME",
+local message_board = {
+    "Welcome to %server_name%",
     "Message Board created by Chalwk (Jericho Crosby)",
     "https://github.com/Chalwk77/HALO-SCRIPT-PROJECTS"
 }
+
 -- How long should the message be displayed on screen for? (in seconds) --
-Message_Duration = 10
+local duration = 10
 
 -- Message Alignment:
 -- Left = l,    Right = r,    Center = c,    Tab: t
-Message_Alignment = "l"
+local alignment = "l"
 -- Configuration Ends --
 --=====================================================================================================--
 
--- Do not touch anything below (unless you know what you're doing) --
+local messages, players, gsub = { }, { }, string.gsub
+
+local function cls(p)
+    for _ = 1, 25 do
+        rprint(p, " ")
+    end
+end
+
+function messages:show(p)
+    players[p] = players[p] or { }
+    players[p].timer = 0
+    players[p].show = true
+end
+
+function messages:hide(p)
+    players[p] = nil
+    cls(p)
+end
+
 function OnScriptLoad()
     register_callback(cb['EVENT_TICK'], "OnTick")
     register_callback(cb['EVENT_JOIN'], "OnPlayerJoin")
@@ -52,7 +73,7 @@ function OnScriptLoad()
     register_callback(cb['EVENT_GAME_END'], "OnGameEnd")
     for i = 1, 16 do
         if player_present(i) then
-            players[get_var(i, "$n")].new_timer = 0
+            messages:hide(p)
         end
     end
 end
@@ -61,9 +82,20 @@ function OnNewGame()
     for i = 1, 16 do
         if player_present(i) then
             if player_present(i) then
-                players[get_var(i, "$n")].new_timer = 0
+                messages:hide(p)
             end
         end
+    end
+    local function read_widestring(address, length)
+        local count = 0
+        local byte_table = {}
+        for i = 1, length do
+            if read_byte(address + count) ~= 0 then
+                byte_table[i] = string.char(read_byte(address + count))
+            end
+            count = count + 2
+        end
+        return table.concat(byte_table)
     end
     local network_struct = read_dword(sig_scan("F3ABA1????????BA????????C740??????????E8????????668B0D") + 3)
     servername = read_widestring(network_struct + 0x8, 0x42)
@@ -73,8 +105,7 @@ function OnGameEnd()
     for i = 1, 16 do
         if player_present(i) then
             if player_present(i) then
-                welcome_timer[i] = false
-                players[get_var(i, "$n")].new_timer = 0
+                messages:hide(p)
             end
         end
     end
@@ -82,54 +113,26 @@ end
 
 function OnTick()
     for i = 1, 16 do
-        if player_present(i) then
-            if (welcome_timer[i] == true) then
-                players[get_var(i, "$n")].new_timer = players[get_var(i, "$n")].new_timer + 0.030
-                cls(i)
-                for _, v in pairs(message_board) do
-                    for j = 1, #message_board do
-                        if string.find(message_board[j], "$SERVER_NAME") then
-                            message_board[j] = string.gsub(message_board[j], "$SERVER_NAME", servername)
-                        elseif string.find(message_board[j], "$PLAYER_NAME") then
-                            message_board[j] = string.gsub(message_board[j], "$PLAYER_NAME", get_var(i, "$name"))
-                        end
-                    end
-                    rprint(i, "|" .. Message_Alignment .. " " .. v)
+        if player_present(i) and (players[i] ~= nil) and (players[i].show) then
+            players[i].timer = players[i].timer + 0.030
+            cls(i)
+            for _, v in pairs(message_board) do
+                for j = 1, #message_board do
+                    message_board[j] = gsub(gsub(message_board[j], "%%server_name%%", servername), "%%player_name%%", get_var(i, "$name"))
                 end
-                if players[get_var(i, "$n")].new_timer >= math.floor(Message_Duration) then
-                    welcome_timer[i] = false
-                    players[get_var(i, "$n")].new_timer = 0
-                end
+                rprint(i, "|" .. alignment .. " " .. v)
+            end
+            if players[i].timer >= math.floor(duration) then
+                messages:hide(i)
             end
         end
     end
 end
 
 function OnPlayerJoin(PlayerIndex)
-    players[get_var(PlayerIndex, "$n")] = { }
-    players[get_var(PlayerIndex, "$n")].new_timer = 0
-    welcome_timer[PlayerIndex] = true
+    messages:show(PlayerIndex)
 end
 
 function OnPlayerLeave(PlayerIndex)
-    welcome_timer[PlayerIndex] = false
-    players[get_var(PlayerIndex, "$n")].new_timer = 0
-end
-
-function cls(PlayerIndex)
-    for _ = 1, 25 do
-        rprint(PlayerIndex, " ")
-    end
-end
-
-function read_widestring(address, length)
-    local count = 0
-    local byte_table = {}
-    for i = 1, length do
-        if read_byte(address + count) ~= 0 then
-            byte_table[i] = string.char(read_byte(address + count))
-        end
-        count = count + 2
-    end
-    return table.concat(byte_table)
+    messages:hide(PlayerIndex)
 end
