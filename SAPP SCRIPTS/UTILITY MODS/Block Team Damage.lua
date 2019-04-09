57d8f9103ae405afb1f24280e26d5a99d21d2e89
Jericho <Chalwk77@users.noreply.github.com>
2019-04-09 17:46:22 +1200

    Update Block Team Damage.lua

--- SAPP SCRIPTS/UTILITY MODS/Block Team Damage.lua
+++ SAPP SCRIPTS/UTILITY MODS/Block Team Damage.lua
@@ -1,11 +1,9 @@
 --[[
     --=====================================================================================================--
 Script Name: Block Team Damage, for SAPP (PC & CE)
-
 Copyright (c) 2019, Jericho Crosby <jericho.crosby227@gmail.com>
 * Notice: You can use this document subject to the following conditions:
 https://github.com/Chalwk77/Halo-Scripts-Phasor-V2-/blob/master/LICENSE
-
 * Written by Jericho Crosby (Chalwk)
 --=====================================================================================================--
 ]]--
@@ -13,25 +11,31 @@ https://github.com/Chalwk77/Halo-Scripts-Phasor-V2-/blob/master/LICENSE
 
 api_version = "1.12.0.0"
 
-function OnScriptLoad()
-    register_callback(cb['EVENT_DAMAGE_APPLICATION'], "OnDamageApplication")
-end
-
-function OnScriptUnload()
-    --
-end
-
 local function getTeamPlay()
     if (get_var(0, "$ffa") == "0") then
         return true
     end
 end
 
+function OnScriptLoad()
+    register_callback(cb['EVENT_NEW_GAME'], "OnGameStart")
+end
+
+function OnGameStart()
+    if getTeamPlay() then
+        register_callback(cb['EVENT_DAMAGE_APPLICATION'], "OnDamageApplication")
+    end
+end
+
+function OnScriptUnload()
+    --
+end
+
 function OnDamageApplication(PlayerIndex, CauserIndex, MetaID, Damage, HitString, Backtap)
     if (tonumber(CauserIndex) > 0 and PlayerIndex ~= CauserIndex) then
         local vTeam = get_var(PlayerIndex, "$team")
         local kTeam = get_var(CauserIndex, "$team")
-        if (getTeamPlay() and vTeam == kTeam) then
+        if (vTeam == kTeam) then
             return false
         end
     end
