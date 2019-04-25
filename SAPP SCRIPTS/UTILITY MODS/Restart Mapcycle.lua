149f23a443c844d290ece0d3a0f9573e611018b7
Jericho <Chalwk77@users.noreply.github.com>
2019-04-25 12:18:54 +1200

    Update Restart Mapcycle.lua

--- SAPP SCRIPTS/UTILITY MODS/Restart Mapcycle.lua
+++ SAPP SCRIPTS/UTILITY MODS/Restart Mapcycle.lua
@@ -9,7 +9,7 @@ https://github.com/Chalwk77/Halo-Scripts-Phasor-V2-/blob/master/LICENSE
 
 * Written by Jericho Crosby (Chalwk)
 --=====================================================================================================--
-]]--
+]] --
 
 api_version = "1.12.0.0"
 
@@ -23,64 +23,64 @@ local sapp_command = "mapcycle_begin"
 local script_version = "1.0"
 local player_count, countdown, init_timer
 local function playerCount()
-	if (player_count ~= nil) then
-		return tonumber(player_count)
-	end
+    if (player_count ~= nil) then
+        return tonumber(player_count)
+    end
 end
 
 function OnScriptLoad()
-	register_callback(cb["EVENT_JOIN"], "OnPlayerConnect")
-	register_callback(cb["EVENT_LEAVE"], "OnPlayerDisconnect")
-	register_callback(cb["EVENT_TICK"], "OnTick")
-	
-	player_count, countdown, init_timer = 0, 0
-	
-	for i = 1,16 do
-		if player_present(i) then
-			player_count = player_count + 1
-		end
-	end
+    register_callback(cb["EVENT_JOIN"], "OnPlayerConnect")
+    register_callback(cb["EVENT_LEAVE"], "OnPlayerDisconnect")
+    register_callback(cb["EVENT_TICK"], "OnTick")
+
+    player_count, countdown, init_timer = 0, 0
+
+    for i = 1, 16 do
+        if player_present(i) then
+            player_count = player_count + 1
+        end
+    end
 end
 
 function OnScriptUnload()
-	--
+    --
 end
 
 function OnPlayerConnect(p)
-	player_count = player_count + 1
+    player_count = player_count + 1
 end
 
 function OnPlayerDisconnect(p)
-	player_count = player_count - 1
-	if (playerCount() <= 0) then
-		
-		-- Ensures player count never goes into negatives
-		player_count = 0 
-		
-		-- Initialize Countdown Timer
-		init_timer = true
-	end
+    player_count = player_count - 1
+    if (playerCount() <= 0) then
+
+        -- Ensures player count never goes into negatives
+        player_count = 0
+
+        -- Initialize Countdown Timer
+        init_timer = true
+    end
 end
 
 local floor = math.floor
 local function CountdownLoop()
     countdown = countdown + 0.030
-	
-	-- Debugging:
-	-- cprint("Time Repaining: " .. duration - floor(countdown))
-	
+
+    -- Debugging:
+    -- cprint("Time Repaining: " .. duration - floor(countdown))
+
     if (countdown >= (duration)) then
         countdown = 0
-		init_timer = false
+        init_timer = false
         execute_command(sapp_command)
-		cprint("RESTARTING MAPCYCLE", 2+8)
+        cprint("RESTARTING MAPCYCLE", 2 + 8)
     end
 end
 
 function OnTick()
-	if (playerCount() <= 0) and (init_timer) then
-		CountdownLoop()
-	end
+    if (playerCount() <= 0) and (init_timer) then
+        CountdownLoop()
+    end
 end
 
 function report()
