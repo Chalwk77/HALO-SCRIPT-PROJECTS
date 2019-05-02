1140d74dc3cf338904741e61ef058ca05d4effd6
Jericho <Chalwk77@users.noreply.github.com>
2019-05-03 10:29:23 +1200

    Update Velocity Multi-Mod.lua

--- INDEV/Velocity Multi-Mod.lua
+++ INDEV/Velocity Multi-Mod.lua
@@ -6611,7 +6611,7 @@ function RecordChanges()
     cl[#cl + 1] = ""
     cl[#cl + 1] = "[5/3/19]"
     cl[#cl + 1] = "1). Bug fix for List Players feature."
-    cl[#cl + 1] = "1). Other minor tweaks + Documentation Edits."
+    cl[#cl + 1] = "2). Other minor tweaks and documentation edits."
     cl[#cl + 1] = "Script Updated to v1.30"
     file:write(concat(cl, "\n"))
     file:close()
