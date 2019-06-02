1525d594d6575aa31f6345c01072e4b9d86f67c8
Jericho <Chalwk77@users.noreply.github.com>
2019-06-02 13:43:11 +1200

    Update Map Vote System.lua

--- INDEV/Map Vote System.lua
+++ INDEV/Map Vote System.lua
@@ -23,7 +23,7 @@ local mapvote = { }
 mapvote.players_needed = 1
 
 -- Map Cycle timeout (In Seconds):
-mapvote.timeout = 10 -- Do not set to 0!
+mapvote.timeout = 15 -- Do not set to 0!
 
 -- If this is true, Map Vote options will be displayed in the Console Environment (mapvote.show_in_chat must be false):
 mapvote.show_in_console = true
@@ -38,7 +38,7 @@ mapvote.maxresults = 5
 
 -- If enabled, players will who are level 'mapvote.extrapower' (or greater) 
 -- will have extra power over their vote and will add 'mapvote.extra' votes to their selection.
-mapvote.extra_vote = true
+mapvote.extra_vote = false
 mapvote.extrapower = 4
 mapvote.extra = 2
 
@@ -57,9 +57,7 @@ mapvote.messages = {
     on_vote = "%name% voted for [ %mapname% ]  -  [ %gametype% ]  -  Votes: %votes%",
     already_voted = "You have already voted!",
     on_win_vote = "%mapname% [ %gametype% ] won the vote ",
-    
-    -- TODO: 
-    -- Add remaining custom messages.
+    no_one_voted = "No votes were processed! Chosing random map in (%seconds%) seconds",
 }
 
 mapvote.maps = { -- Create map settings array
@@ -102,7 +100,6 @@ local concat = table.concat
 function OnScriptLoad()
     register_callback(cb['EVENT_GAME_END'], "OnGameEnd")
     register_callback(cb['EVENT_GAME_START'], "OnGameStart")
-    execute_command("sv_mapcycle_timeout " .. tostring(mapvote.timeout))
 end
 
 local sendToConsole = function()
@@ -204,6 +201,7 @@ local function hasExtraVotePower(p)
 end
 
 function OnGameStart()
+    execute_command("sv_mapcycle_timeout " .. mapvote.timeout)
 
     -- Clear all arrays and counts
     results, votes, map_count = { }, { }, 0
@@ -224,16 +222,33 @@ function OnGameStart()
             
             for j = 1,#gametype do
                 if (gametype[j] ~= nil) then
-                    votes[#votes + 1] = {
-                        [mapname] = {
-                            [gametype[j]] = {
-                                map_id = tonumber(i),
-                                gametype_id = j,
-                                votes = 0,
-                            },
-                        },
-                    }
+                    votes[#votes + 1] = {[mapname] = {[gametype[j]] = {votes = 0}}}
                 end
+            end
+        end
+    end
+end
+
+local getRandomMap = function()
+    local map_table = mapvote.maps
+    
+    local count = 0
+    for _, _ in pairs(map_table) do
+        count = count + 1
+    end
+    
+    for i = 1,#mapvote.maps do
+        print(i)
+    end
+
+    math.randomseed(os.time())
+    local x = math.random(1, count)
+    for mapname, _ in pairs(map_table) do
+        local gametype_table = votes[x][mapname]
+        if (gametype_table ~= nil) then
+            for gametype,_ in pairs(gametype_table) do
+                local map = mapname .. " - " .. gametype
+                return mapname, gametype
             end
         end
     end
@@ -241,27 +256,26 @@ end
 
 function mapvote:calculate_votes()
     local final_results = { }
-    for i = 1, #results do
-        if (results[i]) then
-        
-            local mapname = results[i]
-            local gametype = mapvote.maps[mapname]
-
-            for j = 1,#votes do
-                if (votes[j] ~= nil and votes[j][mapname] ~= nil) then
-                    for k = 1,#gametype do
-                        local tab = votes[j][mapname][gametype[k]]
-                        if (tab) then
-                            if (tab.votes > 0) then
-                                final_results[#final_results + 1] = (tab.votes .. "|" .. mapname .. "|" .. gametype[k])
-                            end
-                        end
-                    end                    
+    
+    local map_table = mapvote.maps
+    for mapname, _ in pairs(map_table) do
+        -- mapname = mapname string
+        for index,_ in pairs(votes) do
+            local gametype_table = votes[index][mapname]
+            if (gametype_table ~= nil) then
+                for gametype,_ in pairs(gametype_table) do
+                    -- gametype = gametype string
+                    local votes = gametype_table[gametype].votes
+                    if (votes ~= nil) and (votes > 0) then
+                        local map = (votes .. "|" .. mapname .. "|" .. gametype)
+                        final_results[#final_results + 1] = map
+                    end
                 end
-            end           
+            end
         end
     end
     
+    local messages = mapvote.messages
     if (#final_results > 0) then
         table.sort(final_results)
         local result = {}
@@ -281,17 +295,33 @@ function mapvote:calculate_votes()
                         local mapname, gametype = result[2], result[3]
                         execute_command("map " .. mapname .. " " .. gametype)
                         
-                        local messages = mapvote.messages
                         local msg = gsub(gsub(messages.on_win_vote, "%%mapname%%", mapname), "%%gametype%%", gametype)
                         SayAll(msg)
                         break
                     end
                 end
             end
-        else
-            -- TODO: Choose a random map & gametype
         end
+    else
+        -- RANDOM MAP SELECTION ...
+    
+        local delay = 3
+        function delay_map_selection()
+            local map, gametype = select(1, getRandomMap()), select(2, getRandomMap())
+            execute_command("map " .. map .. " " .. gametype)
+            
+            local msg = map .. " [ " .. gametype .. " ] has been randomly selected."
+            SayAll(msg)            
+        end
+        timer(1000*delay, "delay_map_selection")
+    
+        local msg = gsub(messages.no_one_voted, "%%seconds%%", delay)
+        SayAll(msg)
     end
+end
+
+function delay_clear_chat()
+    cls(0, 25, true, "chat")
 end
 
 function OnGameEnd()
@@ -303,6 +333,7 @@ function OnGameEnd()
         -- Register a hook into SAPP's tick event.
         register_callback(cb['EVENT_TICK'], "OnTick")
         -- Begin voting process.
+        timer(50, "delay_clear_chat")
         mapvote:begin()
     end
 end
@@ -315,7 +346,6 @@ function mapvote:begin()
             mapvote.timer[i], mapvote.start[i] = 0, true
         end
     end
-    
     -- Map Cycle countdown
     mapvote.timer[0], mapvote.start[0] = 0, true
 end
@@ -335,7 +365,7 @@ function OnTick()
                         mapvote.start[i], mapvote.start[i] = nil, nil
                     else
                         cls(i, 25)
-                        mapvote:showResults(i)
+                        mapvote:showMapVoteOptions(i)
                         rprint(i, ' ')
                         rprint(i, "[Page " .. cur_page[i] .. '/' .. total_pages .. "] Type 'n' -> Next Page  |  Type 'p' -> Previous Page")
                         
@@ -343,6 +373,7 @@ function OnTick()
                         local char = getChar(mapvote.timeout - floor(seconds))
                         rprint(i, "Vote Time Remaining: " .. mapvote.timeout - floor(seconds) .. " second" .. char)
 
+                        rprint(i, ' ')
                         rprint(i, ' ')
                         rprint(i, ' ')
                         rprint(i, ' ')
@@ -357,18 +388,27 @@ function OnTick()
                 mapvote.start[0], mapvote.start[0] = nil, nil
                 for i = 1,16 do
                     if player_present(i) then
+                        cls(i, 25)
                         mapvote.start[i], mapvote.start[i] = nil, nil
                     end
                 end
                 mapvote:calculate_votes()
             else
                 -- TODO: Show player the remaining time (again)
+                for i = 1,16 do
+                    if player_present(i) and (mapvote.start[i] == nil) then
+                        cls(i, 25)
+                        local days, hours, minutes, seconds = secondsToTime(mapvote.timer[0], 4)
+                        local char = getChar(mapvote.timeout - floor(seconds))
+                        rprint(i, "Vote Time Remaining: " .. mapvote.timeout - floor(seconds) .. " second" .. char)
+                    end
+                end
             end
         end
     end
 end
 
-function mapvote:showResults(p)
+function mapvote:showMapVoteOptions(p)
 
     if (vote_options[p][1] == nil) then
         vote_options[p] = { }
@@ -381,7 +421,7 @@ function mapvote:showResults(p)
                         local mapname, m = results[i], { }
                         local gametypes = mapvote.maps[mapname]
                     
-                        local part_one, part_two = k .. spacing(2) .. mapname .. ":" .. spacing(1), ""
+                        local part_one, part_two = k .. spacing(2) .. mapname .. ":" .. spacing(1) .. ""
 
                         for j = 1,#gametypes do
                             if (gametypes[j] ~= nil) then
@@ -484,8 +524,8 @@ function OnPlayerChat(PlayerIndex, Message, type)
     end
 end
 
-function cls(PlayerIndex, count)
-    if (PlayerIndex) then
+function cls(PlayerIndex, count, clear_all, type)
+    if (PlayerIndex) and not (clear_all) then
         count = count or 25
         for _ = 1, count do
             if sendToConsole() then
@@ -494,6 +534,20 @@ function cls(PlayerIndex, count)
                 execute_command("msg_prefix \"\"")
                 say(PlayerIndex, " ")
                 execute_command("msg_prefix \" " .. mapvote.serverprefix .. "\"")
+            end
+        end
+    elseif (clear_all) then
+        for i = 1,16 do
+            if player_present(i) then
+                for _ = 1, count do
+                    if (type == "chat") then
+                        execute_command("msg_prefix \"\"")
+                        say(i, " ")
+                        execute_command("msg_prefix \" " .. mapvote.serverprefix .. "\"")
+                    else
+                        rprint(i, " ")
+                    end
+                end
             end
         end
     end
