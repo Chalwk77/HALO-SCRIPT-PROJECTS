d7340a07454b644e20e8444f2339fa47e0d39031
Jericho <Chalwk77@users.noreply.github.com>
2019-06-07 19:33:35 +1200

    Update Battle Royale.lua

--- INDEV/Battle Royale.lua
+++ INDEV/Battle Royale.lua
@@ -18,12 +18,12 @@ local boundry = { }
 
 -- ==== Battle Royale Configuration [starts] ==== --
 local players_needed = 1
+-- If players are out of bounds for this amount of time, the script will auto-the player.
+local time_until_kill = 5
 local server_prefix = "**LNZ**"
 
 boundry.maps = {
-
     -- IMPORTANT: (1 world unit = 10 feet or ~3.048 meters)
-
     ["timberland"] = {
         -- Maximum Boundry size:
         max_size = 4500,
@@ -39,10 +39,16 @@ boundry.maps = {
         -- Note: Radius must be the same as 'max_size' 
         1.179, -1.114, -21.197, 4500
     },
-    
+        
+    ["sidewinder"] = {
+        max_size = 5500, min_size = 150, extra_time = 2, duration = 60, shrink_amount = 50,
+        1.680, 31.881, -3.922, 5500
+    },
+    ["ratrace"] = {
+        max_size = 415, min_size = 40, extra_time = 2, duration = 60, shrink_amount = 30,
+        8.340, -10.787, 0.222, 415
+    },
     -- Not yet Implemented --
-    ["sidewinder"] = { nil },
-    ["ratrace"] = { nil },
     ["bloodgulch"] = { nil },
     ["beavercreek"] = { nil },
     ["boardingaction"] = { nil },
@@ -67,14 +73,29 @@ local min_size, max_size, extra_time, shrink_cycle, shrink_amount
 local start_trigger, game_in_progress, game_time = true, false
 local time_scale = 0.030
 local console_paused = { }
+local out_of_bounds = { }
+
 local floor, format = math.floor, string.format
+local globals = nil
+local red_flag, blue_flag
 
 function OnScriptLoad()
     register_callback(cb["EVENT_JOIN"], "OnPlayerConnect")
+    register_callback(cb["EVENT_GAME_START"], "OnGameStart")
+    register_callback(cb["EVENT_DAMAGE_APPLICATION"], "OnDamageApplication")
+    local gp = sig_scan("8B3C85????????3BF9741FE8????????8B8E2C0200008B4610") + 3
+    if (gp == 3) then
+        return
+    end
+    globals = read_dword(gp)
 end
 
 function OnScriptUnload()
     --
+end
+
+function OnGameStart()
+    red_flag, blue_flag = read_dword(globals + 0x8), read_dword(globals + 0xC)
 end
 
 local Say = function(Player, Message)
@@ -96,6 +117,9 @@ function OnPlayerConnect(PlayerIndex)
     
     local function setup_params(p)
         console_paused[p] = false
+        out_of_bounds[p] = { }
+        out_of_bounds[p].yes = false
+        out_of_bounds[p].timer = 0
     end
     
     if (start_trigger) and (enough_players) then
@@ -140,6 +164,7 @@ function boundry:inSphere(p, px, py, pz, x, y, z, r)
     local coords = ( (px - x) ^ 2 + (py - y) ^ 2 + (pz - z) ^ 2)
     if (coords < r) then
         console_paused[p] = false
+        out_of_bounds[p].yes = false
         return true
     elseif (coords >= r + 1) and (coords < max_size) then
         console_paused[p] = false
@@ -203,6 +228,7 @@ function OnTick()
                         end
                         
                         if (send_timestamp) then
+                            out_of_bounds[i].timer = 0
                             rprint(i, "|c" .. header .. shrink_time_msg)
                         end
                     end
@@ -210,16 +236,26 @@ function OnTick()
                 else
                     if not (console_paused[i]) then
                         rprint(i, "|cWARNING:")
-                        rprint(i, "|cYOU ARE OUTSIDE THE CIRCLE!")
+                        rprint(i, "|cYOU ARE OUTSIDE THE BOUNDRY!")
                         local rUnits = ( (px - bX) ^ 2 + (py - bY) ^ 2 + (pz - bZ) ^ 2)
                         rprint(i, "|cUNITS FROM CENTER: " .. floor(rUnits) .. "/" .. bR)
+                        out_of_bounds[i].yes = true
                     else
+                        out_of_bounds[i].yes = true
+                        out_of_bounds[i].timer = out_of_bounds[i].timer + time_scale
+                        local time_remaining = ((time_until_kill + 1) - out_of_bounds[i].timer)
+                        local seconds = select(2, secondsToTime(time_remaining))
+                        
                         rprint(i, "|c--------- WARNING ---------")
                         rprint(i, "|cYOU ARE LEAVING THE COMBAT AREA!")
                         rprint(i, "|cRETURN NOW OR YOU WILL BE SHOT!")
+                        rprint(i, "|c(" .. seconds .. ")")
+                        if (out_of_bounds[i].timer >= time_until_kill) then
+                            out_of_bounds[i].timer = 0
+                            killSilently(i)
+                        end
                     end
-                    -- Camo serves as a visual indication to the player
-                    -- that they are outside the boundry:
+                    
                     execute_command("camo " .. i .. " 1")
                 end
             end
@@ -230,10 +266,10 @@ function OnTick()
                     if (bR > min_size and bR <= max_size) then
                         boundry_timer = 0
                         boundry:shrink()
-                        Say(i, "[ BOUNDRY REDUCTION ] Zone Size: " .. bR, 4+8)
+                        Say(i, "[ BOUNDRY REDUCTION ] Zone Size Decreased: " .. bR, 4+8)
                     elseif (bR <= min_size) then
                         boundry_timer = nil
-                        Say(i, "BOUNDRY IS NOW AT ITS SMALLEST POSSIBLE SIZE", 4+8)
+                        Say(i, "BOUNDRY IS NOW AT ITS SMALLEST POSSIBLE SIZE!", 4+8)
                     end
                 end
             end
@@ -262,5 +298,56 @@ function secondsToTime(seconds, ExtraTime)
             mins = (mins + extra_time)
         end
         return mins, secs
+    end
+end
+
+function OnDamageApplication(PlayerIndex, CauserIndex, MetaID, Damage, HitString, Backtap)
+    local killer = tonumber(CauserIndex)
+    if (killer > 0 and PlayerIndex ~= CauserIndex) then
+        if (out_of_bounds[killer].yes) then
+            return false
+        end
+    end
+end
+
+function DestroyObject(object)
+    if (object) then
+        destroy_object(object)
+    end
+end
+
+local function DeleteWeapons(PlayerIndex)
+    local player_object = get_dynamic_player(PlayerIndex)
+    if (player_object ~= 0) then
+        write_word(player_object + 0x31E, 0)
+        write_word(player_object + 0x31F, 0)
+        local weaponId = read_dword(player_object + 0x118)
+        if (weaponId ~= 0) then
+            for j = 0, 3 do
+                local weapon = read_dword(player_object + 0x2F8 + 4 * j)
+                if (weapon ~= red_flag) and (weapon ~= blue_flag) then
+                    DestroyObject(weapon)
+                end
+            end
+        end
+        return true
+    end
+end
+
+function killSilently(PlayerIndex)
+    if DeleteWeapons(PlayerIndex) then
+        local kill_message_addresss = sig_scan("8B42348A8C28D500000084C9") + 3
+        local original = read_dword(kill_message_addresss)
+        safe_write(true)
+        write_dword(kill_message_addresss, 0x03EB01B1)
+        safe_write(false)
+        execute_command("kill " .. tonumber(PlayerIndex))
+        safe_write(true)
+        write_dword(kill_message_addresss, original)
+        safe_write(false)
+        write_dword(get_player(PlayerIndex) + 0x2C, 0 * 33)
+        -- Deduct one death
+        local deaths = tonumber(get_var(PlayerIndex, "$deaths"))
+        execute_command("deaths " .. tonumber(PlayerIndex) .. " " .. deaths - 1)
     end
 end
