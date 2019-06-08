88a0f85eaee8fe6e2365e2e49f5bd796c0a5a15b
Jericho <Chalwk77@users.noreply.github.com>
2019-06-08 15:34:18 +1200

    Update Battle Royale.lua

--- INDEV/Battle Royale.lua
+++ INDEV/Battle Royale.lua
@@ -19,7 +19,7 @@ local boundry = { }
 -- ==== Battle Royale Configuration [starts] ==== --
 
 -- Players needed to start the game:
-local players_needed = 1
+local players_needed = 2
 
 -- Players will be auto-killed if Out Of Bounds for this many seconds:
 local time_until_kill = 5
@@ -124,13 +124,14 @@ local min_size, max_size, extra_time, shrink_cycle, shrink_amount
 local start_trigger, game_in_progress, game_time = true, false
 local monitor_coords
 local time_scale = 0.030
-local console_paused = { }
+local console_paused, paused = { }, { }
 local out_of_bounds = { }
 local last_man_standing = { }
 
 local gamestart_countdown, init_countdown
 
 local floor, format = math.floor, string.format
+local gmatch, sub = string.gmatch, string.sub
 local globals = nil
 local red_flag, blue_flag
 
@@ -138,6 +139,7 @@ function OnScriptLoad()
     register_callback(cb["EVENT_JOIN"], "OnPlayerConnect")
     register_callback(cb["EVENT_LEAVE"], "OnPlayerDisconnect")
     register_callback(cb["EVENT_GAME_START"], "OnGameStart")
+    
     local gp = sig_scan("8B3C85????????3BF9741FE8????????8B8E2C0200008B4610") + 3
     if (gp == 3) then
         return
@@ -180,21 +182,40 @@ local function init_params(reset)
         
         monitor_coords = true
         
+        local function set(reset_scores)
+            for i = 1, 16 do
+                if player_present(i) then
+                    paused[i] = paused[i] or { }
+                    paused[i].start, paused[i].timer = false, 0
+                    if (reset_scores) then
+                        execute_command("score " .. i .. " 0")
+                        execute_command("kills " .. i .. " 0")
+                        execute_command("assists " .. i .. " 0")
+                        execute_command("deaths " .. i .. " 0")
+                    end
+                end
+            end
+        end
+        
         if (reset) then
+            set(false)
             stopTimer()
             last_man_standing.count = 0
             last_man_standing.player = nil
-            start_trigger = true
             unregister_callback(cb['EVENT_DIE'])
             unregister_callback(cb['EVENT_TICK'])
+            unregister_callback(cb['EVENT_CHAT'])
+            unregister_callback(cb['EVENT_COMMAND'])
             unregister_callback(cb['EVENT_GAME_END'])
             unregister_callback(cb['EVENT_DAMAGE_APPLICATION'])
         else
+            set(true)
             startTimer()
-            
-            -- Register a hook into SAPP's tick event.
+            -- Register hooks into SAPP Events:
             register_callback(cb["EVENT_TICK"], "OnTick")
             register_callback(cb["EVENT_GAME_END"], "OnGameEnd")
+            register_callback(cb['EVENT_CHAT'], "OnPlayerChat")
+            register_callback(cb['EVENT_COMMAND'], "OnServerCommand")
         end
     end
 end
@@ -241,6 +262,8 @@ function OnPlayerConnect(PlayerIndex)
         out_of_bounds[p] = { }
         out_of_bounds[p].yes = false
         out_of_bounds[p].timer = 0
+        paused[p] = { }
+        paused[p].start, paused[p].timer = false, 0
     end
     
     if (start_trigger) and (enough_players) then
@@ -252,7 +275,9 @@ function OnPlayerConnect(PlayerIndex)
         player_setup(p)
         
     elseif (game_in_progress and enough_players) then
-        --
+        player_setup(p)
+    elseif not (enough_players) then
+        player_setup(p)
     end
 end
 
@@ -303,39 +328,63 @@ function boundry:inSphere(p, px, py, pz, x, y, z, r)
     end
 end
 
+function checkForPause()
+    for i = 1,16 do
+        if player_present(i) then
+            if (paused[i] ~= nil) then
+                if (paused[i].start) then
+                    paused[i].timer = paused[i].timer + time_scale
+                    if (paused[i].timer >= 3) then
+                        paused[i].start = false
+                        paused[i].timer = 0
+                        cls(i, 25)
+                    end
+                end
+            end
+        end
+    end
+end
+
 function OnTick()
     if (init_countdown) then
         GameStartCountdown()
     elseif not (init_countdown) then
+    
+        local time_stamp, until_next_shrink
+        local time_remaining 
+        
+        if (game_timer ~= nil) then
+            game_timer = game_timer + time_scale
+            local time = ( (game_time + time_scale) - (game_timer) )
+            time_remaining = time
+            local mins, secs = select(1, secondsToTime(time)), select(2, secondsToTime(time))
+            time_stamp = (mins..":"..secs)
+            if (reduction_timer ~= nil) then
+                reduction_timer = reduction_timer + time_scale
+                local time = ( (shrink_duration + time_scale) - (reduction_timer) )
+                if (time <= 0) then
+                    reduction_timer = 0
+                end
+                local mins, secs = select(1, secondsToTime(time)), select(2, secondsToTime(time))
+                until_next_shrink = (mins..":"..secs)
+            end
+        end
+        
         for i = 1,16 do
             if player_present(i) then
                 local player_object = get_dynamic_player(i)
                 if (player_object ~= 0) then
-                    cls(i, 25)
-                    
-                    local time_stamp, until_next_shrink
-                    local time_remaining 
-                    
-                    if (game_timer ~= nil) then
-                        game_timer = game_timer + time_scale
-                        local time = ( (game_time + time_scale) - (game_timer) )
-                        time_remaining = time
-                        local mins, secs = select(1, secondsToTime(time)), select(2, secondsToTime(time))
-                        time_stamp = (mins..":"..secs)
-                        if (reduction_timer ~= nil) then
-                            reduction_timer = reduction_timer + time_scale
-                            local time = ( (shrink_duration + time_scale) - (reduction_timer) )
-                            if (time <= 0) then
-                                reduction_timer = 0
-                            end
-                            local mins, secs = select(1, secondsToTime(time)), select(2, secondsToTime(time))
-                            until_next_shrink = (mins..":"..secs)
-                        end
+                
+                    checkForPause()
+                
+                    if (not paused[i].start) then
+                        cls(i, 25)
                     end
                     
                     local px,py,pz = read_vector3d(player_object + 0x5c) 
                     if boundry:inSphere(i, px,py,pz, bX, bY, bZ, bR) and (monitor_coords) then
-                        if not (console_paused[i]) then
+                        if (not console_paused[i]) and (not paused[i].start) then
+                            
                             local rUnits = ( (px - bX) ^ 2 + (py - bY) ^ 2 + (pz - bZ) ^ 2)
                             rprint(i, "|c-- INSIDE SAFE ZONE --")
                             rprint(i, "|cUNITS FROM CENTER: " .. floor(rUnits) .. "/" .. bR .. " (final size: " .. min_size .. ")")
@@ -365,17 +414,22 @@ function OnTick()
                                 rprint(i, "|c" .. header .. shrink_time_msg)
                             end
                         end
-                        -- 
+
                     elseif (monitor_coords) then
-                        if not (console_paused[i]) then
+                        if (not console_paused[i]) and (not paused[i].start) then
+                        
                             rprint(i, "|cWARNING:")
                             rprint(i, "|cYOU ARE OUTSIDE THE BOUNDRY!")
+                            
                             local rUnits = ( (px - bX) ^ 2 + (py - bY) ^ 2 + (pz - bZ) ^ 2)
                             rprint(i, "|cUNITS FROM CENTER: " .. floor(rUnits) .. "/" .. bR)
                             out_of_bounds[i].yes = true
-                        else
+                            
+                        elseif (not paused[i].start) and console_paused[i] then
+                        
                             out_of_bounds[i].yes = true
                             out_of_bounds[i].timer = out_of_bounds[i].timer + time_scale
+                            
                             local time_remaining = ((time_until_kill + 1) - out_of_bounds[i].timer)
                             local seconds = select(2, secondsToTime(time_remaining))
                             
@@ -534,6 +588,32 @@ function cls(PlayerIndex, count, clear_chat, type)
     end
 end
 
+function OnPlayerChat(PlayerIndex, Message, type)
+    local msg = stringSplit(Message)
+    local p = tonumber(PlayerIndex)
+
+    if (#msg == 0) then
+        return false
+    elseif (sub(msg[1], 1, 1) == "/" or sub(msg[1], 1, 1) == "\\") then
+        if (paused[p].start ~= true) then
+            cls(p, 25)
+            paused[p].start = true
+        end
+    end
+end
+
+function OnServerCommand(PlayerIndex, Command, Environment, Password)
+    local msg = stringSplit(Command)
+    local p = tonumber(PlayerIndex)
+
+    if (#msg == 0) then
+        return false
+    elseif (paused[p].start ~= true) then
+        cls(p, 25)
+        paused[p].start = true
+    end
+end
+
 function secondsToTime(seconds)
     local seconds = tonumber(seconds)
     if (seconds <= 0) then
@@ -597,24 +677,24 @@ function killSilently(PlayerIndex)
     end
 end
 
-
 function GameStartCountdown()
     gamestart_countdown = gamestart_countdown + 0.030
     local seconds = select(2, secondsToTime(gamestart_countdown))
     local time_remaining = gamestart_delay - math.floor(seconds)
-    if (time_remaining <= 0) then
+    if (time_remaining <= 1) then
         stopTimer()
         register_callback(cb['EVENT_DIE'], "OnPlayerDeath")
         register_callback(cb["EVENT_DAMAGE_APPLICATION"], "OnDamageApplication")
-    end
-    
-    if (init_countdown) then
+    elseif (init_countdown) then
+        checkForPause()
         for i = 1,16 do
             if player_present(i) then
-                cls(i, 25)
-                local char = getChar(time_remaining)
-                rprint(i, "|cGame will begin in " .. time_remaining .. " second" .. char, 4+8)
-                for _ = 1,7 do rprint(i, " ") end
+                if (not paused[i].start) then
+                    cls(i, 25)
+                    local char = getChar(time_remaining)
+                    rprint(i, "|cGame will begin in " .. time_remaining .. " second" .. char, 4+8)
+                    for _ = 1,7 do rprint(i, " ") end
+                end
             end
         end
     end
@@ -640,4 +720,16 @@ function getChar(input)
         char = ""
     end
     return char
+end
+
+function stringSplit(inp, sep)
+    if (sep == nil) then
+        sep = "%s"
+    end
+    local t, i = {}, 1
+    for str in gmatch(inp, "([^" .. sep .. "]+)") do
+        t[i] = str
+        i = i + 1
+    end
+    return t
 end
