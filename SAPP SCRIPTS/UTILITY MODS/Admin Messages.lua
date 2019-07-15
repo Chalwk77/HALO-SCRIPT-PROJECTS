7bd2ee070a9bfb5409fdb7d8d053696d55a21611
Jericho <Chalwk77@users.noreply.github.com>
2019-07-16 10:56:30 +1200

    Updated with new feature
    Added an 'alignment' option - use this to specify where messages are displayed on the screen.

--- SAPP SCRIPTS/UTILITY MODS/Admin Messages.lua
+++ SAPP SCRIPTS/UTILITY MODS/Admin Messages.lua
@@ -3,6 +3,9 @@
 Script Name: Admin Join Messages, for SAPP (PC & CE)
 Description: Customize admin join messages on a per-admin-level basis.
 
+-- Change Log: 16/067/2019
+   1). Added new option 'alignment' - specify where messages are displayed on screen.
+
 Copyright (c) 2019, Jericho Crosby <jericho.crosby227@gmail.com>
 Notice: You can use this document subject to the following conditions:
 https://github.com/Chalwk77/Halo-Scripts-Phasor-V2-/blob/master/LICENSE
@@ -10,15 +13,18 @@ https://github.com/Chalwk77/Halo-Scripts-Phasor-V2-/blob/master/LICENSE
 --=====================================================================================================--
 ]]--
 api_version = "1.11.0.0"
-custom_messages = {} -- do not touch!
+local join_message = {} -- do not touch!
 
 -- Configuration [starts]
 -- output:            [prefix]              [message] (note: player name is automatically inserted between [prefix] and [message])
-custom_messages[1] = {"[TRIAL-MOD] ", " joined the server. Everybody hide!"}
-custom_messages[2] = {"[MODERATOR] ", " just showed up. Hold my beer!"}
-custom_messages[3] = {"[ADMIN] ", " just joined. Hide your bananas!"}
-custom_messages[4] = {"[SENIOR-ADMIN] ", " joined the server."}
+join_message[1] = {"[TRIAL-MOD] ", " joined the server. Everybody hide!"}
+join_message[2] = {"[MODERATOR] ", " just showed up. Hold my beer!"}
+join_message[3] = {"[ADMIN] ", " just joined. Hide your bananas!"}
+join_message[4] = {"[SENIOR-ADMIN] ", " joined the server."}
+
+local alignment = "|l" -- Left = l, Right = r, Center = c, Tab: t
 -- Configuration [ends]
+
 
 -- do not touch below this point unless you know what you're doing
 function OnScriptLoad()
@@ -29,22 +35,20 @@ function OnScriptUnload()
 
 end
 
-function OnPlayerJoin(PlayerIndex)
-    local name = get_var(PlayerIndex, "$name")
-    local level = tonumber(get_var(PlayerIndex, "$lvl"))
-    local join_message
-    if (level >= 1) then
-        join_message = custom_messages[level][1] .. name .. custom_messages[level][2]
+function OnPlayerJoin(p)
+    local name, lvl = get_var(p, "$name"), tonumber(get_var(p, "$lvl"))
+    if (lvl >= 1) then
+        local join_message = "|" .. alignment .. " " .. join_message[lvl][1] .. name .. join_message[lvl][2]
+        announceJoin(join_message)
     else
         return false
     end
-    announceJoin(join_message)
 end
 
-function announceJoin(join_message)
+function announceJoin(msg)
     for i = 1, 16 do
         if player_present(i) then
-            rprint(i, join_message)
+            rprint(i, msg)
         end
     end
 end
