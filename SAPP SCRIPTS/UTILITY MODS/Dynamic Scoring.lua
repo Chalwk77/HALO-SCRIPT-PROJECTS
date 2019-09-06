2a953c24ed8bbea82946f82339e256e1fb82c7f7
Jericho <Chalwk77@users.noreply.github.com>
2019-09-06 17:53:46 +1200

    New Feature!
    Scoring is now set on a per-gametype/per-gamemode basis

--- SAPP SCRIPTS/UTILITY MODS/Dynamic Scoring.lua
+++ SAPP SCRIPTS/UTILITY MODS/Dynamic Scoring.lua
@@ -3,6 +3,10 @@
 Script Name: Dynamic Scoring (utility), for SAPP (PC & CE)
 Description: Scorelimit changes automatically, depending on number of players currently online.
 
+* Updated 6/09/19
+- Scoring is now set on a per-gametype/per-gamemode basis.
+
+
 Copyright (c) 2019, Jericho Crosby <jericho.crosby227@gmail.com>
 * Notice: You can use this document subject to the following conditions:
 https://github.com/Chalwk77/Halo-Scripts-Phasor-V2-/blob/master/LICENSE
@@ -13,20 +17,98 @@ Inspiration taken from a mod made by {OZ}Shadow.
 --=====================================================================================================--
 ]]--
 
--- Configuration [starts] ------------------------
+-- Configuration [starts] ----------------------------------------
 api_version = "1.12.0.0"
+local scorelimit, scoreTable = { }
 
-local scorelimit = { }
-scorelimit[1] = 15 -- 4 Players or less
-scorelimit[2] = 25 -- 4-8 players
-scorelimit[3] = 35 -- 8-12 players
-scorelimit[4] = 50 -- 12-16 players
-scorelimit.txt = "Score limit changed to: %scorelimit%"
--- Configuration [ends] ------------------------
+-- CAPTURE THE FLAG ----------------------------------------------------
+scorelimit.ctf = {
+    [1] = 3, -- 4 players or less
+    [2] = 5, -- 4-8 players
+    [3] = 7, -- 8-12 players
+    [4] = 9, -- 12-16 players
+    txt = "Score limit changed to: %scorelimit%",
+}
+
+-- SLAYER ----------------------------------------------------
+scorelimit.slayer = {
+    {   -- FFA:
+        [1] = 15, -- 4 players or less
+        [2] = 25, -- 4-8 players
+        [3] = 45, -- 8-12 players
+        [4] = 50, -- 12-16 players
+        txt = "Score limit changed to: %scorelimit%"
+    },
+    {   -- TEAM:
+        [1] = 25, -- 4 players or less
+        [2] = 35, -- 4-8 players
+        [3] = 45, -- 8-12 players
+        [4] = 50, -- 12-16 players
+        txt = "Score limit changed to: %scorelimit%"
+    }
+}
+
+-- KING OF THE HILL ----------------------------------------------------
+scorelimit.king = {
+    {   -- FFA
+        [1] = 2, -- 4 players or less
+        [2] = 3, -- 4-8 players
+        [3] = 4, -- 8-12 players
+        [4] = 5, -- 12-16 players
+        txt = "Score limit changed to: %scorelimit% minute%s%"
+    },
+    {   -- TEAM
+        [1] = 3, -- 4 players or less
+        [2] = 4, -- 4-8 players
+        [3] = 5, -- 8-12 players
+        [4] = 6, -- 12-16 players
+        txt = "Score limit changed to: %scorelimit% minute%s%"
+    }
+}
+
+-- ODDBALL ----------------------------------------------------
+scorelimit.oddball = {
+    {   -- FFA
+        [1] = 2, -- 4 players or less
+        [2] = 3, -- 4-8 players
+        [3] = 4, -- 8-12 players
+        [4] = 5, -- 12-16 players
+        txt = "Score limit changed to: %scorelimit% minute%s%"
+        
+    },
+    {   -- TEAM
+        [1] = 3, -- 4 players or less
+        [2] = 4, -- 4-8 players
+        [3] = 5, -- 8-12 players
+        [4] = 6, -- 12-16 players
+        txt = "Score limit changed to: %scorelimit% minute%s%"
+        
+    }
+}
+
+-- RACE ----------------------------------------------------
+scorelimit.race = {
+    {   -- FFA
+        [1] = 3, -- 4 players or less
+        [2] = 4, -- 4-8 players
+        [3] = 5, -- 8-12 players
+        [4] = 6, -- 12-16 players
+        txt = "Score limit changed to: %scorelimit% lap%s%"
+    },
+    {   -- TEAM
+        [1] = 4, -- 4 players or less
+        [2] = 5, -- 4-8 players
+        [3] = 6, -- 8-12 players
+        [4] = 7, -- 12-16 players
+        txt = "Score limit changed to: %scorelimit% lap%s%"
+    }
+}
+
+-- Configuration [ends] ----------------------------------------
 
 local current_players, current_scorelimit = nil, nil
 
-local function Reset()
+function scorelimit.Reset()
     current_players, current_scorelimit = 0, 0
 end
 
@@ -35,21 +117,67 @@ function OnScriptLoad()
     register_callback(cb["EVENT_GAME_END"], "OnGameEnd")
     register_callback(cb["EVENT_JOIN"], "OnPlayerConnect")
     register_callback(cb["EVENT_LEAVE"], "OnPlayerDisconnect")
-    Reset()
-    for i = 1, 16 do
-        if player_present(i) then
-            current_players = current_players + 1
+
+    scorelimit.Reset()
+    
+    if (get_var(0, "$gt") ~= "n/a") then
+        for i = 1, 16 do
+            if player_present(i) then
+                current_players = current_players + 1
+            end
         end
+        
+        scorelimit.Modify()
     end
-    modifyScore()
 end
 
 function OnGameStart()
-    setScore(scorelimit[1])
+
+    -- Returns the current game type:
+    local gametype = get_var(0, "$gt")
+    
+    -- Returns TRUE if team based gamemode.
+    local function isTeamPlay()
+        if (get_var(0, "$ffa") == "0") then
+            return true
+        end
+    end
+    
+    -- Determine what scorelimit table to use:
+    if (gametype == "ctf") then   
+        scoreTable = scorelimit.ctf
+    elseif (gametype == "slayer") then
+        if isTeamPlay() then
+            scoreTable = scorelimit.slayer[1]
+        else
+            scoreTable = scorelimit.slayer[2]
+        end
+    elseif (gametype == "king") then
+        if isTeamPlay() then
+            scoreTable = scorelimit.king[1]
+        else
+            scoreTable = scorelimit.king[2]
+        end
+    elseif (gametype == "oddball") then
+        if isTeamPlay() then
+            scoreTable = scorelimit.oddball[1]
+        else
+            scoreTable = scorelimit.oddball[2]
+        end
+    elseif (gametype == "race") then
+        if isTeamPlay() then
+            scoreTable = scorelimit.race[1]
+        else
+            scoreTable = scorelimit.race[2]
+        end
+    end
+    
+    -- Set initial scorelimit:
+    scorelimit.Set(scoreTable[1])
 end
 
 function OnGameEnd()
-    Reset()
+    scorelimit.Reset()
 end
 
 function OnScriptUnload()
@@ -57,35 +185,58 @@ function OnScriptUnload()
 end
 
 function OnPlayerConnect(PlayerIndex)
+    -- Increment player count by 1
     current_players = current_players + 1
-    modifyScore()
 end
 
 function OnPlayerDisconnect(PlayerIndex)
+    -- Decrement player count by 1
     current_players = current_players - 1
-    modifyScore()
+    scorelimit.Modify()
 end
 
-function modifyScore()
-    if (current_players <= 4 and current_scorelimit ~= scorelimit[1]) then
-        setScore(scorelimit[1])
-        say_all(string.gsub(scorelimit.txt, "%%scorelimit%%", scorelimit[1]))
+-- Determine whether to pluralize 'lap' or 'minute' input (text) from scoreTable.txt:
+local function getChar(input)
+    local char = ""
+    if (tonumber(input) > 1) then
+        char = "s"
+    elseif (tonumber(input) <= 1) then
+        char = ""
+    end
+    return char
+end
 
-    elseif (current_players > 4 and current_players <= 8 and current_scorelimit ~= scorelimit[2]) then
-        setScore(scorelimit[2])
-        say_all(string.gsub(scorelimit.txt, "%%scorelimit%%", scorelimit[2]))
+-- Update the current scorelimit (based on current player count)
+function scorelimit.Modify()
+    local gsub = string.gsub
+    if (current_players <= 4 and current_scorelimit ~= scoreTable[1]) then
+        scorelimit.Set(scoreTable[1])
+        
+        local char = getChar(scoreTable[1])
+        say_all(gsub(gsub(scoreTable.txt, "%%scorelimit%%", scoreTable[1]), "%%s%%", char))
 
-    elseif (current_players > 9 and current_players <= 12 and current_scorelimit ~= scorelimit[3]) then
-        setScore(scorelimit[3])
-        say_all(string.gsub(scorelimit.txt, "%%scorelimit%%", scorelimit[3]))
+    elseif (current_players > 4 and current_players <= 8 and current_scorelimit ~= scoreTable[2]) then
+        scorelimit.Set(scoreTable[2])
+        
+        local char = getChar(scoreTable[2])
+        say_all(gsub(gsub(scoreTable.txt, "%%scorelimit%%", scoreTable[2]), "%%s%%", char))
 
-    elseif (current_players > 12 and current_scorelimit ~= scorelimit[4]) then
-        setScore(scorelimit[4])
-        say_all(string.gsub(scorelimit.txt, "%%scorelimit%%", scorelimit[4]))
+    elseif (current_players > 9 and current_players <= 12 and current_scorelimit ~= scoreTable[3]) then
+        scorelimit.Set(scoreTable[3])
+        
+        local char = getChar(scoreTable[3])
+        say_all(gsub(gsub(scoreTable.txt, "%%scorelimit%%", scoreTable[3]), "%%s%%", char))
+
+    elseif (current_players > 12 and current_scorelimit ~= scoreTable[4]) then
+        scorelimit.Set(scoreTable[4])
+        
+        local char = getChar(scoreTable[4])
+        say_all(gsub(gsub(scoreTable.txt, "%%scorelimit%%", scoreTable[4]), "%%s%%", char))
     end
 end
 
-function setScore(score)
+-- Execute SAPP command 'scorelimit' to change the current scorelimit
+function scorelimit.Set(score)
     current_scorelimit = score
     execute_command("scorelimit " .. score)
 end
