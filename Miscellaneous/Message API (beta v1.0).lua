f0606a8133f6c3f2886175793e10974c4bf23ec0
Jericho <Chalwk77@users.noreply.github.com>
2019-10-11 01:47:20 +1300

    Update Message API (beta v1.0).lua

--- Miscellaneous/Message API (beta v1.0).lua
+++ Miscellaneous/Message API (beta v1.0).lua
@@ -5,12 +5,13 @@ Script Name: Message API (beta v1.0), for SAPP (PC & CE)
 Call this to init a new timed message:
 API:NewMessage(Message, Duration, Player, Type)
 
-* Message (string) is the message to display
+* Message (string/table) is the message (or array of messages) to display
 * Duration (number) is the amount of time that message will appear on screen
 * Player (number) is the target player index
 
 * Type ("string", "table")
 This script supports tables in place of Message and will output the contents of it.
+Be sure to specify what input type it is.
 
 Copyright (c) 2019, Jericho Crosby <jericho.crosby227@gmail.com>
 --=====================================================================================================--
@@ -19,38 +20,69 @@ Copyright (c) 2019, Jericho Crosby <jericho.crosby227@gmail.com>
 api_version = "1.12.0.0"
 
 local API = { }
-local delta_time = 0.03333333333333333
+API.Messages, delta_time = { }, 0.03333333333333333
 
 function OnScriptLoad()
-    if (get_var(0, '$gt') ~= 'n/a') then
-        API = { }
-    end
+    if (get_var(0, '$gt') ~= 'n/a') then API = { } end
     register_callback(cb["EVENT_TICK"], "OnTick")
+    register_callback(cb["EVENT_JOIN"], "OnPlayerConnect")
+    register_callback(cb["EVENT_LEAVE"], "OnPlayerDisconnect")
 end
 
 function API:NewMessage(Message, Duration, Player, Type)
+    
+    API.Messages[Player] = API.Messages[Player] or { }
+    local messages = API.Messages[Player]
 
-    API.Messages = API.Messages or { }
-
-    local function Add(a, b, c, d)
+    local function Add(a, b, c, d, State)
         return {
             message = a,
             duration = b,
             player = c,
             type = d,
-            time = 0,
+            time = 0, pause_timer = 0,
+            paused = State,
         }
     end
 
-    table.insert(API.Messages, Add(Message, Duration, Player, Type))
+    local function isDupe(msg)
+        for _,console in pairs(messages) do
+            if (console.type == "table") then
+                for i = 1,#console.message do          
+                    for j = 1,#msg do
+                        if (console.message[i] == msg[j]) then
+                            return true
+                        end
+                    end
+                end
+            elseif (console.type == "string") then
+                if (console.message == msg) then
+                    return true
+                end
+            end
+        end
+        return false
+    end
+    
+    if (#messages <= 0) then
+        table.insert(messages, Add(Message, Duration, Player, Type, false))
+    elseif game:isPaused(Player) then
+        if not isDupe(Message) then
+            table.insert(messages, Add(Message, Duration, Player, Type, true))
+        end
+    elseif not isDupe(Message) then
+        table.insert(messages, Add(Message, Duration, Player, Type, false))
+    end
 end
 
 function API:Pause(PlayerIndex)
-    local messages = API.Messages
-    for k,v in pairs(messages) do
-        if (v.player == PlayerIndex) then
-            v.time = v.duration
-            v.paused = true
+    local messages = API.Messages[PlayerIndex]
+    if (messages) then
+        for _,console in pairs(messages) do
+            if (console.player == PlayerIndex) then
+                console.time = console.duration
+                console.paused = true
+            end
         end
     end
 end
@@ -62,39 +94,66 @@ function API:ClearConsole(PlayerIndex, Buffer)
     end
 end
 
+function API:isPaused(PlayerIndex)
+    local messages = API.Messages[PlayerIndex]
+    if (messages ~= nil) then
+        for _,console in pairs(messages) do
+            if (console.paused) then
+                return true
+            end
+        end
+    end
+end
+
 function OnTick()
-
-    local messages = API.Messages
-
-    if (#messages > 0) then
-        for k,v in pairs(messages) do
-            if v.player and player_present(v.player) then
+    for i = 1,16 do
+        if player_present(i) then
+        
+            local messages = API.Messages[i]
+            if (messages ~= nil) then
                 
-                if (not v.paused) then
-                    v.time = v.time + delta_time
-                    
-                    API:ClearConsole(v.player, 25)
-                    
-                    if type(v.message) == "table" then
-                        for i = 1,#v.message do                    
-                            rprint(v.player, v.message[i])
+                if not API:isPaused(i) then
+                    API:ClearConsole(i, 25)
+                end
+                
+                for index,console in pairs(messages) do
+                    if (not console.paused) then
+                        if (console.type == "table") then
+                            for j = 1,#console.message do                    
+                                rprint(console.player, console.message[j])
+                            end
+                        elseif (console.type == "string") then           
+                            rprint(console.player, console.message)
                         end
-                    else                    
-                        rprint(v.player, v.message)
-                    end
-                    
-                    if (v.time >= v.duration) then
-                        messages[k] = nil
-                    end
-                else
-                    
-                    v.pause_timer = v.pause_timer + delta_time
-                    if (v.pause_timer >= 5) then
-                        v.paused = false
+                        
+                        console.time = console.time + delta_time
+                        if (console.time >= console.duration) then
+                            messages[k] = nil
+                        end
+                    else
+                        console.pause_timer = console.pause_timer + delta_time
+                        if (console.pause_timer >= 5) then
+                            console.paused = false
+                        end
                     end
                 end
             end
         end
+    end
+end
+
+function OnPlayerConnect(PlayerIndex)
+    -- TEST:
+    local message = "This seems to be working!"
+    local duration = 10 -- in seconds
+    
+    API:NewMessage(message, duration, PlayerIndex, "string")
+end
+
+function OnPlayerDisconnect(PlayerIndex)
+    local messages = API.Messages[PlayerIndex]
+    if (messages) then
+        message = nil
     end
 end
 
