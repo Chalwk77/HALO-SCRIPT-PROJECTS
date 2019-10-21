f01997bcd78ee7302c858e2e97636f8c17d04bb9
Jericho <Chalwk77@users.noreply.github.com>
2019-10-22 09:35:57 +1300

    Update and rename Zpocalypse (simplified v1.3).lua to Zpocalypse (Simple Message Output v1.3).lua

--- SAPP SCRIPTS/CUSTOM GAMES/Zpocalypse (Simple Message Output v1.3).lua
+++ SAPP SCRIPTS/CUSTOM GAMES/Zpocalypse (Simple Message Output v1.3).lua
@@ -1,6 +1,6 @@
 --[[
 --=====================================================================================================--
-Script Name: Zpocalypse (simplified v1.3), for SAPP (PC & CE)
+Script Name: Zpocalypse (Simple Message Output v1.3), for SAPP (PC & CE)
 Description: A custom Zombies Game designed for Team-Slayer game types.
 
 ### Game Play Mechanics:
