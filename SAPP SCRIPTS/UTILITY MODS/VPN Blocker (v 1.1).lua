6f43cd19d1b49cadd698d3f76b8d95bc0f36970f
Jericho <Chalwk77@users.noreply.github.com>
2019-11-16 23:41:46 +1300

    Update VPN Blocker (v 1.1).lua

--- SAPP SCRIPTS/UTILITY MODS/VPN Blocker (v 1.1).lua
+++ SAPP SCRIPTS/UTILITY MODS/VPN Blocker (v 1.1).lua
@@ -50,82 +50,82 @@ https://github.com/Chalwk77/Halo-Scripts-Phasor-V2-/blob/master/LICENSE
 ]] --
 
 api_version = "1.12.0.0"
-local vpn_blocker = { 
+local vpn_blocker = {
     --====================================== --
     -- Configuration [STARTS]
     --====================================== --
-    
+
     -- I recommend reading the API Documentation before changing any settings:
     -- https://www.ipqualityscore.com/documentation/proxy-detection/overview
 
     -- Paste your API KEY here (from the above link)
-    api_key = "API_KEY", 
-    
+    api_key = "API_KEY",
+
     -- If the player is using a VPN Connection, do this action:
     -- k = kick, b = ban, c = crash (Warning: Crash function only works on stock maps!)
     action = "k",
-    
+
     -- If action is "b" the player will be banned for this amount of time ((in minutes) set to 0 for permanent ban)
     bantime = 10,
-    
+
     -- The reason for being kicked or banned:
     reason = "VPN Connection",
-    
+
     -- Message output to the joining player:
     feedback1 = "We\'ve detected that you\'re using a VPN or Proxy - we do not allow these!'",
-    
+
     -- Message output to Dedicated Server Console:
     feedback2 = "%name% was %action% for using a VPN or Proxy (IP: %ip%)",
-    
+
     -- Request Parameters:
     checks = {
         --Check if IP is associated with being a confirmed crawler 
         --such as Googlebot, Bingbot, etc based on hostname or IP address verification.
         is_crawler = true,
-        
+
         --Check if IP suspected of being a VPN connection?
         vpn = true,
-        
+
         --Check if IP suspected of being a Tor connection?
         tor = true,
-        
+
         --Check if IP address suspected to be a proxy? (SOCKS, Elite, Anonymous, VPN, Tor, etc.)
         proxy = true,
-        
+
         --Fraud Score Threshold: 
         --Fraud Scores >= 75 are suspicious, but not necessarily fraudulent. 
         --I recommend flagging or blocking traffic with Fraud Scores >= 85, 
         --but you may find it beneficial to use a higher or lower threshold.
         fraud_score = 85,
-        
+
         --Premium Account Feature:
         --Indicates if bots or non-human traffic has recently used this IP address to engage 
         --in automated fraudulent behavior. Provides stronger confidence that the IP address is suspicious.
         bot_status = true,
     },
-    
+
     parameters = {
         -- How in depth (strict) do you want this query to be? 
         -- Higher values take longer to process and may provide a higher false-positive rate. 
         -- It is recommended to start at "0", the lowest strictness setting, and increasing to "1" or "2" depending on your needs.
         strictness = 1,
-        
+
         -- Bypasses certain checks for IP addresses from education and research institutions, schools, and some corporate connections 
         -- to better accommodate audiences that frequently use public connections.
         allow_public_access_points = true,
-        
+
         -- Enable this setting to lower detection rates and Fraud Scores for mixed quality IP addresses. 
         -- If you experience any false-positives with your traffic then enabling this feature will provide better results.
         lighter_penalties = false,
-        
+
         -- This setting is used for time-sensitive lookups that require a faster response time. 
         -- Accuracy is slightly degraded with the "fast" approach, but not significantly noticeable.
         fast = false,
-        
+
         -- You can optionally specify that this lookup should be treated as a mobile device.
         mobile = false,
     },
-    
+
     exclusion_list = {
         "127.0.0.1", -- localhost
         "000.000.000.000",
@@ -148,33 +148,33 @@ function OnScriptLoad()
     end
 end
 
-function OnPreJoin(p) 
+function OnPreJoin(p)
     local player = vpn_blocker:GetCredentials(p)
     if (player) then
-        
+
         local site = "https://www.ipqualityscore.com/api/json/ip/api_key/"
         local key = gsub(site, "api_key", vpn_blocker.api_key)
-        
+
         local query_link = tostring(key .. player.ip .. "?")
         local i_iteration = 0
-        for k,v in pairs(vpn_blocker.parameters) do
+        for k, v in pairs(vpn_blocker.parameters) do
             if (i_iteration == 0) then
-                query_link = query_link ..k.."="..tostring(v)
+                query_link = query_link .. k .. "=" .. tostring(v)
             else
-                query_link = query_link .."&"..k.."="..tostring(v)
+                query_link = query_link .. "&" .. k .. "=" .. tostring(v)
             end
             i_iteration = i_iteration + 1
         end
-        
+
         local JsonData = vpn_blocker:Query(query_link)
         if (JsonData) then
-            
+
             local ip_lookup = json:decode(JsonData)
             if (ip_lookup.success) then
-                
+
                 local connected = function()
-                    cprint("VPN Blocker -> Running Ip Lookup ^ Please wait...", 4+8)
-                    for k,v in pairs(vpn_blocker.checks) do
+                    cprint("VPN Blocker -> Running Ip Lookup ^ Please wait...", 4 + 8)
+                    for k, v in pairs(vpn_blocker.checks) do
                         if (type(v) == "boolean") then
                             if (v) and (ip_lookup[k]) then
                                 return false
@@ -189,10 +189,10 @@ function OnPreJoin(p)
                 end
 
                 if (not connected()) then
-                                
+
                     say(p, vpn_blocker.feedback1)
                     local state = "none"
-                    
+
                     if (vpn_blocker.action == "k") then
                         state = "kicked"
                         execute_command("k" .. " " .. p .. " \"" .. vpn_blocker.reason .. "\"")
@@ -204,27 +204,29 @@ function OnPreJoin(p)
                         vpn_blocker[p] = {}
                         vpn_blocker[p].crash = true
                     end
-                    
+
                     local logtime = true
-                    for key,value in pairs(ip_lookup) do
-                        vpn_blocker:WriteLog(key,value, logtime)
-                        if logtime then logtime = false end
+                    for key, value in pairs(ip_lookup) do
+                        vpn_blocker:WriteLog(key, value, logtime)
+                        if logtime then
+                            logtime = false
+                        end
                     end
-                    
+
                     local msg = gsub(gsub(gsub(vpn_blocker.feedback2, "%%name%%", player.name),
-                    "%%action%%", state), 
-                    "%%ip%%", player.ip)
-                    cprint(msg, 4+8)
-                else        
-                    cprint("VPN Blocker -> Player connected successfully.", 2+8)
+                            "%%action%%", state),
+                            "%%ip%%", player.ip)
+                    cprint(msg, 4 + 8)
+                else
+                    cprint("VPN Blocker -> Player connected successfully.", 2 + 8)
                 end
             else
                 -- Error Handling
-                cprint("[VPN BLOCKER ERROR]", 4+8)
-                for k,v in pairs(ip_lookup) do
-                    print(k,v)
+                cprint("[VPN BLOCKER ERROR]", 4 + 8)
+                for k, v in pairs(ip_lookup) do
+                    print(k, v)
                 end
-                cprint("=========================================================", 4+8)
+                cprint("=========================================================", 4 + 8)
             end
         end
     end
@@ -247,11 +249,11 @@ end
 function vpn_blocker:CrashPlayer(PlayerIndex)
     local player_object = get_dynamic_player(PlayerIndex)
     if (player_object ~= 0) then
-        local x,y,z = read_vector3d(player_object + 0x5C)
-        local VehicleID = spawn_object("vehi", "vehicles\\rwarthog\\rwarthog")
+        local x, y, z = read_vector3d(player_object + 0x5C)
+        local VehicleID = spawn_object("vehi", "vehicles\\rwarthog\\rwarthog", x, y, z)
         local VehicleObject = get_object_memory(VehicleID)
         if (VehicleObject ~= 0) then
-            for j = 0,20 do
+            for j = 0, 20 do
                 enter_vehicle(VehicleID, PlayerIndex, j)
                 exit_vehicle(PlayerIndex)
             end
@@ -260,14 +262,14 @@ function vpn_blocker:CrashPlayer(PlayerIndex)
     end
 end
 
-function vpn_blocker:WriteLog(k,v, logtime)
+function vpn_blocker:WriteLog(k, v, logtime)
     local file = io.open("VPN Blocker.log", "a+")
     if file then
-    
+
         if (logtime) then
-            file:write(os.date("[%H:%M:%S - %d/%m/%Y]: ").."\n")
+            file:write(os.date("[%H:%M:%S - %d/%m/%Y]: ") .. "\n")
         end
-    
+
         local line = format("%s\t%s\n", tostring(k), tostring(v))
         file:write(line)
         file:close()
@@ -277,7 +279,7 @@ end
 function isExcluded(IP)
     -- Check if IP is in the exclusion list.
     local t = vpn_blocker.exclusion_list
-    for i = 1,#t do
+    for i = 1, #t do
         if (t[i]) then
             if (IP == t[i]) then
                 return true
@@ -286,12 +288,12 @@ function isExcluded(IP)
     end
     return false
 end
-    
+
 function vpn_blocker:GetCredentials(p)
     local ip = get_var(p, "$ip"):match('(%d+.%d+.%d+.%d+)')
     if (not isExcluded(ip)) then
         local name = get_var(p, "$name")
-        return {ip = ip, name = name}
+        return { ip = ip, name = name }
     else
         return nil
     end
