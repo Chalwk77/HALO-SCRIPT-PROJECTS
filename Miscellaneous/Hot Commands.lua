44c977e5901851e516cbd01e2b493e3744b896fe
Jericho <Chalwk77@users.noreply.github.com>
2020-02-10 07:47:20 +1300

    Update Hot Commands.lua

--- Miscellaneous/Hot Commands.lua
+++ Miscellaneous/Hot Commands.lua
@@ -4,7 +4,7 @@ api_version = "1.12.0.0"
 -- Config Starts -----------------------------------------------------------------------------------------
 local hot_commands = {
     ["sayrules"] = {
-        "No portal camping or blocking, no any kind of cheating", 
+        "No portal camping or blocking, no any kind of cheating",
         "No vehicles in the tunnels or on the flags"
         "No Cursing or bad names",
         "No harassing an AG member or anyone in the AG Server",
@@ -14,14 +14,14 @@ local hot_commands = {
     ["sayhi"] = {
         "Welcome to the «ÅG~ unlimited rocket server",
         "Enjoy your time with awesome «ÅG~ Clan",
-        "Please follow the rules", 
+        "Please follow the rules",
         "Bienvenid@ a «ÅG~ unlimited rocket server",
         "Disfruta de tu tiempo con el increíble «ÅG~ Clan",
         "Por favor, sigue las reglas"
     },
-	
+
     ["_other"] = {
-		"add something here"
+        "add something here"
     },
     -- repeat the structure to add more entries
 }
@@ -39,18 +39,18 @@ function OnServerCommand(Admin, Command, _, _)
     local Cmd = CmdSplit(Command)
     if (#Cmd == 0) then
         return false
-    elseif (tonumber(Admin, "$lvl") >= 1)
-		Cmd[1] = lower(Cmd[1]) or upper(Cmd[1])
-		for command,content in pairs(hot_commands) do
-			if (Cmd[1] == command) then
-				execute_command("msg_prefix \"\"")
-				for i = 1,#content do
-					say_all(get_var(Admin, "$name") .. ": " .. content[i])
-				end
-				execute_command("msg_prefix \" **" .. server_prefix .. "**\"")
-				return false
-			end
-		end
+    elseif (tonumber(Admin, "$lvl") >= 1) then
+        Cmd[1] = lower(Cmd[1]) or upper(Cmd[1])
+        for command, content in pairs(hot_commands) do
+            if (Cmd[1] == command) then
+                execute_command("msg_prefix \"\"")
+                for i = 1, #content do
+                    say_all(get_var(Admin, "$name") .. ": " .. content[i])
+                end
+                execute_command("msg_prefix \" **" .. server_prefix .. "**\"")
+                return false
+            end
+        end
     end
 end
 
