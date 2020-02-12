f056184aa60966e8080556f53c8fed4d3f953f1c
jericho <jericho.crosby227@gmail.com>
2020-02-13 10:22:40 +1300

    Fixed vehicle command for map: tusken_raid

--- SAPP SCRIPTS/ATTRACTIVE MODS/Apri Fools - Chat.lua
+++ SAPP SCRIPTS/ATTRACTIVE MODS/Apri Fools - Chat.lua
@@ -18,10 +18,11 @@ api_version = "1.12.0.0"
 -- Insert your server prefix here:
 local server_prefix = "** LNZ ** "
 local trigger_words = { "that", "que" }
+local permission_level = 1
 -- Config [ends]----------------------------------
 
 local modify_chat
-local gmatch = string.gmatch
+local gmatch, sub = string.gmatch, string.sub
 
 function OnScriptLoad()
     register_callback(cb["EVENT_CHAT"], "OnPlayerChat")
@@ -44,31 +45,38 @@ end
 function OnPlayerChat(PlayerIndex, Message, Type)
     if (Type ~= 6) then
         local Str = stringSplit(Message)
+        local is_command = (sub(Message, 1, 1) == "/") or (sub(Message, 1, 1) == "\\")
+        if not (is_command) then
 
-        for i = 1, #Str do
-            for j = 1, #trigger_words do
-                if (Str[i] == trigger_words[j]) then
-                    if (not modify_chat) then
-                        modify_chat = true
-                    else
-                        modify_chat = false
+            for i = 1, #Str do
+                for j = 1, #trigger_words do
+                    if (Str[i] == trigger_words[j]) then
+                        if (not modify_chat) then
+                            local level = tonumber(get_var(PlayerIndex, "$lvl"))
+                            if (level >= permission_level) then
+                                modify_chat = true
+                            end
+                        else
+                            print('turning off')
+                            modify_chat = false
+                        end
                     end
                 end
             end
-        end
 
-        if (modify_chat) then
-            local new_player = GetPlayers(PlayerIndex)
-            if (new_player) then
-                local name = get_var(new_player, "$name")
-                execute_command("msg_prefix \"\"")
-                if (Type == 0) then
-                    say_all(name .. ": " .. Message)
-                elseif (Type == 1 or Type == 2) then
-                    say_all("[" .. name .. "]: " .. Message)
+            if (modify_chat) then
+                local new_player = GetPlayers(PlayerIndex)
+                if (new_player) then
+                    local name = get_var(new_player, "$name")
+                    execute_command("msg_prefix \"\"")
+                    if (Type == 0) then
+                        say_all(name .. ": " .. Message)
+                    elseif (Type == 1 or Type == 2) then
+                        say_all("[" .. name .. "]: " .. Message)
+                    end
+                    execute_command("msg_prefix \" " .. server_prefix .. "\"")
+                    return false
                 end
-                execute_command("msg_prefix \" " .. server_prefix .. "\"")
-                return false
             end
         end
     end
