6a56a8322d7db7e1338e5ee302508aabf0c8098d
Jericho <Chalwk77@users.noreply.github.com>
2020-05-03 17:38:08 +1200

    Update Clear Chat.lua

--- SAPP SCRIPTS/UTILITY MODS/Clear Chat.lua
+++ SAPP SCRIPTS/UTILITY MODS/Clear Chat.lua
@@ -4,7 +4,7 @@ Script Name: Clear Chat, for SAPP (PC & CE)
 Description: A simple script that allows you to clear the global server chat. 
 Command Syntax: /clear or /cc
      
-Copyright (c) 2016-2018, Jericho Crosby <jericho.crosby227@gmail.com>
+Copyright (c) 2016-2020, Jericho Crosby <jericho.crosby227@gmail.com>
 Notice: You can use this document subject to the following conditions:
 https://github.com/Chalwk77/Halo-Scripts-Phasor-V2-/blob/master/LICENSE
 ~ Written by Jericho Crosby (Chalwk)
@@ -15,7 +15,7 @@ https://github.com/Chalwk77/Halo-Scripts-Phasor-V2-/blob/master/LICENSE
 api_version = '1.12.0.0'
 
 -- config starts
-local command_aliases = {"cc", "clear"}
+local command_aliases = { "cc", "clear" }
 
 local permission_level = 1
 
@@ -30,35 +30,35 @@ end
 
 function OnServerCommand(Executor, Command, _, _)
     local Args = CMDSplit(Command)
-	if (Args == nil or Args == "") then
-		return
-	else
-		Args[1] = lower(Args[1]) or upper(Args[1])
-		for CmdIndex = 1,#command_aliases do
-			if (Args[1] == command_aliases[CmdIndex]) then
-				local level = tonumber(get_var(Executor, "$lvl"))
-				if (level >= permission_level or Executor == 0) then
-					for _ = 1, 20 do
-						execute_command("msg_prefix \"\"")
-						say_all(" ")
-						execute_command("msg_prefix \" " .. server_prefix .. " \" ")
-					end
-					Send(Executor, "Chat was cleared")
-				else
-					Send(Executor, "Insufficient Permission")
-				end
-				return false
-			end
+    if (Args == nil or Args == "") then
+        return
+    else
+        Args[1] = lower(Args[1]) or upper(Args[1])
+        for CmdIndex = 1, #command_aliases do
+            if (Args[1] == command_aliases[CmdIndex]) then
+                local level = tonumber(get_var(Executor, "$lvl"))
+                if (level >= permission_level or Executor == 0) then
+                    for _ = 1, 20 do
+                        execute_command("msg_prefix \"\"")
+                        say_all(" ")
+                        execute_command("msg_prefix \" " .. server_prefix .. " \" ")
+                    end
+                    Send(Executor, "Chat was cleared")
+                else
+                    Send(Executor, "Insufficient Permission")
+                end
+                return false
+            end
         end
     end
 end
 
 function Send(Executor, Message)
-	if (Executor == 0) then
-		cprint(Message)
-	else
-		rprint(Executor, Message)
-	end
+    if (Executor == 0) then
+        cprint(Message)
+    else
+        rprint(Executor, Message)
+    end
 end
 
 function CMDSplit(CMD)
