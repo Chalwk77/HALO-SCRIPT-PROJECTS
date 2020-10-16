58e8ddb6683cd30fa88da507284baf7db89aa500
jericho <jericho.crosby227@gmail.com>
2020-10-17 09:01:03 +1300

    Began writing some code to get started.

--- INDEV/Loadouts.lua
+++ INDEV/Loadouts.lua
@@ -1,1 +1,35 @@
--- Nothing to report! Please check back later.
+--[[
+--=====================================================================================================--
+Script Name: LoadOut, for SAPP (PC & CE)
+Description: N/A
+
+~ acknowledgements ~
+Concept credit goes to OSH Clan, a gaming community operating on Halo CE.
+- website:  https://oldschoolhalo.boards.net/
+
+Copyright (c) 2020, Jericho Crosby <jericho.crosby227@gmail.com>
+* Notice: You can use this document subject to the following conditions:
+https://github.com/Chalwk77/Halo-Scripts-Phasor-V2-/blob/master/LICENSE
+
+* Written by Jericho Crosby (Chalwk)
+--=====================================================================================================--
+]]--
+
+api_version = "1.12.0.0"
+
+local loadouts = {
+    classes = {
+        ["Regeneration"] = {
+            command = "regen",
+        },
+        ["Armor Boost"] = {
+            command = "armor",
+        },
+        ["Partial Camo"] = {
+            command = "camo",
+        },
+        ["Recon"] = {
+            command = "speed",
+        }
+    }
+}
