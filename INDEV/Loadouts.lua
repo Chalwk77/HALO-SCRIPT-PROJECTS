473eb1f49cca0e301dd22cdc403bfec24d6bd84c
jericho <jericho.crosby227@gmail.com>
2020-10-28 22:38:26 +1300

    Fixed a major crash bug.

--- INDEV/Loadouts.lua
+++ INDEV/Loadouts.lua
@@ -1,6 +1,6 @@
 --[[
 --=====================================================================================================--
-Script Name: Loadout (Alpha 1.2), for SAPP (PC & CE)
+Script Name: Loadout (Alpha 1.3), for SAPP (PC & CE)
 Description: N/A
 
 todo: add support for custom maps
