e752f5ee3319e8ace41635aed133c05adb5c4c0b
jericho <jericho.crosby227@gmail.com>
2020-10-30 13:02:01 +1300

    Initial Commit

--- SAPP SCRIPTS/ATTRACTIVE MODS/Tea Bagging.lua
+++ SAPP SCRIPTS/ATTRACTIVE MODS/Tea Bagging.lua
@@ -2,7 +2,7 @@
 --=====================================================================================================--
 Script Name: T-Bagging, for SAPP (PC & CE)
 Description: Humiliate your friends with this nifty little script.
-             Crouch over your vi tim corpse 3 times to trigger a funny message "name is t-bagging victim"
+Crouch over your victims corpse 3 times to trigger a funny message "name is t-bagging victim"
 
 Copyright (c) 2020, Jericho Crosby <jericho.crosby227@gmail.com>
 * Notice: You can use this document subject to the following conditions:
