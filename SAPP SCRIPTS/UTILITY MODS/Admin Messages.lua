418cdcadf6ea5d52515327a96fd1b4f4397ee1b0
jericho <jericho.crosby227@gmail.com>
2020-10-30 20:39:17 +1300

    Added new thing

--- SAPP SCRIPTS/UTILITY MODS/Admin Messages.lua
+++ SAPP SCRIPTS/UTILITY MODS/Admin Messages.lua
@@ -20,6 +20,12 @@ join_message[1] = { "[TRIAL-MOD] ", " joined the server. Everybody hide!" }
 join_message[2] = { "[MODERATOR] ", " just showed up. Hold my beer!" }
 join_message[3] = { "[ADMIN] ", " just joined. Hide your bananas!" }
 join_message[4] = { "[SENIOR-ADMIN] ", " joined the server." }
+join_message.clan_leaders = {
+    ["72.222.168.191"] = { "[CLAN LEADER] ", " joined the server." },
+    ["127.0.0.1"] = { "[CLAN LEADER] ", " joined the server." },
+    ["192.168.1.1"] = { "[CLAN LEADER] ", " joined the server." },
+    -- repeat structure to add more entries...,
+}
 
 local environment = 'chat' -- valid environments: "chat", "rcon"
 
@@ -36,10 +42,29 @@ function OnScriptLoad()
     end
 end
 
+local function IsClanLeader(IP)
+    for ip, v in pairs(join_message.clan_leaders) do
+        if (IP == ip) then
+            return v
+        end
+    end
+    return false
+end
+
 function OnPlayerConnect(p)
     local name, lvl = get_var(p, "$name"), tonumber(get_var(p, "$lvl"))
     if (lvl >= 1) then
-        local msg = "|" .. alignment .. " " .. join_message[lvl][1] .. name .. join_message[lvl][2]
+
+        local msg = ""
+        local ip = get_var(p, '$ip'):match("%d+.%d+.%d+.%d+")
+        local leader = IsClanLeader(ip)
+
+        if (not leader) then
+            msg = "|" .. alignment .. " " .. join_message[lvl][1] .. name .. join_message[lvl][2]
+        else
+            msg = "|" .. alignment .. " " .. leader[1] .. name .. leader[2]
+        end
+
         for i = 1, 16 do
             if (i ~= p and player_present(i)) then
                 SAPPFunc(i, msg)
