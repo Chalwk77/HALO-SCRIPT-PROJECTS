23e6d9dc59645304ed657fe651001be45d26a935
jericho <jericho.crosby227@gmail.com>
2020-11-01 10:36:12 +1300

    Refactored some shit

--- SAPP SCRIPTS/ATTRACTIVE MODS/Top Ranked (1.9).lua
+++ SAPP SCRIPTS/ATTRACTIVE MODS/Top Ranked (1.9).lua
@@ -1,6 +1,6 @@
 --[[
 --=====================================================================================================--
-Script Name: Top Ranked (v1.8), for SAPP (PC & CE)
+Script Name: Top Ranked (v1.9), for SAPP (PC & CE)
 Description: A fully integrated ranking system for SAPP servers.
 Players earn credits for killing, scoring and achievements, such as sprees, kill-combos and more.
 
@@ -24,6 +24,9 @@ api_version = "1.12.0.0"
 local Rank = {
 
     dir = "ranks.json",
+
+    -- Suggestions: cR, $, Points, Candy
+    currency_symbol = "cR",
 
     double_exp = false,
 
@@ -79,56 +82,56 @@ local Rank = {
             enabled = true,
             human_team = "red",
             zombie_team = "blue",
-            credits = { 25, "%credits% cR (Zombie-Bite)" },
+            credits = { 25, "+25 %currency_symbol% (Zombie-Bite)" },
         },
 
-        tbag = { 1, "+%credits% cR (T-Bagging)" },
+        tbag = { 1, "+1 %currency_symbol% (T-Bagging)" },
 
         -- Score (credits added):
-        score = { 25, "+%credits% cR (Flag Cap)" },
+        score = { 25, "+25 %currency_symbol% (Flag Cap)" },
 
         -- Killed by Server (credits deducted):
-        server = { -0, "%credits% cR (Server)" },
+        server = { -0, "-0 %currency_symbol% (Server)" },
 
         -- killed by guardians (credits deducted):
-        guardians = { -5, "%credits% cR (Guardians)" },
+        guardians = { -5, "-5 %currency_symbol% (Guardians)" },
 
         -- suicide (credits deducted):
-        suicide = { -10, "%credits% cR (Suicide)" },
+        suicide = { -10, "-10 %currency_symbol% (Suicide)" },
 
         -- betrayal (credits deducted):
-        betrayal = { -15, "%credits% cR (Betrayal)" },
+        betrayal = { -15, "-15 %currency_symbol% (Betrayal)" },
 
         -- Killed from the grave (credits added to killer)
-        killed_from_the_grave = { 5, "+%credits% cR (Killed From Grave)" },
+        killed_from_the_grave = { 5, "+5 %currency_symbol% (Killed From Grave)" },
 
         -- {consecutive kills, xp rewarded}
         spree = {
-            { 5, 5, "+%credits% cR (spree)" },
-            { 10, 10, "+%credits% cR (spree)" },
-            { 15, 15, "+%credits% cR (spree)" },
-            { 20, 20, "+%credits% cR (spree)" },
-            { 25, 25, "+%credits% cR (spree)" },
-            { 30, 30, "+%credits% cR (spree)" },
-            { 35, 35, "+%credits% cR (spree)" },
-            { 40, 40, "+%credits% cR (spree)" },
-            { 45, 45, "+%credits% cR (spree)" },
+            { 5, 5, "+5 %currency_symbol% (spree)" },
+            { 10, 10, "+10 %currency_symbol% (spree)" },
+            { 15, 15, "+15 %currency_symbol% (spree)" },
+            { 20, 20, "+20 %currency_symbol% (spree)" },
+            { 25, 25, "+25 %currency_symbol% (spree)" },
+            { 30, 30, "+30 %currency_symbol% (spree)" },
+            { 35, 35, "+35 %currency_symbol% (spree)" },
+            { 40, 40, "+40 %currency_symbol% (spree)" },
+            { 45, 45, "+45 %currency_symbol% (spree)" },
             -- Award 50 credits for every 5 kills at or above 50
-            { 50, 50, "+%credits% cR (spree)" },
+            { 50, 50, "+50 %currency_symbol% (spree)" },
         },
 
         -- kill-combo required, credits awarded
         multi_kill = {
-            { 2, 8, "+%credits% cR (multi-kill)" },
-            { 3, 10, "+%credits% cR (multi-kill)" },
-            { 4, 12, "+%credits% cR (multi-kill)" },
-            { 5, 14, "+%credits% cR (multi-kill)" },
-            { 6, 16, "+%credits% cR (multi-kill)" },
-            { 7, 18, "+%credits% cR (multi-kill)" },
-            { 8, 20, "+%credits% cR (multi-kill)" },
-            { 9, 23, "+%credits% cR (multi-kill)" },
+            { 2, 8, "+8 %currency_symbol% (multi-kill)" },
+            { 3, 10, "+10 %currency_symbol% (multi-kill)" },
+            { 4, 12, "+12 %currency_symbol% (multi-kill)" },
+            { 5, 14, "+14 %currency_symbol% (multi-kill)" },
+            { 6, 16, "+16 %currency_symbol% (multi-kill)" },
+            { 7, 18, "+18 %currency_symbol% (multi-kill)" },
+            { 8, 20, "+20 %currency_symbol% (multi-kill)" },
+            { 9, 23, "+23 %currency_symbol% (multi-kill)" },
             -- Award 25 credits every 2 kills at or above 10 kill-combos
-            { 10, 25, "+%credits% cR (multi-kill)" },
+            { 10, 25, "+25 %currency_symbol% (multi-kill)" },
         },
 
         tags = {
@@ -138,64 +141,65 @@ local Rank = {
             --
 
             -- FALL DAMAGE --
-            { "jpt!", "globals\\falling", -3, "%credits% cR (Fall Damage)" },
-            { "jpt!", "globals\\distance", -4, "%credits% cR (Distance Damage)" },
+            { "jpt!", "globals\\falling", -3, "-3 %currency_symbol% (Fall Damage)" },
+            { "jpt!", "globals\\distance", -4, "-4 %currency_symbol% (Distance Damage)" },
 
             -- VEHICLE PROJECTILES --
-            { "jpt!", "vehicles\\ghost\\ghost bolt", 7, "+%credits% cR (Ghost Bolt)" },
-            { "jpt!", "vehicles\\scorpion\\bullet", 6, "+%credits% cR (Tank Bullet)" },
-            { "jpt!", "vehicles\\warthog\\bullet", 6, "+%credits% cR (Warthog Bullet)" },
-            { "jpt!", "vehicles\\c gun turret\\mp bolt", 7, "+%credits% cR (Turret Bolt)" },
-            { "jpt!", "vehicles\\banshee\\banshee bolt", 7, "+%credits% cR (Banshee Bolt)" },
-            { "jpt!", "vehicles\\scorpion\\shell explosion", 10, "+%credits% cR (Tank Shell)" },
-            { "jpt!", "vehicles\\banshee\\mp_fuel rod explosion", 10, "+%credits% cR (Banshee Fuel-Rod Explosion)" },
+            { "jpt!", "vehicles\\ghost\\ghost bolt", 7, "+7 %currency_symbol% (Ghost Bolt)" },
+            { "jpt!", "vehicles\\scorpion\\bullet", 6, "+6 %currency_symbol% (Tank Bullet)" },
+            { "jpt!", "vehicles\\warthog\\bullet", 6, "+6 %currency_symbol% (Warthog Bullet)" },
+            { "jpt!", "vehicles\\c gun turret\\mp bolt", 7, "+7 %currency_symbol% (Turret Bolt)" },
+            { "jpt!", "vehicles\\banshee\\banshee bolt", 7, "+7 %currency_symbol% (Banshee Bolt)" },
+            { "jpt!", "vehicles\\scorpion\\shell explosion", 10, "+10 %currency_symbol% (Tank Shell)" },
+            { "jpt!", "vehicles\\banshee\\mp_fuel rod explosion", 10, "+10 %currency_symbol% (Banshee Fuel-Rod Explosion)" },
 
             -- WEAPON PROJECTILES --
-            { "jpt!", "weapons\\pistol\\bullet", 5, "+%credits% cR (Pistol Bullet)" },
-            { "jpt!", "weapons\\shotgun\\pellet", 6, "+%credits% cR (Shotgun Pallet)" },
-            { "jpt!", "weapons\\plasma rifle\\bolt", 4, "+%credits% cR (Plasma Rifle Bolt)" },
-            { "jpt!", "weapons\\needler\\explosion", 8, "+%credits% cR (Needler Explosion)" },
-            { "jpt!", "weapons\\plasma pistol\\bolt", 4, "+%credits% cR (Plasma Bolt)" },
-            { "jpt!", "weapons\\assault rifle\\bullet", 5, "+%credits% cR (Assault Rifle Bullet)" },
-            { "jpt!", "weapons\\needler\\impact damage", 4, "+%credits% cR (Needler Impact Damage)" },
-            { "jpt!", "weapons\\flamethrower\\explosion", 5, "+%credits% cR (Flamethrower)" },
-            { "jpt!", "weapons\\rocket launcher\\explosion", 8, "+%credits% cR (Rocket Launcher Explosion)", "+%credits% cR (R-Hog Rocket Launcher Explosion)" },
-            { "jpt!", "weapons\\needler\\detonation damage", 3, "+%credits% cR (Needler Detonation Damage)" },
-            { "jpt!", "weapons\\plasma rifle\\charged bolt", 4, "+%credits% cR (Plasma Rifle Bolt)" },
-            { "jpt!", "weapons\\sniper rifle\\sniper bullet", 6, "+%credits% cR (Sniper Rifle Bullet)" },
-            { "jpt!", "weapons\\plasma_cannon\\effects\\plasma_cannon_explosion", 8, "+%credits% cR (Plasma Cannon Explosion)" },
+            { "jpt!", "weapons\\pistol\\bullet", 5, "+5 %currency_symbol% (Pistol Bullet)" },
+            { "jpt!", "weapons\\shotgun\\pellet", 6, "+6 %currency_symbol% (Shotgun Pallet)" },
+            { "jpt!", "weapons\\plasma rifle\\bolt", 4, "+4 %currency_symbol% (Plasma Rifle Bolt)" },
+            { "jpt!", "weapons\\needler\\explosion", 8, "+8 %currency_symbol% (Needler Explosion)" },
+            { "jpt!", "weapons\\plasma pistol\\bolt", 4, "+4 %currency_symbol% (Plasma Bolt)" },
+            { "jpt!", "weapons\\assault rifle\\bullet", 5, "+5 %currency_symbol% (Assault Rifle Bullet)" },
+            { "jpt!", "weapons\\needler\\impact damage", 4, "+4 %currency_symbol% (Needler Impact Damage)" },
+            { "jpt!", "weapons\\flamethrower\\explosion", 5, "+5 %currency_symbol% (Flamethrower)" },
+            { "jpt!", "weapons\\rocket launcher\\explosion", 8, "+8 %currency_symbol% (Rocket Launcher Explosion)" },
+            { "jpt!", "weapons\\needler\\detonation damage", 3, "+3 %currency_symbol% (Needler Detonation Damage)" },
+            { "jpt!", "weapons\\plasma rifle\\charged bolt", 4, "+4 %currency_symbol% (Plasma Rifle Bolt)" },
+            { "jpt!", "weapons\\sniper rifle\\sniper bullet", 6, "+6 %currency_symbol% (Sniper Rifle Bullet)" },
+            { "jpt!", "weapons\\plasma_cannon\\effects\\plasma_cannon_explosion", 8, "+8 %currency_symbol% (Plasma Cannon Explosion)" },
 
             -- GRENADES --
-            { "jpt!", "weapons\\frag grenade\\explosion", 8, "+%credits% cR (Frag Explosion)" },
-            { "jpt!", "weapons\\plasma grenade\\attached", 7, "+%credits% cR (Plasma Grenade - attached)" },
-            { "jpt!", "weapons\\plasma grenade\\explosion", 5, "+%credits% cR (Plasma Grenade explosion)" },
+            { "jpt!", "weapons\\frag grenade\\explosion", 8, "+8 %currency_symbol% (Frag Explosion)" },
+            { "jpt!", "weapons\\plasma grenade\\attached", 7, "+7 %currency_symbol% (Plasma Grenade - attached)" },
+            { "jpt!", "weapons\\plasma grenade\\explosion", 5, "+5 %currency_symbol% (Plasma Grenade explosion)" },
 
             -- MELEE --
-            { "jpt!", "weapons\\flag\\melee", 5, "+%credits% cR (Melee: Flag)" },
-            { "jpt!", "weapons\\ball\\melee", 5, "+%credits% cR (Melee: Ball)" },
-            { "jpt!", "weapons\\pistol\\melee", 4, "+%credits% cR (Melee: Pistol)" },
-            { "jpt!", "weapons\\needler\\melee", 4, "+%credits% cR (Melee: Needler)" },
-            { "jpt!", "weapons\\shotgun\\melee", 5, "+%credits% cR (Melee: Shotgun)" },
-            { "jpt!", "weapons\\flamethrower\\melee", 5, "+%credits% cR (Melee: Flamethrower)" },
-            { "jpt!", "weapons\\sniper rifle\\melee", 5, "+%credits% cR (Melee: Sniper Rifle)" },
-            { "jpt!", "weapons\\plasma rifle\\melee", 4, "+%credits% cR (Melee: Plasma Rifle)" },
-            { "jpt!", "weapons\\plasma pistol\\melee", 4, "+%credits% cR (Melee: Plasma Pistol)" },
-            { "jpt!", "weapons\\assault rifle\\melee", 4, "+%credits% cR (Melee: Assault Rifle)" },
-            { "jpt!", "weapons\\rocket launcher\\melee", 10, "+%credits% cR (Melee: Rocket Launcher)" },
-            { "jpt!", "weapons\\plasma_cannon\\effects\\plasma_cannon_melee", 10, "+%credits% cR (Melee: Plasma Cannon)" },
+            { "jpt!", "weapons\\flag\\melee", 5, "+5 %currency_symbol% (Melee: Flag)" },
+            { "jpt!", "weapons\\ball\\melee", 5, "+5 %currency_symbol% (Melee: Ball)" },
+            { "jpt!", "weapons\\pistol\\melee", 4, "+4 %currency_symbol% (Melee: Pistol)" },
+            { "jpt!", "weapons\\needler\\melee", 4, "+4 %currency_symbol% (Melee: Needler)" },
+            { "jpt!", "weapons\\shotgun\\melee", 5, "+5 %currency_symbol% (Melee: Shotgun)" },
+            { "jpt!", "weapons\\flamethrower\\melee", 5, "+5 %currency_symbol% (Melee: Flamethrower)" },
+            { "jpt!", "weapons\\sniper rifle\\melee", 5, "+5 %currency_symbol% (Melee: Sniper Rifle)" },
+            { "jpt!", "weapons\\plasma rifle\\melee", 4, "+4 %currency_symbol% (Melee: Plasma Rifle)" },
+            { "jpt!", "weapons\\plasma pistol\\melee", 4, "+4 %currency_symbol% (Melee: Plasma Pistol)" },
+            { "jpt!", "weapons\\assault rifle\\melee", 4, "+4 %currency_symbol% (Melee: Assault Rifle)" },
+            { "jpt!", "weapons\\rocket launcher\\melee", 10, "+10 %currency_symbol% (Melee: Rocket Launcher)" },
+            { "jpt!", "weapons\\plasma_cannon\\effects\\plasma_cannon_melee", 10, "+10 %currency_symbol% (Melee: Plasma Cannon)" },
 
             -- VEHICLE COLLISION --
             vehicles = {
                 collision = { "jpt!", "globals\\vehicle_collision" },
-                { "vehi", "vehicles\\ghost\\ghost_mp", 5, "+%credits% cR (Vehicle Squash: GHOST)" },
-                { "vehi", "vehicles\\rwarthog\\rwarthog", 6, "+%credits% cR (Vehicle Squash: R-Hog)" },
-                { "vehi", "vehicles\\warthog\\mp_warthog", 7, "+%credits% cR (Vehicle Squash: Warthog)" },
-                { "vehi", "vehicles\\banshee\\banshee_mp", 8, "+%credits% cR (Vehicle Squash: Banshee)" },
-                { "vehi", "vehicles\\scorpion\\scorpion_mp", 10, "+%credits% cR (Vehicle Squash: Tank)" },
-                { "vehi", "vehicles\\c gun turret\\c gun turret_mp", 1000, "+%credits% cR (Vehicle Squash: Turret)" },
+                { "vehi", "vehicles\\ghost\\ghost_mp", 5, "+5 %currency_symbol% (Vehicle Squash: GHOST)" },
+                { "vehi", "vehicles\\rwarthog\\rwarthog", 6, "+6 %currency_symbol% (Vehicle Squash: R-Hog)" },
+                { "vehi", "vehicles\\warthog\\mp_warthog", 7, "+7 %currency_symbol% (Vehicle Squash: Warthog)" },
+                { "vehi", "vehicles\\banshee\\banshee_mp", 8, "+8 %currency_symbol% (Vehicle Squash: Banshee)" },
+                { "vehi", "vehicles\\scorpion\\scorpion_mp", 10, "+10 %currency_symbol% (Vehicle Squash: Tank)" },
+                { "vehi", "vehicles\\c gun turret\\c gun turret_mp", 1000, "+1000 %currency_symbol% (Vehicle Squash: Turret)" },
             }
         }
     },
+
 
     --
     -- Advanced users only:
@@ -681,7 +685,7 @@ function Rank:UpdateCredits(Ply, Params)
     local str = Params[2]
     self.players[Ply].credits = self.players[Ply].credits + cr
 
-    str = gsub(str, "%%credits%%", cr)
+    str = gsub(str, "%%currency_symbol%%", self.currency_symbol)
     self:Respond(Ply, str, rprint, 10)
 
     if (self.players[Ply].credits < 0) then
