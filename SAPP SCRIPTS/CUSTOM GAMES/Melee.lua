776a910fcf1657952d5873186f51c8ec843c1710
jericho <jericho.crosby227@gmail.com>
2020-12-06 14:53:33 +1300

    Refactored to improve code base

--- SAPP SCRIPTS/CUSTOM GAMES/Melee.lua
+++ SAPP SCRIPTS/CUSTOM GAMES/Melee.lua
@@ -1,12 +1,9 @@
 --[[
 --=====================================================================================================--
 Script Name: Melee, for SAPP (PC & CE)
-Description: Custom Melee game (requested by RoadHog)
+Description: Custom Melee game
 
-Fight to the death with melee attacks only!
-* You cannot pick up or drop weapons nor enter vehicles, however, you can pick up health packs, camouflage and overshield.
-
-Copyright (c) 2016-2019, Jericho Crosby <jericho.crosby227@gmail.com>
+Copyright (c) 2016-2020, Jericho Crosby <jericho.crosby227@gmail.com>
 * Notice: You can use this document subject to the following conditions:
 https://github.com/Chalwk77/Halo-Scripts-Phasor-V2-/blob/master/LICENSE
 
@@ -16,203 +13,167 @@ https://github.com/Chalwk77/Halo-Scripts-Phasor-V2-/blob/master/LICENSE
 ]]--
 
 api_version = "1.12.0.0"
-local melee_object = { }
 
--- Configuration [starts]
-melee_object[1] = "weapons\\ball\\ball"
-local scorelimit = 10
+local Melee = {
+    -- Configuration [starts] >> ----------
+    scorelimit = 50,
+    -- Melee Object (weapon index from objects table)"
+    weapon = 12,
+    objects = {
+        -- false = prevent spawning | true = allow spawning
+        [1] = { "eqip", "powerups\\health pack", true },
+        [2] = { "eqip", "powerups\\over shield", true },
+        [3] = { "eqip", "powerups\\active camouflage", true },
+        [4] = { "eqip", "weapons\\frag grenade\\frag grenade", false },
+        [5] = { "eqip", "weapons\\plasma grenade\\plasma grenade", false },
 
-local objects = {
+        [6] = { "vehi", "vehicles\\ghost\\ghost_mp", false },
+        [7] = { "vehi", "vehicles\\rwarthog\\rwarthog", false },
+        [8] = { "vehi", "vehicles\\banshee\\banshee_mp", false },
+        [9] = { "vehi", "vehicles\\warthog\\mp_warthog", false },
+        [10] = { "vehi", "vehicles\\scorpion\\scorpion_mp", false },
+        [11] = { "vehi", "vehicles\\c gun turret\\c gun turret_mp", false },
 
-    -- FALSE = prevent spawning | TRUE = allow spawning
-    { "eqip", "powerups\\active camouflage", true },
-    { "eqip", "powerups\\health pack", true },
-    { "eqip", "powerups\\over shield", true },
-    { "eqip", "weapons\\frag grenade\\frag grenade", false },
-    { "eqip", "weapons\\plasma grenade\\plasma grenade", false },
+        [12] = { "weap", "weapons\\ball\\ball", true }, -- DO NOT DISABLE if this is the melee object.
+        [13] = { "weap", "weapons\\flag\\flag", false },
+        [14] = { "weap", "weapons\\pistol\\pistol", false },
+        [15] = { "weap", "weapons\\shotgun\\shotgun", false },
+        [16] = { "weap", "weapons\\needler\\mp_needler", false },
+        [17] = { "weap", "weapons\\flamethrower\\flamethrower", false },
+        [18] = { "weap", "weapons\\plasma rifle\\plasma rifle", false },
+        [19] = { "weap", "weapons\\sniper rifle\\sniper rifle", false },
+        [20] = { "weap", "weapons\\plasma pistol\\plasma pistol", false },
+        [21] = { "weap", "weapons\\plasma_cannon\\plasma_cannon", false },
+        [22] = { "weap", "weapons\\assault rifle\\assault rifle", false },
+        [23] = { "weap", "weapons\\rocket launcher\\rocket launcher", false }
+    },
+    -- Configuration [ends] << ----------
 
-    { "vehi", "vehicles\\banshee\\banshee_mp", false },
-    { "vehi", "vehicles\\c gun turret\\c gun turret_mp", false },
-    { "vehi", "vehicles\\ghost\\ghost_mp", false },
-    { "vehi", "vehicles\\scorpion\\scorpion_mp", false },
-    { "vehi", "vehicles\\rwarthog\\rwarthog", false },
-    { "vehi", "vehicles\\warthog\\mp_warthog", false },
-
-    { "weap", "weapons\\assault rifle\\assault rifle", false },
-    { "weap", "weapons\\ball\\ball", true }, -- DO NOT disable (if this is the melee object)
-    { "weap", "weapons\\flag\\flag", false },
-    { "weap", "weapons\\flamethrower\\flamethrower", false },
-    { "weap", "weapons\\needler\\mp_needler", false },
-    { "weap", "weapons\\pistol\\pistol", false },
-    { "weap", "weapons\\plasma pistol\\plasma pistol", false },
-    { "weap", "weapons\\plasma rifle\\plasma rifle", false },
-    { "weap", "weapons\\plasma_cannon\\plasma_cannon", false },
-    { "weap", "weapons\\rocket launcher\\rocket launcher", false },
-    { "weap", "weapons\\shotgun\\shotgun", false },
-    { "weap", "weapons\\sniper rifle\\sniper rifle", false }
+    -- DO NOT TOUCH --
+    players = { }
+    --
 }
 
--- Configuration [ends] << ----------
-
-local assign, drones, clean_up_dones = {}, {}, {}
-for i = 1, 16 do
-    drones[i] = {}
-end
-local obj_in_memory, gamestarted = {}
-
 function OnScriptLoad()
-    gamestarted = false
-    register_callback(cb['EVENT_GAME_START'], "OnNewGame")
+    register_callback(cb['EVENT_GAME_START'], "OnGameStart")
+    OnGameStart()
 end
 
 function OnScriptUnload()
-    --
+    -- N/A
 end
 
-function OnNewGame()
-    if not isTeamPlay() then
-        register_callback(cb['EVENT_JOIN'], "OnPlayerJoin")
-        register_callback(cb['EVENT_LEAVE'], "OnPlayerLeave")
-        register_callback(cb['EVENT_SPAWN'], "OnPlayerSpawn")
-        register_callback(cb['EVENT_TICK'], "OnTick")
-        register_callback(cb['EVENT_DIE'], "OnPlayerDeath")
-        register_callback(cb['EVENT_WEAPON_DROP'], "OnWeaponDrop")
-        register_callback(cb['EVENT_OBJECT_SPAWN'], "OnObjectSpawn")
-
-        -- Disable all vehicles
-        execute_command("disable_all_vehicles 0 1")
-
-        -- Disable weapon pick ups
-        execute_command("disable_object 'weapons\\assault rifle\\assault rifle'")
-        execute_command("disable_object 'weapons\\flamethrower\\flamethrower'")
-        execute_command("disable_object 'weapons\\needler\\mp_needler'")
-        execute_command("disable_object 'weapons\\pistol\\pistol'")
-        execute_command("disable_object 'weapons\\plasma pistol\\plasma pistol'")
-        execute_command("disable_object 'weapons\\plasma rifle\\plasma rifle'")
-        execute_command("disable_object 'weapons\\plasma_cannon\\plasma_cannon'")
-        execute_command("disable_object 'weapons\\rocket launcher\\rocket launcher'")
-        execute_command("disable_object 'weapons\\shotgun\\shotgun'")
-        execute_command("disable_object 'weapons\\sniper rifle\\sniper rifle'")
-
-        -- disable grenade pick ups
-        execute_command("disable_object 'weapons\\frag grenade\\frag grenade'")
-        execute_command("disable_object 'weapons\\plasma grenade\\plasma grenade'")
-
-        execute_command("scorelimit " .. tonumber(scorelimit))
-        gamestarted = true
-    else
-        cprint("[!] Error (melee.lua) | This script doesn't support team play.", 4 + 8)
+function OnGameStart()
+    if (get_var(0, "$gt") ~= "n/a") then
+        if (get_var(0, "$ffa") ~= "0") then
+            register_callback(cb['EVENT_TICK'], "OnTick")
+            register_callback(cb['EVENT_DIE'], "OnPlayerDeath")
+            register_callback(cb['EVENT_SPAWN'], "OnPlayerSpawn")
+            register_callback(cb['EVENT_JOIN'], "OnPlayerConnect")
+            register_callback(cb['EVENT_LEAVE'], "OnPlayerDisconnect")
+            register_callback(cb['EVENT_WEAPON_DROP'], "OnWeaponDrop")
+            register_callback(cb['EVENT_OBJECT_SPAWN'], "OnObjectSpawn")
+            execute_command("disable_all_vehicles 0 1")
+            execute_command("scorelimit " .. Melee.scorelimit)
+            for i = 1, 16 do
+                if player_present(i) then
+                    Melee:CleanUpDrones(i, false)
+                    Melee:InitPlayer(i, false)
+                end
+            end
+        else
+            unregister_callback(cb['EVENT_DIE'])
+            unregister_callback(cb['EVENT_TICK'])
+            unregister_callback(cb['EVENT_JOIN'])
+            unregister_callback(cb['EVENT_LEAVE'])
+            unregister_callback(cb['EVENT_SPAWN'])
+            unregister_callback(cb['EVENT_WEAPON_DROP'])
+            unregister_callback(cb['EVENT_OBJECT_SPAWN'])
+        end
     end
 end
 
-function OnPlayerJoin(PlayerIndex)
-    obj_in_memory[get_var(PlayerIndex, "$n")] = nil
+function OnPlayerConnect(Ply)
+    Melee:InitPlayer(Ply, false)
 end
 
-function OnTick()
-    for i = 1, 16 do
-        if player_present(i) then
-            if (player_alive(i)) then
-                if (assign[i] == true) then
-                    local function assignWeapon(i)
-                        execute_command("wdel " .. i)
-                        local player = get_dynamic_player(i)
-                        local x, y, z = read_vector3d(player + 0x5C)
+function OnPlayerDisconnect(Ply)
+    Melee:InitPlayer(Ply, true)
+end
 
-                        local oddball = spawn_object("weap", melee_object[1], x, y, z)
-                        local object_spawned = assign_weapon(oddball, i)
+function Melee:InitPlayer(Ply, Reset)
+    if (not Reset) then
+        self.players[Ply] = { assign = false, drones = {} }
+    else
+        self:CleanUpDrones(Ply, false)
+    end
+end
 
-                        local object = get_object_memory(oddball)
-                        drones[i] = drones[i] or {}
-                        table.insert(drones[i], oddball)
-                        obj_in_memory[get_var(i, "$n")] = object
-                        clean_up_dones[i] = true
-
-                        assign[i] = false
-                    end
-                    assignWeapon(i)
+function Melee:OnTick()
+    for i, v in pairs(self.players) do
+        if player_present(i) and player_alive(i) then
+            if (v.assign) then
+                v.assign = false
+                execute_command("nades " .. i .. " 0")
+                execute_command("wdel " .. i)
+                local DyN = get_dynamic_player(i)
+                if (DyN ~= 0) then
+                    local x, y, z = read_vector3d(DyN + 0x5C)
+                    local weapon = spawn_object("weap", self.objects[self.weapon][2], x, y, z)
+                    assign_weapon(weapon, i)
+                    table.insert(v.drones, weapon)
                 end
             end
         end
     end
 end
 
-function OnPlayerSpawn(PlayerIndex)
-    if player_alive(PlayerIndex) then
-        local PlayerObject = get_dynamic_player(PlayerIndex)
-        if (PlayerObject ~= 0) then
-            write_word(PlayerObject + 0x31E, 0)
-            write_word(PlayerObject + 0x31F, 0)
-            assign[PlayerIndex] = true
+function OnPlayerSpawn(Ply)
+    Melee.players[Ply].assign = true
+end
+
+function OnPlayerDeath(Victim)
+    local DyN = get_dynamic_player(Victim)
+    local WeaponID = read_dword(DyN + 0x118)
+    if (WeaponID ~= 0) then
+        for j = 0, 3 do
+            destroy_object(read_dword(DyN + 0x2F8 + j * 4))
         end
     end
 end
 
-function OnPlayerDeath(PlayerIndex, KillerIndex)
-    local function deleteWeapons(PlayerIndex)
-        local PlayerObject = get_dynamic_player(PlayerIndex)
-        local WeaponID = read_dword(PlayerObject + 0x118)
-        if WeaponID ~= 0 then
-            for j = 0, 3 do
-                local ObjectID = read_dword(PlayerObject + 0x2F8 + j * 4)
-                destroy_object(ObjectID)
-            end
+function Melee:CleanUpDrones(Ply, Assign)
+
+    for _, weapon in pairs(self.players[Ply].drones) do
+        if (weapon) then
+            destroy_object(weapon)
         end
     end
-    deleteWeapons(PlayerIndex)
-end
 
-function isTeamPlay()
-    if get_var(0, "$ffa") == "0" then
-        return true
+    if (Assign) then
+        self.players[Ply].assign = true
     else
-        return false
+        self.players[Ply] = nil
     end
 end
 
-local function CleanUpDrones(PlayerIndex)
-    if (clean_up_dones[PlayerIndex] == true) then
-        if drones[PlayerIndex] ~= nil then
-            for k, v in pairs(drones[PlayerIndex]) do
-                if drones[PlayerIndex][k] > 0 then
-                    if v then
-                        if obj_in_memory[get_var(PlayerIndex, "$n")] then
-                            destroy_object(v)
-                        end
-                        drones[PlayerIndex][k] = nil
-                    end
-                end
-            end
-        end
-    end
+function OnWeaponDrop(Ply)
+    Melee:CleanUpDrones(Ply, true)
 end
 
-function OnPlayerLeave(PlayerIndex)
-    CleanUpDrones(PlayerIndex)
-end
-
-function OnWeaponDrop(PlayerIndex)
-    if player_alive(PlayerIndex) then
-        CleanUpDrones(PlayerIndex)
-        assign[PlayerIndex] = true
-    end
-end
-
-local function TagInfo(obj_type, obj_name)
+local function GetTag(obj_type, obj_name)
     local tag = lookup_tag(obj_type, obj_name)
     return tag ~= 0 and read_dword(tag + 0xC) or nil
 end
 
-function OnObjectSpawn(PlayerIndex, MapID, ParentID, ObjectID)
-    if (gamestarted == true) then
-        for i = 1, #objects do
-            local tag = objects[i][1]
-            local name = objects[i][2]
-            local bool = objects[i][3]
-            if (MapID == TagInfo(tag, name)) and (bool == false) then
-                --cprint("removing " .. tag .. ", " .. name)
-                return false;
-            end
+function OnObjectSpawn(_, MapID, _, _)
+    for _, v in pairs(Melee.objects) do
+        if (MapID == GetTag(v[1], v[2])) and (not v[3]) then
+            return false
         end
     end
+end
+
+function OnTick()
+    return Melee:OnTick()
 end
