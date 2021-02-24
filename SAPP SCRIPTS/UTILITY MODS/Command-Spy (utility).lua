176878a5bd9c8e53e7c47dec553005e382da09ba
jericho <jericho.crosby227@gmail.com>
2021-02-24 18:55:36 +1300

    Added command blacklist (see config)

--- SAPP SCRIPTS/UTILITY MODS/Command-Spy (utility).lua
+++ SAPP SCRIPTS/UTILITY MODS/Command-Spy (utility).lua
@@ -24,6 +24,16 @@ local commands_to_hide = {
     -- Repeat the structure to add more commands.
 }
 
+-- Command(s) containing these words will not be sent:
+local command_blacklist = {
+    "login",
+    "admin_add",
+    "sv_password",
+    "change_password",
+    "admin_change_pw",
+    "admin_add_manually",
+}
+
 api_version = "1.12.0.0"
 
 local lower = string.lower
@@ -34,18 +44,29 @@ function OnScriptLoad()
 end
 
 local function CMDSplit(Str)
-    local Args, index = { }, 1
+    local Args = { }
     for Params in gmatch(Str, "([^%s]+)") do
-        Args[index] = lower(Params)
-        index = index + 1
+        Args[#Args + 1] = lower(Params)
     end
     return Args
+end
+
+--
+-- This function checks if the command contains any blacklisted keywords:
+--
+local function BlackListed(CMD)
+    for _, word in pairs(command_blacklist) do
+        if CMD:lower():find(word) then
+            return true
+        end
+    end
+    return false
 end
 
 function OnServerCommand(Ply, Command, _, _)
 
     local CMD = CMDSplit(Command)
-    if (#CMD > 0) then
+    if (#CMD > 0 and not BlackListed(Command)) then
 
         for i = 1, #commands_to_hide do
             if (CMD[1] == commands_to_hide[i]) then
