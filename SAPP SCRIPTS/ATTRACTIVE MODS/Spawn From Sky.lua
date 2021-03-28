8a52f5669d6c84b872b88aafaee8cd74a8b93663
jericho <jericho.crosby227@gmail.com>
2021-03-28 20:35:00 +1300

    Refactored + Bug fixes

--- SAPP SCRIPTS/ATTRACTIVE MODS/Spawn From Sky.lua
+++ SAPP SCRIPTS/ATTRACTIVE MODS/Spawn From Sky.lua
@@ -1,176 +1,151 @@
 --[[
 --=====================================================================================================--
 Script Name: Spawn From Sky, for SAPP (PC & CE)
-Description: This mod will spawn the player from the sky.
-             The player will have 7s of invulnerability.
+Description: Spawn from the sky!
+             The height above the ground can be edited on a per-map basis
 
-~ requested by RhaegarTargaryen on opencarnage.net
-
-Copyright (c) 2016-2018, Jericho Crosby <jericho.crosby227@gmail.com>
+Copyright (c) 2016-2021, Jericho Crosby <jericho.crosby227@gmail.com>
 Notice: You can use this document subject to the following conditions:
 https://github.com/Chalwk77/Halo-Scripts-Phasor-V2-/blob/master/LICENSE
-~ Created by Jericho Crosby (Chalwk)
 --=====================================================================================================--
 ]]
 
-api_version = '1.12.0.0'
+api_version = "1.12.0.0"
 
--- Configuration [starts]
-local height_from_ground = 35
-local invulnerability_time = 9
+-- Height above ground:
+local maps = {
+    ["bloodgulch"] = 35,
+    ["deathisland"] = 35,
+    ["icefields"] = 35,
+    ["infinity"] = 35,
+    ["sidewinder"] = 35,
+    ["timberland"] = 35,
+    ["dangercanyon"] = 35,
+    ["beavercreek"] = 35,
+    ["boardingaction"] = 35,
+    ["carousel"] = 35,
+    ["chillout"] = 35,
+    ["damnation"] = 35,
+    ["gephyrophobia"] = 35,
+    ["hangemhigh"] = 35,
+    ["longest"] = 35,
+    ["prisoner"] = 35,
+    ["putput"] = 35,
+    ["ratrace"] = 35,
+    ["wizard"] = 35,
+}
 
-base_loc = { }
-base_loc["bloodgulch"] = {
-    -- red x,y,z
-    { 95.687797546387, -159.44900512695, -0.10000000149012 },
-    -- blue x,y,z
-    { 40.240600585938, -79.123199462891, -0.10000000149012 }
-}
-base_loc["deathisland"] = {
-    { -26.576030731201, -6.9761986732483, 9.6631727218628 },
-    { 29.843469619751, 15.971487045288, 8.2952880859375 }
-}
-base_loc["icefields"] = {
-    { 24.85000038147, -22.110000610352, 2.1110000610352 },
-    { -77.860000610352, 86.550003051758, 2.1110000610352 }
-}
-base_loc["infinity"] = {
-    { 0.67973816394806, -164.56719970703, 15.039022445679 },
-    { -1.8581243753433, 47.779975891113, 11.791272163391 }
-}
-base_loc["sidewinder"] = {
-    { -32.038200378418, -42.066699981689, -3.7000000476837 },
-    { 30.351499557495, -46.108001708984, -3.7000000476837 }
-}
-base_loc["timberland"] = {
-    { 17.322099685669, -52.365001678467, -17.751399993896 },
-    { -16.329900741577, 52.360000610352, -17.741399765015 }
-}
-base_loc["dangercanyon"] = {
-    { -12.104507446289, -3.4351840019226, -2.2419033050537 },
-    { 12.007399559021, -3.4513700008392, -2.2418999671936 }
-}
-base_loc["beavercreek"] = {
-    { 29.055599212646, 13.732000350952, -0.10000000149012 },
-    { -0.86037802696228, 13.764800071716, -0.0099999997764826 }
-}
-base_loc["boardingaction"] = {
-    { 1.723109960556, 0.4781160056591, 0.60000002384186 },
-    { 18.204000473022, -0.53684097528458, 0.60000002384186 }
-}
-base_loc["carousel"] = {
-    { 5.6063799858093, -13.548299789429, -3.2000000476837 },
-    { -5.7499198913574, 13.886699676514, -3.2000000476837 }
-}
-base_loc["chillout"] = {
-    { 7.4876899719238, -4.49059009552, 2.5 },
-    { -7.5086002349854, 9.750340461731, 0.10000000149012 }
-}
-base_loc["damnation"] = {
-    { 9.6933002471924, -13.340399742126, 6.8000001907349 },
-    { -12.17884349823, 14.982703208923, -0.20000000298023 }
-}
-base_loc["gephyrophobia"] = {
-    { 26.884338378906, -144.71551513672, -16.049139022827 },
-    { 26.727857589722, 0.16621616482735, -16.048349380493 }
-}
-base_loc["hangemhigh"] = {
-    { 13.047902107239, 9.0331249237061, -3.3619771003723 },
-    { 32.655700683594, -16.497299194336, -1.7000000476837 }
-}
-base_loc["longest"] = {
-    { -12.791899681091, -21.6422996521, -0.40000000596046 },
-    { 11.034700393677, -7.5875601768494, -0.40000000596046 }
-}
-base_loc["prisoner"] = {
-    { -9.3684597015381, -4.9481601715088, 5.6999998092651 },
-    { 9.3676500320435, 5.1193399429321, 5.6999998092651 }
-}
-base_loc["putput"] = {
-    { -18.89049911499, -20.186100006104, 1.1000000238419 },
-    { 34.865299224854, -28.194700241089, 0.10000000149012 }
-}
-base_loc["ratrace"] = {
-    { -4.2277698516846, -0.85564690828323, -0.40000000596046 },
-    { 18.613000869751, -22.652599334717, -3.4000000953674 }
-}
-base_loc["wizard"] = {
-    { -9.2459697723389, 9.3335800170898, -2.5999999046326 },
-    { 9.1828498840332, -9.1805400848389, -2.5999999046326 }
-}
--- Configuration [ends] -------------------------
+local ctf_globals
+local rx, ry, rz
+local bx, by, bz
 
-players = {}
-init_timer = {}
-first_join = {}
+local height
+local players = { }
 
 function OnScriptLoad()
-    register_callback(cb['EVENT_TICK'], "OnTick")
-    register_callback(cb['EVENT_JOIN'], "OnPlayerJoin")
-    register_callback(cb['EVENT_LEAVE'], "OnPlayerLeave")
-    register_callback(cb['EVENT_PRESPAWN'], 'OnPlayerPrespawn')
-end
 
-function OnScriptUnload()
-    --
-end
+    register_callback(cb["EVENT_GAME_START"], "OnGameStart")
 
-function OnPlayerJoin(PlayerIndex)
-    players[get_var(PlayerIndex, "$name")] = { }
-    players[get_var(PlayerIndex, "$name")].timer = 0
-
-    init_timer[PlayerIndex] = true
-    first_join[PlayerIndex] = true
-end
-
-function OnPlayerLeave(PlayerIndex)
-    if init_timer == true then
-        init_timer[PlayerIndex] = false
+    local ctf_globals_pointer = sig_scan("8B3C85????????3BF9741FE8????????8B8E2C0200008B4610") + 3
+    if (ctf_globals_pointer == 3) then
+        return
     end
-    if first_join == true then
-        first_join[PlayerIndex] = false
-    end
-    players[get_var(PlayerIndex, "$name")].timer = 0
+    ctf_globals = read_dword(ctf_globals_pointer)
+
+    OnGameStart()
 end
 
 function OnTick()
-    for i = 1, 16 do
-        if player_present(i) then
-            if (init_timer[i] == true) then
-                timeUntilRestore(i)
+    for i, v in pairs(players) do
+        if (i and v.init) then
+            local DyN = get_dynamic_player(i)
+            if (DyN ~= 0) then
+                local state = read_byte(DyN + 0x2A3)
+                if (state == 21 or state == 22) then
+                    v.init = false
+                    execute_command("ungod " .. i)
+                end
             end
         end
     end
 end
 
-function timeUntilRestore(PlayerIndex)
-    players[get_var(PlayerIndex, "$name")].timer = players[get_var(PlayerIndex, "$name")].timer + 0.030
-    if (players[get_var(PlayerIndex, "$name")].timer >= (invulnerability_time)) then
-        players[get_var(PlayerIndex, "$name")].timer = 0
-        init_timer[tonumber(PlayerIndex)] = false
-        execute_command("ungod " .. tonumber(PlayerIndex))
+local function InitPlayer(Ply, Reset)
+    if (not Reset) then
+        players[Ply] = { init = true }
+    else
+        players[Ply] = nil
     end
 end
 
-function OnPlayerPrespawn(PlayerIndex)
-    if (first_join[PlayerIndex] == true) then
-        first_join[PlayerIndex] = false
+function OnGameStart()
+    if (get_var(0, "$gt") ~= "n/a") then
 
-        local team = get_var(PlayerIndex, "$team")
-        if (team == "red") then
-            Teleport(PlayerIndex, 1)
-        elseif (team == "blue") then
-            Teleport(PlayerIndex, 2)
+        local map = get_var(0, "$map")
+        height = maps[map]
+
+        if (height) then
+
+            RegisterSAPPEvents(true)
+
+            players = { }
+            for i = 1, 16 do
+                if player_present(i) then
+                    InitPlayer(i, false)
+                end
+            end
+
+            rx, ry, rz = read_vector3d(read_dword(ctf_globals))
+            bx, by, bz = read_vector3d(read_dword(ctf_globals + 4))
+        else
+            RegisterSAPPEvents(false)
         end
-
     end
 end
 
-function Teleport(PlayerIndex, TableIndex)
-    local map = get_var(0, "$map")
-    write_vector3d(get_dynamic_player(PlayerIndex) + 0x5C,
-            base_loc[map][tonumber(TableIndex)][1],
-            base_loc[map][tonumber(TableIndex)][2],
-            base_loc[map][tonumber(TableIndex)][3] + math.floor(height_from_ground))
-    execute_command("god " .. tonumber(PlayerIndex))
+function OnPlayerJoin(Ply)
+    InitPlayer(Ply, false)
+end
+
+function OnPlayerDisconnect(Ply)
+    InitPlayer(Ply, true)
+end
+
+function OnPlayerSpawn(Ply)
+    if (players[Ply].init) then
+        execute_command("god " .. Ply)
+    end
+end
+
+function OnPreSpawn(Ply)
+    if (players[Ply].init) then
+
+        local DyN = get_dynamic_player(Ply)
+        if (DyN ~= 0) then
+            local x, y, z
+            local team = get_var(Ply, "$team")
+            if (team == "red") then
+                x, y, z = rx, ry, rz
+            else
+                x, y, z = bx, by, bz
+            end
+            write_vector3d(DyN + 0x5C, x, y, z + height)
+        end
+    end
+end
+
+function RegisterSAPPEvents(Load)
+    if (Load) then
+        register_callback(cb["EVENT_TICK"], "OnTick")
+        register_callback(cb["EVENT_JOIN"], "OnPlayerJoin")
+        register_callback(cb['EVENT_SPAWN'], 'OnPlayerSpawn')
+        register_callback(cb['EVENT_PRESPAWN'], 'OnPreSpawn')
+        register_callback(cb["EVENT_LEAVE"], "OnPlayerDisconnect")
+    else
+        unregister_callback(cb["EVENT_TICK"])
+        unregister_callback(cb["EVENT_JOIN"])
+        unregister_callback(cb['EVENT_SPAWN'])
+        unregister_callback(cb['EVENT_PRESPAWN'])
+        unregister_callback(cb["EVENT_LEAVE"])
+    end
 end
---
+++ SAPP SCRIPTS/UTILITY MODS/Item Sky Spawner.lua
@@ -0,0 +1,203 @@
+--[[
+Script Name: Item Sky Spawner, for SAPP (PC & CE)
+Description: This script will spawn a random weapon or vehicle
+             at a random pre-defined map coordinate,.
+
+Copyright (c) 2021, Jericho Crosby <jericho.crosby227@gmail.com>
+* Notice: You can use this document subject to the following conditions:
+https://github.com/Chalwk77/Halo-Scripts-Phasor-V2-/blob/master/LICENSE
+]]--
+
+-- config starts --
+-- How often should we spawn an object (in seconds)?
+local min = 5
+local max = 60
+
+-- While an object is not being interacted with, how long until it de-spawns?
+local half_life = 30
+
+-- Height from ground that objects will spawn (in world units)
+local height_from_ground = 30
+
+local items = {
+    { "vehi", "vehicles\\ghost\\ghost_mp" },
+    { "vehi", "vehicles\\rwarthog\\rwarthog" },
+    { "vehi", "vehicles\\banshee\\banshee_mp" },
+    { "vehi", "vehicles\\c gun turret\\c gun turret_mp" },
+    { "vehi", "vehicles\\warthog\\mp_warthog", "Warthog" },
+    { "vehi", "vehicles\\scorpion\\scorpion_mp" },
+
+    { "weap", "weapons\\gravity rifle\\gravity rifle" },
+    { "weap", "weapons\\flag\\flag" },
+    { "weap", "weapons\\ball\\ball" },
+    { "weap", "weapons\\pistol\\pistol" },
+    { "weap", "weapons\\shotgun\\shotgun" },
+    { "weap", "weapons\\needler\\mp_needler" },
+    { "weap", "weapons\\plasma rifle\\plasma rifle" },
+    { "weap", "weapons\\flamethrower\\flamethrower" },
+    { "weap", "weapons\\plasma_cannon\\plasma_cannon" },
+    { "weap", "weapons\\plasma pistol\\plasma pistol" },
+    { "weap", "weapons\\assault rifle\\assault rifle" },
+    { "weap", "weapons\\sniper rifle\\sniper rifle" },
+    { "weap", "weapons\\rocket launcher\\rocket launcher" },
+}
+
+local coordinates = {
+    ["bloodgulch"] = {
+        { 41.101623535156, -90.281707763672, 0.12476693093777 },
+        { 46.077514648438, -90.439193725586, 0.77358269691467 },
+        { 56.91703414917, -101.42024993896, 0.076191321015358 },
+        { 65.814765930176, -104.81482696533, 1.938010931015 },
+        { 73.550834655762, -111.81549835205, 2.6525256633759 },
+        { 77.765007019043, -125.80402374268, 0.71494293212891 },
+        { 74.582389831543, -139.28483581543, 1.9513947963715 },
+        { 85.483337402344, -163.49174499512, 0.11511813104153 },
+        { 103.53232574463, -173.73710632324, 0.74547308683395 },
+        { 109.66470336914, -163.90553283691, 0.10314647853374 },
+        { 105.28534698486, -155.08737182617, 0.67522388696671 },
+        { 93.378700256348, -145.28199768066, 0.31651598215103 },
+        { 88.151893615723, -142.88500976563, 1.4771356582642 },
+        { 78.458541870117, -138.9543762207, 3.5617597103119 },
+        { 73.147125244141, -136.68130493164, 3.500504732132 },
+        { 56.038143157959, -131.96516418457, 2.0096452236176 },
+        { 47.858089447021, -125.94813537598, 1.7084130048752 },
+        { 37.010986328125, -106.34159088135, 1.9382441043854 },
+        { 23.565832138062, -77.041366577148, 0.90631490945816 },
+        { 26.064079284668, -59.849906921387, 3.1582815647125 },
+        { 36.09298324585, -56.705558776855, 3.3356153964996 },
+        { 49.803821563721, -61.756992340088, 2.4028491973877 },
+        { 62.824329376221, -78.13745880127, 1.5507658720016 },
+    }
+}
+
+-- config starts --
+
+local coords
+local game_started
+
+local interval = 0
+local spawn_timer = 0
+
+local objects = { }
+local time_scale = 1 / 30
+local gmatch, lower = string.gmatch, string.lower
+
+api_version = "1.12.0.0"
+
+function OnScriptLoad()
+    register_callback(cb["EVENT_TICK"], "OnTick")
+    register_callback(cb["EVENT_GAME_START"], "OnGameStart")
+    register_callback(cb["EVENT_COMMAND"], "OnServerCommand")
+    OnGameStart()
+end
+
+local function NewInterval()
+    spawn_timer = 0
+    math.randomseed(os.clock())
+    interval = math.random(min, max)
+end
+
+function OnGameStart()
+    if (get_var(0, "$gt") ~= "n/a") then
+
+        OnScriptUnload(true)
+
+        game_started = true
+        coords = nil
+
+        local map = get_var(0, "$map")
+        coords = coordinates[map] ~= nil and coordinates[map]
+    end
+end
+
+local function Delete(Object)
+    destroy_object(Object)
+    objects[Object] = nil
+end
+
+function OnTick()
+    if (game_started and coords) then
+
+        spawn_timer = spawn_timer + time_scale
+
+        if (spawn_timer >= interval) then
+            NewInterval()
+            math.randomseed(os.clock())
+            local pos = coords[math.random(1, #coords)]
+            local item = items[math.random(1, #items)]
+            local object = spawn_object(item[1], item[2], pos[1], pos[2], pos[3] + height_from_ground)
+            local object_memory = get_object_memory(object)
+            table.insert(objects, { tag_type = item, object_memory = object_memory, object = object, timer = 0 })
+        end
+
+        for k, v in pairs(objects) do
+            if (k and v.object_memory ~= 0) then
+                if (v.tag_type[1] == "vehi") then
+                    if (v.object_memory ~= 0xFFFFFFFFF) then
+                        v.timer = v.timer + time_scale
+                        if (v.timer >= half_life) then
+                            Delete(k)
+                        end
+                    else
+                        v.timer = 0
+                    end
+                elseif (v.tag_type[1] == "weap") then
+
+                    local held
+                    for i = 1, 16 do
+                        if player_alive(i) and player_present(i) then
+                            local DyN = get_dynamic_player(i)
+                            for j = 0, 3 do
+                                local WeaponID = read_dword(DyN + 0x2F8 + (j * 4))
+                                if (WeaponID ~= 0xFFFFFFFF) then
+                                    local WeaponObject = get_object_memory(WeaponID)
+                                    if (WeaponObject == v.object_memory) then
+                                        held = true
+                                    end
+                                end
+                            end
+                        end
+                    end
+
+                    if (not held) then
+                        v.timer = v.timer + time_scale
+                        if (v.timer >= half_life) then
+                            Delete(k)
+                        end
+                    else
+                        v.timer = 0
+                    end
+                end
+            else
+                Delete(k)
+            end
+        end
+    end
+end
+
+local function CMDSplit(CMD)
+    local Args = { }
+    for Params in gmatch(CMD, "([^%s]+)") do
+        Args[#Args + 1] = lower(Params)
+    end
+    return Args
+end
+
+function OnServerCommand(Ply, Command)
+    local Args = CMDSplit(Command)
+    local access = (tonumber(get_var(Ply, "$lvl")) == 4)
+    if (access) and (Args and Args[1] == "sv_map_reset") then
+        OnScriptUnload(true)
+    end
+end
+
+function OnScriptUnload(NewTime)
+
+    if (NewTime) then
+        NewInterval()
+    end
+
+    for k, _ in pairs(objects) do
+        Delete(k)
+    end
+end
