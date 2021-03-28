85a8a402a9411a611b03974c1d9ad834feba8eb7
jericho <jericho.crosby227@gmail.com>
2021-03-28 20:43:11 +1300

    Refactored + Bug fixes

--- SAPP SCRIPTS/ATTRACTIVE MODS/Spawn From Sky.lua
+++ SAPP SCRIPTS/ATTRACTIVE MODS/Spawn From Sky.lua
@@ -1,8 +1,11 @@
 --[[
 --=====================================================================================================--
 Script Name: Spawn From Sky, for SAPP (PC & CE)
-Description: Spawn from the sky!
-             The height above the ground can be edited on a per-map basis
+Description: Read the title!
+
+             The height above the ground can be edited on a per-map basis.
+             All vanilla maps have a custom height.
+             Maps not listed have a default height above ground of 20 world units.
 
 Copyright (c) 2016-2021, Jericho Crosby <jericho.crosby227@gmail.com>
 Notice: You can use this document subject to the following conditions:
@@ -11,6 +14,10 @@ https://github.com/Chalwk77/Halo-Scripts-Phasor-V2-/blob/master/LICENSE
 ]]
 
 api_version = "1.12.0.0"
+-- config begins --
+
+-- This is the height used if the current map is not listed below:
+local default_height = 20
 
 -- Height above ground:
 local maps = {
@@ -33,24 +40,31 @@ local maps = {
     ["putput"] = 35,
     ["ratrace"] = 35,
     ["wizard"] = 35,
-}
 
-local ctf_globals
+    -- repeat the structure to add more maps (see example below)
+    ["your_map_name_here"] = 10,
+}
+-- config ends --
+
 local rx, ry, rz
 local bx, by, bz
 
-local height
 local players = { }
+local height, ctf_globals
 
 function OnScriptLoad()
-
+    register_callback(cb["EVENT_TICK"], "OnTick")
+    register_callback(cb["EVENT_JOIN"], "OnPlayerJoin")
+    register_callback(cb['EVENT_SPAWN'], 'OnPlayerSpawn')
+    register_callback(cb['EVENT_PRESPAWN'], 'OnPreSpawn')
     register_callback(cb["EVENT_GAME_START"], "OnGameStart")
+    register_callback(cb["EVENT_LEAVE"], "OnPlayerDisconnect")
 
-    local ctf_globals_pointer = sig_scan("8B3C85????????3BF9741FE8????????8B8E2C0200008B4610") + 3
-    if (ctf_globals_pointer == 3) then
+    local globals = sig_scan("8B3C85????????3BF9741FE8????????8B8E2C0200008B4610") + 3
+    if (globals == 3) then
         return
     end
-    ctf_globals = read_dword(ctf_globals_pointer)
+    ctf_globals = read_dword(globals)
 
     OnGameStart()
 end
@@ -83,23 +97,20 @@ function OnGameStart()
 
         local map = get_var(0, "$map")
         height = maps[map]
-
-        if (height) then
-
-            RegisterSAPPEvents(true)
-
-            players = { }
-            for i = 1, 16 do
-                if player_present(i) then
-                    InitPlayer(i, false)
-                end
-            end
-
-            rx, ry, rz = read_vector3d(read_dword(ctf_globals))
-            bx, by, bz = read_vector3d(read_dword(ctf_globals + 4))
-        else
-            RegisterSAPPEvents(false)
+        if (not height) then
+            height = default_height
         end
+
+        players = { }
+        for i = 1, 16 do
+            if player_present(i) then
+                InitPlayer(i, false)
+            end
+        end
+
+        rx, ry, rz = read_vector3d(read_dword(ctf_globals))
+        bx, by, bz = read_vector3d(read_dword(ctf_globals + 4))
+
     end
 end
 
@@ -131,21 +142,5 @@ function OnPreSpawn(Ply)
             end
             write_vector3d(DyN + 0x5C, x, y, z + height)
         end
-    end
-end
-
-function RegisterSAPPEvents(Load)
-    if (Load) then
-        register_callback(cb["EVENT_TICK"], "OnTick")
-        register_callback(cb["EVENT_JOIN"], "OnPlayerJoin")
-        register_callback(cb['EVENT_SPAWN'], 'OnPlayerSpawn')
-        register_callback(cb['EVENT_PRESPAWN'], 'OnPreSpawn')
-        register_callback(cb["EVENT_LEAVE"], "OnPlayerDisconnect")
-    else
-        unregister_callback(cb["EVENT_TICK"])
-        unregister_callback(cb["EVENT_JOIN"])
-        unregister_callback(cb['EVENT_SPAWN'])
-        unregister_callback(cb['EVENT_PRESPAWN'])
-        unregister_callback(cb["EVENT_LEAVE"])
     end
 end
