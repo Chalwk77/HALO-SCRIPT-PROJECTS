45177075ed15ab31a9f7b917c2a13c5880f3b37a
jericho <jericho.crosby227@gmail.com>
2021-04-02 12:49:32 +1300

    Refactored some code (improvements) and added option to re initialise countdown when manually setting new server p/w
    Signed-off-by: jericho <jericho.crosby227@gmail.com>

--- Miscellaneous/BK Clan/Timed Server Password.lua
+++ Miscellaneous/BK Clan/Timed Server Password.lua
@@ -1,38 +1,33 @@
 --[[
---======================================================================================================--
 Script Name: Timed Server Password (v1.0), for SAPP (PC & CE)
-Description: This script will automatically remove the server password after a given amount of time.
+Description: This script will automatically remove the server password after X seconds.
 
-Planned feature:
-      There will be a future update for this script.
-      Version 1.1 will have an option to init the countdown timer immediately after a new server password is set.
-      Currently, the timer begins as soon as a new game starts.
+Script Updated: 02/04/2021
+                1). Refactored some code (improvements)
+                2). Added option to re initialise countdown when manually setting new server p/w
 
 For @Rev - BK Clan.
 
-Copyright (c) 2020, Jericho Crosby <jericho.crosby227@gmail.com>
+Copyright (c) 2020-2021, Jericho Crosby <jericho.crosby227@gmail.com>
 * Notice: You can use this document subject to the following conditions:
 https://github.com/Chalwk77/Halo-Scripts-Phasor-V2-/blob/master/LICENSE
-
-* Written by Jericho Crosby (Chalwk)
-
---======================================================================================================--
 ]]--
+
 -- Config [starts] ------------------------------
 local duration = 300 -- in seconds
 -- Config [ends] --------------------------------
 
 api_version = "1.12.0.0"
 
-local server, time_scale = {}, 0.03333333333333333
+local server, time_scale = {}, 1 / 30
 local floor, format = math.floor, string.format
+local gmatch, lower = string.gmatch, string.lower
 
 function OnScriptLoad()
     register_callback(cb["EVENT_TICK"], "OnTick")
+    register_callback(cb["EVENT_CHAT"], "ChatCommand")
     register_callback(cb["EVENT_GAME_START"], "OnGameStart")
-    if (get_var(0, "$gt") ~= "n/a") then
-        StartTimer()
-    end
+    OnGameStart()
 end
 
 function OnScriptUnload()
@@ -45,21 +40,34 @@ function OnGameStart()
     end
 end
 
+local function TimerRemaining(s)
+
+    s = tonumber(s)
+    if (s <= 0) then
+        return "00", "00";
+    end
+
+    local h, m = format("%02.f", floor(s / 3600));
+    m = format("%02.f", floor(s / 60 - (h * 60)));
+    s = format("%02.f", floor(s - h * 3600 - m * 60));
+    return m, s
+end
+
 function OnTick()
     if (server.init) then
         server.time = server.time + time_scale
         local delta_time = ((duration) - (server.time))
-        local minutes, seconds = select(1, secondsToTime(delta_time)), select(2, secondsToTime(delta_time))
 
-        -- Debugging:
-        -- print("Server Password will be removed in: " .. minutes .. ":" .. seconds)
-        --
+        local m, s = TimerRemaining(delta_time)
 
-        local time_up = ((tonumber(minutes) <= 0) and (tonumber(seconds) <= 0))
+        -- debugging:
+        -- print("Server Password will be removed in: " .. m .. ":" .. s)
+
+        local time_up = ((tonumber(m) <= 0) and (tonumber(s) <= 0))
         if (time_up) then
             execute_command('sv_password ""')
             StopTimer()
-            say_all("Server password has been automatically removed.")
+            say_all("Server password has been removed!")
         end
     end
 end
@@ -72,14 +80,30 @@ function StopTimer()
     server.time, server.init = 0, false
 end
 
-function secondsToTime(seconds)
-    local seconds = tonumber(seconds)
-    if (seconds <= 0) then
-        return "00", "00";
-    else
-        local hours, mins, secs = format("%02.f", floor(seconds / 3600));
-        mins = format("%02.f", floor(seconds / 60 - (hours * 60)));
-        secs = format("%02.f", floor(seconds - hours * 3600 - mins * 60));
-        return mins, secs
+local function CMDSplit(CMD)
+    local Args = { }
+    for Params in gmatch(CMD, "([^%s]+)") do
+        Args[#Args + 1] = lower(Params)
+    end
+    return Args
+end
+
+local function IsCommand(Str)
+    return (Str:sub(1,1) == "/" or Str:sub(1,1) == "\\")
+end
+
+local function IsAdmin(Ply)
+    return (tonumber(get_var(Ply, "$lvl")) >= 4) or (Ply == 0)
+end
+
+function ChatCommand(Ply, MSG, _)
+    if IsCommand(MSG) then
+        local Args = CMDSplit(MSG)
+        if (Args and Args[1] ==  "/sv_password") then
+            if IsAdmin(Ply) and (Args[2]) then
+                StartTimer()
+                rprint(Ply, "Server password will be removed in " .. duration .. " seconds")
+            end
+        end
     end
 end
