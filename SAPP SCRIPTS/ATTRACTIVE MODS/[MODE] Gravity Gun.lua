922c5cb3a548d3c48b60494a72bac27cd6aa3485
jericho <jericho.crosby227@gmail.com>
2021-04-09 12:17:26 +1200

    A few tweaks
    Signed-off-by: jericho <jericho.crosby227@gmail.com>

--- SAPP SCRIPTS/ATTRACTIVE MODS/[MODE] Gravity Gun.lua
+++ SAPP SCRIPTS/ATTRACTIVE MODS/[MODE] Gravity Gun.lua
@@ -14,24 +14,39 @@ local GGun = {
 
     base_command = "ggun", -- short for gravity gun
 
+
     -- Minimum permission required to execute /base_command for yourself
     permission = -1,
+
 
     -- Minimum permission required to toggle GGUN on/off for other players:
     permission_other = 4,
 
+
     -- Distance (in world units) a vehicle will be suspended in front of player:
     -- 1 w/unit = 10 feet or ~3.048 meters
     distance = 5,
+    --
+
 
     -- If true, gravity gun will be disabled on death:
     disable_on_death = false,
     --
+
+
+    -- vehicle velocities --
+    --
+    -- initial launch velocity --
+    launch_velocity = 0.6,
+
+    -- Suspended yaw, pitch & roll velocities:
+    yaw = 0.1, pitch = 0.1, roll = 0.1,
 }
 -- config ends --
 
 api_version = "1.12.0.0"
 
+local sin = math.sin
 local gmatch, lower = string.gmatch, string.lower
 
 function OnScriptLoad()
@@ -58,6 +73,16 @@ local function Respond(Ply, Msg)
     rprint(Ply, Msg)
 end
 
+function GGun:ShotFired(DyN, PT)
+    local shot_fired = read_float(DyN + 0x490)
+    if (shot_fired ~= PT.weapon_state and shot_fired == 1) then
+        PT.weapon_state = shot_fired
+        return true
+    end
+    PT.weapon_state = shot_fired
+    return false
+end
+
 function GGun:OnTick()
     for i, v in pairs(self.players) do
         if (v.enabled) then
@@ -71,16 +96,16 @@ function GGun:OnTick()
                 local zAim = read_float(DyN + 0x238)
 
                 local px, py, pz
-
                 local VehicleID = read_dword(DyN + 0x11C)
                 local VehicleObject = get_object_memory(VehicleID)
+
                 if (VehicleID == 0xFFFFFFFF) then
                     px, py, pz = read_vector3d(DyN + 0x5c)
                 elseif (VehicleObject ~= 0) then
                     px, py, pz = read_vector3d(VehicleObject + 0x5c)
                 end
 
-                -- Account for Z-Coordinate change when crouching:
+                -- Update Z-Coordinate change when crouching:
                 local couching = read_float(DyN + 0x50C)
                 if (couching == 0) then
                     pz = pz + 0.65
@@ -88,41 +113,58 @@ function GGun:OnTick()
                     pz = pz + (0.35 * couching)
                 end
 
+                local shot_fired = self:ShotFired(DyN, v)
+
                 if (not v.target_object) then
 
                     -- test for successful intersect and make sure vehicle is not occupied:
                     local ignore_player = read_dword(get_player(i) + 0x34)
                     local success, _, _, _, target = intersect(px, py, pz, xAim * 1000, yAim * 1000, zAim * 1000, ignore_player)
-                    if (success and target ~= 0xFFFFFFFF) then
-                        local shot_fired = read_float(DyN + 0x490)
-                        if (shot_fired ~= v.weapon_state and shot_fired == 1) then
-                            v.target_object = get_object_memory(target)
-                        end
-                        v.weapon_state = shot_fired
+                    if (success and target ~= 0xFFFFFFFF and shot_fired) then
+                        v.target_object = get_object_memory(target)
                     end
                     --
 
                 else
 
+                    local obj = v.target_object
+
                     -- Distance from player:
                     local distance = self.distance
 
+                    local newX = px + distance * sin(xAim)
+                    local newY = py + distance * sin(yAim)
+                    local newZ = pz + distance * sin(zAim)
+
                     -- Write to vehicle x,y,z coordinates:
-                    write_float(v.target_object + 0x5C, px + distance * math.sin(xAim))
-                    write_float(v.target_object + 0x60, py + distance * math.sin(yAim))
-                    write_float(v.target_object + 0x64, pz + distance * math.sin(zAim))
+                    write_float(obj + 0x5C, newX)
+                    write_float(obj + 0x60, newY)
+                    write_float(obj + 0x64, newZ)
 
                     -- Update vehicle velocities:
-                    write_float(v.target_object + 0x68, 0) -- x vel
-                    write_float(v.target_object + 0x6C, 0) -- y vel
-                    write_float(v.target_object + 0x70, 0.01285) -- z vel
-                    write_float(v.target_object + 0x90, 0.15) -- yaw
-                    write_float(v.target_object + 0x8C, 0.15) -- pitch
-                    write_float(v.target_object + 0x94, 0.15) -- roll
+                    write_float(obj + 0x68, 0) -- x vel
+                    write_float(obj + 0x6C, 0) -- y vel
+                    write_float(obj + 0x70, 0.01285) -- z vel
+
+                    write_float(obj + 0x90, self.yaw) -- yaw
+                    write_float(obj + 0x8C, self.pitch) -- pitch
+                    write_float(obj + 0x94, self.roll) -- roll
 
                     -- Update vehicle physics:
-                    write_bit(v.target_object + 0x10, 0, 0) -- Unset noCollisions bit.
-                    write_bit(v.target_object + 0x10, 5, 0) -- Unset ignorePhysics.
+                    write_bit(obj + 0x10, 0, 0) -- Unset noCollisions bit.
+                    write_bit(obj + 0x10, 5, 0) -- Unset ignorePhysics.
+
+                    -- Launch vehicle:
+                    if (shot_fired) then
+
+                        local vel = self.launch_velocity
+
+                        write_float(obj + 0x68, vel * sin(xAim)) -- x vel
+                        write_float(obj + 0x6C, vel * sin(yAim)) -- y vel
+                        write_float(obj + 0x70, vel * sin(zAim)) -- z vel
+
+                        v.target_object = nil
+                    end
                 end
             end
         end
