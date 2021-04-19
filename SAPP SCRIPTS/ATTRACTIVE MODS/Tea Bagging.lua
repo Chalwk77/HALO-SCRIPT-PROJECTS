5f35135e7d22bdf304e8372a6e668601d6268c82
jericho <jericho.crosby227@gmail.com>
2021-04-19 13:36:47 +1200

    Refactored this script (tea bagging)
    Signed-off-by: jericho <jericho.crosby227@gmail.com>

--- SAPP SCRIPTS/ATTRACTIVE MODS/Tea Bagging.lua
+++ SAPP SCRIPTS/ATTRACTIVE MODS/Tea Bagging.lua
@@ -16,77 +16,114 @@ https://github.com/Chalwk77/Halo-Scripts-Phasor-V2-/blob/master/LICENSE
 api_version = "1.12.0.0"
 
 -- Configuration Starts --
+local TBag = {
 
--- Output: "Chalwk is dancing on FIG-Noob's body!"
-local on_tbag = "%name% is dancing on %victim%'s body!"
+    -- Message sent when a player is t-bagging
+    --
+    on_tbag = "%name% is lap-dancing on %victim%'s body!",
 
--- Radius (in world units) a player must be from a victim's corpse to trigger a t-bag:
-local trigger_radius = 2
+    -- Radius (in w/units) a player must be from a victim's corpse to trigger a t-bag:
+    --
+    radius = 2.5,
 
--- A player's death coordinates expire after this many seconds:
-local coordinate_expiration = 60
+    -- A player's death coordinates expire after this many seconds:
+    --
+    coordinate_expiration = 60,
 
--- A player must crouch over a victim's corpse this many times in order to trigger the t-bag:
-local crouch_count = 3
--- Configuration Ends --
+    -- A player must crouch over a victim's corpse this many times in order to trigger the t-bag:
+    --
+    crouch_count = 4,
+    -- Configuration Ends --
+}
 
-local players = { }
 local time_scale = 1 / 30
 local sqrt, gsub = math.sqrt, string.gsub
 
 function OnScriptLoad()
     register_callback(cb["EVENT_TICK"], "OnTick")
     register_callback(cb["EVENT_DIE"], "OnPlayerDeath")
+    register_callback(cb["EVENT_SPAWN"], "OnPlayerSpawn")
     register_callback(cb["EVENT_JOIN"], "OnPlayerConnect")
+    register_callback(cb["EVENT_GAME_START"], "OnGameStart")
     register_callback(cb["EVENT_LEAVE"], "OnPlayerDisconnect")
+    TBag:Init()
+end
+
+function OnGameStart()
+    TBag:Init()
+end
+
+function TBag:Init()
     if (get_var(0, "$gt") ~= "n/a") then
-        players = { }
+        self.players = { }
         for i = 1, 16 do
             if player_present(i) then
-                InitPlayer(i, false)
+                TBag:InitPlayer(i, false)
             end
         end
     end
 end
 
-local function GetRadius(pX, pY, pZ, X, Y, Z)
-    if (pX) then
-        return sqrt((pX - X) ^ 2 + (pY - Y) ^ 2 + (pZ - Z) ^ 2)
-    end
-    return nil
+function TBag:InProximity(pX, pY, pZ, X, Y, Z)
+    return sqrt((pX - X) ^ 2 + (pY - Y) ^ 2 + (pZ - Z) ^ 2) <= self.radius
 end
 
-function OnTick()
-    for i = 1, 16 do
-        if player_present(i) then
-            for j, ply in pairs(players) do
-                if (i ~= j and ply.coords) then
+function TBag:Monitor()
 
-                    local pos = GetXYZ(i)
-                    if (pos) and (not pos.invehicle) then
-                        local px, py, pz = pos.x, pos.y, pos.z
+    -- 1st Loop (tea baggers)
+    --
+    for i, itab in pairs(self.players) do
 
-                        for k, v in pairs(ply.coords) do
-                            v.timer = v.timer + time_scale
-                            if (v.timer >= coordinate_expiration) then
-                                ply.coords[k] = nil
-                            else
+        -- 2nd Loop (victims)
+        --
+        for j, jtab in pairs(self.players) do
+            if (i ~= j) then
 
-                                local x, y, z = v.x, v.y, v.z
-                                local distance = GetRadius(px, py, pz, x, y, z)
-                                if (distance) and (distance <= trigger_radius) then
+                -- Loop through all victim coordinate tables:
+                --
 
-                                    local crouch = read_bit(pos.DyN + 0x208, 0)
-                                    if (crouch ~= players[i].crouch_state and crouch == 1) then
-                                        players[i].crouch_count = players[i].crouch_count + 1
+                for cIndex, CTab in pairs(jtab.coordinates) do
 
-                                    elseif (players[i].crouch_count >= crouch_count) then
-                                        players[i].crouch_count = 0
-                                        say_all(gsub(gsub(on_tbag, "%%name%%", players[i].name), "%%victim%%", ply.name))
-                                        ply.coords[k] = nil
-                                    end
-                                    players[i].crouch_state = crouch
+                    -- increment expiration timer:
+                    --
+                    CTab.timer = CTab.timer + time_scale
+
+                    -- Delete coordinate table on expire:
+                    --
+
+                    if (CTab.timer >= self.coordinate_expiration) then
+                        jtab.coordinates[cIndex] = nil
+                    else
+
+                        -- Get x,y,z position of tea bagger:
+                        local i_pos = self:GetXYZ(i)
+                        if (i_pos and not i_pos.in_vehicle) then
+
+                            -- tea bagger coordinates:
+                            local px, py, pz = i_pos.x, i_pos.y, i_pos.z
+                            --
+
+                            -- corpse coordinates:
+                            local x, y, z = CTab.x, CTab.y, CTab.z
+                            --
+
+                            -- Check if tea bagger is within proximity of victim's corpse:
+                            if self:InProximity(px, py, pz, x, y, z) then
+
+                                -- Check if player is crouching & increment crouch count:
+                                local crouch = read_bit(i_pos.dyn + 0x208, 0)
+                                if (crouch ~= itab.crouch_state and crouch == 1) then
+                                    itab.crouch_count = itab.crouch_count + 1
+
+
+                                    -- Broadcast tea bag message:
+                                    --
+                                elseif (itab.crouch_count >= self.crouch_count) then
+                                    say_all(gsub(gsub(self.on_tbag, "%%name%%", itab.name), "%%victim%%", jtab.name))
+                                    itab.crouch_count = 0
+                                    jtab.coordinates[cIndex] = nil
                                 end
+                                itab.crouch_state = crouch
                             end
                         end
                     end
@@ -96,53 +133,67 @@ function OnTick()
     end
 end
 
-function OnPlayerDeath(V, _)
-    local victim = tonumber(V)
-    local pos = GetXYZ(victim)
-    if (pos) then
-        players[victim].coords[#players[victim].coords + 1] = {
-            timer = 0, x = pos.x, y = pos.y, z = pos.z,
-        }
+function TBag:OnDeath(Ply)
+    local pos = self:GetXYZ(Ply)
+    if (pos and self.players[Ply]) then
+        table.insert(self.players[Ply].coordinates, {
+            timer = 0,
+            x = pos.x,
+            y = pos.y,
+            z = pos.z
+        })
     end
 end
 
 function OnPlayerConnect(Ply)
-    InitPlayer(Ply, false)
+    TBag:InitPlayer(Ply, false)
 end
 
 function OnPlayerDisconnect(Ply)
-    InitPlayer(Ply, true)
+    TBag:InitPlayer(Ply, true)
 end
 
-function InitPlayer(Ply, Reset)
+function OnPlayerSpawn(Ply)
+    TBag.players[Ply].crouch_state = 0
+    TBag.players[Ply].crouch_count = 0
+end
+
+function TBag:InitPlayer(Ply, Reset)
     if (not Reset) then
-        players[Ply] = {
-            coords = { },
-            name = get_var(Ply, "$name"),
-            crouch_state = 0, crouch_count = 0,
+        self.players[Ply] = {
+            coordinates = { },
+            name = get_var(Ply, "$name")
         }
-    else
-        players[Ply] = nil
+        return
     end
+    self.players[Ply] = nil
 end
 
-function GetXYZ(Ply)
-    local coords, x, y, z = { }
+function TBag:GetXYZ(Ply)
+    local pos = { }
     local DyN = get_dynamic_player(Ply)
-    if (DyN ~= 0) then
+    if (player_alive(Ply) and DyN ~= 0) then
+        pos.dyn = DyN
         local VehicleID = read_dword(DyN + 0x11C)
+        local VObject = get_object_memory(VehicleID)
         if (VehicleID == 0xFFFFFFFF) then
-            coords.invehicle = false
-            x, y, z = read_vector3d(DyN + 0x5c)
-        else
-            coords.invehicle = true
-            x, y, z = read_vector3d(get_object_memory(VehicleID) + 0x5c)
+            pos.in_vehicle = false
+            pos.x, pos.y, pos.z = read_vector3d(DyN + 0x5c)
+        elseif (VObject ~= 0) then
+            pos.in_vehicle = true
+            pos.x, pos.y, pos.z = read_vector3d(VObject + 0x5c)
         end
-        coords.x, coords.y, coords.z, coords.DyN = x, y, z, DyN
     end
-    return coords
+    return pos
 end
 
 function OnScriptUnload()
+    -- N/A
+end
 
+function OnTick()
+    return TBag:Monitor()
+end
+function OnPlayerDeath(P)
+    return TBag:OnDeath(P)
 end
