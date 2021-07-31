5311352f6e2f024de4e850b14f3094150c07f3c0
jericho <jericho.crosby227@gmail.com>
2021-07-31 13:54:25 +1200

    ...

--- Miscellaneous/Name Replacer.lua
+++ Miscellaneous/Name Replacer.lua
@@ -2,9 +2,9 @@ api_version = "1.12.0.0"
 
 local blacklist = {
     { "Hacker", "bobby" },
-    { "{BK} DIEGO", "sandwich" },
     { "TᑌᗰᗷᗩᑕᑌᒪOᔕ", "salmon" },
     { "TUMBACULOS", "tuna" },
+    { "{BK} DIEGO", "sandwich" },
 }
 
 local network_struct
@@ -14,29 +14,34 @@ function OnScriptLoad()
     network_struct = read_dword(sig_scan("F3ABA1????????BA????????C740??????????E8????????668B0D") + 3)
 end
 
-local function WriteWideString(address, str, len)
-    local Count = 0
-    for _ = 1, len do
-        write_byte(address + Count, 0)
-        Count = Count + 2
-    end
-    Count = 0
-    local length = str:len()
-    for i = 1, length do
-        local newbyte = string.byte(str:sub(i, i))
-        write_byte(address + Count, newbyte)
-        Count = Count + 2
-    end
-end
-
 function OnPreJoin(Ply)
     local name_on_join = get_var(Ply, "$name")
     for i = 1, #blacklist do
+
         local blacklisted_name = blacklist[i][1]
         local replacement_name = blacklist[i][2]
+
         if (name_on_join == blacklisted_name) then
-            local ns = network_struct + 0x1AA + 0x40 + to_real_index(Ply) * 0x20
-            WriteWideString(ns, replacement_name:sub(1, 11), 12)
+
+            local count = 0
+            local address = network_struct + 0x1AA + 0x40 + to_real_index(Ply) * 0x20
+
+            for _ = 1, 12 do
+                write_byte(address + count, 0)
+                count = count + 2
+            end
+
+            count = 0
+
+            local str = replacement_name:sub(1, 11)
+            local length = str:len()
+
+            for j = 1, length do
+                local new_byte = string.byte(str:sub(j, j))
+                write_byte(address + count, new_byte)
+                count = count + 2
+            end
+
             break
         end
     end
