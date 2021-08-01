b10c2f2a84b66ae00cedd8d071670ae6f1a5195c
jericho <jericho.crosby227@gmail.com>
2021-08-01 21:43:13 +1200

    A few tweaks

--- Miscellaneous/Name Replacer.lua
+++ Miscellaneous/Name Replacer.lua
@@ -1,28 +1,146 @@
 api_version = "1.12.0.0"
 
-local blacklist = {
-    -- from -> to
-    { "Hacker", "bobby" },
-    { "TᑌᗰᗷᗩᑕᑌᒪOᔕ", "salmon" },
-    { "TUMBACULOS", "tuna" },
-    { "{BK} DIEGO", "sandwich" },
+local NameReplacer = {
+
+    -----
+    -- When a player joins the server, their name is cross-checked against the blacklist.
+    -- If a match is made, their name will be changed to a random name from the below names table.
+    -----
+
+    blacklist = {
+        "Hacker",
+        "{BK} DIEGO",
+        "TᑌᗰᗷᗩᑕᑌᒪOᔕ",
+        "TUMBACULOS",
+    },
+
+    names = {
+        { "iLoveAG" },
+        { "iLoveV3" },
+        { "loser4Eva" },
+        { "iLoveChalwk" },
+        { "iLoveSe7en" },
+        { "iLoveAussie" },
+        { "benDover" },
+        { "clitEruss" },
+        { "tinyDick" },
+        { "cumShot" },
+        { "PonyGirl" },
+        { "iAmGroot" },
+        { "twi$t3d" },
+        { "maiBahd" },
+        { "frown" },
+        { "Laugh@me" },
+        { "imaDick" },
+        { "facePuncher" },
+        { "TEN" },
+        { "whatElse" },
+
+    }
 }
 
 local network_struct
 
 function OnScriptLoad()
+
     register_callback(cb["EVENT_PREJOIN"], "OnPreJoin")
+    register_callback(cb["EVENT_LEAVE"], "OnPlayerQuit")
+
+    register_callback(cb["EVENT_GAME_START"], "OnGameStart")
+
     network_struct = read_dword(sig_scan("F3ABA1????????BA????????C740??????????E8????????668B0D") + 3)
+    OnGameStart()
 end
 
-function OnPreJoin(Ply)
+local function CheckNameExists(Ply)
+    local name = get_var(Ply, "$name")
+    for j = 1, #self.names do
+        if (name == self.names[j][1]) then
+            self.names[j][1].used = true
+        end
+    end
+end
+
+function OnGameStart()
+    if (get_var(0, "$gt") ~= "n/a") then
+
+        NameReplacer.players = { }
+
+        for i = 1, #NameReplacer.names do
+            NameReplacer.names[i].used = false
+        end
+
+        for i = 1, 16 do
+            if player_present(i) then
+
+                NameReplacer:InitPlayer(i, true)
+
+                CheckNameExists(i)
+            end
+        end
+    end
+end
+
+function NameReplacer:InitPlayer(Ply, Reset)
+
+    if (not Reset) then
+
+        CheckNameExists(Ply)
+
+        self.players[Ply] = { n_id = nil }
+        return
+    end
+
+    self.players[Ply] = nil
+end
+
+function OnPlayerQuit(Ply)
+
+    Ply = NameReplacer.players[Ply]
+    if (Ply ~= nil) then
+        NameReplacer.names[Ply.n_id].used = false
+    end
+
+    NameReplacer:InitPlayer(Ply, true)
+end
+
+function NameReplacer:GetRandomName(Ply)
+
+    if (self.players[Ply]) then
+
+        local t = { }
+        for i = 1, #self.names do
+            if (self.names[i][1]:len() < 12 and not self.names[i].used) then
+                t[#t + 1] = { self.names[i][1], i }
+            end
+        end
+
+        if (#t > 0) then
+
+            local rand = rand(1, #t - 1)
+            local name = t[rand][1]
+            local n_id = t[rand][2]
+
+            self.names[n_id].used = true
+            self.players[Ply].n_id = n_id
+
+            return name
+        end
+    end
+
+    return "no name"
+end
+
+function NameReplacer:PreJoin(Ply)
+
+    self:InitPlayer(Ply, false)
+
     local name_on_join = get_var(Ply, "$name")
-    for i = 1, #blacklist do
+    for _, name in pairs(self.blacklist) do
 
-        local blacklisted_name = blacklist[i][1]
-        local replacement_name = blacklist[i][2]
+        if (name_on_join == name) then
 
-        if (name_on_join == blacklisted_name) then
+            local new_name = self:GetRandomName(Ply)
 
             local count = 0
             local address = network_struct + 0x1AA + 0x40 + to_real_index(Ply) * 0x20
@@ -34,7 +152,7 @@ function OnPreJoin(Ply)
 
             count = 0
 
-            local str = replacement_name:sub(1, 11)
+            local str = new_name:sub(1, 11)
             local length = str:len()
 
             for j = 1, length do
@@ -47,3 +165,9 @@ function OnPreJoin(Ply)
         end
     end
 end
+
+function OnPreJoin(Ply)
+    return NameReplacer:PreJoin(Ply)
+end
+
+return NameReplacer
