a7203d1eb398b390377dad15c443ab7b57ec792f
jericho <jericho.crosby227@gmail.com>
2021-08-01 22:19:14 +1200

    A few tweaks

--- Miscellaneous/Name Replacer.lua
+++ Miscellaneous/Name Replacer.lua
@@ -1,19 +1,37 @@
+--[[
+--=====================================================================================================--
+Script Name: Name Ban, for SAPP (PC & CE)
+Description: Players have names that you don't like? No problem!
+
+NOTE: This is not an anti-impersonator script!
+
+Copyright (c) 2021, Jericho Crosby <jericho.crosby227@gmail.com>
+* Notice: You can use this document subject to the following conditions:
+https://github.com/Chalwk77/HALO-SCRIPT-PROJECTS/blob/master/LICENSE
+--=====================================================================================================--
+]]--
+
 api_version = "1.12.0.0"
 
-local NameReplacer = {
+local NameBan = {
 
-    -----
-    -- When a player joins the server, their name is cross-checked against the blacklist.
-    -- If a match is made, their name will be changed to a random name from the below names table.
-    -----
+    -- When a player joins the server, their name is cross-checked against the BLACKLIST TABLE.
+    -- If a match is made, their name will be changed to a random name from the below NAMES TABLE.
 
+
+    --
+    -- BLACKLIST TABLE:
+    --
     blacklist = {
         "Hacker",
-        "{BK} DIEGO",
         "TᑌᗰᗷᗩᑕᑌᒪOᔕ",
         "TUMBACULOS",
     },
 
+
+    --
+    -- NAMES TABLE:
+    --
     names = {
         { "iLoveAG" },
         { "iLoveV3" },
@@ -36,6 +54,9 @@ local NameReplacer = {
         { "TEN" },
         { "whatElse" },
 
+        -- repeat the structure to add more entries
+        --
+        --
     }
 }
 
@@ -52,7 +73,7 @@ function OnScriptLoad()
     OnGameStart()
 end
 
-local function CheckNameExists(Ply)
+function NameBan:CheckNameExists(Ply)
     local name = get_var(Ply, "$name")
     for j = 1, #self.names do
         if (name == self.names[j][1]) then
@@ -64,76 +85,57 @@ end
 function OnGameStart()
     if (get_var(0, "$gt") ~= "n/a") then
 
-        NameReplacer.players = { }
+        NameBan.players = { }
 
-        for i = 1, #NameReplacer.names do
-            NameReplacer.names[i].used = false
+        for i = 1, #NameBan.names do
+            NameBan.names[i].used = false
         end
 
         for i = 1, 16 do
             if player_present(i) then
-
-                NameReplacer:InitPlayer(i, true)
-
-                CheckNameExists(i)
+                NameBan:CheckNameExists(i)
             end
         end
     end
-end
-
-function NameReplacer:InitPlayer(Ply, Reset)
-
-    if (not Reset) then
-
-        CheckNameExists(Ply)
-
-        self.players[Ply] = { n_id = nil }
-        return
-    end
-
-    self.players[Ply] = nil
 end
 
 function OnPlayerQuit(Ply)
 
-    Ply = NameReplacer.players[Ply]
+    Ply = NameBan.players[Ply]
     if (Ply ~= nil) then
-        NameReplacer.names[Ply.n_id].used = false
+        NameBan.names[Ply].used = false
     end
 
-    NameReplacer:InitPlayer(Ply, true)
+    NameBan.players[Ply] = nil
 end
 
-function NameReplacer:GetRandomName(Ply)
+function NameBan:GetRandomName(Ply)
 
-    if (self.players[Ply]) then
-
-        local t = { }
-        for i = 1, #self.names do
-            if (self.names[i][1]:len() < 12 and not self.names[i].used) then
-                t[#t + 1] = { self.names[i][1], i }
-            end
+    local t = { }
+    for i = 1, #self.names do
+        if (self.names[i][1]:len() < 12 and not self.names[i].used) then
+            t[#t + 1] = { self.names[i][1], i }
         end
+    end
 
-        if (#t > 0) then
+    if (#t > 0) then
 
-            local rand = rand(1, #t - 1)
-            local name = t[rand][1]
-            local n_id = t[rand][2]
+        local rand = rand(1, #t - 1)
+        local name = t[rand][1]
+        local n_id = t[rand][2]
 
-            self.names[n_id].used = true
-            self.players[Ply].n_id = n_id
+        self.players[Ply] = n_id
+        self.names[n_id].used = true
 
-            return name
-        end
+        return name
     end
 
     return "no name"
 end
 
-function NameReplacer:PreJoin(Ply)
+function NameBan:PreJoin(Ply)
 
-    self:InitPlayer(Ply, false)
+    self:CheckNameExists(Ply)
 
     local name_on_join = get_var(Ply, "$name")
     for _, name in pairs(self.blacklist) do
@@ -167,7 +169,7 @@ function NameReplacer:PreJoin(Ply)
 end
 
 function OnPreJoin(Ply)
-    return NameReplacer:PreJoin(Ply)
+    return NameBan:PreJoin(Ply)
 end
 
-return NameReplacer
+return NameBan
