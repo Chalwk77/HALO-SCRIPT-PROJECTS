6c75aecbb64f90328336cf6927681029fe779f0e
jericho <jericho.crosby227@gmail.com>
2021-08-21 09:48:01 +1200

    For Luis
    Signed-off-by: jericho <jericho.crosby227@gmail.com>

---
+++ SAPP SCRIPTS/ATTRACTIVE MODS/Headshot Blocker.lua
@@ -0,0 +1,20 @@
+-- Script: Headshot Blocker
+-- Author: Chalwk
+-- Version: 1.0
+
+api_version = "1.12.0.0"
+
+function OnScriptLoad()
+    register_callback(cb["EVENT_DAMAGE_APPLICATION"], "BlockHeadshots")
+end
+
+function BlockHeadshots(Victim, Killer, _, _, HitString, _)
+    local k, v = tonumber(Killer), tonumber(Victim)
+    if (k > 0 and k ~= v and HitString == "head") then
+        return false
+    end
+end
+
+function OnScriptUnload()
+
+end
