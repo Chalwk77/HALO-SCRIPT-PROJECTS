9a6db2579fef9b677a17cf77d673204bca6911e8
Chalwk77 <jericho.crosby227@gmail.com>
2022-01-06 22:46:50 +1300

    ......

--- SAPP SCRIPTS/UTILITY MODS/Name Replacer.lua
+++ SAPP SCRIPTS/UTILITY MODS/Name Replacer.lua
@@ -9,7 +9,7 @@ Description: Change blacklisted names into something funny!
              As random names get assigned, they become marked as "used" until the player quits the server.
              This is to prevent someone else from being assigned the same random name.
 
-Copyright (c) 2021, Jericho Crosby <jericho.crosby227@gmail.com>
+Copyright (c) 2022, Jericho Crosby <jericho.crosby227@gmail.com>
 * Notice: You can use this document subject to the following conditions:
 https://github.com/Chalwk77/HALO-SCRIPT-PROJECTS/blob/master/LICENSE
 --=====================================================================================================--
@@ -57,11 +57,9 @@ local NameReplacer = {
         "Sneak",
         "Stompy",
         "Stumpy",
-        "TUMBACULOS",
         "The Bear",
         "The Big L",
         "Tooth",
-        "TᑌᗰᗷᗩᑕᑌᒪOᔕ",
         "Walla Walla",
         "Weasel",
         "Wheezy",
@@ -74,27 +72,47 @@ local NameReplacer = {
     -- NAMES TABLE:
     --
     random_names = {
-        { "iLoveAG" },
-        { "iLoveV3" },
-        { "loser4Eva" },
-        { "iLoveChalwk" },
-        { "iLoveSe7en" },
-        { "iLoveAussie" },
-        { "benDover" },
-        { "clitEruss" },
-        { "tinyDick" },
-        { "cumShot" },
-        { "PonyGirl" },
-        { "iAmGroot" },
-        { "twi$t3d" },
-        { "maiBahd" },
-        { "frown" },
-        { "Laugh@me" },
-        { "imaDick" },
-        { "facePuncher" },
-        { "TEN" },
-        { "whatElse" },
-
+        { "Liam" },
+        { "Noah" },
+        { "Oliver" },
+        { "Elijah" },
+        { "William" },
+        { "James" },
+        { "Benjamin" },
+        { "Lucas" },
+        { "Henry" },
+        { "Alexander" },
+        { "Mason" },
+        { "Michael" },
+        { "Ethan" },
+        { "Daniel" },
+        { "Jacob" },
+        { "Logan" },
+        { "Jackson" },
+        { "Levi" },
+        { "Sebastian" },
+        { "Mateo" },
+        { "Jack" },
+        { "Owen" },
+        { "Theodore" },
+        { "Aiden" },
+        { "Samuel" },
+        { "Joseph" },
+        { "John" },
+        { "David" },
+        { "Wyatt" },
+        { "Matthew" },
+        { "Luke" },
+        { "Asher" },
+        { "Carter" },
+        { "Julian" },
+        { "Grayson" },
+        { "Leo" },
+        { "Jayden" },
+        { "Gabriel" },
+        { "Isaac" },
+        { "Lincoln" },
+        { "Anthony" },
         -- repeat the structure to add more entries
         --
     }
@@ -106,49 +124,46 @@ local network_struct
 
 function OnScriptLoad()
 
+    register_callback(cb["EVENT_LEAVE"], "OnQuit")
     register_callback(cb["EVENT_PREJOIN"], "OnPreJoin")
-    register_callback(cb["EVENT_LEAVE"], "OnPlayerQuit")
 
-    register_callback(cb["EVENT_GAME_START"], "OnGameStart")
+    register_callback(cb["EVENT_GAME_START"], "OnStart")
 
     network_struct = read_dword(sig_scan("F3ABA1????????BA????????C740??????????E8????????668B0D") + 3)
 
-    OnGameStart()
+    OnStart()
 end
 
-function OnGameStart()
+function NameReplacer:Init()
     if (get_var(0, "$gt") ~= "n/a") then
 
-        NameReplacer.players = { }
+        self.players = { }
 
         -- Set all names to "unused" by default:
         --
-        for _, v in pairs(NameReplacer.random_names) do
+        for _, v in pairs(self.random_names) do
             v.used = false
         end
 
-        -- This block of code is executed if:
-        -- 1). SAPP is loaded/reloaded
-        -- 2). The script is loaded manually
-        --
         for i = 1, 16 do
             if player_present(i) then
-                NameReplacer:CheckNameExists(i)
+                self:CheckNameExists(i)
             end
         end
     end
 end
 
-function OnPlayerQuit(Ply)
+function NameReplacer:OnQuit(Ply)
 
     -- Mark name as "unused":
     --
-    local id = NameReplacer.players[Ply]
+    local id = self.players[Ply]
+
     if (id ~= nil) then
-        NameReplacer.random_names[id].used = false
+        self.random_names[id].used = false
     end
 
-    NameReplacer.players[Ply] = nil
+    self.players[Ply] = nil
 end
 
 function NameReplacer:GetRandomName(Ply)
@@ -162,16 +177,13 @@ function NameReplacer:GetRandomName(Ply)
         end
     end
 
+    --
     -- Pick random name candidate:
     if (#t > 0) then
-        math.randomseed(os.clock())
-        math.random();
-        math.random();
-        math.random();
-        local rand = math.random(1, #t)
 
-        local name = t[rand][1]
-        local n_id = t[rand][2] -- table index from names
+        local n = rand(1, #t + 1)
+        local name = t[n][1]
+        local n_id = t[n][2] -- table index from names
 
         self.players[Ply] = n_id
         self.random_names[n_id].used = true
@@ -179,7 +191,14 @@ function NameReplacer:GetRandomName(Ply)
         return name
     end
 
-    return "no name"
+    -- If the script was unable to pick a random name,
+    -- generate a random 11 character
+    local name = ""
+    for _ = 1, math.random(1, 11) do
+        name = name .. string.char(math.random(97, 122))
+    end
+
+    return name
 end
 
 function NameReplacer:PreJoin(Ply)
@@ -231,6 +250,14 @@ end
 
 function OnPreJoin(Ply)
     return NameReplacer:PreJoin(Ply)
+end
+
+function OnStart()
+    NameReplacer:Init()
+end
+
+function OnQuit(Ply)
+    NameReplacer:OnQuit(Ply)
 end
 
 return NameReplacer
