2a71ded797054d0619d5e60d97bf79c0126c9c74
Chalwk77 <jericho.crosby227@gmail.com>
2022-01-12 16:52:38 +1300

    ...

--- Miscellaneous/Open Carnage/Vehicle Spawner.lua
+++ Miscellaneous/Open Carnage/Vehicle Spawner.lua
@@ -7,6 +7,10 @@ Description: A custom vehicle spawner that works on any game mode.
              spawn_object() on race game modes - they do not respawn at the origin x,y,z.
              This fixes that.
 
+             NOTE:
+             If you're using this on any RACE game mode,
+             set the game type flag "VEHICLE RESPAWN TIME" to NEVER.
+
 
 Copyright (c) 2022, Jericho Crosby <jericho.crosby227@gmail.com>
 * Notice: You can use this document subject to the following conditions:
@@ -23,17 +27,74 @@ https://github.com/Chalwk77/HALO-SCRIPT-PROJECTS/blob/master/LICENSE
 -- that an unoccupied vehicle must be (or greater) before the respawn timer for that vehicle is triggered.
 --
 local vehicles = {
-    { "vehi", "vehicles\\warthog\\mp_warthog", 47.15, -79.28, 0.12, 0, 30, 1 },
+    ["bloodgulch"] = {
+        { "vehi", "vehicles\\warthog\\mp_warthog", 47.15, -79.28, 0.12, 0, 30, 1 },
+    },
+    ["deathisland"] = {
+        { "vehi", "", 0, 0, 0, 0, 30, 1 },
+    },
+    ["icefields"] = {
+        { "vehi", "", 0, 0, 0, 0, 30, 1 },
+    },
+    ["infinity"] = {
+        { "vehi", "", 0, 0, 0, 0, 30, 1 },
+    },
+    ["sidewinder"] = {
+        { "vehi", "", 0, 0, 0, 0, 30, 1 },
+    },
+    ["timberland"] = {
+        { "vehi", "", 0, 0, 0, 0, 30, 1 },
+    },
+    ["dangercanyon"] = {
+        { "vehi", "", 0, 0, 0, 0, 30, 1 },
+    },
+    ["beavercreek"] = {
+        { "vehi", "", 0, 0, 0, 0, 30, 1 },
+    },
+    ["boardingaction"] = {
+        { "vehi", "", 0, 0, 0, 0, 30, 1 },
+    },
+    ["carousel"] = {
+        { "vehi", "", 0, 0, 0, 0, 30, 1 },
+    },
+    ["chillout"] = {
+        { "vehi", "", 0, 0, 0, 0, 30, 1 },
+    },
+    ["damnation"] = {
+        { "vehi", "", 0, 0, 0, 0, 30, 1 },
+    },
+    ["gephyrophobia"] = {
+        { "vehi", "", 0, 0, 0, 0, 30, 1 },
+    },
+    ["hangemhigh"] = {
+        { "vehi", "", 0, 0, 0, 0, 30, 1 },
+    },
+    ["longest"] = {
+        { "vehi", "", 0, 0, 0, 0, 30, 1 },
+    },
+    ["prisoner"] = {
+        { "vehi", "", 0, 0, 0, 0, 30, 1 },
+    },
+    ["putput"] = {
+        { "vehi", "", 0, 0, 0, 0, 30, 1 },
+    },
+    ["ratrace"] = {
+        { "vehi", "", 0, 0, 0, 0, 30, 1 },
+    },
+    ["wizard"] = {
+        { "vehi", "", 0, 0, 0, 0, 30, 1 },
+    },
 }
 -- config ends --
 
 api_version = "1.12.0.0"
 
+local object_spawns
+
 -- Register needed event callbacks:
 --
 function OnScriptLoad()
     register_callback(cb["EVENT_GAME_START"], "OnStart")
-    register_callback(cb["EVENT_TICK"], "GameTick")
     OnStart()
 end
 
@@ -87,12 +148,20 @@ end
 -- Respawn a vehicle that has moved from its starting location:
 -- Called every 1/30th second.
 --
+
 function GameTick()
-    for _, v in pairs(vehicles) do
+    for _, v in pairs(object_spawns) do
         local object = get_object_memory(v.vehicle)
         if (object ~= 0 and not Occupied(v)) then
+
             local x, y, z = read_vector3d(object + 0x5C)
-            local dist = GetDist(x, y, z, v[3], v[4], v[5])
+            local fx, fy, fz = 40.240600585938, -79.123199462891, -0.10000000149012
+            local dist = GetDist(x, y, z, fx, fy, fz)
+            if (dist <= 3) then
+                return SpawnVehicle(v)
+            end
+
+            dist = GetDist(x, y, z, v[3], v[4], v[5])
             v.timer = (dist > v[8] and v.timer + 1 / 30) or 0
             if (v.timer >= v[7]) then
                 SpawnVehicle(v)
@@ -105,10 +174,21 @@ end
 -- Responsible for spawning all vehicles initially.
 --
 function OnStart()
+
+    object_spawns = nil
+
     if (get_var(0, "$gt") ~= "n/a") then
-        for _, v in pairs(vehicles) do
-            SpawnVehicle(v)
+        local map = get_var(0, "$map")
+        object_spawns = vehicles[map]
+        if (object_spawns) then
+            for _, v in pairs(object_spawns) do
+                SpawnVehicle(v)
+            end
+            register_callback(cb["EVENT_TICK"], "GameTick")
+            goto done
         end
+        unregister_callback(cb["EVENT_TICK"])
+        :: done ::
     end
 end
 
