16babc5461cd84ee4c70bf92195c73b0dbeba36b
Chalwk77 <jericho.crosby227@gmail.com>
2022-02-05 14:25:38 +1300

    ...

---
+++ Miscellaneous/VPS Setup Instructions.md
@@ -0,0 +1,120 @@
+# Prerequisite applications:
+
+[BitVise SSH Client](https://www.bitvise.com/ssh-client-download)
+
+[FileZilla Client](https://filezilla-project.org/download.php?platform=win64)
+
+## STEPS:
+
+### 1). Download and install "BitVise SSH Client" and "FileZilla Client".
+
+### 2). Install Ubuntu, Linux (version 21.10x64).
+
+### 3). Open BitVise SSH Client.
+
+- Fill in the host/username/password fields (get from VPS control panel).
+- Click the "New Terminal Window" button.
+
+### 4). Enter the following commands (in order):
+
+> Verify 64-bit architecture. The following command should respond with "amd64":
+
+- **dpkg --print-architecture**
+
+> See if 32-bit architecture is installed. The following command should respond with "i386":
+
+- dpkg --print-foreign-architectures
+
+> If "i386" is not displayed, execute the following:
+
+- sudo dpkg --add-architecture i386
+
+> Recheck with:
+
+- dpkg --print-foreign-architectures
+
+> Add the WineHQ Ubuntu repository.
+
+- wget -nc https://dl.winehq.org/wine-builds/winehq.key
+
+> Get and install the repository key.
+
+- sudo -H gpg -o /etc/apt/trusted.gpg.d/winehq.key.gpg --dearmor winehq.key
+
+> Add the repository.
+
+- sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ impish main'
+
+> Update the package database.
+
+- sudo apt update
+
+> Install Wine.
+> The next command will install Wine Stable. If you prefer Wine Development or Wine Staging, replace winehq-stable with either winehq-devel or with winehq-staging.
+
+- sudo apt install --install-recommends winehq-stable
+
+> Verify the installation succeeded
+
+- wine --version
+
+### 5). Installing TightVNC Server.
+
+> The graphical environment is not installed by default on server versions of Ubuntu. Therefore, if we want to connect to a remote desktop, we need to install a graphical shell. Let’s install the TightVNC Server itself at the same time.
+
+- apt install xfce4 xfce4-goodies tightvncserver
+
+> Let's start the TightVNC Server for the first time. It will create the files necessary for work and ask to create a password.
+> If you need to restrict remote desktop control, select a read-only password.
+
+- vncserver
+
+> Now stop your TightVNC session to adjust other settings.
+
+- vncserver -kill :1
+
+> Open the TightVNC config file with:
+
+- nano ~/.vnc/xstartup
+
+> Add the following line to the end:
+> startxfce4
+> Start the server again.
+
+- vncserver
+
+### 6). Setting up autorun.
+
+> By default, TightVNC does not have a daemon and does not turn on after a system reboot. To fix this, let's create a new unit in systemd.
+
+- nano /etc/systemd/system/vncserver.service
+
+> Insert the following config there:
+
+```diff
++ [Unit]
++ Description=TightVNC server
++ After=syslog.target network.target
+
++ [Service]
++ Type=forking
++ User=root
++ PAMName=login
++ PIDFile=/root/.vnc/%H:1.pid
++ ExecStartPre=-/usr/bin/vncserver -kill :1 > /dev/null 2>&1
++ ExecStart=/usr/bin/vncserver
++ ExecStop=/usr/bin/vncserver -kill :1
+
++ [Install]
++ WantedBy=multi-user.target
+```
+
+> Reload systemd:
+
+- systemctl daemon-reload
+
+> Enable autorun of the TightVNC server and start it.
+
+- systemctl enable --now vncserver
+
+## 6). Upload Server files.
---
+++ Miscellaneous/incomplete archive/Anti AFK.lua
@@ -0,0 +1,191 @@
+local AntiAFK = {
+
+    -- Time (in seconds) a player must be afk before the script will kick them:
+    --
+    max_afk_time = 15,
+
+    kick_reason = "AFK for too long!"
+}
+
+api_version = "1.12.0.0"
+
+function OnScriptLoad()
+    register_callback(cb["EVENT_TICK"], "OnTick")
+    register_callback(cb["EVENT_JOIN"], "OnJoin")
+    register_callback(cb["EVENT_LEAVE"], "OnQuit")
+    register_callback(cb["EVENT_SPAWN"], "OnSpawn")
+    register_callback(cb["EVENT_CHAT"], "ChatCommand")
+    register_callback(cb["EVENT_GAME_START"], "OnStart")
+    register_callback(cb["EVENT_PRESPAWN"], "OnPreSpawn")
+    register_callback(cb["EVENT_COMMAND"], "ChatCommand")
+end
+
+function OnStart()
+    if (get_var(0, '$gt') ~= "n/a") then
+
+        AntiAFK.players = { }
+
+        for i = 1, 16 do
+            if player_present(i) then
+                AntiAFK:InitPlayer(i)
+            end
+        end
+
+    end
+end
+
+function ChatCommand(Ply)
+    if (Ply > 0) then
+        AntiAFK.players[Ply].timer = 0
+    end
+end
+
+local function UpdateAim(DyN, Camera)
+    if (DyN ~= 0) then
+        Camera[1] = read_float(DyN + 0x230)
+        Camera[2] = read_float(DyN + 0x234)
+        Camera[3] = read_float(DyN + 0x238)
+    end
+end
+
+local function CheckInputs(DyN, Player)
+    if (DyN ~= 0) then
+        for _, input in pairs(Player.inputs) do
+
+            local func, address = input[1], input[2]
+            local state = func(DyN + address)
+
+            if (state ~= input.state and Player.update_aim) then
+                Player.timer = 0
+            end
+            input.state = state
+        end
+    end
+end
+
+local function NoAimChange(x1, y1, z1, x2, y2, z2)
+    return (x1 == x2 and y1 == y2 and z1 == z2)
+end
+
+function AntiAFK:OnTick()
+
+    for i, v in pairs(self.players) do
+
+        local DyN = get_dynamic_player(i)
+
+        CheckInputs(DyN, v)
+
+        -- update current aim:
+        UpdateAim(DyN, v.camera[1])
+
+        -- current Aim:
+        local x1 = v.camera[1][1]
+        local y1 = v.camera[1][2]
+        local z1 = v.camera[1][3]
+
+        -- old Aim:
+        local x2 = v.camera[2][1]
+        local y2 = v.camera[2][2]
+        local z2 = v.camera[2][3]
+
+        -- update old aim:
+        UpdateAim(DyN, v.camera[2])
+
+        local no_aim_change = NoAimChange(x1, y1, z1, x2, y2, z2)
+        local case = (no_aim_change or not v.update_aim)
+
+        v.timer = (case and v.timer + 1 / 30) or 0
+        print(v.timer)
+
+        if (v.timer >= self.max_afk_time) then
+            execute_command('k ' .. i .. ' "' .. self.kick_reason .. '"')
+        end
+    end
+end
+
+function AntiAFK:OnPreSpawn(Ply)
+    self.players[Ply].update_aim = false
+end
+
+function AntiAFK:OnSpawn(Ply, DelayUpdate)
+    Ply = tonumber(Ply)
+    if (not DelayUpdate) then
+        timer(500, "OnSpawn", Ply, 1)
+    else
+        self.players[Ply].update_aim = true
+    end
+end
+
+function AntiAFK:InitPlayer(Ply, Reset)
+    if (not Reset) then
+        self.players[Ply] = {
+            timer = 0,
+
+            update_aim = false,
+
+            camera = {
+
+                -- current:
+                { 0, 0, 0 },
+
+                -- old:
+                { 0, 0, 0 }
+            },
+
+            inputs = {
+
+                -- shooting:
+                { read_float, 0x490, state = 0 },
+
+                -- forward, backward, left, right, grenade throw:
+                { read_byte, 0x2A3, state = 0 },
+
+                -- weapon switch:
+                { read_byte, 0x47C, state = 0 },
+
+                -- grenade switch:
+                { read_byte, 0x47E, state = 0 },
+
+                -- weapon reload:
+                { read_byte, 0x2A4, state = 0 },
+
+                -- zoom:
+                { read_word, 0x480, state = 0 },
+
+                -- melee, flashlight, action, crouch, jump:
+                { read_word, 0x208, state = 0 }
+            }
+        }
+        return
+    end
+
+    self.players[Ply] = nil
+end
+
+function OnJoin(Ply)
+    AntiAFK:InitPlayer(Ply)
+end
+
+function OnQuit(Ply)
+    AntiAFK:InitPlayer(Ply, true)
+end
+
+function OnTick()
+    AntiAFK:OnTick()
+end
+
+function OnSpawn(P, U)
+    AntiAFK:OnSpawn(P, U)
+end
+
+function OnPreSpawn(P)
+    AntiAFK:OnPreSpawn(P)
+end
+
+function AimTick()
+    return AntiAFK:AimTick()
+end
+
+function OnScriptUnload()
+    -- N/A
+end
