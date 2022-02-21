55173f50f78dde011b4d51cf4ac1ebd4bd2578ae
Chalwk77 <jericho.crosby227@gmail.com>
2022-02-22 09:38:11 +1300

    refactored

--- SAPP SCRIPTS/UTILITY MODS/Admin Chat.lua
+++ SAPP SCRIPTS/UTILITY MODS/Admin Chat.lua
@@ -8,13 +8,11 @@ Description: This is a utility that allows you to chat privately with other admi
              When Admin Chat is enabled your messages will appear in the rcon console environment.
              Only admins will see these messages.
 
-Copyright (c) 2021, Jericho Crosby <jericho.crosby227@gmail.com>
+Copyright (c) 2022, Jericho Crosby <jericho.crosby227@gmail.com>
 * Notice: You can use this document subject to the following conditions:
 https://github.com/Chalwk77/HALO-SCRIPT-PROJECTS/blob/master/LICENSE
 --=====================================================================================================--
 ]] --
-
-api_version = "1.12.0.0"
 
 -- config starts --
 
@@ -29,44 +27,44 @@ local AdminChat = {
     -- Minimum permission needed to toggle admin chat for other players:
     permission_others = 4,
 
-    -- Fully customizable messages:
-    messages = {
-
-        -- Admin Chat output format:
-        [1] = "$name [$id]: $message",
-
-        -- This message is sent to (you) when you enable/disable for yourself.
-        [2] = "Admin Chat $state!",
-
-        -- This message is sent to (you) when you enable/disable for others.
-        [3] = "Admin Chat $state for $target_name",
-
-        -- This message is sent to (target player) when you enable/disable for them.
-        [4] = "Your Admin Chat was $state by $executor_name",
-
-        -- This message is sent to (you) when your Admin Chat is already enabled/disabled.
-        [5] = "Your Admin Chat is already $state!",
-
-        -- This message is sent to (target player) when their Admin Chat is already enabled/disabled.
-        [6] = "$target_name's Admin Chat is already $state!",
-
-        -- This message is sent when a player connects to the server (if previously activated).
-        -- This requires the 'restore' setting to be TRUE.
-        [7] = "Your Admin Chat is Enabled! (auto-restore)",
-
-        -- This message is sent to (you) when there is a command syntax error.
-        [8] = "Invalid Syntax: Usage: /$cmd on|off [me | id | */all]",
-
-        -- If command executors permission level is < permission_others, send this message:
-        [9] = "You lack permission to execute this command on other players",
-    },
-
     -- Should A-Chat be restored for returning players? (if previously activated)
     restore = true,
 
-    --
+    -------------------------------
+    -- Fully customizable messages:
+    -------------------------------
+
+    -- Admin Chat output format:
+    [1] = "$name [$id]: $str",
+
+    -- This message is sent to (you) when you enable/disable for yourself.
+    [2] = "Admin Chat $state!",
+
+    -- This message is sent to (you) when you enable/disable for others.
+    [3] = "Admin Chat $state for $target_name",
+
+    -- This message is sent to (target player) when you enable/disable for them.
+    [4] = "Your Admin Chat was $state by $executor_name",
+
+    -- This message is sent to (you) when your Admin Chat is already enabled/disabled.
+    [5] = "Your Admin Chat is already $state!",
+
+    -- This message is sent to (target player) when their Admin Chat is already enabled/disabled.
+    [6] = "$target_name's Admin Chat is already $state!",
+
+    -- This message is sent when a player connects to the server (if previously activated).
+    -- This requires the 'restore' setting to be TRUE.
+    [7] = "Your Admin Chat is Enabled! (auto-restore)",
+
+    -- This message is sent to (you) when there is a command syntax error.
+    [8] = "Invalid Syntax: Usage: /$cmd on|off [me | id | */all]",
+
+    -- If command executors permission level is < permission_others, send this message:
+    [9] = "You lack permission to execute this command on other players",
+
+    -----------------------
     -- Advanced users only:
-    --
+    -----------------------
 
     -- The table index for each client will either be "IP", or "IP:PORT".
     -- Set to 1 for ip-only indexing.
@@ -74,6 +72,8 @@ local AdminChat = {
     ClientIndexType = 2
 }
 -- config ends --
+
+api_version = "1.12.0.0"
 
 function OnScriptLoad()
 
@@ -109,30 +109,34 @@ function OnQuit(Ply)
 end
 
 local function STRSplit(CMD)
-    local Args = { }
+    local args = { }
 
     for Params in CMD:gsub('"', ""):gmatch("([^%s]+)") do
-        Args[#Args + 1] = Params:lower()
+        args[#args + 1] = Params:lower()
     end
 
-    return Args
+    return args
 end
 
-function AdminChat:IsAdmin(Ply, CMD)
+local function Respond(Ply, Msg)
+    return (Ply == 0 and cprint(Msg) or rprint(Ply, Msg))
+end
+
+local function IsAdmin(Ply, CMD)
 
     local lvl = tonumber(get_var(Ply, "$lvl"))
-    if (lvl >= self.permission) or (Ply == 0) then
+    if (lvl >= AdminChat.permission) or (Ply == 0) then
         return true, lvl
     end
 
     -- Return false if player is not admin.
     -- Send player Insufficient Permission message if callback came from OnCommand()
-    return false, (CMD and self:Respond(Ply, "Insufficient Permission", 10))
+    return false, (CMD and Respond(Ply, "Insufficient Permission", 10))
 end
 
 function AdminChat:InitPlayer(Ply, Reset)
 
-    if (self:IsAdmin(Ply)) then
+    if (IsAdmin(Ply)) then
 
         local ip = self:GetIP(Ply)
         if (not Reset) then
@@ -141,45 +145,80 @@ function AdminChat:InitPlayer(Ply, Reset)
 
             -- Restores this player Admin Chat:
             if (self.restore) and (self[ip]) then
-                self:Respond(Ply, self.messages[7])
+                Respond(Ply, self.messages[7])
             end
 
             -- Disable Admin Chat:
-        elseif (not self.restore) or (not self[ip]) then
+        elseif (not self.restore or not self[ip]) then
             self[ip] = nil
         end
     end
 end
 
-function AdminChat:OnCommand(Executor, CMD)
-
-    local Args = STRSplit(CMD)
-    if (#Args > 0 and Args[1] == self.command) then
-
-        -- Admin [boolean], lvl [int]
-        local Admin, lvl = self:IsAdmin(Executor, true)
-
-        if (Admin) then
-            if (Args[2] ~= nil) then
-                local pl = self:GetPlayers(Executor, Args[3])
-                if (pl) then
-                    local params = { }
-                    for i = 1, #pl do
-                        local player = tonumber(pl[i])
-                        if (player ~= Executor and lvl < self.permission_others and Executor ~= 0) then
-                            self:Respond(Executor, self.messages[9], 10)
-                        else
-                            params.state = Args[2] -- on|off
-                            params.eid, params.en = Executor, get_var(Executor, '$name')
-                            params.tid, params.tn, params.tip = player, get_var(player, '$name'), self:GetIP(player)
-                            self:Toggle(params)
-                        end
-                    end
-                end
-            else
-                self:Respond(Executor, "Invalid Syntax. Usage: /" .. self.command .. " on|off [me | id | */all]", 10)
+local function GetPlayers(Ply, Args)
+    local pl = { }
+    if (Args == nil or Args == "me") then
+        if (Ply ~= 0) then
+            pl[#pl + 1] = Ply
+        else
+            Respond(Ply, "Please enter a valid player id")
+        end
+    elseif (Args ~= nil and Args:match("^%d+$")) then
+        if player_present(Args) then
+            pl[#pl + 1] = Args
+        else
+            Respond(Ply, "Player #" .. Args .. " is not online")
+        end
+    elseif (Args == "all" or Args == "*") then
+        for i = 1, 16 do
+            if player_present(i) then
+                pl[#pl + 1] = i
             end
         end
+        if (#pl == 0) then
+            Respond(Ply, "There are no players online!")
+        end
+    else
+        Respond(Ply, "Invalid Command Syntax. Please try again!")
+    end
+    return pl
+end
+
+function AdminChat:OnCommand(Ply, CMD)
+
+    local args = STRSplit(CMD)
+    if (#args > 0 and args[1] == self.command) then
+
+        -- Admin [boolean], lvl [number]
+        local admin, lvl = IsAdmin(Ply, true)
+
+        if (not admin) then
+            return false
+        end
+
+        if (args[2] ~= nil) then
+            local pl = GetPlayers(Ply, args[3])
+            if (pl) then
+                for i = 1, #pl do
+                    local player = tonumber(pl[i])
+                    if (player ~= Ply and lvl < self.permission_others and Ply ~= 0) then
+                        Respond(Ply, self[9], 10)
+                    else
+                        self:Toggle({
+                            eid = Ply,
+                            tid = player,
+                            state = args[2],
+                            tip = self:GetIP(player),
+                            en = get_var(Ply, '$name'),
+                            tn = get_var(player, '$name')
+                        })
+                    end
+                end
+            end
+        else
+            Respond(Ply, "Invalid Syntax. Usage: /" .. self.command .. " on|off [me | id | */all]", 10)
+        end
+
         return false
     end
 end
@@ -190,8 +229,15 @@ function AdminChat:ActivationState(Executor, State)
     elseif (State == "off" or State == "0" or State == "false") then
         return 0
     else
-        return false, self:Respond(Executor, self.messages[8]:gsub("$cmd", self.command), 12)
+        return false, Respond(Executor, self[8]:gsub("$cmd", self.command), 12)
     end
+end
+
+local function Feedback(str, t)
+    for k, v in pairs(t) do
+        str = str:gsub(k, v)
+    end
+    return str
 end
 
 function AdminChat:Toggle(params)
@@ -233,73 +279,24 @@ function AdminChat:Toggle(params)
 
         if (valid_state) then
 
-            local Feedback = function(str)
-                local words = {
-                    ["$state"] = state,
-                    ["$executor_name"] = en,
-                    ["$target_name"] = tn,
-                }
-                for k, v in pairs(words) do
-                    str = str:gsub(k, v)
-                end
-                return str
-            end
-
-            local msg = self.messages
             local is_self = (eid == tid)
+            local t = { ["$state"] = state, ["$target_name"] = tn, ["$executor_name"] = en }
+
             if (not already_set) then
                 if (is_self) then
-                    self:Respond(eid, Feedback(msg[2]), 2 + 8)
+                    Respond(eid, Feedback(self[2], t))
                 else
-                    self:Respond(eid, Feedback(msg[3]), 2 + 8)
-                    self:Respond(tid, Feedback(msg[4]), 2 + 8)
+                    Respond(eid, Feedback(self[3], t))
+                    Respond(tid, Feedback(self[4], t))
                 end
             else
                 if (is_self) then
-                    self:Respond(eid, Feedback(msg[5]), 4 + 8)
+                    Respond(eid, Feedback(self[5], t))
                 else
-                    self:Respond(eid, Feedback(msg[6]), 4 + 8)
+                    Respond(eid, Feedback(self[6], t))
                 end
             end
         end
-    end
-end
-
-function AdminChat:GetPlayers(Executor, Param)
-    local pl = { }
-    if (Param == nil or Param == "me") then
-        if (Executor ~= 0) then
-            table.insert(pl, Executor)
-        else
-            self:Respond(Executor, "Please enter a valid player id", 10)
-        end
-    elseif (Param ~= nil and Param:match("^%d+$")) then
-        if player_present(Param) then
-            table.insert(pl, Param)
-        else
-            self:Respond(Executor, "Player #" .. Param .. " is not online", 10)
-        end
-    elseif (Param == "all" or Param == "*") then
-        for i = 1, 16 do
-            if player_present(i) then
-                table.insert(pl, i)
-            end
-        end
-        if (#pl == 0) then
-            self:Respond(Executor, "There are no players online!", 10)
-        end
-    else
-        self:Respond(Executor, "Invalid Command Syntax. Please try again!", 10)
-    end
-    return pl
-end
-
-function AdminChat:Respond(Ply, Message, Color)
-    Color = Color or 10
-    if (Ply == 0) then
-        cprint(Message, Color)
-    else
-        rprint(Ply, Message)
     end
 end
 
@@ -325,19 +322,19 @@ function AdminChat:SendMessage(Ply, Msg, Type)
             if (self[ip]) then
 
                 -- check if incoming message string is chat command:
-                local chat_command = (args[1]:sub(1, 1) == "/" or args[1]:sub(1, 1) == "\\")
+                local cmd = (args[1]:sub(1, 1) == "/" or args[1]:sub(1, 1) == "\\")
 
-                if (not chat_command) then
+                if (not cmd) then
 
                     -- Admin Chat message formatter:
                     local name = get_var(Ply, "$name")
-                    local msg = self.messages[1]:gsub("$name", name):gsub("$id", Ply):gsub("$message", Msg)
+                    local msg = self[1]:gsub("$name", name):gsub("$id", Ply):gsub("$str", Msg)
 
                     -- Check if player is an admin and send them the message:
                     for i = 1, 16 do
                         if player_present(i) then
                             if (tonumber(get_var(i, '$lvl')) >= self.permission) then
-                                rprint(i, "|l" .. msg)
+                                Respond(i, msg)
                             end
                         end
                     end
