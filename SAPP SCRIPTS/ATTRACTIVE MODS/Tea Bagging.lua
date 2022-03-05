0b7b059cb1e416aee3c3bb4c059f3ea8e5927d94
Chalwk77 <jericho.crosby227@gmail.com>
2022-03-05 18:50:08 +1300

    ...

--- SAPP SCRIPTS/ATTRACTIVE MODS/Tea Bagging.lua
+++ SAPP SCRIPTS/ATTRACTIVE MODS/Tea Bagging.lua
@@ -6,6 +6,8 @@ Description: Humiliate your friends with this nifty little script.
 Crouch over your victims corpse 3 times to trigger a funny message "$name is lap-dancing on $victim's body!".
 T-bag any corpse within 60 seconds after they die.
 
+By design, you can Tea-bag any player (even team mates!)
+
 Copyright (c) 2022, Jericho Crosby <jericho.crosby227@gmail.com>
 * Notice: You can use this document subject to the following conditions:
 https://github.com/Chalwk77/HALO-SCRIPT-PROJECTS/blob/master/LICENSE
@@ -76,17 +78,17 @@ end
 
 local function GetXYZ(Ply)
     local x, y, z
-    local DyN = get_dynamic_player(Ply)
-    if (DyN ~= 0) then
-        local vehicle = read_dword(DyN + 0x11C)
+    local dyn = get_dynamic_player(Ply)
+    if (dyn ~= 0) then
+        local vehicle = read_dword(dyn + 0x11C)
         local object = get_object_memory(vehicle)
         if (vehicle == 0xFFFFFFFF) then
-            x, y, z = read_vector3d(DyN + 0x5c)
+            x, y, z = read_vector3d(dyn + 0x5c)
         elseif (object ~= 0) then
             x, y, z = read_vector3d(object + 0x5c)
         end
     end
-    return x, y, z, DyN
+    return x, y, z, dyn
 end
 
 local function Dist(x1, y1, z1, x2, y2, z2, r)
@@ -98,12 +100,12 @@ function OnTick()
         for _, v in pairs(players) do
             if (k and v and k.pid ~= v.pid and #v.loc > 0) then
                 for i, pos in pairs(v.loc) do
-                    local x2, y2, z2 = pos.x, pos.y, pos.z
-                    if (pos.start() >= pos.finish) then
+                    local x1, y1, z1 = pos.x, pos.y, pos.z
+                    if (time() >= pos.finish) then
                         v.loc[i] = nil
                     elseif player_alive(k.pid) then
-                        local x1, y1, z1, dyn = GetXYZ(k.pid)
-                        if Dist(x1, y1, z1, x2, y2, z2, k.radius) then
+                        local x2, y2, z2, dyn = GetXYZ(k.pid)
+                        if (x2 and Dist(x1, y1, z1, x2, y2, z2, k.radius)) then
                             local crouch = read_bit(dyn + 0x208, 0)
                             if (crouch ~= k.state and crouch == 1) then
                                 k.count = k.count + 1
@@ -140,7 +142,6 @@ function OnDeath(Victim)
             x = x,
             y = y,
             z = z,
-            start = time,
             finish = time() + v.expiration
         }
     end
