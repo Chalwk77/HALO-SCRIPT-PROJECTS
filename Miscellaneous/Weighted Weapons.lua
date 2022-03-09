cd86dafd98ecb02298331a67e40b8012991402a0
Chalwk77 <jericho.crosby227@gmail.com>
2022-03-10 09:06:17 +1300

    ...

--- Miscellaneous/Weighted Weapons.lua
+++ Miscellaneous/Weighted Weapons.lua
@@ -15,7 +15,7 @@ local Weights = {
     -- combined weight reduction of all the weapons in your inventory.
     -- Otherwise the speed will be based on weight of the weapon currently held.
     --
-    combined_weight = false,
+    combined = true,
 
     -- Weapon tags (NOTE: Normal walking speed is 1).
     -- Speed = 1-weapon weight
@@ -51,36 +51,30 @@ local function GetTagPath(object)
     return (read_string(read_dword(read_word(object) * 32 + 0x40440038)) or 0)
 end
 
+local function GetWeight(weapon)
+    local object = get_object_memory(weapon)
+    if (weapon ~= 0xFFFFFFFFF and object ~= 0) then
+        return Weights[GetTagPath(object)]
+    end
+    return 0
+end
+
 local function GetSpeed(Dyn)
-
     local speed = 1
-
-    if (not Weights.combined_weight) then
-        local weapon = read_dword(Dyn + 0x118)
-        local object = get_object_memory(weapon)
-        if (weapon ~= 0xFFFFFFFFF and object ~= 0) then
-            speed = speed - (Weights[GetTagPath(object)])
-        end
-        --print('combined weight: ', speed)
-        return speed
+    if (not Weights.combined) then
+        return GetWeight(read_dword(Dyn + 0x118))
     end
-
     for i = 0, 3 do
-        local weapon = read_dword(Dyn + 0x2F8 + (i * 4))
-        local object = get_object_memory(weapon)
-        if (weapon ~= 0xFFFFFFFFF and object ~= 0) then
-            speed = speed - (Weights[GetTagPath(object)])
-        end
+        speed = speed - GetWeight(read_dword(Dyn + 0x2F8 + (i * 4)))
     end
-    --print('combined weight: ', speed)
-
     return speed
 end
 
 function OnTick()
     for i = 1, 16 do
         local dyn = get_dynamic_player(i)
-        if player_present(i) and player_alive(i) and (dyn ~= 0) then
+        if (player_present(i) and player_alive(i) and dyn ~= 0) then
+            print(GetSpeed(dyn))
             execute_command('s ' .. i .. ' ' .. GetSpeed(dyn))
         end
     end
