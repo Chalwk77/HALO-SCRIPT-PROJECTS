8ad688f7e5085d4ca2964ca75e75fef145a81f6c
Chalwk77 <jericho.crosby227@gmail.com>
2022-03-10 10:08:28 +1300

    ...

--- INDEV/Alias System.lua
+++ INDEV/Alias System.lua
@@ -14,8 +14,16 @@ local Alias = {
     -- File that contains the json database:
     --
     file = 'aliases.json',
+
+    -- If true, the .json database will be updated
+    -- every time a player joins.
+    --
+    -- If false, the .json database will only be updated
+    -- when the game ends (recommended).
+    update_file_on_join = false
 }
 
+local date = os.date
 local open = io.open
 local json = loadfile('./json.lua')()
 
@@ -23,6 +31,8 @@ api_version = "1.12.0.0"
 
 function OnScriptLoad()
 
+    register_callback(cb['EVENT_JOIN'], 'OnJoin')
+    register_callback(cb['EVENT_GAME_END'], 'OnEnd')
     register_callback(cb['EVENT_GAME_START'], 'OnStart')
     OnStart()
 
@@ -37,14 +47,31 @@ local function WriteToFile(self, t)
     end
 end
 
+function Alias:UpdateRecords(o)
+
+    local day = date('*t').day
+    local month = date('*t').month
+    local year = date('*t').year
+
+    self.records.hashes[o.hash] = {
+        [o.name] = { day = day, month = month, year = year }
+    }
+
+    self.records.ip_addresses[o.ip] = {
+        [o.name] = { day = day, month = month, year = year }
+    }
+
+    WriteToFile(self, self.records)
+end
+
 function Alias:CheckFile(ScriptLoad)
 
     local dir = read_string(read_dword(sig_scan('68??????008D54245468') + 0x1))
     self.dir = dir .. '\\sapp\\' .. self.file
 
-    self.database = (ScriptLoad and nil or self.database)
+    self.records = (ScriptLoad and nil or self.records)
 
-    if (get_var(0, '$gt') ~= 'n/a' and self.database == nil) then
+    if (get_var(0, '$gt') ~= 'n/a' and self.records == nil) then
         local content = ''
         local file = open(self.dir, 'r')
         if (file) then
@@ -53,9 +80,10 @@ function Alias:CheckFile(ScriptLoad)
         end
         local data = json:decode(content)
         if (not data) then
-            WriteToFile(self, {})
+            data = { hashes = {}, ip_addresses = {} }
+            WriteToFile(self, data)
         end
-        self.database = data or {}
+        self.records = data
     end
 end
 
@@ -65,6 +93,18 @@ function OnStart()
     end
 end
 
+function OnEnd()
+    WriteToFile(Alias, Alias.records)
+end
+
+function OnJoin(Ply)
+    Alias:UpdateRecords({
+        name = get_var(Ply, '$name'),
+        hash = get_var(Ply, '$hash'),
+        ip = get_var(Ply, '$ip'):match('%d+.%d+.%d+.%d+')
+    })
+end
+
 function OnScriptUnload()
-    -- N/A
+    WriteToFile(Alias, Alias.records)
 end
