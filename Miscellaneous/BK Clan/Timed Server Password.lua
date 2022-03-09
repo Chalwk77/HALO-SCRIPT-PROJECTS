ca36351f31f91b0700825fcc12426440be84b213
Chalwk77 <jericho.crosby227@gmail.com>
2022-03-09 19:10:29 +1300

    Refactored

--- Miscellaneous/BK Clan/Timed Server Password.lua
+++ Miscellaneous/BK Clan/Timed Server Password.lua
@@ -1,86 +1,71 @@
 --[[
-Script Name: Timed Server Password (v1.0), for SAPP (PC & CE)
+Script Name: Timed Server Password (v1.1), for SAPP (PC & CE)
 Description: This script will automatically remove the server password after X seconds.
 
 For @Rev - BK Clan.
 
-Copyright (c) 2020-2021, Jericho Crosby <jericho.crosby227@gmail.com>
+Copyright (c) 2022, Jericho Crosby <jericho.crosby227@gmail.com>
 * Notice: You can use this document subject to the following conditions:
 https://github.com/Chalwk77/HALO-SCRIPT-PROJECTS/blob/master/LICENSE
 ]]--
 
 -- Config [starts] ------------------------------
-local duration = 300 -- in seconds
+local duration = 1 -- in seconds
 -- Config [ends] --------------------------------
 
-api_version = "1.12.0.0"
+api_version = '1.12.0.0'
 
-local server, time_scale = {}, 1 / 30
-local floor, format = math.floor, string.format
+local countdown
+local time = os.time
 
 function OnScriptLoad()
-    register_callback(cb["EVENT_TICK"], "OnTick")
-    register_callback(cb["EVENT_COMMAND"], "OnCommand")
-    register_callback(cb["EVENT_GAME_START"], "StartTimer")
+    register_callback(cb['EVENT_TICK'], 'OnTick')
+    register_callback(cb['EVENT_COMMAND'], 'OnCommand')
+    register_callback(cb['EVENT_GAME_START'], 'StartTimer')
     StartTimer()
 end
 
-local function Respond(Ply, MSG)
-    if (Ply == 0) then
-        cprint(MSG)
-        return
+function StartTimer()
+    countdown = nil
+    if (get_var(0, '$gt') ~= 'n/a') then
+        countdown = time() + duration
     end
-    rprint(Ply, MSG)
+end
+
+function OnTick()
+    if (countdown and time() >= countdown) then
+        countdown = nil
+        execute_command('sv_password ""')
+        say_all('Server password has been removed!')
+    end
+end
+
+local function CommandArgs(CMD)
+    local args = { }
+    for arg in CMD:gmatch('([^%s]+)') do
+        args[#args + 1] = arg:lower()
+    end
+    return args
+end
+
+local function Allowed(Ply)
+    return (Ply == 0 or tonumber(get_var(Ply, '$lvl')) >= 4)
+end
+
+local function Respond(Ply, Msg)
+    return (Ply == 0 and cprint(Msg) or rprint(Ply, Msg))
+end
+
+function OnCommand(Ply, CMD, _)
+    local args = CommandArgs(CMD)
+    if (#args > 0 and args[1] == 'sv_password') then
+        if (Allowed(Ply) and args[2]) then
+            StartTimer()
+            Respond(Ply, 'Server password will be removed in ' .. duration .. ' seconds')
+        end
+    end
 end
 
 function OnScriptUnload()
     -- N/A
-end
-
-function StartTimer()
-    if (get_var(0, "$gt") ~= "n/a") then
-        server.time, server.init = 0, true
-    end
-end
-
-local function TimerRemaining(s)
-
-    s = tonumber(s)
-    if (s <= 0) then
-        return "00", "00";
-    end
-
-    local h, m = format("%02.f", floor(s / 3600));
-    m = format("%02.f", floor(s / 60 - (h * 60)));
-    s = format("%02.f", floor(s - h * 3600 - m * 60));
-    return m, s
-end
-
-function OnTick()
-    if (server.init) then
-        server.time = server.time + time_scale
-        local delta_time = ((duration) - (server.time))
-        local m, s = TimerRemaining(delta_time)
-        local time_up = ((tonumber(m) <= 0) and (tonumber(s) <= 0))
-        if (time_up) then
-            execute_command('sv_password ""')
-            server.time, server.init = 0, false
-            say_all("Server password has been removed!")
-        end
-    end
-end
-
-function OnCommand(Ply, CMD, _)
-
-    local Args = { }
-    for Params in CMD:gmatch("([^%s]+)") do
-        Args[#Args + 1] = Params:lower()
-    end
-
-    if (#Args > 0 and Args[1] == "sv_password") then
-        if (Ply == 0 or tonumber(get_var(Ply, "$lvl")) >= 4 and Args[2]) then
-            StartTimer()
-            Respond(Ply, "Server password will be removed in " .. duration .. " seconds")
-        end
-    end
 end
