e4cd99f2bc4abf81d287e143d943d90878e28a79
Chalwk77 <jericho.crosby227@gmail.com>
2022-03-10 09:18:10 +1300

    ...

---
+++ Miscellaneous/Libraries/WriteToJSON.lua
@@ -0,0 +1,70 @@
+--[[
+--=====================================================================================================--
+Script Name: WriteToJSON, for SAPP (PC & CE)
+Description:
+
+Copyright (c) 2022, Jericho Crosby <jericho.crosby227@gmail.com>
+* Notice: You can use this document subject to the following conditions:
+https://github.com/Chalwk77/HALO-SCRIPT-PROJECTS/blob/master/LICENSE
+--=====================================================================================================--
+]]--
+
+local Mod = {
+
+    -- File that contains the json database:
+    --
+    file = 'file_name.json',
+}
+
+local open = io.open
+local json = loadfile('./json.lua')()
+
+api_version = "1.12.0.0"
+
+function OnScriptLoad()
+
+    register_callback(cb['EVENT_GAME_START'], 'OnStart')
+    OnStart()
+
+    Mod:CheckFile(true)
+end
+
+local function WriteToFile(self, t)
+    local file = open(self.dir, 'w')
+    if (file) then
+        file:write(json:encode_pretty(t))
+        file:close()
+    end
+end
+
+function Mod:CheckFile(ScriptLoad)
+
+    local dir = read_string(read_dword(sig_scan('68??????008D54245468') + 0x1))
+    self.dir = dir .. '\\sapp\\' .. self.file
+
+    self.databas = (ScriptLoad and nil or self.databas)
+
+    if (get_var(0, '$gt') ~= 'n/a' and self.databas == nil) then
+        local content = ''
+        local file = open(self.dir, 'r')
+        if (file) then
+            content = file:read('*all')
+            file:close()
+        end
+        local data = json:decode(content)
+        if (not data) then
+            WriteToFile(self, {})
+        end
+        self.databas = data or {}
+    end
+end
+
+function OnStart()
+    if (get_var(0, '$gt') ~= 'n/a') then
+        Mod:CheckFile(false)
+    end
+end
+
+function OnScriptUnload()
+    -- N/A
+end
