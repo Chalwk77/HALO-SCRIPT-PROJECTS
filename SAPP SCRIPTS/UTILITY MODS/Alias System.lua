c3f81270aeaa11c6376de9bad7828afc52ce91e0
Chalwk77 <jericho.crosby227@gmail.com>
2022-03-10 15:29:43 +1300

    Refactored this script and removed some unpopular features.

--- SAPP SCRIPTS/UTILITY MODS/Alias System.lua
+++ SAPP SCRIPTS/UTILITY MODS/Alias System.lua
@@ -24,14 +24,14 @@ https://github.com/Chalwk77/HALO-SCRIPT-PROJECTS/blob/master/LICENSE
 local Alias = {
 
     -- File that contains the json database of aliases:
-    -- This file will be created in "cg/sapp/"
+    -- This file will be created in 'cg/sapp/'
     --
     file = 'aliases.json',
 
 
     -- Base command:
     --
-    command = "alias",
+    command = 'alias',
 
 
     -- Minimum permission required to execute /command:
@@ -56,9 +56,9 @@ local Alias = {
 
 
     -- When executing /alias <pid>, what should the default look up table be?
-    -- Valid Tables: "ip_addresses" or "hashes"
+    -- Valid Tables: 'ip_addresses' or 'hashes'
     --
-    default_table = "ip_addresses",
+    default_table = 'ip_addresses',
 
 
     --------------------------------------------------------------------------
@@ -81,37 +81,37 @@ local Alias = {
     -- If someone has a pirated hash, the script will tell you when you hash-alias them:
     --
     known_pirated_hashes = {
-        ["388e89e69b4cc08b3441f25959f74103"] = true,
-        ["81f9c914b3402c2702a12dc1405247ee"] = true,
-        ["c939c09426f69c4843ff75ae704bf426"] = true,
-        ["13dbf72b3c21c5235c47e405dd6e092d"] = true,
-        ["29a29f3659a221351ed3d6f8355b2200"] = true,
-        ["d72b3f33bfb7266a8d0f13b37c62fddb"] = true,
-        ["76b9b8db9ae6b6cacdd59770a18fc1d5"] = true,
-        ["55d368354b5021e7dd5d3d1525a4ab82"] = true,
-        ["d41d8cd98f00b204e9800998ecf8427e"] = true,
-        ["c702226e783ea7e091c0bb44c2d0ec64"] = true,
-        ["f443106bd82fd6f3c22ba2df7c5e4094"] = true,
-        ["10440b462f6cbc3160c6280c2734f184"] = true,
-        ["3d5cd27b3fa487b040043273fa00f51b"] = true,
-        ["b661a51d4ccf44f5da2869b0055563cb"] = true,
-        ["740da6bafb23c2fbdc5140b5d320edb1"] = true,
-        ["7503dad2a08026fc4b6cfb32a940cfe0"] = true,
-        ["4486253cba68da6786359e7ff2c7b467"] = true,
-        ["f1d7c0018e1648d7d48f257dc35e9660"] = true,
-        ["40da66d41e9c79172a84eef745739521"] = true,
-        ["2863ab7e0e7371f9a6b3f0440c06c560"] = true,
-        ["34146dc35d583f2b34693a83469fac2a"] = true,
-        ["b315d022891afedf2e6bc7e5aaf2d357"] = true,
-        ["63bf3d5a51b292cd0702135f6f566bd1"] = true,
-        ["6891d0a75336a75f9d03bb5e51a53095"] = true,
-        ["325a53c37324e4adb484d7a9c6741314"] = true,
-        ["0e3c41078d06f7f502e4bb5bd886772a"] = true,
-        ["fc65cda372eeb75fc1a2e7d19e91a86f"] = true,
-        ["f35309a653ae6243dab90c203fa50000"] = true,
-        ["50bbef5ebf4e0393016d129a545bd09d"] = true,
-        ["a77ee0be91bd38a0635b65991bc4b686"] = true,
-        ["3126fab3615a94119d5fe9eead1e88c1"] = true,
+        ['388e89e69b4cc08b3441f25959f74103'] = true,
+        ['81f9c914b3402c2702a12dc1405247ee'] = true,
+        ['c939c09426f69c4843ff75ae704bf426'] = true,
+        ['13dbf72b3c21c5235c47e405dd6e092d'] = true,
+        ['29a29f3659a221351ed3d6f8355b2200'] = true,
+        ['d72b3f33bfb7266a8d0f13b37c62fddb'] = true,
+        ['76b9b8db9ae6b6cacdd59770a18fc1d5'] = true,
+        ['55d368354b5021e7dd5d3d1525a4ab82'] = true,
+        ['d41d8cd98f00b204e9800998ecf8427e'] = true,
+        ['c702226e783ea7e091c0bb44c2d0ec64'] = true,
+        ['f443106bd82fd6f3c22ba2df7c5e4094'] = true,
+        ['10440b462f6cbc3160c6280c2734f184'] = true,
+        ['3d5cd27b3fa487b040043273fa00f51b'] = true,
+        ['b661a51d4ccf44f5da2869b0055563cb'] = true,
+        ['740da6bafb23c2fbdc5140b5d320edb1'] = true,
+        ['7503dad2a08026fc4b6cfb32a940cfe0'] = true,
+        ['4486253cba68da6786359e7ff2c7b467'] = true,
+        ['f1d7c0018e1648d7d48f257dc35e9660'] = true,
+        ['40da66d41e9c79172a84eef745739521'] = true,
+        ['2863ab7e0e7371f9a6b3f0440c06c560'] = true,
+        ['34146dc35d583f2b34693a83469fac2a'] = true,
+        ['b315d022891afedf2e6bc7e5aaf2d357'] = true,
+        ['63bf3d5a51b292cd0702135f6f566bd1'] = true,
+        ['6891d0a75336a75f9d03bb5e51a53095'] = true,
+        ['325a53c37324e4adb484d7a9c6741314'] = true,
+        ['0e3c41078d06f7f502e4bb5bd886772a'] = true,
+        ['fc65cda372eeb75fc1a2e7d19e91a86f'] = true,
+        ['f35309a653ae6243dab90c203fa50000'] = true,
+        ['50bbef5ebf4e0393016d129a545bd09d'] = true,
+        ['a77ee0be91bd38a0635b65991bc4b686'] = true,
+        ['3126fab3615a94119d5fe9eead1e88c1'] = true,
     }
 }
 
@@ -124,7 +124,7 @@ local date, time, diff = os.date, os.time, os.difftime
 
 local json = loadfile('./json.lua')()
 
-api_version = "1.12.0.0"
+api_version = '1.12.0.0'
 
 function OnScriptLoad()
 
@@ -231,7 +231,7 @@ function CheckStale()
                 local whole_days = floor(days_from)
                 if (whole_days >= Alias.stale_period) then
                     hash_table[name] = nil
-                    cprint("Deleting stale name record for " .. type .. ":" .. name, 12)
+                    cprint('Deleting stale name record for ' .. type .. ':' .. name, 12)
                 end
             end
         end
@@ -255,57 +255,6 @@ local function Respond(Ply, Msg)
     return (Ply == 0 and cprint(Msg) or rprint(Ply, Msg))
 end
 
-function Alias:OnQuery(Ply, CMD)
-    local args = CMDSplit(CMD)
-    if (#args > 0 and args[1] == self.command and HasPermission(Ply)) then
-
-        if (args[2]) then
-
-            local player = tonumber((args[2]:match("^%d+$")))
-            if (not player_present(player)) then
-                Respond(Ply, "Player #" .. player .. " is not online.")
-                return false
-            elseif (args[2] == "me") then
-                player = Ply
-            end
-
-            -- /alias <pid>
-            local player_lookup = (player and not args[3])
-
-            -- /alias <pid> <-ip> <opt page>
-            local player_ip_lookup = (player and args[3] == "-ip")
-
-            -- /alias <pid> <-hash> <opt page>
-            local player_hash_lookup = (player and args[3] == "-hash")
-
-            local t = players[player]
-            local page = (args[4] and args[4]:match("^%d+$") or 1)
-
-            if (player_lookup) then
-                if (self.default_table == 'ip_addresses') then
-                    ShowResults(Ply, "ip_addresses", 1, t.ip)
-                else
-                    ShowResults(Ply, "hashes", 1, t.hash)
-                end
-            elseif (player_ip_lookup) then
-                ShowResults(Ply, "ip_addresses", page, t.ip)
-            elseif (player_hash_lookup) then
-                ShowResults(Ply, "hashes", page, t.hash)
-            end
-        end
-
-        return false
-    end
-end
-
-OnCommand = function(a, b)
-    return Alias:OnQuery(a, b)
-end
-
-function OnScriptUnload()
-    WriteToFile(Alias, Alias.records)
-end
-
 local function GetPage(page)
     local start = (Alias.max_results) * page
     local start_page = (start - Alias.max_results + 1)
@@ -322,14 +271,14 @@ local function GetPageCount(total)
 end
 
 local function spacing(n)
-    local Str = ""
+    local Str = ''
     for i = 1, n do
         if i == floor(n / 2) then
-            Str = Str .. ""
+            Str = Str .. ''
         end
-        Str = Str .. " "
+        Str = Str .. ' '
     end
-    return "," .. Str
+    return ',' .. Str
 end
 
 local function FormatTable(table)
@@ -344,9 +293,9 @@ local function FormatTable(table)
     local rows, row, count = {}, 1, 1
     for k, v in pairs(table) do
         if (count % Alias.max_results == 0) or (k == #table) then
-            rows[row] = (rows[row] or "") .. v
+            rows[row] = (rows[row] or '') .. v
         else
-            rows[row] = (rows[row] or "") .. v .. spacing(longest - v:len() + Alias.spaces)
+            rows[row] = (rows[row] or '') .. v .. spacing(longest - v:len() + Alias.spaces)
         end
         if (count % Alias.max_results == 0) then
             row = row + 1
@@ -365,14 +314,14 @@ local function GetNames(t)
     return results
 end
 
-function ShowResults(Ply, Table, Page, Artifact)
+function Alias:ShowResults(Ply, Table, Page, Artifact)
 
-    Table = GetNames(Alias.records[Table][Artifact])
+    Table = GetNames(self.records[Table][Artifact])
     local total_pages = GetPageCount(#Table)
 
     if (Page > 0 and Page <= total_pages) then
 
-        local start_index, end_index = 1, Alias.max_columns
+        local start_index, end_index = 1, self.max_columns
         local start_page, end_page = GetPage(Page)
 
         local results = { }
@@ -382,7 +331,7 @@ function ShowResults(Ply, Table, Page, Artifact)
             end
         end
 
-        while (end_index < #Table + Alias.max_columns) do
+        while (end_index < #Table + self.max_columns) do
 
             local t, row = { }
             for i = start_index, end_index do
@@ -390,16 +339,83 @@ function ShowResults(Ply, Table, Page, Artifact)
                 row = FormatTable(t)
             end
 
-            if (row ~= nil and row ~= "" and row ~= " ") then
+            if (row ~= nil and row ~= '' and row ~= ' ') then
                 Respond(Ply, row, 10)
             end
 
             start_index = (end_index + 1)
-            end_index = (end_index + (Alias.max_columns))
+            end_index = (end_index + (self.max_columns))
         end
-
-        Respond(Ply, '[Page ' .. Page .. '/' .. total_pages .. '] Showing ' .. #results .. '/' .. #Table .. ' aliases for: "' .. Artifact .. '"')
+        Respond(Ply, '[Page ' .. Page .. '/' .. total_pages .. '] Showing ' .. #results .. '/' .. #Table .. ' aliases for: ' .. Artifact)
+        if (self.known_pirated_hashes[Artifact]) then
+            Respond(Ply, 'This hash is pirated.')
+        end
     else
         Respond(Ply, 'Invalid Page ID. Please type a page between 1-' .. total_pages)
     end
+end
+
+function Alias:OnQuery(Ply, CMD)
+    local args = CMDSplit(CMD)
+    if (#args > 0 and args[1] == self.command and HasPermission(Ply)) then
+
+        if (args[2]) then
+
+            local player = tonumber((args[2]:match('^%d+$')))
+            if (not player_present(player)) then
+                Respond(Ply, 'Player #' .. player .. ' is not online.')
+                return false
+            elseif (args[2] == 'me') then
+                player = Ply
+            end
+
+            -- /alias <pid>
+            local player_lookup = (player and not args[3])
+
+            -- /alias <pid> <-ip> <opt page>
+            local player_ip_lookup = (player and args[3] == '-ip')
+
+            -- /alias <pid> <-hash> <opt page>
+            local player_hash_lookup = (player and args[3] == '-hash')
+
+            local t = players[player]
+            local page = (args[4] and args[4]:match('^%d+$') or 1)
+
+            if (player_lookup) then
+                if (self.default_table == 'ip_addresses') then
+                    self:ShowResults(Ply, 'ip_addresses', 1, t.ip)
+                else
+                    self:ShowResults(Ply, 'hashes', 1, t.hash)
+                end
+            elseif (player_ip_lookup) then
+                self:ShowResults(Ply, 'ip_addresses', page, t.ip)
+            elseif (player_hash_lookup) then
+                self:ShowResults(Ply, 'hashes', page, t.hash)
+            else
+                self:CmdHelp(Ply)
+            end
+        else
+            self:CmdHelp(Ply)
+        end
+
+        return false
+    end
+end
+
+function Alias:CmdHelp(Ply)
+    Respond(Ply, "Invalid Command Syntax or Lookup Parameter.")
+    Respond(Ply, "Usage:")
+    Respond(Ply, "/" .. self.command .. " <pid> -ip <opt page>")
+    Respond(Ply, "/" .. self.command .. " <pid> -hash <opt page>")
+    Respond(Ply, "/" .. self.command .. " <ip> <opt page>")
+    --Respond(Ply, "/" .. self.command .. " <32 char hash> <opt page>")
+    Respond(Ply, ".....................................................")
+end
+
+function OnCommand(a, b)
+    return Alias:OnQuery(a, b)
+end
+
+function OnScriptUnload()
+    WriteToFile(Alias, Alias.records)
 end
