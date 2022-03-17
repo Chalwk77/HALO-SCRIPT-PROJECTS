b9e46d18bc4b3689a96e4558d49391f1c200c197
Chalwk77 <jericho.crosby227@gmail.com>
2022-03-18 09:40:56 +1300

    ...

--- SAPP SCRIPTS/ATTRACTIVE MODS/Rage Quit.lua
+++ SAPP SCRIPTS/ATTRACTIVE MODS/Rage Quit.lua
@@ -1,12 +1,12 @@
 --[[
---=====================================================================================================--
+--====================================================================--
 Script Name: Rage Quit, for SAPP (PC & CE)
 Description: Announces a simple message when someone rage quits.
 
 Copyright (c) 2022, Jericho Crosby <jericho.crosby227@gmail.com>
 * Notice: You can use this document subject to the following conditions:
 https://github.com/Chalwk77/HALO-SCRIPT-PROJECTS/blob/master/LICENSE
---=====================================================================================================--
+--====================================================================--
 ]]--
 
 -- config starts --
