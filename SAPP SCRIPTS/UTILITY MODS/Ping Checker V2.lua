9a991186c099b5a058f5bbe78d1b9035827c4dea
Chalwk77 <jericho.crosby227@gmail.com>
2022-08-24 14:48:54 +1200

    Refactored

--- SAPP SCRIPTS/UTILITY MODS/Ping Checker V2.lua
+++ SAPP SCRIPTS/UTILITY MODS/Ping Checker V2.lua
@@ -1,102 +1,76 @@
 --[[
 --=====================================================================================================--
-Script Name: Ping Checker V2, for SAPP (PC & CE)
+Script Name: Ping Checker, for SAPP (PC & CE)
 Description: A simple addon to check your ping (or others)
 
-Copyright (c) 2020, Jericho Crosby <jericho.crosby227@gmail.com>
+Copyright (c) 2020-2022, Jericho Crosby <jericho.crosby227@gmail.com>
 * Notice: You can use this document subject to the following conditions:
 https://github.com/Chalwk77/HALO-SCRIPT-PROJECTS/blob/master/LICENSE
 --=====================================================================================================--
 ]]--
 
--- Config Starts
-local ping_command = "ping"
+-- config starts:
+
+-- Custom ping command:
+--
+local ping_command = 'ping'
+
+-- Minimum permission level to execute the custom command:
+--
 local permission_level = -1
-local messages = {
-    "Your ping is %ping%",
-    "%name%'s ping is %ping%",
-    "Invalid Player ID. Usage: /%cmd% [number: 1-16] | */all | me",
-    "Something went wrong. Unable to get %name%'s ping Please try again!"
-}
 
--- Config Ends
+-- Message to omit when you execute the custom command:
+-- The $name & $ping placeholders get replaced automagically.
+--
+local output = '$name: $ping'
+-- config ends (do not touch anything below this point)
 
-api_version = "1.12.0.0"
-local gmatch, gsub = string.gmatch, string.gsub
+api_version = '1.12.0.0'
 
 function OnScriptLoad()
-    register_callback(cb["EVENT_COMMAND"], "OnServerCommand")
+    register_callback(cb['EVENT_COMMAND'], 'OnCommand')
 end
 
-local hasAccess = function(PlayerIndex)
-    return (tonumber(get_var(PlayerIndex, "$lvl")) >= permission_level)
+local function HasAccess(Ply)
+    local lvl = tonumber(get_var(Ply, '$lvl'))
+    return (Ply == 0 or lvl >= permission_level)
 end
 
-function OnServerCommand(Executor, Command, _, _)
-    local Args = CmdSplit(Command)
-    if (Args[1] == nil or Args[1] == "") then
-        return
-    elseif (Args[1] == ping_command) and hasAccess(Executor) then
+local function CMDSplit(s)
+    local args = {}
+    for arg in s:gmatch('([^%s]+)') do
+        args[#args + 1] = arg:lower()
+    end
+    return args
+end
 
-        local pl = GetPlayers(Executor, Args)
-        if (pl) then
-            for i = 1, #pl do
-                local TargetID = pl[i]
-                local ping = GetPing(TargetID)
-                if (ping) then
-                    if (tonumber(TargetID) == tonumber(Executor)) then
-                        rprint(Executor, gsub(messages[1], "%%ping%%", ping))
-                    else
-                        local name = get_var(TargetID, "$name")
-                        rprint(Executor, gsub(gsub(messages[2], "%%ping%%", ping), "%%name%%", name))
-                    end
-                else
-                    rprint(Executor, messages[4])
-                end
-            end
+local function Respond(Ply, Str)
+    return (Ply == 0 and cprint(Str) or rprint(Ply, Str))
+end
+
+function OnCommand(Ply, CMD, _, _)
+
+    local args = CMDSplit(CMD)
+    if (args and args[1] == ping_command and HasAccess(Ply)) then
+
+        local player = (args[2] and tonumber(args[2]))
+        if (player and player_present(player)) then
+
+            local ping = get_var(player, '$ping')
+            local name = get_var(player, '$name')
+            local str = output :
+            gsub('$ping', ping):
+            gsub('$name', name)
+
+            Respond(Ply, str)
+        else
+            Respond(Ply, 'Invalid Player ID or Player not online!')
         end
+
         return false
     end
 end
 
 function OnScriptUnload()
-
-end
-
-function GetPlayers(Executor, Args)
-    local pl = { }
-    if (Args[2] == nil or Args[2] == "me") then
-        table.insert(pl, Executor)
-    elseif (Args[2]:match("%d+") and player_present(Args[2])) then
-        table.insert(pl, Args[2])
-    elseif (Args[2] == "all" or Args[2] == "*") then
-        for i = 1, 16 do
-            if player_present(i) then
-                table.insert(pl, i)
-            end
-        end
-    else
-        rprint(Executor, gsub(messages[3], "%%cmd%%", Args[1]))
-    end
-    return pl
-end
-
-function CmdSplit(CMD)
-    local t, i = {}, 1
-    for Args in gmatch(CMD, "([^%s]+)") do
-        t[i] = Args
-        i = i + 1
-    end
-    return t
-end
-
-function GetPing(TargetID)
-    if (TargetID ~= 0) then
-        local player = get_player(TargetID)
-        if (player) then
-            return read_word(player + 0xDC)
-        end
-    else
-        cprint("You cannot execute this command from the server terminal", 4 + 8)
-    end
+    -- N/A
 end
