79ecbcc1ed1de2bae836b3ee1f7fc6f989b8f8e2
Chalwk77 <jericho.crosby227@gmail.com>
2022-08-31 14:20:53 +1200

    Refactored

--- SAPP SCRIPTS/ATTRACTIVE MODS/Killer Reward.lua
+++ SAPP SCRIPTS/ATTRACTIVE MODS/Killer Reward.lua
@@ -1,143 +1,206 @@
 --[[
 --=====================================================================================================--
 Script Name: Killer Reward, for SAPP (PC & CE)
-Description: This script will drop 1 of 20 (configurable) random items at your victims death location.
+Description: Random objects (weapons/equipment) will drop at your victims death location.
+             See config section for more.
 
-                MODES:
-                Kills Not Required:
-                    Your victim will drop 1 random "weapon" or "equipment" item (indefinitely), by default.
-
-                Kills Required:
-                    Your victim will drop 1 random "weapon" or "equipment" item when you reach a specific kill threashold.
-                    For every 5 (non-consecutive) kills, your victim will drop an item.
-
-Copyright (c) 2016-2021, Jericho Crosby <jericho.crosby227@gmail.com>
+Copyright (c) 2022, Jericho Crosby <jericho.crosby227@gmail.com>
 * Notice: You can use this document subject to the following conditions:
 https://github.com/Chalwk77/HALO-SCRIPT-PROJECTS/blob/master/LICENSE
 --=====================================================================================================--
 ]]--
 
-api_version = "1.12.0.0"
+local KillerRewards = {
 
-local MOD = {
-
-    -- ITEM SPAWN SETTINGS --
-    -- note: only one option can be enabled!
-
-    -- If true, a combination of weapons and equipment will spawn:
-    ["WeaponsAndEquipment"] = true,
-
-    -- If true, only equipment will spawn:
-    ["JustEquipment"] = false,
-
-    -- If true, only weapons will spawn:
-    ["JustWeapons"] = false,
-
-
-    --==============================================--
-    -- MODES --
-    -- note: only one mode can be enabled!
-
-    -- No kills required (victim will drop item indefinitely):
+    -- Weapons & equipment drop candidates:
+    -- Default: true
     --
-    ["mode 1"] = true,
+    weapons_and_equipment = true,
 
-    -- Kills required (an item will drop every N non-consecutive kills):
+    -- Weapon only drop candidates:
+    -- Default: false
     --
-    ["mode 2"] = false,
+    weapons = false,
+
+    -- Equipment only drop candidates:
+    -- Default: false
+    --
+    equipment = false,
+
+    -- Players must have "consecutive_kills" in order for their victim
+    -- to drop an item:
+    -- Default: true
+    --
+    consecutive = true,
+
+    -- Number of consecutive kills required to trigger a drop:
+    -- Default: 5
+    --
     consecutive_kills = 5,
 
+    -------------------------------------------------------
+    -- Items in these lists are candidates for drops.
+    -- Remove a line to prevent it from dropping.
+    -------------------------------------------------------
 
-    --==============================================--
-
-    -- stock weapon tags --
+    -- weapon tags:
+    -- (class, name, label)
+    --
     weapons = {
-        { "weap", "weapons\\pistol\\pistol" },
-        { "weap", "weapons\\shotgun\\shotgun" },
-        { "weap", "weapons\\needler\\mp_needler" },
-        { "weap", "weapons\\flamethrower\\flamethrower" },
-        { "weap", "weapons\\plasma rifle\\plasma rifle" },
-        { "weap", "weapons\\sniper rifle\\sniper rifle" },
-        { "weap", "weapons\\assault rifle\\assault rifle" },
-        { "weap", "weapons\\plasma pistol\\plasma pistol" },
-        { "weap", "weapons\\plasma_cannon\\plasma_cannon" },
-        { "weap", "weapons\\rocket launcher\\rocket launcher" },
+        { 'weap', 'weapons\\pistol\\pistol', 'Pistol' },
+        { 'weap', 'weapons\\shotgun\\shotgun', 'Shotgun' },
+        { 'weap', 'weapons\\needler\\mp_needler', 'Needler' },
+        { 'weap', 'weapons\\flamethrower\\flamethrower', 'Flamethrower' },
+        { 'weap', 'weapons\\plasma rifle\\plasma rifle', 'Plasma Rifle' },
+        { 'weap', 'weapons\\sniper rifle\\sniper rifle', 'Sniper Rifle' },
+        { 'weap', 'weapons\\assault rifle\\assault rifle', 'Assault Rifle' },
+        { 'weap', 'weapons\\plasma pistol\\plasma pistol', 'Plasma Pistol' },
+        { 'weap', 'weapons\\plasma_cannon\\plasma_cannon', 'Plasma Cannon' },
+        { 'weap', 'weapons\\rocket launcher\\rocket launcher', 'Rocket Launcher' }
     },
 
-    -- stock equipment tags --
+    -- equipment tags:
+    -- (class, name, label)
+    --
     equipment = {
-        { "eqip", "powerups\\health pack" },
-        { "eqip", "powerups\\over shield" },
-        { "eqip", "powerups\\active camouflage" },
-        { "eqip", "powerups\\pistol ammo\\pistol ammo" },
-        { "eqip", "powerups\\shotgun ammo\\shotgun ammo" },
-        { "eqip", "powerups\\needler ammo\\needler ammo" },
-        { "eqip", "powerups\\flamethrower ammo\\flamethrower ammo" },
-        { "eqip", "powerups\\sniper rifle ammo\\sniper rifle ammo" },
-        { "eqip", "powerups\\assault rifle ammo\\assault rifle ammo" },
-        { "eqip", "powerups\\rocket launcher ammo\\rocket launcher ammo" },
+        { 'eqip', 'powerups\\health pack', 'Health Pack' },
+        { 'eqip', 'powerups\\over shield', 'Overshield' },
+        { 'eqip', 'powerups\\active camouflage', 'Camouflage' },
+        { 'eqip', 'powerups\\pistol ammo\\pistol ammo', 'Pistol Ammo' },
+        { 'eqip', 'powerups\\shotgun ammo\\shotgun ammo', 'Shotgun Ammo' },
+        { 'eqip', 'powerups\\needler ammo\\needler ammo', ' Needler Ammo' },
+        { 'eqip', 'powerups\\flamethrower ammo\\flamethrower ammo', 'Flamethrower Ammo' },
+        { 'eqip', 'powerups\\sniper rifle ammo\\sniper rifle ammo', 'Sniper Ammo' },
+        { 'eqip', 'powerups\\assault rifle ammo\\assault rifle ammo', 'Assault Rifle Ammo' },
+        { 'eqip', 'powerups\\rocket launcher ammo\\rocket launcher ammo', ' Rocket Launcher Ammo' }
     }
 }
--- config ends --
+
+local weapons = { }
+local equipment = { }
+local players = { }
+
+api_version = '1.12.0.0'
 
 function OnScriptLoad()
-    register_callback(cb['EVENT_DIE'], "OnDeath")
+    register_callback(cb['EVENT_DIE'], 'OnDeath')
+    register_callback(cb['EVENT_JOIN'], 'OnJoin')
+    register_callback(cb['EVENT_LEAVE'], 'OnQuit')
+    register_callback(cb['EVENT_GAME_START'], 'OnStart')
+    OnStart()
 end
 
-local function GetTag(Type, Name)
-    local Tag = lookup_tag(Type, Name)
-    return Tag ~= 0 and read_dword(Tag + 0xC) or nil
+-- Returns the memory address (meta id) of a tag using its tag class and name:
+-- @return meta id (number)
+local function GetTag(class, name)
+    local tag = lookup_tag(class, name)
+    return (tag ~= 0 and read_dword(tag + 0xC)) or nil
 end
 
-local function GetPos(DyN)
+-- Populate weapons & equipment tables with the meta id's of
+-- the weapon/equipment tag addresses:
+function KillerRewards:TagsToID()
+    local w, e = { }, { }
 
+    for i = 1, #self.weapons do
+        local v = self.weapons[i]
+        local class = v[1]
+        local name = v[2]
+        local label = v[3]
+        local tag = GetTag(class, name)
+        if (tag) then
+            w[i] = { tag, label }
+        end
+    end
+
+    for i = 1, #self.equipment do
+        local v = self.equipment[i]
+        local class = v[1]
+        local name = v[2]
+        local label = v[3]
+        local tag = GetTag(class, name)
+        if (tag) then
+            e[i] = { tag, label }
+        end
+    end
+
+    return w, e
+end
+
+function KillerRewards:NewPlayer(o)
+    setmetatable(o, { __index = self })
+    self.__index = self
+    return o
+end
+
+function KillerRewards:Kills()
+    return tonumber(get_var(self.id, '$kills'))
+end
+
+local function GetXYZ(Ply)
     local x, y, z
 
-    local VehicleID = read_dword(DyN + 0x11C)
-    local Object = get_object_memory(VehicleID)
-
-    if (VehicleID == 0xFFFFFFFF) then
-        x, y, z = read_vector3d(DyN + 0x5c)
-    elseif (Object ~= 0) then
-        x, y, z = read_vector3d(Object + 0x5c)
+    local dyn = get_dynamic_player(Ply)
+    if (dyn ~= 0) then
+        local vehicle = read_dword(dyn + 0x11C)
+        local object = get_object_memory(vehicle)
+        if (vehicle == 0xFFFFFFFF) then
+            x, y, z = read_vector3d(dyn + 0x5C)
+        elseif (object ~= 0) then
+            x, y, z = read_vector3d(object + 0x5C)
+        end
     end
 
     return x, y, z
 end
 
-function MOD:SpawnItem(x, y, z)
+local function GetRandWeap()
+    return weapons[rand(1, #weapons + 1)]
+end
 
-    local n, type, name
+local function GetRandEquip()
+    return equipment[rand(1, #equipment + 1)]
+end
 
-    if (MOD["WeaponsAndEquipment"]) then
-        n = rand(0, 2)
-        if (n == 0) then
-            n = MOD.weapons[rand(1, #MOD.weapons + 1)]
-        else
-            n = MOD.equipment[rand(1, #MOD.equipment + 1)]
-        end
+function KillerRewards:SpawnItem(k)
 
-    elseif (MOD["JustEquipment"]) then
-        n = MOD.equipment[rand(1, #MOD.equipment + 1)]
-
-    elseif (MOD["JustWeapons"]) then
-        n = MOD.weapons[rand(1, #MOD.weapons + 1)]
+    local object
+    if (self.weapons_and_equipment) then
+        local n = rand(1, 3)
+        object = (n == 1 and GetRandWeap() or GetRandEquip())
+    elseif (self.weapons) then
+        object = GetRandWeap()
+    elseif (self.equipment) then
+        object = GetRandEquip()
     end
 
-    type, name = n[1], n[2]
-    if (GetTag(type, name)) then
+    if (not object) then
+        return
+    end
 
-        -- Technical Note --
-        -- Objects are not anchored to the ground.
-        -- We need to delay the spawn logic otherwise they might
-        -- fly away if the victim is blown up.
+    local x, y, z = GetXYZ(self.id)
+    local z_off = 0.3
 
-        timer(500, "SpawnObject", type, name, x, y, z)
+    spawn_object('', '', x, y, z + z_off, 0, object[1])
+    rprint(k.id, object[2] .. ' dropped at ' .. self.name .. 's death location')
+end
+
+function OnStart()
+    if (get_var(0, '$gt') ~= 'n/a') then
+        players = {}
+        weapons, equipment = KillerRewards:TagsToID()
     end
 end
 
-function SpawnObject(type, name, x, y, z)
-    spawn_object(type, name, x, y, z + 0.5)
+function OnJoin(Ply)
+    players[Ply] = KillerRewards:NewPlayer({
+        id = Ply,
+        name = get_var(Ply, '$name')
+    })
+end
+
+function OnQuit(Ply)
+    players[Ply] = nil
 end
 
 function OnDeath(Victim, Killer)
@@ -145,18 +208,13 @@ function OnDeath(Victim, Killer)
     local victim = tonumber(Victim)
     local killer = tonumber(Killer)
 
-    if (killer > 0 and player_present(killer) and killer ~= victim) then
+    local k = players[killer]
+    local v = players[victim]
 
-        local DyN = get_dynamic_player(victim)
-        if (DyN ~= 0) then
+    local pvp = (killer > 0 and k and v and killer ~= victim)
 
-            local x, y, z = GetPos(DyN)
-            local consecutive = (tonumber(get_var(killer, "$kills")) % MOD.consecutive_kills == 0)
-
-            if (MOD["mode 1"] or (MOD["mode 2"] and consecutive)) then
-                MOD:SpawnItem(x, y, z)
-            end
-        end
+    if (pvp and (k.consecutive and k:Kills() % k.consecutive_kills == 0 or not k.consecutive)) then
+        v:SpawnItem(k)
     end
 end
 
