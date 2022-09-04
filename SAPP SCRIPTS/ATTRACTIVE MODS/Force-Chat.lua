5ae6f074f7eabb076d3ee43bfdf3a00cce03cffd
Chalwk77 <jericho.crosby227@gmail.com>
2022-09-04 14:32:59 +1200

    ...

--- SAPP SCRIPTS/ATTRACTIVE MODS/Force-Chat.lua
+++ SAPP SCRIPTS/ATTRACTIVE MODS/Force-Chat.lua
@@ -13,67 +13,30 @@ https://github.com/Chalwk77/HALO-SCRIPT-PROJECTS/blob/master/LICENSE
 --=====================================================================================================--
 ]]--
 
-local ForceChat = {
+-- Custom command used to force a player to say something:
+local command = 'fc'
 
-    -- Custom command used to force a player to say something:
-    command = 'fc',
+-- Minimum permission level required to use the command:
+--
+local permission_level = 1
 
-    -- Minimum permission level required to use the command:
-    --
-    permission_level = 1,
+-- Chat message format:
+--
+local format = '$name: $msg'
 
-    -- Chat message format:
-    --
-    format = '$name: $msg',
-
-    -- A message relay function temporarily removes the "msg_prefix" and will
-    -- restore it to this when finished:
-    --
-    prefix = '**SAPP**'
-}
+-- A message relay function temporarily removes the "msg_prefix" and will
+-- restore it to this when finished:
+--
+local prefix = '**SAPP**'
 
 api_version = '1.12.0.0'
 
-local players = { }
-
 function OnScriptLoad()
-    register_callback(cb['EVENT_JOIN'], 'OnJoin')
-    register_callback(cb['EVENT_LEAVE'], 'OnQuit')
     register_callback(cb['EVENT_COMMAND'], 'OnCommand')
-    register_callback(cb['EVENT_GAME_START'], 'OnStart')
-    OnStart()
 end
 
-function ForceChat:NewPlayer(o)
-    setmetatable(o, self)
-    self.__index = self
-    return o
-end
-
-function ForceChat:HasPerm()
-    return (tonumber(get_var(self.id, "$lvl")) >= self.permission_level)
-end
-
-function OnStart()
-    if (get_var(0, '$gt') ~= 'n/a') then
-        players = {}
-        for i = 1, 16 do
-            if player_present(i) then
-                OnJoin(i)
-            end
-        end
-    end
-end
-
-function OnJoin(Ply)
-    players[Ply] = ForceChat:NewPlayer({
-        id = Ply,
-        name = get_var(Ply, '$name')
-    })
-end
-
-function OnQuit(Ply)
-    players[Ply] = nil
+local function HasPerm(Ply)
+    return (tonumber(get_var(Ply, "$lvl")) >= permission_level)
 end
 
 local function CMDSplit(s)
@@ -86,28 +49,26 @@ end
 
 function OnCommand(Ply, CMD)
     local args = CMDSplit(CMD)
-    local player = players[Ply]
-    if (args and player) then
+    if (args) then
 
-        local command = (args[1]:sub(1, player.command:len()) == player.command)
+        local cmd = (args[1]:sub(1, command:len()) == command)
         local victim = (args[2] and tonumber(args[2]:match('%d+')))
 
-        if (not victim) then
+        if (not victim or not args[3]) then
             rprint(Ply, 'Usage: /fc <player> <message>')
             return false
-        elseif (command) then
+        elseif (cmd) then
 
-            if (player:HasPerm()) then
+            if (HasPerm(Ply)) then
 
                 if (victim and player_present(victim)) then
 
                     local message = table.concat(args, ' ', 3)
                     if (message and message ~= '') then
-                        local name = players[victim].name
-                        local format = player.format
+                        local name = get_var(victim, '$name')
                         execute_command('msg_prefix ""')
                         say_all(format:gsub('$name', name):gsub('$msg', message))
-                        execute_command('msg_prefix "' .. player.prefix .. '"')
+                        execute_command('msg_prefix "' .. prefix .. '"')
                     else
                         rprint(Ply, 'Usage: /fc <player> <message>')
                     end
