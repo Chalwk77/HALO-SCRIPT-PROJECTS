1aa823715720f0f8a0cd95f50d9ea35173ee09fb
Chalwk77 <jericho.crosby227@gmail.com>
2022-09-04 17:47:41 +1200

    ...

--- SAPP SCRIPTS/ATTRACTIVE MODS/Tactical Insertion.lua
+++ SAPP SCRIPTS/ATTRACTIVE MODS/Tactical Insertion.lua
@@ -1,10 +1,11 @@
 --[[
 --=====================================================================================================--
 Script Name: Tactical Insertion, for SAPP (PC & CE)
-Description: Set your next spawn point with a custom command.
-             See config section for more.
+Description: Set your next spawn point with a custom command (/ti).
 
-Copyright (c) 2019-2022, Jericho Crosby <jericho.crosby227@gmail.com>
+Players are limited to 5 (default) uses per game.
+
+Copyright (c) 2022, Jericho Crosby <jericho.crosby227@gmail.com>
 * Notice: You can use this document subject to the following conditions:
 https://github.com/Chalwk77/HALO-SCRIPT-PROJECTS/blob/master/LICENSE
 --=====================================================================================================--
@@ -12,19 +13,10 @@ https://github.com/Chalwk77/HALO-SCRIPT-PROJECTS/blob/master/LICENSE
 
 -- config starts
 -- Set the command you want to use to set your next spawn point:
--- Syntax: /ti
---
 local command = 'ti'
 
 -- Number of uses per game:
---
 local uses_per_game = 5
-
--- Minimum permission level required to execute the custom command:
--- Default: -1
--- Valid levels: (-1 = everyone, 1-4 = admins)
---
-local permission_level = -1
 -- config ends
 
 api_version = '1.12.0.0'
@@ -53,8 +45,6 @@ function OnStart()
     end
 end
 
--- Creates an iterable cache of player data:
--- (player_id, { uses = 0, spawn = nil })
 function OnJoin(Ply)
     players[Ply] = {
         teleport = false,
@@ -65,14 +55,10 @@ function OnJoin(Ply)
     }
 end
 
--- Called when a player quits the server:
---
 function OnQuit(Ply)
     players[Ply] = nil
 end
 
--- Returns a table of strings:
---
 local function CMDSplit(s)
     local args = {}
 
@@ -83,10 +69,6 @@ local function CMDSplit(s)
     return args
 end
 
-
--- Retrieves a player's map coordinates and
--- returns 3x 32 bit floating point numbers
---
 local function GetXYZ(Ply)
     local x, y, z
 
@@ -100,12 +82,10 @@ local function GetXYZ(Ply)
             x, y, z = read_vector3d(object + 0x5c)
         end
     end
+
     return x, y, z
 end
 
--- STAR OF THE SHOW:
--- Called when a player is respawning.
---
 function OnPreSpawn(Ply)
     local dyn = get_dynamic_player(Ply)
     local p = players[Ply]
@@ -117,57 +97,45 @@ function OnPreSpawn(Ply)
     end
 end
 
-local function HasPermission(Ply)
-    local lvl = tonumber(get_var(Ply, '$lvl'))
-    return (lvl >= permission_level)
-end
-
--- Our custom command handler:
---
 function OnCommand(Ply, CMD)
     local args = CMDSplit(CMD)
 
     if (args and CMD:sub(1, command:len()):lower() == command) then
 
-        if HasPermission(Ply) then
-            if player_alive(Ply) then
+        if player_alive(Ply) then
 
-                local p = players[Ply]
-                if (p.uses <= 0) then
-                    rprint(Ply, "You have no more uses left for this game.")
-                    return false
-                end
-
-                p.uses = p.uses - 1
-                local x, y, z = GetXYZ(Ply)
-                players[Ply] = {
-                    teleport = true,
-                    uses = p.uses,
-                    x = x,
-                    y = y,
-                    z = z
-                }
-
-                local str = string.format("You have %s uses left for this game.", p.uses)
-                x = format('%.2f', x)
-                y = format('%.2f', y)
-                z = format('%.2f', z)
-                rprint(Ply, 'Tac-Inset set to ' .. x .. ', ' .. y .. ', ' .. z .. '.')
-                rprint(Ply, str)
-            else
-                rprint(Ply, 'Command failed. You are not alive.')
+            local p = players[Ply]
+            if (p.uses <= 0) then
+                rprint(Ply, 'You have no more uses left for this game.')
+                return false
             end
+
+            p.uses = p.uses - 1
+
+            local x, y, z = GetXYZ(Ply)
+            players[Ply] = {
+                teleport = true,
+                uses = p.uses,
+                x = x,
+                y = y,
+                z = z
+            }
+
+            local str = format('You have %s uses left for this game.', p.uses)
+            x = format('%.2f', x)
+            y = format('%.2f', y)
+            z = format('%.2f', z)
+
+            rprint(Ply, 'Tac-Inset set to ' .. x .. ', ' .. y .. ', ' .. z .. '.')
+            rprint(Ply, str)
         else
-            rprint(Ply, 'Command failed. You do not have permission to use this command')
+            rprint(Ply, 'Command failed. You are not alive.')
         end
 
         return false
     end
 end
 
--- Although not used in this script,
--- without it, SAPP will throw an error.
---
 function OnScriptUnload()
     -- N/A
 end
