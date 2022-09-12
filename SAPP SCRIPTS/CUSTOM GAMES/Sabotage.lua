d5a06e17ba5dcbb784817fb14b087dff528409c6
Chalwk77 <jericho.crosby227@gmail.com>
2022-09-12 21:33:05 +1200

    ...

--- SAPP SCRIPTS/CUSTOM GAMES/Sabotage.lua
+++ SAPP SCRIPTS/CUSTOM GAMES/Sabotage.lua
@@ -3,21 +3,6 @@
 Script Name: Sabotage, for SAPP (PC & CE)
 Description:
 
-> Bomb spawns in the middle of the map.
-> Players have to take the bomb to red zone and plant it.
-- Bomb timer initiates when the bomb is planted.
-- Bomb timer is 30 seconds.
-
-> Enemies can defuse the bomb (crouch to defuse)
-
-Game over if:
-- Bomb explodes
-- Bomb is defused
-
-If the game timer runs out, teams go into sudden death.
-* No respawns
-
-
 Copyright (c) 2022, Jericho Crosby <jericho.crosby227@gmail.com>
 * Notice: You can use this document subject to the following conditions:
 https://github.com/Chalwk77/HALO-SCRIPT-PROJECTS/blob/master/LICENSE
@@ -75,152 +60,152 @@ local Sabotage = {
         -- Format: { x, y, z, trigger radius }
         --
         base_locations = {
-            ['red'] = { 95.687797546387, -159.44900512695, -0.10000000149012, 5 },
-            ['blue'] = { 40.240600585938, -79.123199462891, -0.10000000149012, 5 }
+            ['red'] = { 95.687797546387, -159.44900512695, -0.10000000149012, 0.3 },
+            ['blue'] = { 40.240600585938, -79.123199462891, -0.10000000149012, 0.3 }
         }
     },
 
     ["deathisland"] = {
         spawn_location = { -30.282, 31.312, 16.601 },
         base_locations = {
-            ['red'] = { -26.576030731201, -6.9761986732483, 9.6631727218628, 5 },
-            ['blue'] = { 29.843469619751, 15.971487045288, 8.2952880859375, 5 }
+            ['red'] = { -26.576030731201, -6.9761986732483, 9.6631727218628, 0.3 },
+            ['blue'] = { 29.843469619751, 15.971487045288, 8.2952880859375, 0.3 }
         }
     },
 
     ["icefields"] = {
         spawn_location = { -26.032, 32.365, 9.007 },
         base_locations = {
-            ['red'] = { 24.85000038147, -22.110000610352, 2.1110000610352, 5 },
-            ['blue'] = { -77.860000610352, 86.550003051758, 2.1110000610352, 5 }
+            ['red'] = { 24.85000038147, -22.110000610352, 2.1110000610352, 0.3 },
+            ['blue'] = { -77.860000610352, 86.550003051758, 2.1110000610352, 0.3 }
         }
     },
 
     ["infinity"] = {
         spawn_location = { 9.631, -64.030, 7.776 },
         base_locations = {
-            ['red'] = { 0.67973816394806, -164.56719970703, 15.039022445679, 5 },
-            ['blue'] = { -1.8581243753433, 47.779975891113, 11.791272163391, 5 }
+            ['red'] = { 0.67973816394806, -164.56719970703, 15.039022445679, 0.3 },
+            ['blue'] = { -1.8581243753433, 47.779975891113, 11.791272163391, 0.3 }
         }
     },
 
     ["sidewinder"] = {
         spawn_location = { 2.051, 55.220, -2.801 },
         base_locations = {
-            ['red'] = { -32.038200378418, -42.066699981689, -3.7000000476837, 5 },
-            ['blue'] = { 30.351499557495, -46.108001708984, -3.7000000476837, 5 }
+            ['red'] = { -32.038200378418, -42.066699981689, -3.7000000476837, 0.3 },
+            ['blue'] = { 30.351499557495, -46.108001708984, -3.7000000476837, 0.3 }
         }
     },
 
     ["timberland"] = {
         spawn_location = { 1.250, -1.487, -21.264 },
         base_locations = {
-            ['red'] = { 17.322099685669, -52.365001678467, -17.751399993896, 5 },
-            ['blue'] = { -16.329900741577, 52.360000610352, -17.741399765015, 5 }
+            ['red'] = { 17.322099685669, -52.365001678467, -17.751399993896, 0.3 },
+            ['blue'] = { -16.329900741577, 52.360000610352, -17.741399765015, 0.3 }
         }
     },
 
     ["dangercanyon"] = {
         spawn_location = { -0.477, 55.331, 0.239 },
         base_locations = {
-            ['red'] = { -12.104507446289, -3.4351840019226, -2.2419033050537, 5 },
-            ['blue'] = { 12.007399559021, -3.4513700008392, -2.2418999671936, 5 }
+            ['red'] = { -12.104507446289, -3.4351840019226, -2.2419033050537, 0.3 },
+            ['blue'] = { 12.007399559021, -3.4513700008392, -2.2418999671936, 0.3 }
         }
     },
 
     ["beavercreek"] = {
         spawn_location = { 14.015, 14.238, -0.911 },
         base_locations = {
-            ['red'] = { 29.055599212646, 13.732000350952, -0.10000000149012, 5 },
-            ['blue'] = { -0.86037802696228, 13.764800071716, -0.0099999997764826, 5 }
+            ['red'] = { 29.055599212646, 13.732000350952, -0.10000000149012, 0.3 },
+            ['blue'] = { -0.86037802696228, 13.764800071716, -0.0099999997764826, 0.3 }
         }
     },
 
     ["boardingaction"] = {
         spawn_location = { 4.374, -12.832, 7.220 },
         base_locations = {
-            ['red'] = { 1.723109960556, 0.4781160056591, 0.60000002384186, 5 },
-            ['blue'] = { 18.204000473022, -0.53684097528458, 0.60000002384186, 5 }
+            ['red'] = { 1.723109960556, 0.4781160056591, 0.60000002384186, 0.3 },
+            ['blue'] = { 18.204000473022, -0.53684097528458, 0.60000002384186, 0.3 }
         }
     },
 
     ["carousel"] = {
         spawn_location = { 0.033, 0.003, -0.856 },
         base_locations = {
-            ['red'] = { 5.6063799858093, -13.548299789429, -3.2000000476837, 5 },
-            ['blue'] = { -5.7499198913574, 13.886699676514, -3.2000000476837, 5 }
+            ['red'] = { 5.6063799858093, -13.548299789429, -3.2000000476837, 0.3 },
+            ['blue'] = { -5.7499198913574, 13.886699676514, -3.2000000476837, 0.3 }
         }
     },
 
     ["chillout"] = {
         spawn_location = { 1.392, 4.700, 3.108 },
         base_locations = {
-            ['red'] = { 7.4876899719238, -4.49059009552, 2.5, 5 },
-            ['blue'] = { -7.5086002349854, 9.750340461731, 0.10000000149012, 5 }
+            ['red'] = { 7.4876899719238, -4.49059009552, 2.5, 0.3 },
+            ['blue'] = { -7.5086002349854, 9.750340461731, 0.10000000149012, 0.3 }
         }
     },
 
     ["damnation"] = {
         spawn_location = { -2.002, -4.301, 3.399 },
         base_locations = {
-            ['red'] = { 9.6933002471924, -13.340399742126, 6.8000001907349, 5 },
-            ['blue'] = { -12.17884349823, 14.982703208923, -0.20000000298023, 5 }
+            ['red'] = { 9.6933002471924, -13.340399742126, 6.8000001907349, 0.3 },
+            ['blue'] = { -12.17884349823, 14.982703208923, -0.20000000298023, 0.3 }
         }
     },
 
     ["gephyrophobia"] = {
         spawn_location = { 63.513, -74.088, -1.062 },
         base_locations = {
-            ['red'] = { 26.884338378906, -144.71551513672, -16.049139022827, 5 },
-            ['blue'] = { 26.727857589722, 0.16621616482735, -16.048349380493, 5 }
+            ['red'] = { 26.884338378906, -144.71551513672, -16.049139022827, 0.3 },
+            ['blue'] = { 26.727857589722, 0.16621616482735, -16.048349380493, 0.3 }
         }
     },
 
     ["hangemhigh"] = {
         spawn_location = { 21.020, -4.632, -4.229 },
         base_locations = {
-            ['red'] = { 13.047902107239, 9.0331249237061, -3.3619771003723, 5 },
-            ['blue'] = { 32.655700683594, -16.497299194336, -1.7000000476837, 5 }
+            ['red'] = { 13.047902107239, 9.0331249237061, -3.3619771003723, 0.3 },
+            ['blue'] = { 32.655700683594, -16.497299194336, -1.7000000476837, 0.3 }
         }
     },
 
     ["longest"] = {
         spawn_location = { -0.84, -14.54, 2.41 },
         base_locations = {
-            ['red'] = { -12.791899681091, -21.6422996521, -0.40000000596046, 5 },
-            ['blue'] = { 11.034700393677, -7.5875601768494, -0.40000000596046, 5 }
+            ['red'] = { -12.791899681091, -21.6422996521, -0.40000000596046, 0.3 },
+            ['blue'] = { 11.034700393677, -7.5875601768494, -0.40000000596046, 0.3 }
         }
     },
 
     ["prisoner"] = {
         spawn_location = { 0.902, 0.088, 1.392 },
         base_locations = {
-            ['red'] = { -9.3684597015381, -4.9481601715088, 5.6999998092651, 5 },
-            ['blue'] = { 9.3676500320435, 5.1193399429321, 5.6999998092651, 5 }
+            ['red'] = { -9.3684597015381, -4.9481601715088, 5.6999998092651, 0.3 },
+            ['blue'] = { 9.3676500320435, 5.1193399429321, 5.6999998092651, 0.3 }
         }
     },
 
     ["putput"] = {
         spawn_location = { -2.350, -21.121, 0.902 },
         base_locations = {
-            ['red'] = { -18.89049911499, -20.186100006104, 1.1000000238419, 5 },
-            ['blue'] = { 34.865299224854, -28.194700241089, 0.10000000149012, 5 }
+            ['red'] = { -18.89049911499, -20.186100006104, 1.1000000238419, 0.3 },
+            ['blue'] = { 34.865299224854, -28.194700241089, 0.10000000149012, 0.3 }
         }
     },
 
     ["ratrace"] = {
         spawn_location = { 8.662, -11.159, 0.221 },
         base_locations = {
-            ['red'] = { -4.2277698516846, -0.85564690828323, -0.40000000596046, 5 },
-            ['blue'] = { 18.613000869751, -22.652599334717, -3.4000000953674, 5 }
+            ['red'] = { -4.2277698516846, -0.85564690828323, -0.40000000596046, 0.3 },
+            ['blue'] = { 18.613000869751, -22.652599334717, -3.4000000953674, 0.3 }
         }
     },
 
     ["wizard"] = {
         spawn_location = { -5.035, -5.064, -2.750 },
         base_locations = {
-            ['red'] = { -9.2459697723389, 9.3335800170898, -2.5999999046326, 5 },
-            ['blue'] = { 9.1828498840332, -9.1805400848389, -2.5999999046326, 5 }
+            ['red'] = { -9.2459697723389, 9.3335800170898, -2.5999999046326, 0.3 },
+            ['blue'] = { 9.1828498840332, -9.1805400848389, -2.5999999046326, 0.3 }
         }
     },
 }
@@ -306,6 +291,10 @@ local function ProgressBar(start, finish, plant_time)
     return bar
 end
 
+local function GetOppositeTeam(team)
+    return (team == 'red' and 'blue' or 'red')
+end
+
 function Sabotage:PlantBomb(timer)
 
     local start = timer.start
@@ -320,7 +309,7 @@ function Sabotage:PlantBomb(timer)
         drop_weapon(self.id)
 
         -- set the team bomb belongs to:
-        bomb.team = self.team
+        bomb.team = GetOppositeTeam(self.team)
 
         bomb_planted = true
 
@@ -340,9 +329,14 @@ function Sabotage:PlantBomb(timer)
     end
 end
 
+local function UpdateTeamScore(team)
+    local score = (team == 'red' and get_var(0, '$redscore') or get_var(0, '$bluescore'))
+    execute_command("team_score " .. team .. " " .. score + 1000)
+end
+
 function Sabotage:DefuseBomb(timer)
 
-    local start = timer.start()
+    local start = timer.start
     local finish = timer.finish
 
     if (not bomb.defuser) then
@@ -354,17 +348,17 @@ function Sabotage:DefuseBomb(timer)
         Say(self.id, players[defuser].name .. ' is already defusing the bomb!')
         return
     elseif (self.team_defuse) then
-        start = start + 1
+        --start = start + 1 -- needs to be start()
     end
 
-    if (start >= finish) then
+    if (start() >= finish) then
 
+        UpdateTeamScore(bomb.team)
+        destroy_object(bomb.object_mem)
         Say(_, bomb.team .. ' won!')
         Say(_, 'Bomb has been defused!')
-        bomb = nil
+        bomb, bomb_planted = nil, false
 
-        bomb_planted = false
-        execute_command('sv_map_next')
         return
     end
 
@@ -402,7 +396,6 @@ function OnStart()
                 register_callback(cb['EVENT_LEAVE'], 'OnQuit')
                 register_callback(cb['EVENT_DIE'], 'SpawnDeath')
                 register_callback(cb['EVENT_SPAWN'], 'SpawnDeath')
-                register_callback(cb['EVENT_WEAPON_DROP'], 'OnDrop')
                 register_callback(cb['EVENT_TEAM_SWITCH'], 'OnTeamSwitch')
 
                 Sabotage:SpawnBomb()
@@ -415,7 +408,6 @@ function OnStart()
             unregister_callback(cb['EVENT_JOIN'])
             unregister_callback(cb['EVENT_SPAWN'])
             unregister_callback(cb['EVENT_LEAVE'])
-            unregister_callback(cb['EVENT_WEAPON_DROP'])
             unregister_callback(cb['EVENT_TEAM_SWITCH'])
             error('Map not configured!')
         else
@@ -493,11 +485,18 @@ function Sabotage:OnTick()
         local finish = explosion_timer.finish
 
         if (start() >= finish) then
-            bomb_planted = false
-            execute_command('sv_map_next')
+
+            team = GetOppositeTeam(team)
+            UpdateTeamScore(team)
+            destroy_object(bomb.object_mem)
             Say(_, 'Bomb has exploded!')
-            Say(_, bomb.team .. ' won!')
+            Say(_, team .. ' won!')
+            bomb, bomb_planted = nil, false
             return
+        else
+            for i, _ in ipairs(players) do
+                Say(i, 'Bomb will explode in ' .. (finish - start()) .. ' seconds!')
+            end
         end
     end
 
@@ -509,13 +508,9 @@ function Sabotage:OnTick()
             local has_bomb = v:HasBomb(dyn)
             if (has_bomb and not bomb_planted) then
 
-                if (v.assign) then
-                    v.assign = false
-                    assign_weapon(i, bomb.object)
-                end
-
                 local px, py, pz = GetXYZ(dyn)
-                local loc = map.base_locations[v.team]
+                local team = GetOppositeTeam(v.team)
+                local loc = map.base_locations[team]
 
                 local bx, by, bz = loc[1], loc[2], loc[3]
                 local radius = loc[4]
@@ -531,7 +526,7 @@ function Sabotage:OnTick()
                 else
                     v.timer = nil
                 end
-            elseif (bomb_planted and v.team ~= bomb.team) then
+            elseif (bomb_planted and v.team == bomb.team) then
 
                 local loc = map.base_locations[bomb.team]
                 local bx, by, bz = loc[1], loc[2], loc[3]
@@ -577,21 +572,9 @@ end
 
 function SpawnDeath(Ply)
     local player = players[Ply]
-
     player.timer = nil
-    player.assign = false
     player.has_bomb = false
-
-    if (Ply == bomb.defuser) then
-        bomb.defuser = nil
-    end
-end
-
-function OnDrop(Ply)
-    local player = players[Ply]
-    if (player.has_bomb) then
-        player.assign = true
-    end
+    bomb.defuser = (Ply == bomb.defuser) and nil or bomb.defuser
 end
 
 function OnScriptUnload()
