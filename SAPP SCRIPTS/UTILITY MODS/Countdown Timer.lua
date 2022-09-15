5eae4403982348b1f63c2839fe2aecd74d61fa62
Chalwk77 <jericho.crosby227@gmail.com>
2022-09-15 13:30:52 +1200

    ...

--- SAPP SCRIPTS/UTILITY MODS/Countdown Timer.lua
+++ SAPP SCRIPTS/UTILITY MODS/Countdown Timer.lua
@@ -1,12 +1,14 @@
 --[[
 --=====================================================================================================--
 Script Name: Countdown Timer, for SAPP (PC & CE)
-Description: N/A
+Description: This script is intended for use by other developers.
 
-Call "startTimer()" to initiate the countdown.
-Call "stopTimer()" to cancel the countdown and reset the clock to 0.
+             It provides a simple way to display a countdown timer in the following format:
+             "Game will being in: %02d seconds".
 
-Copyright (c) 2016-2018, Jericho Crosby <jericho.crosby227@gmail.com>
+             This script is not intended to be used as a standalone script.
+
+Copyright (c) 2016-2022, Jericho Crosby <jericho.crosby227@gmail.com>
 * Notice: You can use this document subject to the following conditions:
 https://github.com/Chalwk77/HALO-SCRIPT-PROJECTS/blob/master/LICENSE
 --=====================================================================================================--
@@ -14,57 +16,58 @@ https://github.com/Chalwk77/HALO-SCRIPT-PROJECTS/blob/master/LICENSE
 
 api_version = "1.12.0.0"
 
--- Configuration [starts]
+local delay = 5
+local output = "Game will being in: %02d seconds"
 
-local delay = 5 -- In Seconds
-
--- Configuration [ends] << ----------
-
-local init
-local original_delay = delay
+local timer
 
 function OnScriptLoad()
-    register_callback(cb["EVENT_GAME_START"], "OnStart")
-    register_callback(cb["EVENT_GAME_END"], "OnEnd")
+    register_callback(cb['EVENT_TICK'], 'OnTick')
+    register_callback(cb['EVENT_GAME_END'], 'OnEnd')
+    register_callback(cb['EVENT_GAME_START'], 'OnStart')
     OnStart()
 end
 
+local time = os.time
+local function NewTimer()
+    return {
+        start = time,
+        finish = time() + delay
+    }
+end
+
 function OnStart()
-    init = false
-    if (get_var(0, '$gt') ~= 'n/a') then
-        init = true
-        timer(1000, 'Counter')
-    end
+    timer = (get_var(0, '$gt') ~= 'n/a') and NewTimer() or nil
 end
 
 function OnEnd()
-    init = false
+    timer = nil
 end
 
-local function TimerRemaining(seconds)
-    seconds = seconds % 60
-    return seconds
+local format = string.format
+local function TimerRemaining(s)
+    s = s % 60
+    return format(output, s)
 end
 
-function Counter()
-
-    if (not init) then
-        return false
+local function Say(p, m)
+    for _ = 1, 25 do
+        rprint(p, ' ')
     end
+    rprint(p, m)
+end
 
-    delay = delay - 1
-    local time = TimerRemaining(delay)
-    cprint('Game will begin in ' .. time .. ' seconds', 12)
-
-    if (delay == 0) then
-        cprint('The game has begun!', 10)
-
-        delay = original_delay
-        ----------
-        -- DO SOMETHING HERE
-        ----------
-        return false
+function OnTick()
+    if (timer and timer.start() >= timer.finish) then
+        timer = nil
+        -----------------------
+        -- DO SOMETHING HERE --
+        -----------------------
+    elseif (timer) then
+        for i = 1, 16 do
+            if player_present(i) then
+                Say(i, TimerRemaining(timer.finish - time()))
+            end
+        end
     end
-
-    return (init and delay > 0)
 end
