4603dfa2e817d5aff7ca8071dcc15985d84e7136
Chalwk77 <jericho.crosby227@gmail.com>
2023-06-19 13:53:02 +1200

    ...

--- SAPP SCRIPTS/ATTRACTIVE MODS/Swat.lua
+++ SAPP SCRIPTS/ATTRACTIVE MODS/Swat.lua
@@ -1,7 +1,7 @@
 --[[
 --=====================================================================================================--
 Script Name: Swat, for SAPP (PC & CE)
-Description: An extremely simple adaptation of "swat" from Halo Reach.
+Description: An extremely simple adaptation of 'swat' from Halo Reach.
 
 Copyright (c) 2020, Jericho Crosby <jericho.crosby227@gmail.com>
 * Notice: You can use this document subject to the following conditions:
@@ -9,16 +9,16 @@ https://github.com/Chalwk77/HALO-SCRIPT-PROJECTS/blob/master/LICENSE
 --=====================================================================================================--
 ]]--
 
-api_version = "1.12.0.0"
+api_version = '1.12.0.0'
 
 function OnScriptLoad()
-    register_callback(cb["EVENT_DAMAGE_APPLICATION"], "OnDamageApplication")
+    register_callback(cb['EVENT_DAMAGE_APPLICATION'], 'OnDamage')
 end
 
-function OnDamageApplication(PlayerIndex, CauserIndex, _, Damage, HitString, _)
-    if (CauserIndex > 0 and PlayerIndex ~= CauserIndex) then
-        if (HitString == "head") then
-            return true, Damage * 1000
+function OnDamage(victim, killer, _, damage, hitString)
+    if (killer > 0 and victim ~= killer) then
+        if (hitString == 'head') then
+            return true, damage * 1000
         else
             return false
         end
