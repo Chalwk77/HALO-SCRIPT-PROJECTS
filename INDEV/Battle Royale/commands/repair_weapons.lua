28ac0dc0a1e6c7d473af05b4705d67ccae982aae
Chalwk77 <jericho.crosby227@gmail.com>
2023-07-06 11:14:07 +1200

    ...

---
+++ INDEV/Battle Royale/commands/repair_weapons.lua
@@ -0,0 +1,57 @@
+local Command = {}
+
+-- Executes this command.
+-- @param id = player id (number)
+-- @param args (table of command strings)
+--
+function Command:run(id, args)
+
+    local p = self.players[id]
+
+    if not p:commandEnabled(self.enabled, self.name) then
+        return
+    elseif not p:hasPermission(self.level) then
+        return
+    end
+
+    if (not p.weapon_parts) then
+        p:newMessage("You don't have weapon parts!", 5)
+        return
+    end
+
+    self:repairsWeapons(p)
+end
+
+function Command:repairsWeapons(player)
+
+    local id = player.id
+    local dyn = get_dynamic_player(id)
+    if (dyn == 0 or not player_alive(id)) then
+        self:newMessage('You are dead! Unable to repair weapons.', 5)
+        return
+    end
+
+    local repaired
+    for i = 0, 3 do
+
+        local this_weapon = read_dword(dyn + 0x2F8 + i * 4)
+        local object = get_object_memory(this_weapon)
+
+        if (this_weapon ~= 0xFFFFFFFF and object ~= 0) then
+            local weapon = self.decay[object]
+            if (weapon and weapon.decay > 0) then
+                weapon.decay = 0
+                repaired = true
+            end
+        end
+    end
+
+    player.weapon_parts = nil
+    if (repaired) then
+        player:newMessage('Weapon repaired!', 5)
+    else
+        player:newMessage('Nothing was repaired!', 5)
+    end
+end
+
+return Command
