7ff1bfca765c0a007eeb4b8d97d9e114d5c4585d
Chalwk77 <jericho.crosby227@gmail.com>
2024-08-10 10:20:36 +1200

    Refactored

--- SAPP SCRIPTS/UTILITY MODS/Delay Skip.lua
+++ SAPP SCRIPTS/UTILITY MODS/Delay Skip.lua
@@ -1,56 +1,71 @@
 --[[
 --=====================================================================================================--
-Script Name: Skip Delay, for SAPP (PC & CE)
+Script Name: SkipDelay, for SAPP (PC & CE)
 Description: Prevent players from skipping the map too early.
 
-Copyright (c) 2021-2022, Jericho Crosby <jericho.crosby227@gmail.com>
+Copyright (c) 2020-2024, Jericho Crosby <jericho.crosby227@gmail.com>
 Notice: You can use this script subject to the following conditions:
 https://github.com/Chalwk77/HALO-SCRIPT-PROJECTS/blob/master/LICENSE
 --=====================================================================================================--
 ]]--
 
--- Players have to wait this many seconds before they can skip the map:
--- (Default: 10 seconds)
---
-local delay = 10
+-- Configuration section:
+-- Minimum time players must wait before they can skip the map:
+local skipDelay = 300
 
+-- Configure the skip delay message template:
+local skipDelayMessage = 'Please wait %s %s before skipping the map.'
+
+-- Script API version:
 api_version = "1.12.0.0"
 
-local time
-local clock = os.clock
-local ceil = math.ceil
+-- Configuration ends here.
 
-function OnScriptLoad()
+-- Tracks the time when the game starts:
+local gameStartTime
 
-    register_callback(cb['EVENT_CHAT'], 'Skip')
-    register_callback(cb['EVENT_GAME_END'], 'OnEnd')
-    register_callback(cb['EVENT_GAME_START'], 'OnStart')
-
-    OnStart()
-end
-
-local function Plural(n)
+-- Local helper function to add a plural suffix if needed:
+local function addPluralSuffix(n)
     return (n > 1 and 's') or ''
 end
 
-function Skip(Ply, Msg)
-    if (time and Msg:sub(1, Msg:len()):lower() == 'skip') then
-        local n = ceil(time + delay - clock())
-        if (n > 0) then
-            rprint(Ply, 'Please wait ' .. n .. ' second' .. Plural(n) .. ' before skipping.')
+-- Calculates remaining time before players can skip:
+local function getRemainingTime()
+    return math.ceil(gameStartTime + skipDelay - os.clock())
+end
+
+-- Local references to built-in functions:
+local format = string.format
+
+-- Script events and functions:
+function OnScriptLoad()
+    register_callback(cb['EVENT_CHAT'], 'Skip')
+    register_callback(cb['EVENT_GAME_END'], 'OnEnd')
+    register_callback(cb['EVENT_GAME_START'], 'OnStart')
+end
+
+-- Handles skipping the map:
+function Skip(playerIndex, message)
+    if gameStartTime and message:lower() == 'skip' then
+        local remainingTime = getRemainingTime()
+        if remainingTime > 0 then
+            rprint(playerIndex, format(skipDelayMessage, remainingTime, addPluralSuffix(remainingTime)))
             return false
         end
     end
 end
 
+-- Called when a game starts:
 function OnStart()
-    time = (get_var(0, '$gt') ~= 'n/a') and os.clock() or nil
+    gameStartTime = (get_var(0, '$gt') ~= 'n/a') and os.clock() or nil
 end
 
+-- Called when a game ends:
 function OnEnd()
-    time = nil
+    gameStartTime = nil
 end
 
+-- Placeholder function for unloading the script:
 function OnScriptUnload()
     -- N/A
 end
