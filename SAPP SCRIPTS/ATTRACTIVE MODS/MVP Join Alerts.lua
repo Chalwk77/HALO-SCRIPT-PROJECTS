32c3aff70d09503ca01b138baa4558681df31bea
Chalwk77 <jericho.crosby227@gmail.com>
2024-08-10 20:11:06 +1200

    Completely rewrote Rage Quit

--- SAPP SCRIPTS/ATTRACTIVE MODS/MVP Join Alerts.lua
+++ SAPP SCRIPTS/ATTRACTIVE MODS/MVP Join Alerts.lua
@@ -2,45 +2,64 @@
 --=====================================================================================================--
 Script Name: MVP Join Alerts, for SAPP (PC & CE)
 Description: This mod will automatically broadcast a special message when an MVP joins!
-             Each MVP can have a unique message!
-
+             Each MVP can have a unique message, and general welcome messages are available for other players.
              Supports IP Addresses & Hashes
 
-Copyright (c) 2020, Jericho Crosby <jericho.crosby227@gmail.com>
+Copyright (c) 2020-2024, Jericho Crosby <jericho.crosby227@gmail.com>
 Notice: You can use this script subject to the following conditions:
 https://github.com/Chalwk77/HALO-SCRIPT-PROJECTS/blob/master/LICENSE
 --=====================================================================================================--
 ]]--
 
--- The $name placeholder will be replaced with the players name automagically:
---
+
+-- Config starts here:
+
+-- A table containing custom welcome messages for certain IPs or player hashes
 local MVP = {
     ['127.0.0.1'] = 'YO! MVP $name has joined the server!',
-    ['xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'] = '$name, a brother from another mother has joined!'
+    ['xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'] = '$name, a brother from another mother has joined!',
+    ['AnotherPlayerName'] = 'Welcome, $name! Glad you could join us!',
+}
+
+-- A table containing general welcome messages that will be used for other players
+local GENERAL_WELCOME_MESSAGES = {
+    'Welcome to the server, $name!',
+    'Hello $name, glad to have you!',
+    'Hey $name, enjoy your stay!',
 }
 
 -- A message relay function temporarily removes the server prefix,
 -- and restores it to this when it's finish:
 local server_prefix = '**SAPP**'
 
--- Config Ends ------------
-
 api_version = '1.12.0.0'
+
+-- Config ends here.
 
 function OnScriptLoad()
     register_callback(cb['EVENT_JOIN'], 'OnJoin')
+end
+
+local function send(message, name)
+    execute_command('msg_prefix ""')
+    local finalMessage = message:gsub('$name', name)
+    say_all(finalMessage)
+    execute_command('msg_prefix "' .. server_prefix .. '"')
 end
 
 function OnJoin(Ply)
 
     local ip = get_var(Ply, '$ip'):match('%d+.%d+.%d+.%d+')
     local hash = get_var(Ply, 'hash')
+    local name = get_var(Ply, '$name')
 
-    local mvp = (MVP[hash] or MVP[ip])
-    if (mvp) then
-        execute_command('msg_prefix ""')
-        say_all(mvp:gsub('$name', get_var(Ply, '$name')))
-        execute_command('msg_prefix "' .. server_prefix .. '"')
+    local mvp = MVP[hash] or MVP[ip]
+    if mvp then
+        send(mvp, name)
+    else
+        local general_messages = #GENERAL_WELCOME_MESSAGES + 1
+        local chosen_msg = GENERAL_WELCOME_MESSAGES[rand(1, general_messages)]
+        send(chosen_msg, name)
     end
 end
 
--- SAPP SCRIPTS/ATTRACTIVE MODS/Rage Quit.lua
+++ SAPP SCRIPTS/ATTRACTIVE MODS/Rage Quit.lua
@@ -1,79 +1,86 @@
 --[[
 --====================================================================--
 Script Name: Rage Quit, for SAPP (PC & CE)
-Description: Announces a simple message when someone rage quits.
+Description: Announces a simple message when someone rage quits. Detects both kill-related and general rage quits.
 
-Copyright (c) 2022, Jericho Crosby <jericho.crosby227@gmail.com>
+Copyright (c) 2022-2024, Jericho Crosby <jericho.crosby227@gmail.com>
 Notice: You can use this script subject to the following conditions:
 https://github.com/Chalwk77/HALO-SCRIPT-PROJECTS/blob/master/LICENSE
 --====================================================================--
 ]]--
 
--- config starts --
-
--- A player is considered raging if they quit
--- before the grace period lapses after being killed:
---
-local grace = 5
-
--- Message output when a player rage quits:
---
-local output = '$name rage quit because of $killer'
-
--- config ends --
-
-local players = {}
-local time = os.time
+-- Configuration starts --------------------------------------------------
+local GRACE_PERIOD_AFTER_KILL = 10 -- in seconds
+local GENERAL_RAGE_QUIT_PERIOD = 15 -- in seconds
+-- Messages ---------------------------------------------------
+local KILL_RAGE_QUIT_MESSAGE = '%s rage quit because of %s'
+local GENERAL_RAGE_QUIT_MESSAGE = '%s rage quit!'
+local SERVER_PREFIX = '**SAPP**'
+-- Configuration ends ----------------------------------------------------
 
 api_version = '1.12.0.0'
 
 function OnScriptLoad()
-    register_callback(cb['EVENT_DIE'], 'OnDie')
-    register_callback(cb['EVENT_JOIN'], 'OnJoin')
-    register_callback(cb['EVENT_LEAVE'], 'OnRage')
-    register_callback(cb['EVENT_GAME_START'], 'OnStart')
-    OnStart()
+    register_callback(cb['EVENT_JOIN'], 'onJoin')
+    register_callback(cb['EVENT_LEAVE'], 'onQuit')
+    register_callback(cb['EVENT_DIE'], 'onDeath')
 end
 
-function OnJoin(Ply)
-    players[Ply] = { name = get_var(Ply, '$name') }
+local players = {}
+
+local function send(message, ...)
+    execute_command('msg_prefix ""')
+    cprint(string.format(message, ...))
+    execute_command('msg_prefix "' .. SERVER_PREFIX .. '"')
 end
 
-function OnRage(Ply)
-    local t = players[Ply]
-    if (t.killer and t.finish - t.start() > 0) then
-        local str = output
-        str = str:gsub('$name', t.name):gsub('$killer', t.killer.name)
-        say_all(str)
-    end
-    players[Ply] = nil
+function onJoin(id)
+    local name = get_var(id, '$name')
+    players[id] = {
+        name = name,
+        joined = os.time(),
+    }
 end
 
-function OnStart()
-    if (get_var(0, '$gt') ~= 'n/a') then
-        players = { }
-        for i = 1, 16 do
-            if player_present(i) then
-                OnJoin(i)
-            end
+function onQuit(id)
+    local t = players[id]
+    if t then
+        if t.killer and t.finish - t.joined > 0 then
+            send(KILL_RAGE_QUIT_MESSAGE, t.name, t.killer.name)
+        elseif t.joined + GENERAL_RAGE_QUIT_PERIOD > os.time() then
+            send(GENERAL_RAGE_QUIT_MESSAGE, t.name)
         end
+        players[id] = nil
     end
 end
 
-function OnDie(Victim, Killer)
+function onDeath(Victim, Killer)
+    local victim, killer = tonumber(Victim), tonumber(Killer)
 
-    local victim = tonumber(Victim)
-    local killer = tonumber(Killer)
+    if not (killer > 0 and killer ~= victim) then
+        return
+    end
 
     local v = players[victim]
     local k = players[killer]
 
-    local pvp = (killer > 0 and k and v and killer ~= victim)
+    if not (v and k) then
+        return
+    end
 
-    if (pvp) then
-        v.killer = k
-        v.start = time
-        v.finish = time() + grace
+    v.killer = k
+    v.joined = os.time()
+    v.finish = os.time() + GRACE_PERIOD_AFTER_KILL
+end
+
+function OnStart()
+    if (get_var(0, '$gt') ~= 'n/a') then
+        players = {}
+        for i = 1, 16 do
+            if player_present(i) then
+                onJoin(i)
+            end
+        end
     end
 end
 
