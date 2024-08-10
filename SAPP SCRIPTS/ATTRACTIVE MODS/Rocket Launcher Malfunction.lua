fb9f6c70f86e68333b36ac1955bf2cc521227a60
Chalwk77 <jericho.crosby227@gmail.com>
2024-08-10 21:20:06 +1200

    Completely rewrote Rocket Launcher Malfunction.lua

--- SAPP SCRIPTS/ATTRACTIVE MODS/Rocket Launcher Malfunction.lua
+++ SAPP SCRIPTS/ATTRACTIVE MODS/Rocket Launcher Malfunction.lua
@@ -3,7 +3,7 @@
 Script Name: Rocket Launcher Malfunction, for SAPP (PC & CE)
 Description: Your rocket launcher will spontaneously explode at random times, killing you and nearby players.
 
-Copyright (c) 2020-2022, Jericho Crosby <jericho.crosby227@gmail.com>
+Copyright (c) 2020-2024, Jericho Crosby <jericho.crosby227@gmail.com>
 Notice: You can use this script subject to the following conditions:
 https://github.com/Chalwk77/HALO-SCRIPT-PROJECTS/blob/master/LICENSE
 --=====================================================================================================--
@@ -12,109 +12,128 @@ https://github.com/Chalwk77/HALO-SCRIPT-PROJECTS/blob/master/LICENSE
 -------------------
 -- Config starts --
 -------------------
-
--- Number of rocket projectiles to spawn:
--- These simulate an explosion.
---
 local projectiles = 10
-
--- Minimum possible time (in seconds) that an explosion may occur:
-local min_time = 5
-
--- Max possible time (in seconds) that an explosion may occur:
-local max_time = 10
-
+local minExplosionTime = 5
+local maxExplosionTime = 10
+local EXPLOSION_MESSAGE = "%s's rocket launcher has blown up!"
+local SERVER_PREFIX = '**SERVER**'
+api_version = '1.12.0.0'
 -----------------
 -- Config ends --
 -----------------
 
-api_version = '1.12.0.0'
-
-local rocket_proj
-local rocket_weap
 local players = {}
+local rocketProjectileTag, rocketLauncherTag
 
 function OnScriptLoad()
-    register_callback(cb['EVENT_LEAVE'], 'OnQuit')
-    register_callback(cb['EVENT_SPAWN'], 'OnSpawn')
-    register_callback(cb['EVENT_GAME_START'], 'OnStart')
-    OnStart()
+    register_callback(cb['EVENT_GAME_START'], 'onStart')
 end
 
 local function GetTag(class, name)
     local tag = lookup_tag(class, name)
-    return (tag ~= nil and read_dword(tag + 0xC)) or nil
+    return tag and (read_dword(tag + 0xC) or nil)
 end
 
-function OnStart()
-    if (get_var(0, '$gt') ~= 'n/a') then
-        rocket_proj = GetTag('proj', 'weapons\\rocket launcher\\rocket')
-        rocket_weap = GetTag('weap', 'weapons\\rocket launcher\\rocket launcher')
-        if (rocket_proj and rocket_weap) then
-            register_callback(cb['EVENT_TICK'], 'OnTick')
-            for i,_ in pairs(players) do
-                if player_present(i) then
-                    ResetTimer(i)
-                end
-            end
-        else
-            unregister_callback(cb['EVENT_TICK'])
-        end
+local function getCurrentWeapon(dyn)
+
+    local weapon = read_dword(dyn + 0x118)
+    local object = get_object_memory(weapon)
+
+    if (not object or object == 0) then
+        return nil
     end
+
+    return read_dword(object)
 end
 
-function OnSpawn(Ply)
-    players[Ply] = {
-        timer = 0,
-        interval = rand(min_time, max_time + 1)
-    }
+local function inVehicle(dyn)
+    return read_dword(dyn + 0x11C) ~= 0xFFFFFFFF
 end
 
-function OnQuit(Ply)
-    players[Ply] = nil
+local function resetTime(player)
+    player.lastExplosionTime = os.clock()
+    player.nextExplosionInterval = rand(minExplosionTime, maxExplosionTime + 1)
 end
 
-local function InVehicle(dyn)
-    local vehicle = read_dword(dyn + 0x11C)
-    return (vehicle ~= 0xFFFFFFFF)
+local function send(message, ...)
+    execute_command('msg_prefix ""')
+    say_all(string.format(message, ...))
+    execute_command('msg_prefix "' .. SERVER_PREFIX .. '"')
 end
 
-local function SpawnProjectiles(Ply, dyn)
-
+local function spawnProjectiles(dyn)
     local x, y, z = read_vector3d(dyn + 0x5C)
     for _ = 1, projectiles do
-
-        local payload = spawn_object('', '', x, y, z + 0.1, 0, rocket_proj)
+        local payload = spawn_object('', '', x, y, z + 0.1, 0, rocketProjectileTag)
         local projectile = get_object_memory(payload)
-
-        -- Update z velocity:
-        if (payload and projectile ~= 0) then
+        if payload and projectile ~= 0 then
             write_float(projectile + 0x70, -1)
         end
     end
-
-    rprint(Ply, 'Your rocket launcher has blown up')
 end
 
-local time_scale = 1 / 30
+local function updateExplosionInterval(currentWeapon, player)
+    if currentWeapon == rocketLauncherTag then
+        resetTime(player)
+    else
+        player.nextExplosionInterval = rand(minExplosionTime, maxExplosionTime + 1)
+    end
+end
+
 function OnTick()
-
-    for i, v in pairs(players) do
+    for i, player in pairs(players) do
         local dyn = get_dynamic_player(i)
-        if (player_present(i) and player_alive(i) and dyn ~= 0) then
 
-            local weapon = read_dword(dyn + 0x118)
-            local object = get_object_memory(weapon)
+        if (player_present(i) and player_alive(i) and not inVehicle(dyn)) then
 
-            if (v.timer and object ~= 0 and read_dword(object) == rocket_weap and not InVehicle(dyn)) then
+            local currentWeapon = getCurrentWeapon(dyn)
+            if currentWeapon ~= player.currentWeapon then
+                player.currentWeapon = currentWeapon
+                updateExplosionInterval(currentWeapon, player)
+            end
 
-                v.timer = v.timer + time_scale
-                if (v.timer >= v.interval) then
-                    SpawnProjectiles(i, dyn)
-                end
+            if (os.clock() - player.lastExplosionTime >= player.nextExplosionInterval) and currentWeapon == rocketLauncherTag then
+                spawnProjectiles(dyn)
+                resetTime(player)
+                send(EXPLOSION_MESSAGE, player.name)
             end
         end
     end
+end
+
+function onStart()
+    rocketProjectileTag = GetTag('proj', 'weapons\\rocket launcher\\rocket')
+    rocketLauncherTag = GetTag('weap', 'weapons\\rocket launcher\\rocket launcher')
+
+    if rocketProjectileTag and rocketLauncherTag then
+        register_callback(cb['EVENT_TICK'], 'OnTick')
+        register_callback(cb['EVENT_JOIN'], 'onJoin')
+        register_callback(cb['EVENT_LEAVE'], 'onQuit')
+        register_callback(cb['EVENT_SPAWN'], 'onSpawn')
+    else
+        unregister_callback(cb['EVENT_TICK'])
+        unregister_callback(cb['EVENT_JOIN'])
+        unregister_callback(cb['EVENT_LEAVE'])
+        unregister_callback(cb['EVENT_SPAWN'])
+    end
+end
+
+function onJoin(id)
+    players[id] = {
+        name = get_var(id, '$name'),
+        nextExplosionInterval = rand(minExplosionTime, maxExplosionTime + 1),
+        currentWeapon = nil
+    }
+end
+
+function onQuit(Ply)
+    if players[Ply] then
+        players[Ply] = nil
+    end
+end
+
+function onSpawn(id)
+    resetTime(players[id])
 end
 
 function OnScriptUnload()
