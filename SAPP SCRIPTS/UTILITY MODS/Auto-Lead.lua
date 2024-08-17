edcfddeb6a0a6245a153cbb0f4bd7a106c13d0cf
Chalwk77 <jericho.crosby227@gmail.com>
2024-08-17 14:18:58 +1200

    ...

--- SAPP SCRIPTS/UTILITY MODS/Auto-Lead.lua
+++ SAPP SCRIPTS/UTILITY MODS/Auto-Lead.lua
@@ -1,9 +1,13 @@
 --[[
 --=====================================================================================================--
 Script Name: Auto-Lead, for SAPP (PC & CE)
-Description: Automatically turns on/off SAPP's no-lead feature on a per map/per game type basis.
+Description: This script automates the process of managing SAPP's no-lead feature based on specified
+             map and game type configurations. The script enables or disables the no-lead feature for
+             individual game types within each map, allowing for flexible and customizable control over
+             gameplay mechanics. By configuring a settings table with desired map and game type combinations,
+             the script provides an efficient way to optimize and tailor the gaming experience for various scenarios.
             
-Copyright (c) 2019, Jericho Crosby <jericho.crosby227@gmail.com>
+Copyright (c) 2019-2024, Jericho Crosby <jericho.crosby227@gmail.com>
 Notice: You can use this script subject to the following conditions:
 https://github.com/Chalwk77/HALO-SCRIPT-PROJECTS/blob/master/LICENSE
 --======================================================================================================--
@@ -11,71 +15,50 @@ https://github.com/Chalwk77/HALO-SCRIPT-PROJECTS/blob/master/LICENSE
 
 api_version = "1.12.0.0"
 
+-- Configure settings with a map-gametype structure:
+-- ["mapName"] = {
+--   ["gametype"] = noLeadSetting,
+--   ...
+-- }
+--   ... repeat for each map
+
 local settings = {
-
-    -- Example: ["mapname"] = {["lnz-zombies"] = 1, ["epic-tslayer"] = 0},
-    -- 1 = on, 0 = off
-    -- You can have as many game types per map table as you like.
-
-    ["beavercreek"] = { ["gt1"] = 1, ["gt2"] = 1, ["gt3"] = 1 },
-
-    ["bloodgulch"] = { ["gt1"] = 1, ["gt2"] = 1, ["gt3"] = 1 },
-
-    ["boardingaction"] = { ["gt1"] = 1, ["gt2"] = 1, ["gt3"] = 1 },
-
-    ["carousel"] = { ["gt1"] = 1, ["gt2"] = 1, ["gt3"] = 1 },
-
-    ["dangercanyon"] = { ["gt1"] = 1, ["gt2"] = 1, ["gt3"] = 1 },
-
-    ["deathisland"] = { ["gt1"] = 1, ["gt2"] = 1, ["gt3"] = 1 },
-
-    ["gephyrophobia"] = { ["gt1"] = 1, ["gt2"] = 1, ["gt3"] = 1 },
-
-    ["icefields"] = { ["gt1"] = 1, ["gt2"] = 1, ["gt3"] = 1 },
-
-    ["infinity"] = { ["gt1"] = 1, ["gt2"] = 1, ["gt3"] = 1 },
-
-    ["sidewinder"] = { ["gt1"] = 1, ["gt2"] = 1, ["gt3"] = 1 },
-
-    ["timberland"] = { ["gt1"] = 1, ["gt2"] = 1, ["gt3"] = 1 },
-
-    ["hangemhigh"] = { ["gt1"] = 1, ["gt2"] = 1, ["gt3"] = 1 },
-
-    ["ratrace"] = { ["gt1"] = 1, ["gt2"] = 1, ["gt3"] = 1 },
-
-    ["damnation"] = { ["gt1"] = 1, ["gt2"] = 1, ["gt3"] = 1 },
-
-    ["putput"] = { ["gt1"] = 1, ["gt2"] = 1, ["gt3"] = 1 },
-
-    ["prisoner"] = { ["gt1"] = 1, ["gt2"] = 1, ["gt3"] = 1 },
-
-    ["wizard"] = { ["gt1"] = 1, ["gt2"] = 1, ["gt3"] = 1 },
-
-    -- repeat the structure to add more maps
-    ["mapname"] = { ["gametype here"] = 1 },
-
+    ["example_map"] = { ["gametype_name_here"] = 1, ["another_gametype"] = 1 },
+    ["beavercreek"] = { },
+    ["bloodgulch"] = { },
+    ["boardingaction"] = { },
+    ["carousel"] = { },
+    ["dangercanyon"] = { },
+    ["deathisland"] = { },
+    ["gephyrophobia"] = { },
+    ["icefields"] = { },
+    ["infinity"] = { },
+    ["sidewinder"] = { },
+    ["timberland"] = { },
+    ["hangemhigh"] = { },
+    ["ratrace"] = { },
+    ["damnation"] = { },
+    ["putput"] = { },
+    ["prisoner"] = { },
+    ["wizard"] = { },
 }
 
 function OnScriptLoad()
-    register_callback(cb["EVENT_GAME_START"], "OnGameStart")
-    if (get_var(0, "$gt") ~= "n/a") then
-        timer(1000, "SetLead")
-    end
+    register_callback(cb["EVENT_GAME_START"], "OnStart")
+    OnStart()
 end
 
-function OnGameStart()
+function OnStart()
     if (get_var(0, "$gt") ~= "n/a") then
         timer(1000, "SetLead")
     end
 end
 
 function SetLead()
-    local current_map = get_var(0, "$map")
-    local state = get_var(0, "$mode")
-    for mapname, gametype in pairs(settings) do
-        if (current_map == mapname) and (gametype[state] ~= nil) then
-            execute_command("no_lead " .. tostring(gametype[state]))
-        end
+    local map = get_var(0, "$map")
+    local mode = get_var(0, "$mode")
+    if settings[map] and settings[map][mode] then
+        execute_command("no_lead " .. tostring(settings[map][mode]))
     end
 end
 
