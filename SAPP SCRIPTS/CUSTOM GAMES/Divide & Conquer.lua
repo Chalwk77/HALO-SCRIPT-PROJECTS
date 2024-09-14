1f711247d3734b0efbecbb08f91a665bcab72abd
Chalwk77 <jericho.crosby227@gmail.com>
2024-09-15 09:58:57 +1200

    Refactored a bunch of scripts.

--- SAPP SCRIPTS/CUSTOM GAMES/Divide & Conquer.lua
+++ SAPP SCRIPTS/CUSTOM GAMES/Divide & Conquer.lua
@@ -6,48 +6,291 @@ Description: When you kill someone on the opposing team, the victim is switched 
              When the opposing team has no players left the game is over.
 
 Copyright (c) 2023, Jericho Crosby <jericho.crosby227@gmail.com>
-Notice: You can use this script subject to the following conditions:
+* Notice: You can use this document subject to the following conditions:
 https://github.com/Chalwk77/HALO-SCRIPT-PROJECTS/blob/master/LICENSE
 --=====================================================================================================--
 ]]--
 
 api_version = '1.12.0.0'
 
---- config starts --
-local delay = 5
-local required_players = 3
-local prefix = '**SAPP**'
---- config ends --
+--- Configuration ---
+local config = {
+    delay = 5,
+    required_players = 3,
+    prefix = '**SAPP**'
+}
 
-local game
-local winner
-local clock = os.clock
-local floor = math.floor
-local post_game_carnage_report
-local players, timer = {}, {}
-local death_message_address
-local original_death_message_address
+--- Timer Class ---
+local Timer = {}
+Timer.__index = Timer
 
-function timer:new()
-    local o = {}
-    setmetatable(o, self)
-    self.__index = self
-    return o
+function Timer:new()
+    local self = setmetatable({}, Timer)
+    self.start_time = nil
+    return self
 end
 
-function timer:start()
-    self.start_time = clock()
+function Timer:start()
+    self.start_time = os.clock()
 end
 
-function timer:stop()
+function Timer:stop()
     self.start_time = nil
 end
 
-function timer:get()
-    return (self.start_time and clock() - self.start_time) or 0
+function Timer:get()
+    return (self.start_time and os.clock() - self.start_time) or 0
 end
 
+--- Player Class ---
+local Player = {}
+Player.__index = Player
+
+function Player:new(playerId)
+    local self = setmetatable({}, Player)
+    self.id = playerId
+    self.team = get_var(playerId, '$team')
+    return self
+end
+
+function Player:switchTeam(team)
+    execute_command('st ' .. self.id .. ' ' .. team)
+    self.team = team
+end
+
+--- Game Class ---
+local Game = {}
+Game.__index = Game
+
+function Game:new()
+    local self = setmetatable({}, Game)
+    self.players = {}
+    self.timer = nil
+    self.started = false
+    self.winner = nil
+    self.death_message_address = sig_scan("8B42348A8C28D500000084C9") + 3
+    self.original_death_message_address = read_dword(self.death_message_address)
+    return self
+end
+
+function Game:addPlayer(playerId)
+    self.players[playerId] = Player:new(playerId)
+    self:checkStartCondition()
+end
+
+function Game:removePlayer(playerId)
+    self.players[playerId] = nil
+    self:checkStartCondition(true)
+end
+
+function Game:checkStartCondition(quit)
+    local count = tonumber(get_var(0, '$pn'))
+    count = (quit and count - 1) or count
+
+    if count >= config.required_players and not self.timer then
+        self.timer = Timer:new()
+        self.timer:start()
+    elseif self.timer and self.started then
+        self:endGame()
+    elseif self.timer and not self.started then
+        return
+    else
+        self.timer = nil
+    end
+end
+
+function Game:start()
+    if get_var(0, '$gt') ~= 'n/a' then
+
+        self.players = {}
+        self.winner, self.started = nil, false
+
+        execute_command('sv_tk_ban 0')
+        execute_command('sv_friendly_fire 0')
+        execute_command('scorelimit 99999')
+
+        for playerId = 1, 16 do
+            if player_present(playerId) then
+                self:addPlayer(playerId)
+            end
+        end
+
+        if self.timer then
+            self.timer:stop()
+            self.timer = nil
+        end
+    end
+end
+
+function Game:endGame()
+    local reds, blues = self:getTeamCounts()
+    if reds == 0 or blues == 0 then
+        self.winner = (reds == 0) and 'Blue' or 'Red'
+        execute_command('sv_map_next')
+        if self.timer then
+            self.timer:stop()
+            self.timer = nil
+        end
+    end
+end
+
+function Game:getTeamCounts()
+    local reds, blues = 0, 0
+    for _, player in pairs(self.players) do
+        if player.team == 'red' then
+            reds = reds + 1
+        else
+            blues = blues + 1
+        end
+    end
+    return reds, blues
+end
+
+local function shuffle(t)
+    for i = #t, 2, -1 do
+        local j = math.random(i)
+        t[i], t[j] = t[j], t[i]
+    end
+    return t
+end
+
+function Game:shuffleTeams()
+    local t = {}
+    for _, player in pairs(self.players) do
+        table.insert(t, player)
+    end
+    t = shuffle(t)
+
+    for i, player in ipairs(t) do
+        local team = (i <= #t / 2) and 'red' or 'blue'
+        player:switchTeam(team)
+    end
+end
+
+function Game:disableDeathMessages()
+    safe_write(true)
+    write_dword(self.death_message_address, 0x03EB01B1)
+    safe_write(false)
+end
+
+function Game:enableDeathMessages()
+    safe_write(true)
+    write_dword(self.death_message_address, self.original_death_message_address)
+    safe_write(false)
+end
+
+local function clearScreen(playerId)
+    for _ = 1, 25 do
+        rprint(playerId, ' ')
+    end
+end
+
+function Game:say(message, tick)
+
+    if tick then
+        for playerId, _ in pairs(self.players) do
+            clearScreen(playerId)
+            rprint(playerId, message)
+        end
+        return
+    end
+
+    execute_command('msg_prefix ""')
+    say_all(message)
+    execute_command('msg_prefix "' .. config.prefix .. '"')
+end
+
+function Game:onTick()
+    if not self.timer or self.started then
+        return
+    end
+
+    if self.timer:get() >= config.delay then
+        self.timer:stop()
+        self.started = true
+
+        self:disableDeathMessages()
+        execute_command('sv_map_reset')
+        self:shuffleTeams()
+        self:enableDeathMessages()
+
+        self:say('Game Start!', true)
+    else
+
+
+        self:say('Game will start in ' .. math.floor(config.delay - self.timer:get()) .. ' seconds!', true)
+    end
+end
+
+function Game:announceTeamSwitch(victimPlayer, newTeam)
+    local message = string.format("%s has been switched to the %s team!", get_var(victimPlayer.id, "$name"), newTeam)
+    self:say(message)
+end
+
+function Game:onDeath(victim, killer, meta_id)
+
+    if not self.started then
+        return
+    end
+
+    victim = tonumber(victim)
+    killer = tonumber(killer)
+
+    if killer == 0 or killer == -1 or not killer then
+        return
+    end
+
+    local victimPlayer = self.players[victim]
+    local killerPlayer = self.players[killer]
+
+    if meta_id and killerPlayer.team == victimPlayer.team then
+        return false
+    end
+
+    if killerPlayer.team == 'red' then
+        victimPlayer:switchTeam('red')
+        self:announceTeamSwitch(victimPlayer, 'red')
+    elseif killerPlayer.team == 'blue' then
+        victimPlayer:switchTeam('blue')
+        self:announceTeamSwitch(victimPlayer, 'blue')
+    end
+
+    self:endGame()
+end
+
+function Game:onTeamSwitch(id)
+    self.players[id].team = get_var(id, '$team')
+end
+
+--- Global Game Instance ---
+local game
+
+--- Callbacks ---
 function OnScriptLoad()
+
+    game = Game:new()
+
+    _G['onStart'] = function()
+        game:start()
+    end
+    _G['onEnd'] = function()
+        game:endGame()
+    end
+    _G['onJoin'] = function(...)
+        game:addPlayer(...)
+    end
+    _G['onQuit'] = function(...)
+        game:removePlayer(...)
+    end
+    _G['onDeath'] = function(...)
+        game:onDeath(...)
+    end
+    _G['onTick'] = function()
+        game:onTick()
+    end
+    _G['onTeamSwitch'] = function(...)
+        game:onTeamSwitch(...)
+    end
 
     register_callback(cb['EVENT_DIE'], 'onDeath')
     register_callback(cb['EVENT_DAMAGE_APPLICATION'], 'onDeath')
@@ -61,223 +304,7 @@ function OnScriptLoad()
     register_callback(cb['EVENT_GAME_END'], 'onEnd')
     register_callback(cb['EVENT_GAME_START'], 'onStart')
 
-    onStart()
-
-    death_message_address = sig_scan("8B42348A8C28D500000084C9") + 3
-    original_death_message_address = read_dword(death_message_address)
-end
-
-function onStart()
-    if (get_var(0, '$gt') ~= 'n/a') then
-
-        players = {}
-        winner, game = nil, nil
-        post_game_carnage_report = false
-
-        execute_command('sv_tk_ban 0')
-        execute_command('scorelimit 99999')
-        execute_command('sv_friendly_fire 0')
-
-        for i = 1, 16 do
-            if player_present(i) then
-                onJoin(i)
-            end
-        end
-    end
-end
-
-local function cls(id)
-    for _ = 1, 25 do
-        rprint(id, ' ')
-    end
-end
-
-local function say(message, tick)
-
-    if (tick) then
-        for i, _ in pairs(players) do
-            cls(i)
-            rprint(i, message)
-        end
-        return
-    end
-
-    execute_command('msg_prefix ""')
-    say_all(message)
-    execute_command('msg_prefix "' .. prefix .. '"')
-end
-
-function onEnd()
-
-    if (winner) then
-        say('Game Over! Winner: ' .. winner .. ' team')
-    end
-
-    winner, game = nil, nil
-    post_game_carnage_report = true
-end
-
-local function timeRemaining()
-    return floor(delay - game:get())
-end
-
-local function disableDeathMessages()
-    safe_write(true)
-    write_dword(death_message_address, 0x03EB01B1)
-    safe_write(false)
-end
-
-local function enableDeathMessages()
-    safe_write(true)
-    write_dword(death_message_address, original_death_message_address)
-    safe_write(false)
-end
-
-local function shuffle(t)
-    for i = #t, 2, -1 do
-        local j = rand(i) + 1
-        t[i], t[j] = t[j], t[i]
-    end
-    return t
-end
-
-local function switchTeam(player, team)
-    execute_command('st ' .. player.id .. ' ' .. team)
-end
-
-local function shuffleTeams()
-
-    local t = {}
-    for _, v in pairs(players) do
-        t[#t + 1] = v
-    end
-    t = shuffle(t)
-
-    for i, v in pairs(t) do
-        if (i <= #t / 2) then
-            v.team = 'red'
-        else
-            v.team = 'blue'
-        end
-        switchTeam(v, v.team)
-    end
-end
-
-function onTick()
-
-    if (not game or game.started) then
-        return
-    end
-
-    if (game:get() >= delay) then
-
-        game:stop()
-        game.started = true
-
-        disableDeathMessages()
-        execute_command('sv_map_reset')
-        shuffleTeams()
-        enableDeathMessages()
-
-        say('Game Start!', true)
-
-    else
-        say('Game will start in ' .. timeRemaining() .. ' seconds!', true)
-    end
-end
-
-local function getTeamCounts()
-
-    local reds, blues = 0, 0
-
-    for _, v in pairs(players) do
-        reds = (v.team == 'red') and reds + 1 or reds
-        blues = (v.team == 'blue') and blues + 1 or blues
-    end
-
-    return reds, blues
-end
-
-local function endGame()
-
-    local reds, blues = getTeamCounts()
-    if (reds == 0 or blues == 0) then
-
-        winner = (reds == 0) and 'Blue' or 'Red'
-        execute_command('sv_map_next')
-    end
-end
-
-local function gameCheck(quit)
-
-    if (post_game_carnage_report) then
-        return
-    end
-
-    local count = tonumber(get_var(0, '$pn'))
-    count = (quit and count - 1) or count
-
-    if (count >= required_players and not game) then
-        game = timer:new()
-        game:start()
-    elseif (count >= required_players and game) then
-        return
-    elseif (game and game.started) then
-        endGame()
-    elseif (game and not game.started) then
-        game = nil
-        return
-    end
-end
-
-function onJoin(id)
-    players[id] = {
-        id = id,
-        team = get_var(id, '$team')
-    }
-    gameCheck()
-end
-
-function onQuit(id)
-    players[id] = nil
-    gameCheck(true)
-end
-
-function onDeath(victim, killer, meta_id)
-
-    if (not game or not game.started) then
-        goto next
-    end
-
-    victim = tonumber(victim)
-    killer = tonumber(killer)
-
-    if (killer == 0 or killer == -1 or killer == nil) then
-        goto next
-    end
-
-    victim = players[victim]
-    killer = players[killer]
-
-    if (meta_id and killer.team == victim.team) then
-        return false -- block team killing
-    elseif (meta_id) then
-        return true
-    end
-
-    if (killer.team == 'red') then
-        switchTeam(victim, 'red')
-    elseif (killer.team == 'blue') then
-        switchTeam(victim, 'blue')
-    end
-
-    endGame()
-
-    :: next ::
-end
-
-function onTeamSwitch(id)
-    players[id].team = get_var(id, '$team')
+    game:start()
 end
 
 function OnScriptUnload()
